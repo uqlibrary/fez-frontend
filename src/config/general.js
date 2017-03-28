4117d743fcc8098f545ea463c113366238e17a4e
jannakha <j_malikova@yahoo.com>
2017-03-28 16:01:16 +1000

    rolled back dev/prod api

--- src/config/general.js
+++ src/config/general.js
@@ -3,6 +3,5 @@ export const COOKIE_NAME = 'UQLID';
 export const TOKEN_NAME = 'X-Uql-Token';
 
 // URLS
-// export const API_URL = process.env.NODE_ENV === 'development' ? 'http://dev-api.library.uq.edu.au:8050/' : 'https://api.library.uq.edu.au/v1/';
-export const API_URL = 'https://api.library.uq.edu.au/v1/';
+export const API_URL = process.env.NODE_ENV === 'development' ? 'http://dev-api.library.uq.edu.au:8050/' : 'https://api.library.uq.edu.au/v1/';
 export const AUTH_URL = 'https://auth.library.uq.edu.au/login';
