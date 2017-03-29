48f178f4c5bb70cb3e5f0f7486aaf0e133f08580
jannakha <j_malikova@yahoo.com>
2017-03-29 17:44:31 +1000

    adding ga as middleware [#139521323]

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 // External
 import {AppContainer} from 'react-hot-loader';
 import {applyMiddleware, compose, createStore} from 'redux';
@@ -26,6 +28,49 @@ if (process.env.NODE_ENV === 'production') {
     history = createHashHistory();
 }
 
+// TODO: testing analytics with middleware, does it actually pass data to Analytics???
+
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-4365437-1', 'auto');
+
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PX9H7R');
+
+
+const analytics = () => next => action => {
+    dataLayer = dataLayer || [];
+    dataLayer.push({
+        event: action.type,
+        payload: action.payload
+    });
+
+    console.log(action.type);
+
+    var fieldObject = {
+        hitType: 'event',
+        eventAction: action.type
+    };
+
+
+    ga('send', 'pageview');
+    ga('send', fieldObject);
+
+    return next(action);
+};
+
 const initialState = Immutable.Map();
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
@@ -34,6 +79,7 @@ const store = createStore(
     composeEnhancer(
         applyMiddleware(
             routerMiddleware(history),
+            analytics,
             thunk
         ),
     ),
