74558a32d76578cddb2c1364e3178542421a7af2
uqadient <a.diente@library.uq.edu.au>
2017-04-19 10:58:39 +1000

    added publication type form to main reducer.js file [PT #142206883]

--- src/reducer.js
+++ src/reducer.js
@@ -4,11 +4,13 @@ import {combineReducers} from 'redux-immutable';
 // Load reducers
 import {appReducer} from 'modules/App';
 import {reducer as dashboardReducer} from 'modules/Dashboard';
+import {publicationTypeReducer} from './modules/Forms/PublicationType';
 
 const rootReducer = combineReducers({
     // App reducers
     app: appReducer,
     dashboard: dashboardReducer,
+    publicationTypes: publicationTypeReducer,
 });
 
 export default rootReducer;
