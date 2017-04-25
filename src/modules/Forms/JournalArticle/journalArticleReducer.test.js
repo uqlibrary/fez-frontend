ae9d0fe980af26bec09082502deee2bb3fa5da40
uqadient <a.diente@library.uq.edu.au>
2017-04-26 09:14:30 +1000

    used authors mock data for reducer test [PT #142206777]

--- src/modules/Forms/JournalArticle/journalArticleReducer.test.js
+++ src/modules/Forms/JournalArticle/journalArticleReducer.test.js
@@ -3,13 +3,7 @@ import {PUBLICATION_SUB_TYPES_LOADED, AUTHORS_LOADED} from './actions';
 import Immutable from 'immutable';
 
 import {publicationSubTypes} from '../../../../src/mock/data/publicationSubTypes';
-
-const authors = [
-    {'id': 202, 'name': 'Author 8'},
-    {'id': 263, 'name': 'Author 9'},
-    {'id': 174, 'name': 'Author 10'},
-    {'id': 177, 'name': 'Author 11'}
-];
+import {authors} from '../../../../src/mock/data/authors';
 
 describe('Publication type reducer', () => {
     it('should return the initial state', () => {
