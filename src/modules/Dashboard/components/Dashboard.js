2b693cfb266f729e4c520fe079ba281e4f58d754
Ky <k.lane@uq.edu.au>
2017-05-02 09:32:23 +1000

    Fixed card for graph

--- src/modules/Dashboard/components/Dashboard.js
+++ src/modules/Dashboard/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Card, CardText} from 'material-ui/Card';
+import {Card, CardText, CardHeader} from 'material-ui/Card';
+// import {HelpIcon} from 'uqlibrary-react-toolbox';
 
 import AuthorsPublicationsPerYearChart from './AuthorsPublicationsPerYearChart';
 import TimeDisplay from './TimeDisplay';
@@ -38,16 +39,34 @@ class Dashboard extends React.Component {
                     </div>
                 </div>
 
-                <div className="layout-fill">
-                    <h1 className="page-title display-1">eSpace publications by year</h1>
-                    <Card className="layout-card">
-                        <CardText className="body-1">
-                            <br />
-                            <AuthorsPublicationsPerYearChart data={authorStatsData} yAxisTitle="Total publications ever"/>
-                        </CardText>
-                    </Card>
-                </div>
+                <Card className="layout-card">
+                    <CardHeader className="card-header">
+                        <div className="columns is-gapless">
 
+                            <div className="column">
+                                <h2 className="headline">eSpace publications by year</h2>
+                            </div>
+
+                            <div className="column">
+                                {/* {help && (
+                                    <HelpIcon
+                                        title={help.title}
+                                        text={help.text}
+                                        buttonLabel={help.button}
+                                    />
+                                )}*/}
+                            </div>
+                        </div>
+                    </CardHeader>
+
+                    <CardText className="body-1">
+                        <br />
+                        <div>
+                            <AuthorsPublicationsPerYearChart data={authorStatsData} yAxisTitle="Total publications ever"/>
+                        </div>
+                    </CardText>
+
+                </Card>
             </div>
         );
     }
