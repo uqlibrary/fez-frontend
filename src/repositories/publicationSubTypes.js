92ea42ac88cd843f26b4f5278219caa58baf121c
uqadient <a.diente@library.uq.edu.au>
2017-05-04 14:18:18 +1000

    updated repos to use mock data instead of mocked endpoints in staging [PT #142207015]

--- src/repositories/publicationSubTypes.js
+++ src/repositories/publicationSubTypes.js
@@ -1,16 +1,24 @@
 import {api} from '../config';
+import {publicationSubTypeList} from '../mock/data/publicationSubTypes';
 
 /**
+ * TODO: Remove the USE_MOCK if statement once endpoint is in staging
  * Fetches the publication sub types
  * @returns {Promise}
  */
 export function loadPublicationSubTypeData() {
-    return new Promise((resolve, reject) => {
-        api.get('records/sub/types').then(response => {
-            resolve(response.data);
-        }).catch(e => {
-            reject(e);
-            throw e;
+    if (!process.env.USE_MOCK) {
+        return new Promise((resolve, reject) => {
+            api.get('records/sub/types').then(response => {
+                resolve(response.data);
+            }).catch(e => {
+                reject(e);
+                throw e;
+            });
         });
-    });
+    } else {
+        return new Promise((resolve) => {
+            resolve(publicationSubTypeList);
+        });
+    }
 }
