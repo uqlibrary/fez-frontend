731cd68dc9e5688cae75208cb51e8333af66a02d
uqadient <a.diente@library.uq.edu.au>
2017-05-16 12:57:29 +1000

    claim publication lure [PT #144832177]

--- src/modules/Dashboard/components/Dashboard.js
+++ src/modules/Dashboard/components/Dashboard.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Card, CardText, CardHeader} from 'material-ui/Card';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import FlatButton from 'material-ui/FlatButton';
+import FontIcon from 'material-ui/FontIcon';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import {publicationYearsBig as publicationYearsMockData} from '../../../mock/data/academic/publicationYears';
 
 import {AuthorsPublicationsPerYearChart} from 'uqlibrary-react-toolbox';
@@ -9,12 +15,32 @@ import './Dashboard.scss';
 
 class Dashboard extends React.Component {
     static propTypes = {
-        account: PropTypes.object.isRequired
+        account: PropTypes.object.isRequired,
+        history: PropTypes.object,
+        loadUsersPublications: PropTypes.func,
+        claimPublicationResults: PropTypes.object
     };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showAppbar: true
+        };
+    }
 
     componentDidMount() {
         // fetch data to display here
+        this.props.loadUsersPublications(123);
     }
+
+    hideAppBar = () => {
+        this.setState({showAppbar: false});
+    };
+
+    claimYourPublications = () => {
+        this.props.history.push('/claim-publications');
+    };
 
     render() {
         const {
@@ -31,9 +57,25 @@ class Dashboard extends React.Component {
                             <span className="body-1">{account.get('school')}</span>
                         </div>
                     </div>
-                    <div className="time-display-wrap" />
+                    <div className="notification-wrap">
+                        <CSSTransitionGroup
+                            transitionName="claim-publication-lure"
+                            transitionEnterTimeout={100}
+                            transitionLeaveTimeout={300}
+                        >
+                            {this.props.claimPublicationResults.size > 0 && this.state.showAppbar  && (
+                            <AppBar
+                                className="claim-publication-lure"
+                                title={`We have found ${this.props.claimPublicationResults.size} articles that could possibly be your work.`}
+                                iconElementLeft={<FontIcon className="material-icons error_outline">error_outline</FontIcon>}
+                                iconElementRight={<span className="button-wrap">
+                                    <FlatButton label="Claim your publications now" onTouchTap={this.claimYourPublications} className="claim-publications" />
+                                    <IconButton onTouchTap={this.hideAppBar}><NavigationClose className="hide-appbar" /></IconButton></span>}
+                            />
+                            )}
+                        </CSSTransitionGroup>
+                    </div>
                 </div>
-
                 <Card className="layout-card">
                     <CardHeader className="card-header">
                         <div className="columns is-gapless">
--- src/modules/Dashboard/containers/Dashboard.js
+++ src/modules/Dashboard/containers/Dashboard.js
@@ -2,13 +2,18 @@ import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 
 import Dashboard from '../components/Dashboard';
+import {loadUsersPublications} from 'modules/ClaimPublication/actions';
 
 let DashboardPage = connect(state => {
     return {
-        account: state.getIn(['app', 'account'])
+        account: state.getIn(['app', 'account']),
+        claimPublicationResults: state.get('claimPublication').get('claimPublicationResults')
     };
-}
-)(Dashboard);
+}, dispatch => {
+    return {
+        loadUsersPublications: (userId) => dispatch(loadUsersPublications(userId))
+    };
+})(Dashboard);
 
 // Add router
 DashboardPage = withRouter(DashboardPage);
