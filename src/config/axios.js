b0115c3bd35bf30927cf9ab5e7c1f87a9dd2bc17
uqadient <a.diente@library.uq.edu.au>
2017-06-27 14:24:26 +1000

    revert accidental axios and reducer commit[PT #147403725]

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import {API_URL, TOKEN_NAME} from './general';
+import Cookies from 'js-cookie';
+import {API_URL, SESSION_COOKIE_NAME, TOKEN_NAME} from './general';
 
 export const api = axios.create({
     baseURL: API_URL
@@ -13,5 +14,5 @@ export const generateCancelToken = () => {
 };
 
 
-api.defaults.headers.common[TOKEN_NAME] = 'sveC8OeJbigHD93DW2qUCg31w8VQICu0lDzil3st';
+api.defaults.headers.common[TOKEN_NAME] = Cookies.get(SESSION_COOKIE_NAME);
 api.isCancel = axios.isCancel; // needed for cancelling requests and the instance created does not have this method
