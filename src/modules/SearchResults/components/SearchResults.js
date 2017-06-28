a1fdac8ffb8f474f8db850b0732030bd609f89cb
jannakha <j_malikova@yahoo.com>
2017-06-28 16:36:07 +1000

    added more mock data, fixed nulls

--- src/modules/SearchResults/components/SearchResults.js
+++ src/modules/SearchResults/components/SearchResults.js
@@ -28,14 +28,14 @@ export default class SearchResults extends Component {
         const searchResultEntries = dataSource.map((source, i) => {
             const entry = {
                 title: source.get('rek_title'),
-                journalName: source.get('fez_record_search_key_journal_name').get('rek_journal_name'),
+                journalName: source.get('fez_record_search_key_journal_name') ? source.get('fez_record_search_key_journal_name').get('rek_journal_name') : null,
                 authors: source.get('fez_record_search_key_author'),
                 publisher: source.get('fez_record_search_key_publisher'),
                 volumeNumber: source.get('fez_record_search_key_volume_number').get('rek_volume_number'),
-                issueNumber: source.get('fez_record_search_key_issue_number').get('rek_issue_number'),
-                startPage: source.get('fez_record_search_key_start_page').get('rek_start_page'),
-                endPage: source.get('fez_record_search_key_end_page').get('rek_end_page'),
-                doi: source.get('fez_record_search_key_doi').get('rek_doi'),
+                issueNumber: source.get('fez_record_search_key_issue_number') ? source.get('fez_record_search_key_issue_number').get('rek_issue_number') : null,
+                startPage: source.get('fez_record_search_key_start_page') ? source.get('fez_record_search_key_start_page').get('rek_start_page') : null,
+                endPage: source.get('fez_record_search_key_end_page') ? source.get('fez_record_search_key_end_page').get('rek_end_page') : null,
+                doi: source.get('fez_record_search_key_doi') ? source.get('fez_record_search_key_doi').get('rek_doi') : null,
                 counts: {
                     thomson: source.get('rek_thomson_citation_count')
                 }
