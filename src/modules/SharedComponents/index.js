8e8e0ff13ce3cee3ea848826ee7955b35859782b
uqadient <a.diente@library.uq.edu.au>
2017-06-28 11:10:55 +1000

    merged filuploader branch into master and moved local copy of checkbox into SharedComponents folder[PT #147403679]

--- src/modules/SharedComponents/index.js
+++ src/modules/SharedComponents/index.js
@@ -1,5 +1,6 @@
 export {FileUploader} from './FileUploader';
 export {Authors} from './Authors';
+export {Checkbox} from './Checkbox';
 
 // Reducers
 export {fileUploadReducer} from './FileUploader';
