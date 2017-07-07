ee4db03cfa90ac55f59bdad6b63e8dd1962068a7
uqadient <a.diente@library.uq.edu.au>
2017-07-07 13:31:23 +1000

    updated mock data

--- src/mock/data/claimPublication.js
+++ src/mock/data/claimPublication.js
@@ -68,7 +68,302 @@ export const claimPublication = [
             'rek_start_page': 'A19'
         },
         'rek_title': 'Rapamycin, But Not Tacrolimus, Promotes Cd8 Memory T-Cell Differentiation and Function in the Skin'
->>>>>>> 6e3ff07bf1f737a40bb9ce11a420fa58a6c371b0
+    },
+    {
+        'rek_pid': 'UQ:324578',
+        'rek_display_type': 'Journal Article',
+        'fez_record_search_key_volume_number': {
+            'rek_volume_number': '24'
+        },
+        'fez_record_search_key_end_page': {
+            'rek_end_page': 'A19'
+        },
+        'fez_record_search_key_author': [
+            {
+                'rek_author_id': 29262749,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Allinson, Mayumi',
+                'rek_author_order': 2
+            },
+            {
+                'rek_author_id': 29262750,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Altenburger, Rolf',
+                'rek_author_order': 3
+            },
+            {
+                'rek_author_id': 29262751,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Bain, Peter A.',
+                'rek_author_order': 4
+            },
+            {
+                'rek_author_id': 29262752,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Balaguer, Patrick',
+                'rek_author_order': 5
+            },
+            {
+                'rek_author_id': 29262753,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Busch, Wibke',
+                'rek_author_order': 6
+            },
+            {
+                'rek_author_id': 29262754,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Crago, Jordan',
+                'rek_author_order': 7
+            },
+            {
+                'rek_author_id': 29262755,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Denslow, Nancy D.',
+                'rek_author_order': 8
+            },
+            {
+                'rek_author_id': 29262756,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Dopp, Elke',
+                'rek_author_order': 9
+            },
+            {
+                'rek_author_id': 29262748,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Escher, Beate I.',
+                'rek_author_order': 1
+            },
+            {
+                'rek_author_id': 29262760,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Grimaldi, Marina',
+                'rek_author_order': 13
+            },
+            {
+                'rek_author_id': 29262757,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Hilscherova, Klara',
+                'rek_author_order': 10
+            },
+            {
+                'rek_author_id': 29262758,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Humpage, Andrew R.',
+                'rek_author_order': 11
+            },
+            {
+                'rek_author_id': 29262762,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Jarosova, Barbora',
+                'rek_author_order': 15
+            },
+            {
+                'rek_author_id': 29262761,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Jayasinghe, B. Sumith',
+                'rek_author_order': 14
+            },
+            {
+                'rek_author_id': 29262763,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Jia, Ai',
+                'rek_author_order': 16
+            },
+            {
+                'rek_author_id': 29262759,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Kumar, Anu',
+                'rek_author_order': 12
+            },
+            {
+                'rek_author_id': 29262787,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Leusch, Frederic D. L.',
+                'rek_author_order': 40
+            },
+            {
+                'rek_author_id': 29262764,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Makarov, Sergei',
+                'rek_author_order': 17
+            },
+            {
+                'rek_author_id': 29262765,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Maruya, Keith A.',
+                'rek_author_order': 18
+            },
+            {
+                'rek_author_id': 29262766,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Medvedev, Alex',
+                'rek_author_order': 19
+            },
+            {
+                'rek_author_id': 29262767,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Mehinto, Alvine C.',
+                'rek_author_order': 20
+            },
+            {
+                'rek_author_id': 29262768,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Mendez, Jamie E.',
+                'rek_author_order': 21
+            },
+            {
+                'rek_author_id': 29262769,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Poulsen, Anita',
+                'rek_author_order': 22
+            },
+            {
+                'rek_author_id': 29262770,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Prochazka, Erik',
+                'rek_author_order': 23
+            },
+            {
+                'rek_author_id': 29262771,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Richard, Jessica',
+                'rek_author_order': 24
+            },
+            {
+                'rek_author_id': 29262772,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Schifferli, Andrea',
+                'rek_author_order': 25
+            },
+            {
+                'rek_author_id': 29262773,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Schlenk, Danie',
+                'rek_author_order': 26
+            },
+            {
+                'rek_author_id': 29262774,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Scholz, Stefan',
+                'rek_author_order': 27
+            },
+            {
+                'rek_author_id': 29262775,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Shiraishi, Fujio',
+                'rek_author_order': 28
+            },
+            {
+                'rek_author_id': 29262776,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Snyder, Shane',
+                'rek_author_order': 29
+            },
+            {
+                'rek_author_id': 29262777,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Su, Guanyong',
+                'rek_author_order': 30
+            },
+            {
+                'rek_author_id': 29262778,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Tang, Janet Y. M.',
+                'rek_author_order': 31
+            },
+            {
+                'rek_author_id': 29262779,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'van der Burg, Bart',
+                'rek_author_order': 32
+            },
+            {
+                'rek_author_id': 29262780,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'van der Linden, Sander C.',
+                'rek_author_order': 33
+            },
+            {
+                'rek_author_id': 29262781,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Werner, Inge',
+                'rek_author_order': 34
+            },
+            {
+                'rek_author_id': 29262782,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Westerheide, Sandy D.',
+                'rek_author_order': 35
+            },
+            {
+                'rek_author_id': 29262783,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Wong, Chris K. C.',
+                'rek_author_order': 36
+            },
+            {
+                'rek_author_id': 29262784,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Yang, Min',
+                'rek_author_order': 37
+            },
+            {
+                'rek_author_id': 29262785,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Yeung, Bonnie H. Y.',
+                'rek_author_order': 38
+            },
+            {
+                'rek_author_id': 29262786,
+                'rek_author_pid': 'UQ:324578',
+                'rek_author_xsdmf_id': 6351,
+                'rek_author': 'Zhang, Xiaowei',
+                'rek_author_order': 39
+            }
+        ],
+        'fez_record_search_key_start_page': {
+            'rek_start_page': 'A19'
+        },
+        'rek_title': 'Benchmarking organic micropollutants in wastewater, recycled water and drinking water with in vitro bioassays',
     }
 ];
 export const hidePublications = [];
