643285ad3b4af1bea0aa45f64aca01e76db7e3d1
Vishal Asai <asai_vishal@yahoo.com>
2017-08-01 16:04:58 +1000

    updating publication subtype form [#149209033]

--- src/modules/SharedComponents/PublicationSubtype/components/PublicationSubtypeForm.js
+++ src/modules/SharedComponents/PublicationSubtype/components/PublicationSubtypeForm.js
@@ -1,40 +1,57 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
 import MenuItem from 'material-ui/MenuItem';
-import { SelectField } from 'uqlibrary-react-toolbox';
+import SelectField from 'material-ui/SelectField';
 
 class PublicationSubtypeForm extends Component {
     static propTypes = {
         onChange: PropTypes.func,
         locale: PropTypes.object,
-        list: PropTypes.array
+        subtypes: PropTypes.array,
+        rek_subtype: PropTypes.string
     };
 
     static defaultProps = {
         locale: {
             label: 'Publication subtype'
-        },
-        list: []
+        }
     };
 
-    componentWillUpdate(nextProps, nextState) {
-        if (this.props.onChange) this.props.onChange(nextState);
+    constructor(props) {
+        super(props);
+        this.state = {
+            rek_subtype: props.rek_subtype || null
+        };
     }
 
-    render() {
-        const { locale, list } = this.props;
+    componentWillUpdate(nextProps, nextState) {
+        if (this.props.onChange) this.props.onChange(nextState.rek_subtype);
+    }
 
-        const renderSubTypeItems = list.map((item) => {
+    _onSubtypeSelected = (event, index, value) => {
+        this.setState({
+            rek_subtype: value
+        });
+    };
+
+    render() {
+        const { locale, subtypes } = this.props;
+
+        const renderSubTypeItems = subtypes.map((item) => {
             return <MenuItem value={ item.controlled_vocab.cvo_title } primaryText={ item.controlled_vocab.cvo_title } key={ item.controlled_vocab.cvo_id }/>;
         });
 
         return (
             <div className="column">
                 <SelectField
-                       name="rek_subtype"
-                       fullWidth
-                       className="requiredField"
-                       floatingLabelText={ locale.label }>
+                    name="rek_subtype"
+                    fullWidth
+                    value={ this.state.rek_subtype }
+                    className="requiredField"
+                    maxHeight={ 250 }
+                    onChange={ this._onSubtypeSelected }
+                    floatingLabelText={ locale.label }>
                     <MenuItem
                         primaryText={ locale.label }
                         disabled/>
