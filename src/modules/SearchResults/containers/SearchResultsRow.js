21075ab5e31efa9a51c3e254321e628550d9be96
jannakha <j_malikova@yahoo.com>
2017-08-02 13:06:51 +1000

    clean up unused components [PT#149713963]

--- src/modules/SearchResults/containers/SearchResultsRow.js
+++
@@ -1,8 +0,0 @@
-import SearchResultsRow from '../components/SearchResultsRow';
-import {withRouter} from 'react-router';
-
-// Add router
-const SearchResultsRowContainer = withRouter(SearchResultsRow);
-
-export default SearchResultsRowContainer;
-
