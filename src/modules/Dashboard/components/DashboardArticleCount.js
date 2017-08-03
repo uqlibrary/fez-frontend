ea401dec1f7513345706d9c4bb6ba9d1d475d5d8
kylane <k.lane@uq.edu.au>
2017-08-03 11:54:09 +1000

    [PT#147486285] Added labels to locale

--- src/modules/Dashboard/components/DashboardArticleCount.js
+++ src/modules/Dashboard/components/DashboardArticleCount.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {locale} from 'config';
 
 const DashboardArticleCount = ({articleCount, articleFirstYear, articleLastYear}) => {
+    const txt = locale.components.dashboardArticleCount;
+
     return (
         <div className="authorCounter is-centered">
-            <div className="noOfArticles">{articleCount}</div>
-            <div className="articlesFrom">eSpace articles from</div>
-            <div className="dateRange">{articleFirstYear}<span> to </span>{articleLastYear}</div>
+            {articleCount && articleFirstYear && articleLastYear && (
+                <div>
+                <div className="noOfArticles">{articleCount}</div>
+                <div className="articlesFrom">{txt.countTitle}</div>
+                <div className="dateRange">{articleFirstYear}<span>{txt.yearSeparator}</span>{articleLastYear}</div>
+                </div>
+            )}
         </div>
     );
 };
--- src/modules/Dashboard/components/DashboardResearcherIds.js
+++ src/modules/Dashboard/components/DashboardResearcherIds.js
@@ -11,35 +11,34 @@ const DashboardResearcherIds = ({values}) => {
 
     return (
         <div className="columns researcherIds is-gapless">
-            {Object.keys(values).map((item, index) => (
-                <div key={index} className={`${item} column is-narrow`}>
-                    <a href="https://app.library.uq.edu.au/#/id" target="_blank">
-                        <Badge
-                            badgeStyle={badgeStyle}
-                            className={values[item] ? (`${item.toLowerCase()} researchIdBadge ok`) : (`${item.toLowerCase()} researchIdBadge error`)}
-                            badgeContent={values[item] ? badgeOK : badgeERROR}
-                            title={values[item] ? `Your ${item} Id is ${values[item]}` : `Your ${item} Id is not linked`}
-                            aria-label={values[item] ? `Your ${item} Id is ${values[item]}` : `Your ${item} Id is not linked`} >
-                            <Avatar
-                                className="researchIdAvatar"
-                                src={require(`../../../../src/images/${item.toLowerCase()}_icon.svg`)}
-                                title={`${item} Id`} />
-                        </Badge>
-                    </a>
-                </div>
-                )
-            )}
+                    {values && Object.keys(values).map((item, index) => (
+                            <div key={index} className={`${item} column is-narrow`}>
+                                <a href="https://app.library.uq.edu.au/#/id" target="_blank">
+                                    <Badge
+                                        badgeStyle={badgeStyle}
+                                        className={values[item] ? (`${item.toLowerCase()} researchIdBadge ok`) : (`${item.toLowerCase()} researchIdBadge error`)}
+                                        badgeContent={values[item] ? badgeOK : badgeERROR}
+                                        title={values[item] ? `Your ${item} Id is ${values[item]}` : `Your ${item} Id is not linked`}
+                                        aria-label={values[item] ? `Your ${item} Id is ${values[item]}` : `Your ${item} Id is not linked`}>
+                                        <Avatar
+                                            className="researchIdAvatar"
+                                            src={require(`../../../../src/images/${item.toLowerCase()}_icon.svg`)}
+                                            title={`${item} Id`}/>
+                                    </Badge>
+                                </a>
+                            </div>
+                        )
+                    )}
 
-            {
-                values.orcid &&
-                <div className="column is-narrow">
-                    <a className="orcidLink"
-                       href={'http://orcid.org/' + values.orcid}
-                       target="_blank"
-                       aria-label="Click to visit your ORCId profile"
-                       title="Click to visit your ORCId profile">
-                        orcid.org/{values.orcid}</a>
-                </div>
+            {values.orcid &&
+            <div className="column is-narrow">
+                <a className="orcidLink"
+                   href={'http://orcid.org/' + values.orcid}
+                   target="_blank"
+                   aria-label="Click to visit your ORCId profile"
+                   title="Click to visit your ORCId profile">
+                    orcid.org/{values.orcid}</a>
+            </div>
             }
         </div>
     );
