74a59e331457bd8f5b3a08c0f26b12063a75ba6e
jannakha <j_malikova@yahoo.com>
2017-08-04 15:18:02 +1000

    final code review [PT#147486285]

--- src/config/locale.js
+++ src/config/locale.js
@@ -505,9 +505,8 @@ export default {
             ariaPrefix: 'Photograph of '
         },
         dashboardResearcherIds: {
-            ariaTxt1: 'Your',
-            ariaTxt2: 'Id is',
-            ariaTxt3: 'Id is not linked',
+            researcherIsLinked: 'Your [resource] Id is [id]',
+            researcherIsNotLinked: 'You are not linked to [resource]',
             orcidUrlPrefix: 'http://orcid.org/',
             orcidLinkPrefix: 'orcid.org/',
             orcidlinkLabel: 'Click to visit your ORCId profile'
