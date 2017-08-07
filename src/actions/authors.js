971ccdc4e4cd3b1060bdada9e965ce0a1c456991
jannakha <j_malikova@yahoo.com>
2017-08-07 13:48:52 +1000

    refactoring actions - accounts [#149713963]

--- src/actions/authors.js
+++ src/actions/authors.js
@@ -12,6 +12,12 @@ export const AUTHOR_DETAILS_LOADING = 'AUTHOR_DETAILS_LOADING';
 export const AUTHOR_DETAILS_FAILED = 'AUTHOR_DETAILS_FAILED';
 export const AUTHOR_DETAILS_LOADED = 'AUTHOR_DETAILS_LOADED';
 
+/**
+ * Returns the authors list based on a query, filtered locally by filterBy function
+ * @param {string} query passed on to api call
+ * @param {function} filterBy function to filter/transform results from api list, eg users with org ids only
+ * @returns {action}
+ */
 export function searchAuthors(query, filterBy) {
     return dispatch => {
         dispatch({type: AUTHORS_LOADING});
@@ -30,6 +36,10 @@ export function searchAuthors(query, filterBy) {
     };
 }
 
+/**
+ * Returns currently logged in author (based on X_UQL_TOKEN)
+ * @returns {action}
+ */
 export function getCurrentAuthor() {
     return dispatch => {
         dispatch({type: CURRENT_AUTHOR_LOADING});
