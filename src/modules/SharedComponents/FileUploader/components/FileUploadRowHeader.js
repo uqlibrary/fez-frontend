00a126f31b3d55b2ed99aa9ed488a34956eb5294
Vishal Asai <asai_vishal@yahoo.com>
2017-08-08 15:05:41 +1000

    updated list header [#147403679]

--- src/modules/SharedComponents/FileUploader/components/FileUploadRowHeader.js
+++ src/modules/SharedComponents/FileUploader/components/FileUploadRowHeader.js
@@ -18,7 +18,7 @@ export default class FileUploadRowHeader extends Component {
             deleteAllFiles: 'Remove all files from the upload queue',
             deleteAllFilesConfirmation: {
                 confirmationTitle: 'Delete all',
-                confirmationMessage: 'Are you sure you want to delete all records?',
+                confirmationMessage: 'Are you sure you want to delete all files?',
                 cancelButtonLabel: 'No',
                 confirmButtonLabel: 'Yes'
             }
@@ -34,25 +34,19 @@ export default class FileUploadRowHeader extends Component {
     };
 
     render() {
-        const {filenameColumn, fileAccessColumn, embargoDateColumn, deleteAllFiles, deleteAllFilesConfirmation} = this.props.locale;
+        const { filenameColumn, deleteAllFiles, deleteAllFilesConfirmation } = this.props.locale;
 
         return (
-            <div className="columns is-gapless is-mobile contributorsHeader datalist datalist-header">
-                <ConfirmDialogBox onRef={ref => (this.confirmationBox = ref)}
-                                  onAction={this.props.onDeleteAll}
-                                  locale={deleteAllFilesConfirmation} />
-                <div className="column is-6-desktop is-6-tablet is-12-mobile filename datalist-title" style={{textIndent: '12px'}}>
-                    {filenameColumn}
+            <div className="columns is-gapless is-mobile uploadedFileHeader datalist datalist-header">
+                <ConfirmDialogBox onRef={ ref => (this.confirmationBox = ref) }
+                                  onAction={ this.props.onDeleteAll }
+                                  locale={ deleteAllFilesConfirmation } />
+                <div className="column filename datalist-title" style={{ textIndent: '12px' }}>
+                    { filenameColumn }
                 </div>
-                <div className="column is-3-desktop is-3-tablet is-12-mobile file-access datalist-title">
-                    {fileAccessColumn}
-                </div>
-                <div className="column is-2-desktop is-2-tablet is-12-mobile embargo-date datalist-title">
-                    {embargoDateColumn}
-                </div>
-                <div className="column is-1-desktop is-1-tablet is-12-mobile delete-button datalist-title">
-                    <IconButton tooltip={deleteAllFiles} tooltipPosition="bottom-left" onTouchTap={this._showConfirmation}>
-                        <FontIcon className="material-icons deleteIcon">delete_forever</FontIcon>
+                <div className="column is-narrow buttons datalist-buttons">
+                    <IconButton tooltip={ deleteAllFiles } onTouchTap={ this._showConfirmation }>
+                        <FontIcon className="material-icons">delete_forever</FontIcon>
                     </IconButton>
                 </div>
             </div>
