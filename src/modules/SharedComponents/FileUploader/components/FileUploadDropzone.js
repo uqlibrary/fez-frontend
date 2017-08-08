51fdc7619b5e709cf8d55737af53d4d93b0b099a
Vishal Asai <asai_vishal@yahoo.com>
2017-08-08 15:11:56 +1000

    updated drop zone component to validate files [#147403679]

--- src/modules/SharedComponents/FileUploader/components/FileUploadDropzone.js
+++ src/modules/SharedComponents/FileUploader/components/FileUploadDropzone.js
@@ -1,30 +1,63 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Dropzone from 'react-dropzone';
-import { locale } from 'config';
+import _ from 'lodash';
+import FileUploadDropzoneStaticContent from './FileUploadDropzoneStaticContent';
 
 class FileUploadDropzone extends PureComponent {
     static propTypes = {
-        onFileDropped: PropTypes.func.isRequired
+        onDropped: PropTypes.func.isRequired,
+        maxSize: PropTypes.number.isRequired,
+        uploadedFiles: PropTypes.array
     };
 
-    _onFileDropped = (files) => {
-        this.props.onFileDropped(files);
+    constructor(props) {
+        super(props);
+        this.dropzoneRef = null;
+        this.accepted = [];
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.accepted = nextProps.uploadedFiles;
+    }
+
+    _onDropAccepted = (accepted) => {
+        /**
+         * Filter folders
+         */
+        const invalid = accepted.filter((file) => {
+            return file.type === '' ||                          // no type means folder
+                file.name.length > 45 ||                        // check for filename length
+                _.split(file.name, '.').length > 2;             // check for more than one period
+        });
+
+        if (invalid.length > 0) {
+            console.log('Error');
+        }
+
+        const filtered = _.difference(accepted, invalid);
+
+        this.accepted.push(...filtered);
+
+        /**
+         * Filter duplicates
+         */
+        const unique = _.uniqBy(this.accepted, 'name');
+
+        this.props.onDropped(unique);
     };
 
     render() {
-        const { filenameRestrictions } = locale.sharedComponents.files.fileInformation.fields;
-        let dropzoneRef;
-
         return (
             <div className="columns" style={{ marginTop: '12px' }}>
-                <div className="column"  tabIndex="0" onKeyPress={ () => dropzoneRef.open() }>
+                <div className="column"  tabIndex="0" onKeyPress={ () => this.dropzoneRef.open() }>
                     <Dropzone
-                        ref={ (node) => {dropzoneRef = node;} }
-                        onDrop={ this._onFileDropped }
+                        ref={ (node) => { this.dropzoneRef = node; }}
+                        maxSize={ this.props.maxSize }
+                        onDropAccepted={ this._onDropAccepted.bind(this) }
                         style={{ padding: '10px' }}
                         disablePreview>
-                        { filenameRestrictions }
+                        <FileUploadDropzoneStaticContent />
                     </Dropzone>
                 </div>
             </div>
