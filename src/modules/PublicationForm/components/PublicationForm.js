3406dedcccda0a7e33ebcd2f58e562aa09a4cf85
jannakha <j_malikova@yahoo.com>
2017-08-14 15:00:10 +1000

    prevent users from modifying data on submit

--- src/modules/PublicationForm/components/PublicationForm.js
+++ src/modules/PublicationForm/components/PublicationForm.js
@@ -46,7 +46,14 @@ export default class PublicationForm extends Component {
         const filteredPublicationType = publicationTypeId ?
             this.publicationTypes.filter((item) => { return item.id === publicationTypeId; }) : null;
         return filteredPublicationType && filteredPublicationType.length > 0 && filteredPublicationType[0].formComponent ?
-            React.createElement(filteredPublicationType[0].formComponent, {vocabId: filteredPublicationType[0].vocabId}) : null;
+            React.createElement(
+                filteredPublicationType[0].formComponent,
+                {
+                    vocabId: filteredPublicationType[0].vocabId,
+                    submitting: this.props.submitting
+                })
+            :
+            null;
     };
 
     render() {
