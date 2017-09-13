8cc5bf356d018d56505b4842056f2aab99fad996
jannakha <j_malikova@yahoo.com>
2017-09-13 15:02:17 +1000

    facets from live data for my publication [#150470377]

--- src/reducers/publications.js
+++ src/reducers/publications.js
@@ -1,79 +1,9 @@
 import * as actions from 'actions/publications';
 
-/* eslint-disable */
-const mockFacets = {
-    "Scopus document type": {
-        "doc_count_error_upper_bound": 0,
-        "sum_other_doc_count": 0,
-        "buckets": []
-    },
-    "Mock data: Display type": {
-        "doc_count_error_upper_bound": 0,
-        "sum_other_doc_count": 0,
-        "buckets": [{"key": 179, "doc_count": 2}]
-    },
-    "Mock data: Keywords": {
-        "doc_count_error_upper_bound": 0,
-        "sum_other_doc_count": 5,
-        "buckets": [{"key": "Applied Microbiology and Biotechnology", "doc_count": 1}, {
-            "key": "Biochemistry",
-            "doc_count": 1
-        }, {"key": "Bioengineering", "doc_count": 1}, {
-            "key": "Biomaterials",
-            "doc_count": 1
-        }, {"key": "Biomedical Engineering", "doc_count": 1}]
-    },
-    "Scopus document type (lookup)": {
-        "doc_count_error_upper_bound": 0,
-        "sum_other_doc_count": 0,
-        "buckets": []
-    },
-    "Subject (lookup)": {"doc_count_error_upper_bound": 0, "sum_other_doc_count": 0, "buckets": []},
-    "Collection (lookup)": {
-        "doc_count_error_upper_bound": 0,
-        "sum_other_doc_count": 0,
-        "buckets": [{"key": "Crossref Import", "doc_count": 2}]
-    },
-    "Mock data: Year published": {
-        "doc_count_error_upper_bound": 0,
-        "sum_other_doc_count": 0,
-        "buckets": [{"key": "2017", "doc_count": 2}]
-    },
-    "Author (lookup)": {
-        "doc_count_error_upper_bound": 0,
-        "sum_other_doc_count": 0,
-        "buckets": [{"key": "Schenk, Gerhard (Gary)", "doc_count": 1}]
-    },
-    "SMock data: ubject": {"doc_count_error_upper_bound": 0, "sum_other_doc_count": 0, "buckets": []},
-    "Mock data: Journal name": {
-        "doc_count_error_upper_bound": 0,
-        "sum_other_doc_count": 0,
-        "buckets": [{"key": "Metallomics", "doc_count": 1}, {"key": "Nature biotechnology", "doc_count": 1}]
-    },
-    "Mock data: Collection": {
-        "doc_count_error_upper_bound": 0,
-        "sum_other_doc_count": 0,
-        "buckets": [{"key": "UQ:639325", "doc_count": 2}]
-    },
-    "Mock data: Author": {
-        "doc_count_error_upper_bound": 0,
-        "sum_other_doc_count": 0,
-        "buckets": [{"key": 683, "doc_count": 1}]
-    },
-    "Mock data: Genre": {"doc_count_error_upper_bound": 0, "sum_other_doc_count": 0, "buckets": []},
-    "Subtype": {"doc_count_error_upper_bound": 0, "sum_other_doc_count": 0, "buckets": []},
-    "Display type (lookup)": {
-        "doc_count_error_upper_bound": 0,
-        "sum_other_doc_count": 0,
-        "buckets": [{"key": "Journal Article", "doc_count": 2}]
-    }
-};
-/* eslint-enable */
-
 export const initialState = {
     publicationsList: [],
     publicationsListPagingData: {},
-    publicationsListFacets: mockFacets,
+    publicationsListFacets: {},
     loadingPublicationsList: true,
 
     latestPublicationsList: [],
@@ -142,6 +72,8 @@ const handlers = {
                 to: action.payload.to,
                 per_page: action.payload.per_page
             },
+            publicationsListFacets: action.payload.hasOwnProperty('filters') && action.payload.filters.hasOwnProperty('facets')
+                && action.payload.filters.facets ? action.payload.filters.facets : {},
             loadingPublicationsList: false
         };
     },
@@ -151,6 +83,7 @@ const handlers = {
             ...state,
             publicationsList: [],
             publicationsListPagingData: {},
+            publicationsListFacets: {},
             loadingPublicationsList: false
         };
     },
