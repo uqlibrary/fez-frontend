0c5e9f8a22ba4fc0d8db60e3afbd76310e0d07f5
Vishal Asai <asai_vishal@yahoo.com>
2017-09-21 11:54:25 +1000

    Revert publication form alert component [#150586132]

--- src/modules/SharedComponents/index.js
+++ src/modules/SharedComponents/index.js
@@ -3,4 +3,3 @@ export {PublicationSubtypeField} from './PublicationSubtype';
 export {PublicationStats} from './PublicationStats';
 export {AuthorLinkingField} from './AuthorLinking';
 export {ScrollToTop} from './ScrollToTop';
-export {PublicationFormAlert} from './PublicationFormAlert';
