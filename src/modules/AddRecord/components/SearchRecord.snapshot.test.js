4b1dcc5750de0780802f435d1134379f2352dca1
Vishal Asai <asai_vishal@yahoo.com>
2017-09-29 14:30:13 +1000

    full coverage for SearchRecord component [#151318406]

--- src/modules/AddRecord/components/SearchRecord.snapshot.test.js
+++ src/modules/AddRecord/components/SearchRecord.snapshot.test.js
@@ -1,6 +1,7 @@
 import {shallow} from 'enzyme';
 import toJson from 'enzyme-to-json';
 import React from 'react';
+import Immutable from 'immutable';
 
 import SearchRecord from './SearchRecord';
 
@@ -12,5 +13,17 @@ describe('Search record', () => {
     it('should render stepper and a publication search form', () => {
         const wrapper = setup({history: {}});
         expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should perform search and redirect to results page', () => {
+        const searchPublications = jest.fn();
+        const history = {
+            push: (path) => (expect(path).toBe('/records/add/results'))
+        };
+
+        const wrapper = setup({history: history, actions: {searchPublications: searchPublications}});
+        wrapper.instance()._performSearch(Immutable.Map({searchQuery: 'bla'}));
+
+        expect(searchPublications).toBeCalled();
     });
 });
