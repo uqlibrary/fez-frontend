c444b9725604887fc2f874781b87bb70ad56e4fa
Vishal Asai <asai_vishal@yahoo.com>
2017-10-05 10:02:54 +1000

    export pages from modules directory [#151541430]

---
+++ src/modules/index.js
@@ -0,0 +1,7 @@
+export {App} from './App';
+export {Browse} from './Browse';
+export {Dashboard} from './Dashboard';
+export {Research} from './Research';
+export {SearchRecord, SearchRecordResults, AddNewRecord} from './AddRecord';
+export {ClaimPublication} from './ClaimPublication';
+export {ClaimPublicationForm} from './ClaimPublicationForm';
