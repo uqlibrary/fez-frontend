58f2fb2590666a6f628b0e88344f359539735b26
Vishal Asai <asai_vishal@yahoo.com>
2017-10-16 10:48:49 +1000

    fixed merge [#151670239]

--- src/modules/SharedComponents/index.js
+++
@@ -1,6 +0,0 @@
-export {ContributorsEditorField} from './ContributorsEditor';
-export {PublicationSubtypeField} from './PublicationSubtype';
-export {PublicationStats} from './PublicationStats';
-export {AuthorLinkingField} from './AuthorLinking';
-export {ScrollToTop} from './ScrollToTop';
-export {OrgUnitsSelectorField} from './OrgUnitsSelector';
