20913fe3a2604e69697fdc35f6f16542f6a52716
Ky Lane <k.lane@uq.edu.au>
2017-10-24 11:58:36 +1000

    [PT#151670344] Code review fixes, and importing fixed citation

--- src/modules/SharedComponents/PublicationsList/components/citations/GenericDocumentCitation.js
+++ src/modules/SharedComponents/PublicationsList/components/citations/GenericDocumentCitation.js
@@ -16,10 +16,13 @@ export default class GenericDocumentCitation extends Component {
     render() {
         const genericDocument = {
             id: this.props.publication.rek_pid,
-            contributor: this.props.publication.fez_record_search_key_contributor,
-            publisher: this.props.publication.fez_record_search_key_publisher,
+            publisher: this.props.publication.fez_record_search_key_publisher ? this.props.publication.fez_record_search_key_publisher.rek_publisher : null,
             title: this.props.publication.rek_title,
         };
+        console.log('id          : ' + genericDocument.id);
+        console.log('title       : ' + genericDocument.title);
+        console.log('contributor : ' + genericDocument.contributor);
+        console.log('publisher   : ' + genericDocument.publisher);
 
         // eSpace citation view for Generic article
         // {7630}{7660| (|)}. {7623}. {7638|Edited by |. }{7658||.}
@@ -30,14 +33,18 @@ export default class GenericDocumentCitation extends Component {
                 <FontIcon className="material-icons citationIcon" data-place="left">
                     format_quote
                 </FontIcon>
-                <AuthorsCitationView publication={this.props.publication} /> <YearCitationView publication={this.props.publication} />.
-                {
-                    genericDocument.contributor &&
-                    <span className="citationTitle"> Edited by {genericDocument.contributor}.</span>
-                }
+                <AuthorsCitationView publication={this.props.publication} /> <YearCitationView publication={this.props.publication} />
+                <span className="citationTitle"> {genericDocument.title}.</span>
+                <AuthorsCitationView
+                    className="citationContributors"
+                    prefix=" Edited by "
+                    suffix="."
+                    publication={this.props.publication}
+                    searchKey={{key: 'fez_record_search_key_contributor', order: 'rek_contributor_order', subkey: 'rek_contributor'}}
+                />
                 {
                     genericDocument.publisher &&
-                    <span className="citationTitle"> {genericDocument.publisher}.</span>
+                    <span className="citationPublisher"> {genericDocument.publisher}.</span>
                 }
             </div>
         );
