0db88a92c3319aaced799680820157fff32a6fb1
Ky Lane <k.lane@uq.edu.au>
2017-10-30 17:00:59 +1000

    Missed class name, and remove yarn.lock

--- src/modules/SharedComponents/PublicationForm/components/Forms/CreativeWorkForm.js
+++ src/modules/SharedComponents/PublicationForm/components/Forms/CreativeWorkForm.js
@@ -7,7 +7,7 @@ import {TextField, StandardCard, PartialDateField} from 'uqlibrary-react-toolbox
 import {ContributorsEditorField} from 'modules/SharedComponents/ContributorsEditor';
 import {validation, locale} from 'config';
 
-export default class JournalArticleForm extends Component {
+export default class CreativeWorkForm extends Component {
     static propTypes = {
         submitting: PropTypes.bool,
         subtypeVocabId: PropTypes.number
