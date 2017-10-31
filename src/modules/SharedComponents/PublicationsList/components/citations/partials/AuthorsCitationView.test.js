b6d65bf289599c2805c5e680e61114df2a48cd01
Vishal Asai <asai_vishal@yahoo.com>
2017-10-31 16:04:18 +1000

    added test case for toggle names [#152385379]

--- src/modules/SharedComponents/PublicationsList/components/citations/partials/AuthorsCitationView.test.js
+++ src/modules/SharedComponents/PublicationsList/components/citations/partials/AuthorsCitationView.test.js
@@ -128,4 +128,95 @@ describe('AuthorsCitationView test ', () => {
         const wrapper = setup({ publication: testObject, prefix: 'Authored by: ', suffix: ' people.'});
         expect(toJson(wrapper)).toMatchSnapshot();
     });
+
+    it('should render component with ten authors and show more link', () => {
+        const testObject = {
+            "fez_record_search_key_author": [
+                {
+                    "rek_author_id": null,
+                    "rek_author_pid": "UQ:678742",
+                    "rek_author": "Pedroso, Marcelo Monteiro",
+                    "rek_author_order": 1
+                },
+                {
+                    "rek_author_id": null,
+                    "rek_author_pid": "UQ:678742",
+                    "rek_author": "Smith, J",
+                    "rek_author_order": 2
+                },
+                {
+                    "rek_author_id": null,
+                    "rek_author_pid": "UQ:678742",
+                    "rek_author": "Andersen, J",
+                    "rek_author_order": 3
+                },
+                {
+                    "rek_author_id": null,
+                    "rek_author_pid": "UQ:678742",
+                    "rek_author": "Andersen, J",
+                    "rek_author_order": 4
+                },
+                {
+                    "rek_author_id": null,
+                    "rek_author_pid": "UQ:678742",
+                    "rek_author": "Andersen, J",
+                    "rek_author_order": 5
+                },
+                {
+                    "rek_author_id": null,
+                    "rek_author_pid": "UQ:678742",
+                    "rek_author": "Andersen, J",
+                    "rek_author_order": 6
+                },
+                {
+                    "rek_author_id": null,
+                    "rek_author_pid": "UQ:678742",
+                    "rek_author": "Andersen, J",
+                    "rek_author_order": 7
+                },
+                {
+                    "rek_author_id": null,
+                    "rek_author_pid": "UQ:678742",
+                    "rek_author": "Andersen, J",
+                    "rek_author_order": 8
+                },
+                {
+                    "rek_author_id": null,
+                    "rek_author_pid": "UQ:678742",
+                    "rek_author": "Andersen, J",
+                    "rek_author_order": 9
+                },
+                {
+                    "rek_author_id": null,
+                    "rek_author_pid": "UQ:678742",
+                    "rek_author": "Andersen, J",
+                    "rek_author_order": 10
+                },
+                {
+                    "rek_author_id": null,
+                    "rek_author_pid": "UQ:678742",
+                    "rek_author": "Andersen, J",
+                    "rek_author_order": 11
+                },
+                {
+                    "rek_author_id": null,
+                    "rek_author_pid": "UQ:678742",
+                    "rek_author": "Andersen, J",
+                    "rek_author_order": 12
+                }
+            ]
+        };
+        const wrapper = setup({ publication: testObject, prefix: 'Authored by: ', suffix: ' people.'});
+        expect(toJson(wrapper)).toMatchSnapshot();
+        expect(wrapper.state().showMore).toBeFalsy();
+        expect(wrapper.find('.citationAuthor').length).toEqual(10);
+        expect(wrapper.find('.citationShowMoreAuthors').length).toEqual(1);
+        expect(wrapper.find('.citationShowMoreAuthors').text()).toEqual(' 2 more... ');
+
+        wrapper.instance()._toggleShowMore();
+        expect(toJson(wrapper)).toMatchSnapshot();
+        expect(wrapper.state().showMore).toBeTruthy();
+        expect(wrapper.find('.citationAuthor').length).toEqual(12);
+        expect(wrapper.find('.citationShowMoreAuthors').text()).toEqual(' Show less ');
+    });
 });
