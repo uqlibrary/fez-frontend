9c0e2a3eb91e230962e532c53252ee5f1d3b59f7
jannakha <j_malikova@yahoo.com>
2017-11-06 12:36:58 +1000

    created generic select field from subtypes field [#152497077]

--- src/modules/SharedComponents/PublicationForm/components/Forms/WorkingPaperForm.js
+++ src/modules/SharedComponents/PublicationForm/components/Forms/WorkingPaperForm.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Field} from 'redux-form/immutable';
-
+import {ThesisSubtypeField} from 'modules/SharedComponents/PublicationSubtype';
 import {StandardCard} from 'uqlibrary-react-toolbox';
 import {OrgUnitsField, SeriesField} from 'modules/SharedComponents/AutoSuggestField';
 
@@ -18,6 +18,11 @@ export default class WorkingPaperForm extends Component {
         return (
             <div>
                 <StandardCard title="Placeholder for working paper">
+                    <Field
+                        component={ThesisSubtypeField}
+                        name="rek_subtype"
+                        disabled={this.props.submitting}
+                        className="requiredField" />
                     <Field
                         component={OrgUnitsField}
                         name="testField"
