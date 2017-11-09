cc047c9608d81b9108bf0b796ebeb270d91f95c2
Vishal Asai <asai_vishal@yahoo.com>
2017-11-09 10:15:12 +1000

    fix filename [#152535815]

--- src/modules/RecordView/components/partials/PmcidLink.test.js
+++ src/modules/RecordView/components/partials/PmcidLink.test.js
@@ -3,7 +3,7 @@ jest.dontMock('./PmcidLink');
 import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import React from 'react';
-import PmcidLink from './PmcIdLink';
+import PmcidLink from './PmcidLink';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import PropTypes from 'prop-types';
 import injectTapEventPlugin from 'react-tap-event-plugin';
