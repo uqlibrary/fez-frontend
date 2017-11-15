06d4dd6e9d8bc99a3de23710283fa453eeb03c54
Ky Lane <k.lane@uq.edu.au>
2017-11-16 09:58:38 +1000

    [PT#152875292] Swapped out YearCitationView for DateCitationView

--- src/modules/SharedComponents/PublicationsList/components/citations/DataCollectionCitation.js
+++ src/modules/SharedComponents/PublicationsList/components/citations/DataCollectionCitation.js
@@ -36,7 +36,7 @@ export default class DataCollectionCitation extends Component {
                 <Partials.AuthorsCitationView publication={this.props.publication} suffix="" />
 
                 {/* {Publication Year| (|).} */}
-                <Partials.YearCitationView date={this.props.publication.rek_date} />
+                <Partials.DateCitationView date={this.props.publication.rek_date} />
 
                 {/* <i>{Dataset Title| |.}</i> */}
                 <Partials.CitationView className="citationTitle" value={record.title} />
