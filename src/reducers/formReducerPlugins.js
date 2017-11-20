7a8ea18425ed7a81f037aec77844401502a98393
Vishal Asai <asai_vishal@yahoo.com>
2017-11-20 12:38:04 +1000

    skip fields with initial values [#152914713]

--- src/reducers/formReducerPlugins.js
+++ src/reducers/formReducerPlugins.js
@@ -3,10 +3,14 @@ import {actionTypes} from 'redux-form';
 export const resetValue = (state, action) => {
     switch(action.type) {
         case actionTypes.UNREGISTER_FIELD:
+            const key = action.payload.name.split('.').shift();
+            if (state.hasIn(['initial', key])) {
+                return state;
+            }
             return state
-                .deleteIn(['values', action.payload.name.split('.').shift()])
+                .deleteIn(['values', key])
                 .deleteIn(['registeredFields', action.payload.name])
-                .deleteIn(['fields', action.payload.name.split('.').shift()]);
+                .deleteIn(['fields', key]);
         default:
             return state;
     }
--- src/reducers/formReducerPlugins.test.js
+++ src/reducers/formReducerPlugins.test.js
@@ -30,19 +30,45 @@ describe('Form reducer plugin', () => {
                         visited: true
                     })
                 })
+            }),
+            initial: Map({
+                rek_title: 'Initial value',
+                currentAuthor: [
+                    {
+                        nameAsPublished: 'Test user',
+                        authorId: 123
+                    }
+                ]
             })
         });
 
-        const action = {
+        let nextState = plugins.resetValue(state, {type: 'SOME_OTHER_TYPE'});
+        expect(nextState).toEqual(state);
+
+        let action = {
+            type: actionTypes.UNREGISTER_FIELD,
+            payload: {
+                name: 'currentAuthor.0.nameAsPublished'
+            }
+        };
+        nextState = plugins.resetValue(nextState, action);
+        expect(nextState).toEqual(state);
+
+        action = {
+            type: actionTypes.UNREGISTER_FIELD,
+            payload: {
+                name: 'rek_title'
+            }
+        };
+        nextState = plugins.resetValue(nextState, action);
+        expect(nextState).toEqual(state);
+
+        action = {
             type: actionTypes.UNREGISTER_FIELD,
             payload: {
                 name: 'fez_record_search_key_a.a'
             }
         };
-
-        let nextState = plugins.resetValue(state, {type: 'SOME_OTHER_TYPE'});
-        expect(nextState).toEqual(state);
-
         nextState = plugins.resetValue(nextState, action);
 
         expect(nextState.get('values').has(action.payload.name.split('.').shift())).toBeFalsy();
