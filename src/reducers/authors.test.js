1bca68794b19724a2b8fc291ea559a9fbe250cb8
Ky Lane <k.lane@uq.edu.au>
2017-11-29 15:58:46 +1000

    [PT#152793179] Added tests for authors reducer

---
+++ src/reducers/authors.test.js
@@ -0,0 +1,57 @@
+import * as actions from 'actions/actionTypes';
+import authorsReducer from './authors';
+
+describe('Authors reducer', () => {
+
+    const initialState = {
+        authorsListLoading: false,
+        authorsListLoadingError: false,
+        authorsList: []
+    };
+    const mockAuthor = {"aut_id":410,"aut_org_username":"uqifraze"};
+
+    it('fails to load data', () => {
+        const test = authorsReducer(initialState, {type: actions.AUTHORS_LOAD_FAILED});
+        expect(test).toEqual({
+            authorsList: [],
+            authorsListLoading: false,
+            authorsListLoadingError: true
+        })
+    });
+
+    it('successfully loads authors data', () => {
+        const test = authorsReducer(initialState, {type: actions.AUTHORS_LOADED, payload: mockAuthor});
+        expect(test).toEqual({
+            authorsList: mockAuthor,
+            authorsListLoading: false,
+            authorsListLoadingError: false
+        })
+    });
+
+    it('returns that it is loading author data', () => {
+        const test = authorsReducer(initialState, {type: actions.AUTHORS_LOADING});
+        expect(test).toEqual({
+            authorsList: [],
+            authorsListLoading: true,
+            authorsListLoadingError: false
+        })
+    });
+
+    it('returns the initialState when supplied an invalid action type', () => {
+        const test = authorsReducer(initialState, {type: 'INVALID_ACTION_TYPE'});
+        expect(test).toEqual(initialState);
+    });
+
+});
+
+
+
+/*
+    [actions.AUTHORS_LOADING]: () => ({
+        authorsList: [],
+        authorsListLoading: true,
+        authorsListLoadingError: false
+    })
+
+
+ */
