f81b165ee3a45780d62a35fef011868aba383770
Vishal Asai <asai_vishal@yahoo.com>
2017-12-06 14:52:40 +1000

    Fixed test [#151792799]

--- src/modules/Dashboard/components/DashboardAuthorProfile.snapshot.test.js
+++ src/modules/Dashboard/components/DashboardAuthorProfile.snapshot.test.js
@@ -35,8 +35,19 @@ describe('Dashboard Profile test', () => {
                 org_units: ['Institute for Molecular Bioscience', 'School of Chemistry and Molecular Biosciences', 'The University of Queensland Diamantina Institute'],
                 positions: ['Affiliate Professor', 'Affiliate Professorial Res Fellow', 'ARC Australian Laureate Fellow'],
                 espace: {'first_year': 1990, 'last_year': 2017, 'doc_count': '282'}
+            },
+            author: {
+                aut_title: 'Professor',
+                aut_fname: 'J',
+                aut_lname: 'Researcher',
+                aut_orgUnits: ['Institute for Molecular Bioscience', 'School of Chemistry and Molecular Biosciences', 'The University of Queensland Diamantina Institute'],
+                aut_positions: ['Affiliate Professor', 'Affiliate Professorial Res Fellow', 'ARC Australian Laureate Fellow'],
+                aut_scopus_id: '',
+                aut_google_scholar_id: '',
+                aut_researcher_id: 'G-111-1111',
+                aut_orcid_id: '0000-0001-1111-1111',
+                aut_publons_id: '',
             }
-
         };
         const wrapper = setup(props);
         expect(toJson(wrapper)).toMatchSnapshot();
@@ -60,8 +71,19 @@ describe('Dashboard Profile test', () => {
                 mypub_url: 'uqresearcher',
                 org_units: ['Institute for Molecular Bioscience', 'School of Chemistry and Molecular Biosciences', 'The University of Queensland Diamantina Institute'],
                 positions: ['Affiliate Professor', 'Affiliate Professorial Res Fellow', 'ARC Australian Laureate Fellow']
+            },
+            author: {
+                aut_title: 'Professor',
+                aut_fname: 'J',
+                aut_lname: 'Researcher',
+                aut_orgUnits: ['Institute for Molecular Bioscience', 'School of Chemistry and Molecular Biosciences', 'The University of Queensland Diamantina Institute'],
+                aut_positions: ['Affiliate Professor', 'Affiliate Professorial Res Fellow', 'ARC Australian Laureate Fellow'],
+                aut_scopus_id: '',
+                aut_google_scholar_id: '',
+                aut_researcher_id: 'G-111-1111',
+                aut_orcid_id: '0000-0001-1111-1111',
+                aut_publons_id: '',
             }
-
         };
         const wrapper = setup(props);
         expect(toJson(wrapper)).toMatchSnapshot();
@@ -83,6 +105,18 @@ describe('Dashboard Profile test', () => {
                 orcid_id: '0000-0001-1111-1111',
                 publons_id: '',
                 mypub_url: 'uqresearcher',
+            },
+            author: {
+                aut_title: 'Professor',
+                aut_fname: 'J',
+                aut_lname: 'Researcher',
+                aut_orgUnits: ['Institute for Molecular Bioscience', 'School of Chemistry and Molecular Biosciences', 'The University of Queensland Diamantina Institute'],
+                aut_positions: ['Affiliate Professor', 'Affiliate Professorial Res Fellow', 'ARC Australian Laureate Fellow'],
+                aut_scopus_id: '',
+                aut_google_scholar_id: '',
+                aut_researcher_id: 'G-111-1111',
+                aut_orcid_id: '0000-0001-1111-1111',
+                aut_publons_id: '',
             }
         };
         const wrapper = setup(props);
@@ -105,7 +139,14 @@ describe('Dashboard Profile test', () => {
             author: {
                 aut_id: 410,
                 aut_display_name: 'J. Researcher',
-                aut_title: 'Prof'
+                aut_title: 'Prof',
+                aut_orgUnits: ['Institute for Molecular Bioscience', 'School of Chemistry and Molecular Biosciences', 'The University of Queensland Diamantina Institute'],
+                aut_positions: ['Affiliate Professor', 'Affiliate Professorial Res Fellow', 'ARC Australian Laureate Fellow'],
+                aut_scopus_id: '',
+                aut_google_scholar_id: '',
+                aut_researcher_id: 'G-111-1111',
+                aut_orcid_id: '0000-0001-1111-1111',
+                aut_publons_id: '',
             }
         };
         const wrapper = setup(props);
