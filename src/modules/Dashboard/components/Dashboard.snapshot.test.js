b2b9f89437d4a25cdc4e21e44b0968b939c2d70e
Ky Lane <k.lane@uq.edu.au>
2018-02-07 10:33:23 +1000

    [PT#153844180] Tests added for display of bar/donut graphs when data is/isnt available

--- src/modules/Dashboard/components/Dashboard.snapshot.test.js
+++ src/modules/Dashboard/components/Dashboard.snapshot.test.js
@@ -83,4 +83,21 @@ describe('Dashboard test', () => {
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
+    it('doesn\'t render the bar/donut graph cards when no data is available', () => {
+        const wrapper = setup({
+            authorDetails: mock.authorDetails.uqresearcher,
+            publicationsByYear: {series: {}},
+            publicationTypesCount: []
+        });
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('does render the donut/bar graph cards when data is available', () => {
+        const wrapper = setup({
+            authorDetails: mock.authorDetails.uqresearcher,
+            publicationsByYear: {"series":[{"name":"Journal Article","data":[1,1,3,5,5,8,8,2,5,3,6,4,4,7,8,8,6,4,10,10,8,10,12,7,19,11,11,12,6,8,15,10,9,3,13,6,5,5]},{"name":"Conference Paper","data":[0,0,1,4,0,0,0,0,0,0,0,0,0,6,4,1,0,0,0,0,0,3,1,1,1,1,0,1,0,5,0,0,2,1,1,0,9,0]},{"name":"Book Chapter","data":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,1,0,0,2,1,0,1,0,1,2,0,0,0,0,0,0,0]},{"name":"Book","data":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{"name":"Other","data":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0]}],"categories":[1977,1980,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017]},
+            publicationTypesCount: [["Journal Article",278],["Conference Paper",42],["Book Chapter",12],["Book",1],["Other",1]]
+        });
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
 });
