25d427f7eb2a83c0e555df40f17ae6d2fcbdc232
Ky Lane <k.lane@uq.edu.au>
2018-02-23 15:33:46 +1000

    [PT#153584449] Added verbose validation errors to Claim possibly pubs with tests

--- src/config/axios.js
+++ src/config/axios.js
@@ -35,7 +35,7 @@ export const generateCancelToken = () => {
     return CancelToken.source();
 };
 
-api.defaults.headers.common[TOKEN_NAME] = Cookies.get(SESSION_COOKIE_NAME) || 'XOIiWILhqRWyI6YM7yWLzP8TLB2dIdpj6Zhcqa6z';
+api.defaults.headers.common[TOKEN_NAME] = Cookies.get(SESSION_COOKIE_NAME);
 api.isCancel = axios.isCancel; // needed for cancelling requests and the instance created does not have this method
 
 let isGet = null;
