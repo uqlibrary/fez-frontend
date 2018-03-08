c0e03477a5d717fd944712d90f0f5d86b94a3411
Ky Lane <k.lane@uq.edu.au>
2018-03-08 16:28:48 +1000

    [PT#155729111] Readded submit on keyboard press enter for Googlle Scholar ID form

--- src/config/axios.js
+++ src/config/axios.js
@@ -35,7 +35,7 @@ export const generateCancelToken = () => {
     return CancelToken.source();
 };
 
-api.defaults.headers.common[TOKEN_NAME] = Cookies.get(SESSION_COOKIE_NAME) || 'Qs5r1CY4fXIm9kkZB0gsOFMmNSldSGi9tzeyg7Yt';
+api.defaults.headers.common[TOKEN_NAME] = Cookies.get(SESSION_COOKIE_NAME);
 api.isCancel = axios.isCancel; // needed for cancelling requests and the instance created does not have this method
 
 let isGet = null;
