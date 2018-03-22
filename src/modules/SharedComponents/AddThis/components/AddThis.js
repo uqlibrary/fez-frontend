f8c46522d4c7fc0cad2be122f2e22d486fc0e16a
Lea de Groot <l.degroot@library.uq.edu.au>
2018-03-22 14:57:04 +1000

    remove script call from body [PT #155115472]

--- src/modules/SharedComponents/AddThis/components/AddThis.js
+++ src/modules/SharedComponents/AddThis/components/AddThis.js
@@ -22,9 +22,7 @@ export default class AddThis extends React.Component {
                     <a className="addthis_button_email addthis_20x20_style" />
                     <a className="addthis_button_print addthis_20x20_style" />
                     <a className="addthis_counter addthis_pill_style" />
-                    <a className="orderACopy" href="https://web.library.uq.edu.au/library-services/other-libraries/ordering-copies-uq-theses" target="_blank">Order a copy</a>
                 </span>
-                <script src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-512eb98337c8a4a9" />
             </div>
         );
     }
