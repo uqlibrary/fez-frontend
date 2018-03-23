6ce63b823c70ff38748ddb51654216e956b78324
Lea de Groot <l.degroot@library.uq.edu.au>
2018-03-23 13:54:14 +1000

    finish styling [PT #155115472]

--- src/modules/SharedComponents/AddThis/components/AddThis.js
+++ src/modules/SharedComponents/AddThis/components/AddThis.js
@@ -7,7 +7,7 @@ export default class AddThis extends React.Component {
     };
 
     static defaultProps = {
-        show: false
+        show: true
     };
 
     componentDidMount() {
@@ -26,15 +26,16 @@ export default class AddThis extends React.Component {
     render() {
         if (!this.props.show) return <div className="addthis_empty" />;
         return (
-            <div className="addthis_toolbox addthis_default_style">
-                <div className="column is-narrow is-pulled-right">
+            <div className="addthis_toolbox addthis_default_style columns is-gapless is-clearfix is-marginless">
+                <div className="column" />
+                <div className="column is-narrow">
                     <a className="addthis_button_linkedin addthis_20x20_style" />
                     <a className="addthis_button_facebook_like" />
                     <a className="addthis_button_email addthis_20x20_style" />
                     <a className="addthis_button_print addthis_20x20_style" />
                     <a className="addthis_counter addthis_pill_style" />
                 </div>
-                <div className="column is-narrow is-pulled-right">
+                <div className="column is-narrow">
                     <a className="addthis_button_mendeley addthis_20x20_style" />
                     <a className="addthis_button_researchgate addthis_20x20_style" />
                     <a className="addthis_button_tweet" />
