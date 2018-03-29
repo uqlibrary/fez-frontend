223a38b38bebd1c5844788401d7947510a588024
Lea de Groot <l.degroot@library.uq.edu.au>
2018-03-29 10:10:12 +1000

    second pass of adding researchgate for addthis [PT #155115472]

--- src/modules/SharedComponents/ShareThis/components/ShareThis.js
+++ src/modules/SharedComponents/ShareThis/components/ShareThis.js
@@ -16,14 +16,12 @@ export default class ShareThis extends React.Component {
             const script = document.createElement('script');
             script.src = '//static.addtoany.com/menu/page.js';
             script.id = 'shareThisScript';
-            // script.async = true;
-
-            const code = this.addThisConfigInHead();
-            script.appendChild(document.createTextNode(code));
-
+            script.async = true;
             document.head.appendChild(script);
-            console.log('AddThis in did mount');
+            console.log('added in did mount');
         }
+
+        this.addThisConfigInHead();
     }
 
     componentWillUnmount() {
@@ -33,23 +31,55 @@ export default class ShareThis extends React.Component {
     }
 
     addThisConfigInHead() {
-        const code = 'function my_addtoany_onready() { \n' +
-            'console.log("in my_addtoany_onready"); \n' +
-            '    var a2a_config = a2a_config || {}; \n' +
-            '    a2a_config.custom_services = [ \n' +
-            '        [ \n' +
-            '            "www.researchgate.net", \n' +
-            '            "https://www.researchgate.net/go.Share.html?url=' + encodeURI(window.location.href) + '&title=' + encodeURIComponent(document.title) + '", \n' +
-            '            "https://www.example.com/images/icon_20x20.png" \n' +  // RG background: #ooccbb, white text TODO
-            '        ] \n' +
-            '    ]; \n' +
-            '    a2a.init("page");  \n' +
-            '}';
-        return code;
-        // const script = document.createElement('script');
-        // script.id = 'shareThisHeader';
-        // script.appendChild(document.createTextNode(code));
-        // document.head.appendChild(script);
+        const code = '(function() { \n' +
+            'console.log("in addThisConfigInHead"); \n' +
+
+            // function _waitforAddThis() {
+            //     var numMilliSecondsRecheck = 100;
+            //     this.async(function() {
+            //         if (window.)
+            //     }, numMilliSecondsRecheck);
+            // }
+
+            '  var waitforAddThis = timeoutms => new Promise((r, j)=>{\n' +
+            '    console.log("checking A")\n' +
+            '    var check = function() {\n' +
+            '      console.log("checking")\n' +
+            '      if(window.a2a_config !== undefined) {\n' +
+            '          console.log("found!"); \n' +
+            '          console.log(window.a2a); \n' +
+            '          var a2a_config = window.a2a_config || {}; \n' +
+            '          a2a_config.custom_services = [ \n' +
+            '              [ \n' +
+            '                  "www.researchgate.net", \n' +
+            '                  "https://www.researchgate.net/go.Share.html?url=' + encodeURI(window.location.href) + '&title=' + encodeURIComponent(document.title) + '", \n' +
+            '                  "https://www.example.com/images/icon_20x20.png" \n' +  // RG background: #ooccbb, white text TODO
+            '              ] \n' +
+            '          ]; \n' +
+            '          window.a2a_config = a2a_config; \n' +
+            '          window.a2a.init("page");  \n' +
+            '      } else if((timeoutms -= 100) < 0) {\n' +
+            '        console.log("AddThis timed out")\n' +
+            '      } else {\n' +
+            '        setTimeout(check, 100)\n' +
+            '      } \n' +
+            '    }\n' +
+            '    setTimeout(check, 100)\n' +
+            '  })' +
+            '  (async ()=>{\n' +
+            // '    a.innerHTML = \'waiting..\'\n' +
+            '    waitforAddThis(2000)\n' +
+            '  })()\n' +
+
+
+            '})(window);';
+
+        const script = document.createElement('script');
+        script.id = 'shareThisHeader';
+        script.appendChild(document.createTextNode(code));
+        console.log(script);
+        document.head.appendChild(script);
+        console.log('ran addThisConfigInHead');
     }
 
     render() {
@@ -58,9 +88,9 @@ export default class ShareThis extends React.Component {
         const blockStyle = { paddingBottom: 12 };
 
         return (
-            <div className="shareThis a2a_kit a2a_kit_size_20 a2a_default_style columns is-gapless is-clearfix is-marginless" style={blockStyle}>
+            <div className="shareThis columns is-gapless is-clearfix is-marginless" style={blockStyle}>
                 <div className="column" />
-                <div className="column is-narrow">
+                <div className="column is-narrow a2a_kit a2a_kit_size_20 a2a_default_style">
                     <a className="a2a_button_mendeley" />
 
                     <a className="a2a_button_twitter" />
