e0447c38707b782d55fc15a8d5810c64f7463ab5
Vishal Asai <asai_vishal@yahoo.com>
2018-03-29 13:02:32 +1000

    move test data to mock testing data [#156109371]

--- src/reducers/searchRecords.test.js
+++ src/reducers/searchRecords.test.js
@@ -1,5 +1,6 @@
 import * as actions from '../actions/actionTypes';
 import searchRecordsReducer, {deduplicateResults, getEspaceDuplicatePublicationsById} from './searchRecords';
+import * as records from '../mock/data/testing/searchRecords';
 
 const initialSearchSources = {
     loadingPublicationSources: {
@@ -21,102 +22,9 @@ describe('searchRecords reducer', () => {
     let espaceList, scopusList, wosList;
 
     beforeEach(() => {
-        espaceList = [
-            {
-                "currentSource": "espace",
-                "sources": [{source: "espace", id: "UQ:224457"}],
-                "rek_pid": "UQ:224457",
-                "fez_record_search_key_doi": null,
-                "fez_record_search_key_isi_loc": {
-                    "rek_isi_loc_id": 3901795,
-                    "rek_isi_loc_pid": "UQ:224457",
-                    "rek_isi_loc_xsdmf_id": 10821,
-                    "rek_isi_loc": "000283520500009"
-                },
-                "fez_record_search_key_scopus_id": null
-            },
-            {
-                "currentSource": "espace",
-                "sources": [{source: "espace", id: "UQ:683770"}],
-                "rek_pid": "UQ:683770",
-                "fez_record_search_key_doi": null,
-                "fez_record_search_key_isi_loc": {
-                    "rek_isi_loc_id": 4031803,
-                    "rek_isi_loc_pid": "UQ:683770",
-                    "rek_isi_loc_xsdmf_id": null,
-                    "rek_isi_loc": "000283520500009"
-                },
-                "fez_record_search_key_scopus_id": {
-                    "rek_scopus_id_id": 2552454,
-                    "rek_scopus_id_pid": "UQ:683770",
-                    "rek_scopus_id_xsdmf_id": null,
-                    "rek_scopus_id":"2-s2.0-85020491241"
-                }
-            }
-        ];
-        scopusList = [
-            {
-                "currentSource": "scopus",
-                "sources": [{source: "scopus", id: "2-s2.0-85030864188"}],
-                "fez_record_search_key_doi": {"rek_doi": "10.1186\/s12985-017-0854-x"},
-                "fez_record_search_key_scopus_id": {"rek_scopus_id": "2-s2.0-85030864188"}
-            },
-            {
-                "currentSource": "scopus",
-                "sources": [{source: "scopus", id: "2-s2.0-85020491241"}],
-                "fez_record_search_key_doi": {"rek_doi": "10.1016\/B978-0-12-801573-5.00033-4"},
-                "fez_record_search_key_scopus_id": {"rek_scopus_id": "2-s2.0-85020491241"}
-            },
-            {
-                "currentSource": "scopus",
-                "sources": [{source: "scopus", id: "2-s2.0-84991737284"}],
-                "fez_record_search_key_doi": {"rek_doi": "10.1099\/jgv.0.000580"},
-                "fez_record_search_key_scopus_id": {"rek_scopus_id": "2-s2.0-84991737284"}
-            },
-            {
-                "currentSource": "scopus",
-                "sources": [{source: "scopus", id: "2-s2.0-84971467438"}],
-                "fez_record_search_key_doi": {"rek_doi": "10.1128\/JVI.00737-15"},
-                "fez_record_search_key_scopus_id": {"rek_scopus_id": "2-s2.0-84971467438"}
-            },
-            {
-                "currentSource": "scopus",
-                "sources": [{source: "scopus", id: "2-s2.0-77449149944"}],
-                "fez_record_search_key_doi": {"rek_doi": "10.1146\/annurev-ento-112408-085457"},
-                "fez_record_search_key_scopus_id": {"rek_scopus_id": "2-s2.0-77449149944"}
-            }
-        ];
-        wosList = [
-            {
-                "currentSource": "wos",
-                "sources": [{source: "wos", id: "000412197500001"}],
-                "fez_record_search_key_isi_loc": {"rek_isi_loc": "000412197500001"},
-                "fez_record_search_key_doi": {"rek_doi": "10.1186\/s12985-017-0854-x"}
-            },
-            {
-                "currentSource": "wos",
-                "sources": [{source: "wos", id: "000386872100029"}],
-                "fez_record_search_key_isi_loc": {"rek_isi_loc": "000386872100029"},
-                "fez_record_search_key_doi": {"rek_doi": "10.1099\/jgv.0.000580"},
-            },
-            {
-                "currentSource": "wos",
-                "sources": [{source: "wos", id: "000377227600002"}],
-                "fez_record_search_key_isi_loc": {"rek_isi_loc": "000377227600002"},
-                "fez_record_search_key_doi": {"rek_doi": "10.1128\/JVI.00737-15"},
-            },
-            {
-                "currentSource": "wos",
-                "sources": [{source: "wos", id: "000283520500009"}],
-                "fez_record_search_key_isi_loc": {"rek_isi_loc": "000283520500009"},
-            },
-            {
-                "currentSource": "wos",
-                "sources": [{source: "wos", id: "000273712100008"}],
-                "fez_record_search_key_isi_loc": {"rek_isi_loc": "000273712100008"},
-                "fez_record_search_key_doi": {"rek_doi": "10.1146\/annurev-ento-112408-085457"},
-            }
-        ];
+        espaceList = records.espaceList;
+        scopusList = records.scopusList;
+        wosList = records.wosList;
     });
 
     it('should return deduplicated list of publications', () => {
