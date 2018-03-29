3162d2a6335ad9453c611016f16c887f70baca86
Vishal Asai <asai_vishal@yahoo.com>
2018-03-29 17:57:35 +1000

    construct publons id url using ORCID if publons id is 1 [#156332507]

--- src/modules/Dashboard/components/DashboardAuthorProfile.snapshot.test.js
+++ src/modules/Dashboard/components/DashboardAuthorProfile.snapshot.test.js
@@ -18,4 +18,24 @@ describe('Dashboard Author Profile test', () => {
         const wrapper = setup({}, false);
         expect(toJson(wrapper)).toMatchSnapshot();
     });
+
+    it('Render the authors profile with publon id url constructed using ORCID id if publons id is 1', () => {
+        const wrapper = setup({
+            author: {
+                ...mock.currentAuthor.uqresearcher.data,
+                aut_publons_id: 1
+            },
+        }, false);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('Render the authors profile with publon id url constructed using publons id if publons id is not 1', () => {
+        const wrapper = setup({
+            author: {
+                ...mock.currentAuthor.uqresearcher.data,
+                aut_publons_id: '1111-2222-3333-4444'
+            },
+        }, false);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
 });
