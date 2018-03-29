025983adaab0ad797562a2f711853fbdcc37822f
jannakha <j_malikova@yahoo.com>
2018-03-29 19:51:05 +1000

    browser navigation handling for back/forward

--- src/modules/PossiblyMyRecords/components/PossiblyMyRecords.test.js
+++ src/modules/PossiblyMyRecords/components/PossiblyMyRecords.test.js
@@ -1,17 +1,13 @@
 import PossiblyMyRecords from './PossiblyMyRecords';
 import {possibleUnclaimedList} from 'mock/data';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
-beforeAll(() => {
-    injectTapEventPlugin();
-});
+import {routes} from 'config';
 
 function setup(testProps, isShallow = true) {
     const props = {
-        ...testProps,
         possiblePublicationsList: testProps.possiblePublicationsList || [],
         possiblePublicationsFacets: testProps.possiblePublicationsFacets || {},
         possibleCounts: testProps.possibleCounts || 0,
+
         loadingPossiblePublicationsList: testProps.loadingPossiblePublicationsList || false,
         loadingPossibleCounts: testProps.loadingPossibleCounts || false,
 
@@ -19,27 +15,38 @@ function setup(testProps, isShallow = true) {
         hidePublicationFailed: testProps.hidePublicationFailed || false,
         hidePublicationFailedErrorMessage: testProps.hidePublicationFailedErrorMessage || null,
 
-        account: testProps.account || {id: 12345},
-        author: testProps.author || {aut_id: 12344},
         accountLoading: testProps.accountLoading || false,
 
         actions: {
             searchPossiblyYourPublications: jest.fn(),
-            ...testProps.actions
+            setClaimPublication: jest.fn(),
         },
-        history: testProps.history || { push : jest.fn()}
+        location: {
+            pathname: routes.pathConfig.records.possible,
+            state: null
+        },
+        history: {
+            push: jest.fn(),
+            go: jest.fn()
+        },
+        ...testProps
     };
     return getElement(PossiblyMyRecords, props, isShallow);
 }
 
 describe('Component PossiblyMyRecords', () => {
 
-    it('renders empty list', () => {
+    it('renders nothing while account is loading', () => {
+        const wrapper = setup({ accountLoading: true });
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('renders no results', () => {
         const wrapper = setup({});
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
-    it('renders loading screen while loading publication data', () => {
+    it('renders loading component while loading publication data', () => {
         const wrapper = setup({ loadingPossiblePublicationsList: true });
         expect(toJson(wrapper)).toMatchSnapshot();
     });
@@ -49,50 +56,66 @@ describe('Component PossiblyMyRecords', () => {
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
-    it('renders no results', () => {
-        const wrapper = setup({ author: {} });
-        expect(toJson(wrapper)).toMatchSnapshot();
-    });
+    it('renders list of publications and counts and facets', () => {
+        const wrapper = setup({
+            possibleCounts: 5,
+            possiblePublicationsList: [1, 2, 3],
+            possiblePublicationsFacets: {
+                "Display type": {
+                    "doc_count_error_upper_bound": 0,
+                    "sum_other_doc_count": 3,
+                    "buckets": [{"key": 179, "doc_count": 95}, {"key": 130, "doc_count": 34}, {
+                        "key": 177,
+                        "doc_count": 2
+                    }, {"key": 183, "doc_count": 2}, {"key": 174, "doc_count": 1}]
+                },
+                "Keywords": {
+                    "doc_count_error_upper_bound": 0,
+                    "sum_other_doc_count": 641,
+                    "buckets": [{"key": "Brca1", "doc_count": 15}, {
+                        "key": "Oncology",
+                        "doc_count": 15
+                    }, {"key": "Breast cancer", "doc_count": 13}, {
+                        "key": "Genetics & Heredity",
+                        "doc_count": 12
+                    }, {"key": "Biochemistry & Molecular Biology", "doc_count": 10}]
+                }
 
-    it('renders list of publications and counts', () => {
-        const wrapper = setup({ author: {}, possibleCounts: 5, possiblePublicationsList: possibleUnclaimedList.data });
+            }
+        });
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
     it('renders alert when the hide pub api fails', () => {
         const wrapper = setup({
-            author: {aut_org_username: 'xyz'},
-            possibleCounts: 5,
-            possiblePublicationsList: possibleUnclaimedList.data,
             hidePublicationFailed: true,
             hidePublicationFailedErrorMessage: 'Test error message'
         });
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
-    it('publication for claiming is selected', () => {
+    it('should select publication for claiming', () => {
         const actionFunction = jest.fn();
-        const wrapper = setup({ actions: { setClaimPublication: actionFunction}});
+        const wrapper = setup({ actions: { setClaimPublication: actionFunction, searchPossiblyYourPublications: jest.fn()}});
         wrapper.instance()._claimPublication({pid: 11111});
         expect(actionFunction).toHaveBeenCalled();
     });
 
-    it('loads possible pubs when username has been set', () => {
+    it('should load possibly your publications on load', () => {
         const actionFunction = jest.fn();
-        const wrapper = setup({actions: { searchPossiblyYourPublications: actionFunction}});
-        wrapper.setProps({author: {aut_org_username: 'xyz'}});
+        const wrapper = setup({ actions: { searchPossiblyYourPublications: actionFunction}});
         expect(actionFunction).toHaveBeenCalled();
     });
 
-    it('calls componentDidMount', () => {
-        const actionFunction = jest.fn();
-        const wrapper = setup({ isShallow: false, author: {aut_id: 1111}, actions: { searchPossiblyYourPublications: actionFunction} });
-        expect(actionFunction).toHaveBeenCalled();
+    it('should set ref for confirmation box', () => {
+        const wrapper = setup({});
+        wrapper.instance()._setHideConfirmationBox(1);
+        expect(wrapper.instance().hideConfirmationBox).toEqual(1);
     });
 
     it('calls hide publication', () => {
         const actionFunction = jest.fn();
-        const wrapper = setup({ author: {}, actions: { hideRecord: actionFunction}});
+        const wrapper = setup({ actions: { hideRecord: actionFunction, searchPossiblyYourPublications: jest.fn()}});
         wrapper.setState({ publicationToHide: {pid: 1111} });
         wrapper.instance()._hidePublication();
         expect(actionFunction).toHaveBeenCalled();
@@ -101,14 +124,14 @@ describe('Component PossiblyMyRecords', () => {
 
     it('calls the action to reset error message and status when leaving the page', () => {
         const resetFn = jest.fn();
-        const wrapper = setup({author: {aut_id: 1111}, actions: { hideRecordErrorReset: resetFn} });
+        const wrapper = setup({actions: { hideRecordErrorReset: resetFn, searchPossiblyYourPublications: jest.fn()} });
         wrapper.unmount();
         expect(resetFn).toHaveBeenCalled();
     });
 
     it('sets the state when confirming an item to be hidden', () => {
         const pubToHide = {test: 'This is a test'};
-        const wrapper = setup({author: {aut_id: 1111}});
+        const wrapper = setup({});
         wrapper.instance().hideConfirmationBox = {showConfirmation: jest.fn()};
         wrapper.instance()._confirmHidePublication(pubToHide);
         expect(wrapper.state().publicationToHide).toEqual(pubToHide);
@@ -116,11 +139,44 @@ describe('Component PossiblyMyRecords', () => {
 
     it('sets the state for activeFacets', () => {
         const facetActive = {test: 'This is a test'};
-        const wrapper = setup({author: {aut_id: 1111}});
+        const wrapper = setup({});
         wrapper.instance()._facetsChanged(facetActive);
         expect(wrapper.state().activeFacets).toEqual(facetActive);
     });
 
+    it('renders active filters', () => {
+        const wrapper = setup({location: {state: {hasPublications: true, activeFacets: {filters: {}, ranges: {Year: {from: 2000, to: 2010}}}}}});
+        expect(wrapper.state().hasPublications).toEqual(true);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
 
+    it('sets forever true has publications', () => {
+        const wrapper = setup({loadingPossiblePublicationsList: true});
+        expect(wrapper.state().hasPublications).toEqual(false);
+
+        wrapper.instance().componentWillReceiveProps({loadingPossiblePublicationsList: false, possiblePublicationsList: [1,2,3], history: {}, location: {}});
+        expect(wrapper.state().hasPublications).toEqual(true);
+    });
+
+    it('gets publications when user clicks back and state is set', () => {
+        const testAction = jest.fn();
+        const wrapper = setup({accountLoading: true, actions: {searchPossiblyYourPublications: testAction}});
+
+        wrapper.instance().componentWillReceiveProps({
+            history: {action: 'POP'},
+            location: {pathname: routes.pathConfig.records.possible, state: {hasPublications: true, activeFacets: {filters: {}, ranges: {Year: {from: 2000, to: 2010}}}}}
+        });
+        expect(testAction).toHaveBeenCalled();
+        expect(wrapper.state().hasPublications).toEqual(true);
+        expect(wrapper.state().activeFacets).toEqual({"filters": {}, "ranges": {"Year": {"from": 2000, "to": 2010}}});
+
+        wrapper.instance().componentWillReceiveProps({
+            history: {action: 'POP'},
+            location: {pathname: routes.pathConfig.records.possible, state: null}
+        });
+
+        expect(wrapper.state().activeFacets).toEqual({filters: {}, ranges: {}});
+
+    });
 
 });
