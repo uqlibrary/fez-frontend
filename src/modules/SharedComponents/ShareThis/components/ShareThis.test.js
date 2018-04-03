d52454ac335780a193e3df3ec2b413c855e218aa
Lea de Groot <l.degroot@library.uq.edu.au>
2018-04-03 14:16:28 +1000

    passing tests [PT #155115472]

--- src/modules/SharedComponents/ShareThis/components/ShareThis.test.js
+++ src/modules/SharedComponents/ShareThis/components/ShareThis.test.js
@@ -5,19 +5,32 @@ import ShareThis from './ShareThis';
 function setup(testProps, isShallow = true){
     const props = {
         ...testProps,
-        show: testProps.show || false // : PropTypes.bool,
+        hide: testProps.hide || false
     };
     return getElement(ShareThis, props, isShallow);
 }
 
-describe('Sharethis renders ', () => {
-    it('component with show true', () => {
+describe('Component Sharethis', () => {
+    it('should render 3rd party elements with default props', () => {
         const wrapper = setup({});
+
+        // create snapshot
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
-    it('component with show false', () => {
-        const wrapper = setup({ show: false });
+    it('should render 3rd party elements', () => {
+        // set initial props values
+        const wrapper = setup({ hide: false });
+
+        // create snapshot
+        expect(toJson(wrapper)).toMatchSnapshot();
+   });
+
+    it('should render empty component', () => {
+        // set initial props values
+        const wrapper = setup({ hide: true });
+
+        // create snapshot
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 });
