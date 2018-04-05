666222d5d90d3e0f914d96fa8608b8843fa9fcdf
Vishal Asai <asai_vishal@yahoo.com>
2018-04-05 10:09:18 +1000

    fixed typo [#156526560]

--- src/locale/viewRecord.js
+++ src/locale/viewRecord.js
@@ -102,7 +102,7 @@ export default {
                 fez_record_search_key_proceedings_title: 'Proceedings title',
                 fez_record_search_key_language_of_proceedings_title: 'Language of proceedings title',
                 fez_record_search_key_translated_proceedings_title: 'Translated proceedings title',
-                fez_record_search_key_org_unit_name: 'School, Department or Center',
+                fez_record_search_key_org_unit_name: 'School, Department or Centre',
                 fez_record_search_key_org_name: 'Institution',
                 fez_record_search_key_access_conditions: 'Access conditions',
                 fez_record_search_key_seo_code: 'ANZSRC Socio-Economic Objective (SEO) Code',
@@ -266,7 +266,7 @@ export default {
             'Thesis': {
                 rek_title: 'Thesis title',
                 rek_genre_type: 'Thesis type',
-                fez_record_search_key_org_unit_name: 'School, Center or Institute',
+                fez_record_search_key_org_unit_name: 'School, Centre or Institute',
                 fez_record_search_key_translated_title: 'Translated thesis title'
             },
             'Video Document': {
