1788676a8408113aedcd21aa243675e3548ca188
Ky Lane <k.lane@uq.edu.au>
2018-04-09 16:29:42 +1000

    [PT#155806314] Updated forms with ISBN/ISSN inputs to remind the user to add the value

--- src/locale/components.js
+++ src/locale/components.js
@@ -108,7 +108,8 @@ export default {
                     locale: {
                         inputFieldLabel: 'ISBN value',
                         inputFieldHint: 'Type ISBN value',
-                        addButtonLabel: 'Add ISBN'
+                        addButtonLabel: 'Add ISBN',
+                        remindToAdd: (<span><b>NOTE:</b> You have not yet added this value value to the list.</span>)
                     }
                 },
                 header: {
@@ -152,7 +153,8 @@ export default {
                     locale: {
                         inputFieldLabel: 'ISSN value',
                         inputFieldHint: 'Type ISSN value',
-                        addButtonLabel: 'Add ISSN'
+                        addButtonLabel: 'Add ISSN',
+                        remindToAdd: (<span><b>NOTE:</b> You have not yet added this value value to the list.</span>)
                     }
                 },
                 header: {
--- src/modules/SharedComponents/PublicationForm/components/Forms/ConferencePaperForm.js
+++ src/modules/SharedComponents/PublicationForm/components/Forms/ConferencePaperForm.js
@@ -164,6 +164,7 @@ export default class ConferencePaperForm extends Component {
                     <div>{locale.components.isbnForm.text}</div>
                     <Field
                         component={ListEditorField}
+                        remindToAdd
                         name="fez_record_search_key_isbn"
                         isValid={validation.isValidIsbn}
                         maxCount={5}
@@ -176,6 +177,7 @@ export default class ConferencePaperForm extends Component {
                     <div>{locale.components.issnForm.text}</div>
                     <Field
                         component={ListEditorField}
+                        remindToAdd
                         isValid={validation.isValidIssn}
                         name="fez_record_search_key_issn"
                         maxCount={5}
