47d0e1d9c530d5cbed5b4d040cb2b41b8c38c759
Vishal Asai <asai_vishal@yahoo.com>
2018-04-10 17:55:06 +1000

    refactoring a bit [#155689571]

--- src/modules/ViewRecord/components/Meta.js
+++ src/modules/ViewRecord/components/Meta.js
@@ -9,13 +9,6 @@ export default class Meta extends React.PureComponent {
         publication: PropTypes.object
     };
 
-    getMetaTag = (name, content) => {
-        return (
-            <meta name={name} content={content} {...(name === 'DC.Identifier' ? {scheme: 'URI'} : {})}/>
-        );
-    };
-
-
     getMetaTagContent = (object, key, url, dateFormat) => {
         const {publication} = this.props;
 
@@ -43,51 +36,46 @@ export default class Meta extends React.PureComponent {
         }
     };
 
-    getMultipleTagsForField = (tagName, field, subkey, url) => {
-        const {publication} = this.props;
-
-        return publication[field].map(fieldValue => {
-            const content = this.getMetaTagContent(fieldValue, subkey, url);
-            return content && this.getMetaTag(tagName, content);
-        });
-    };
-
     renderMetaTags = (publication) => {
         // Loop through each meta tag
         return viewRecordsConfig.metaTags.reduce((metaTags, metaTag) => {
             const {field, subkey, tags, url} = metaTag;
 
             // Push dublin core DC.* and/or citation_* meta tags for each field
-            metaTags.push(tags.map(tag => {
-                // Check field is one of the search keys of the publication and it exists (fez_record_search_key_*)
-                if (!!field && !!publication[field]) {
-                    // Check search key field has multiple values
-                    if (publication[field].length > 0) {
-                        // If multiple tags allowed then get meta tag for each value
-                        if (tag.isMultiple) {
-                            return this.getMultipleTagsForField(tag.name, field, subkey, url);
-                        } else {
-                            // Single meta tag for multiple values separated by semicolon
-                            return this.getMetaTag(
-                                tag.name,
-                                publication[field].reduce((metaTagContent, fieldValue) => {
-                                    metaTagContent.push(fieldValue[subkey]);
-                                    return metaTagContent;
-                                }, []).join('; ')
-                            );
+            metaTags.push(
+                ...(tags.reduce((tagsContent, tag) => {
+                    // Check field is one of the search keys of the publication and it exists (fez_record_search_key_*)
+                    if (!!field && !!publication[field]) {
+                        // Check search key field has multiple values
+                        if (publication[field].length > 0) {
+                            // If multiple tags allowed then get meta tag for each value
+                            if (tag.isMultiple) {
+                                publication[field].map(fieldValue => {
+                                    const content = this.getMetaTagContent(fieldValue, subkey, url);
+                                    content && tagsContent.push({name: tag.name, content});
+                                });
+                            } else {
+                                // Single meta tag for multiple values separated by semicolon
+                                tagsContent.push({
+                                    name: tag.name,
+                                    content: publication[field].reduce((metaTagContent, fieldValue) => {
+                                        metaTagContent.push(fieldValue[subkey]);
+                                        return metaTagContent;
+                                    }, []).join('; ')
+                                });
+                            }
+                        } else if (!!publication[field][subkey]) {
+                            // Return meta tag if single value exists in search key
+                            tagsContent.push({name: tag.name, content: publication[field][subkey]});
                         }
-                    } else if (!!publication[field][subkey]) {
-                        // Return meta tag if single value exists in search key
-                        return this.getMetaTag(tag.name, publication[field][subkey]);
                     } else {
-                        return [];
+                        // If field is null and subkey (rek_pid, rek_description, rek_date etc.) exists in publication
+                        const content = !!publication[subkey] && this.getMetaTagContent(publication, subkey, url, tag.format);
+                        content && tagsContent.push({name: tag.name, content});
                     }
-                } else {
-                    // If field is null and subkey (rek_pid, rek_description, rek_date etc.) exists in publication
-                    const content = !!publication[subkey] && this.getMetaTagContent(publication, subkey, url, tag.format);
-                    return content && this.getMetaTag(tag.name, content);
-                }
-            }));
+                    return [...tagsContent];
+                }, []))
+            );
             return metaTags;
         }, []);
     };
@@ -98,7 +86,11 @@ export default class Meta extends React.PureComponent {
             <Helmet>
                 <title>{`${this.props.publication.rek_title} - UQ eSpace ${process.env.TITLE_SUFFIX}`}</title>
                 <link rel="schema.DC" href="http://purl.org/DC/elements/1.0/" />
-                {metaTags}
+                {
+                    metaTags.map((metaTag, index) => {
+                        return metaTag ? (<meta key={`${metaTag.name}-${index}`} name={metaTag.name} content={metaTag.content} {...(metaTag.name === 'DC.Identifier' ? {scheme: 'URI'} : {})}/>) : null;
+                    })
+                }
             </Helmet>
         );
     }
