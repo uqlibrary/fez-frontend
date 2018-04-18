c5a160014e4e32239d66c7ed22c4a2454ce576de
Vishal Asai <asai_vishal@yahoo.com>
2018-04-18 10:48:56 +1000

    Edge is still working on play promise [#156793377]

--- src/modules/ViewRecord/components/partials/AudioPlayer.js
+++ src/modules/ViewRecord/components/partials/AudioPlayer.js
@@ -25,6 +25,8 @@ export default class AudioPlayer extends Component {
 
         if (!!playPromise) {
             playPromise.then(() => this.setState({isPlaying: true}));
+        } else {
+            this.setState({isPlaying: true});
         }
     };
 
