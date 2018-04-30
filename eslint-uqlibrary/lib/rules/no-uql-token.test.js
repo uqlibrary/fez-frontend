de19a0b388978186b7623f09fb9beb49b505e6f4
Vishal Asai <asai_vishal@yahoo.com>
2018-04-30 11:41:21 +1000

    added eslint rule [skip ci][#156301538]

---
+++ eslint-uqlibrary/lib/rules/no-uql-token.test.js
@@ -0,0 +1,47 @@
+"use strict";
+
+var rule = require("./no-uql-token"),
+    RuleTester = require("eslint").RuleTester;
+
+RuleTester.setDefaultConfig({
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module"
+    }
+});
+
+var ruleTester = new RuleTester();
+ruleTester.run("no-uql-token", rule, {
+    valid: [
+        {
+            code: "api.defaults.headers.common[TOKEN_NAME] = Cookies.get(SESSION_COOKIE_NAME);",
+        },
+        {
+            code: "api.defaults.headers.common[TOKEN_NAME] = 'abc123';"
+        }
+    ],
+
+    invalid: [
+        {
+            code: "api.defaults.headers.common[TOKEN_NAME] = Cookies.get(SESSION_COOKIE_NAME) || 'aN7tH1ngLIk3t0Ken';",
+            errors: [{
+                message: "Please remove UQL token from conditional statement",
+                type: "AssignmentExpression"
+            }]
+        },
+        {
+            code: "api.defaults.headers.common[TOKEN_NAME] = 'aN7tH1ngLIk3t0Ken';",
+            errors: [{
+                message: "Please get UQL token from Cookies",
+                type: "AssignmentExpression"
+            }]
+        },
+        {
+            code: "api.defaults.headers.common[TOKEN_NAME] = 'aN7tH1ngLIk3t0Ken' || Cookies.get(SESSION_COOKIE_NAME);",
+            errors: [{
+                message: "Please remove UQL token from conditional statement",
+                type: "AssignmentExpression"
+            }]
+        }
+    ]
+});
