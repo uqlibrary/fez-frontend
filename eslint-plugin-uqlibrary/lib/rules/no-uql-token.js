0f61ccdf318f6a78b6341eb4b89875f73c575fcd
Vishal Asai <asai_vishal@yahoo.com>
2018-05-01 12:54:48 +1000

    removed one liners and formatted with prettier [skip ci][#156301538]

--- eslint-plugin-uqlibrary/lib/rules/no-uql-token.js
+++ eslint-plugin-uqlibrary/lib/rules/no-uql-token.js
@@ -2,41 +2,38 @@ const MOCK_UQL_TOKEN = 'abc123';
 const ERROR_MESSAGE_CONDITIONAL = 'Please remove UQL token from conditional statement';
 const ERROR_MESSAGE_DIRECT_ASSIGNMENT = 'Please get UQL token from Cookies';
 
-const isLeftNodeApiToken = (node) => {
-    if (node.left.hasOwnProperty('object') &&
+const isLeftNodeApiToken = node => {
+    if (
+        node.left.hasOwnProperty('object') &&
         node.left.object.hasOwnProperty('object') &&
         node.left.object.object.hasOwnProperty('object')
     ) {
-        return node.left.object.object.object.property.name === 'defaults' &&
+        return (
+            node.left.object.object.object.property.name === 'defaults' &&
             node.left.object.object.property.name === 'headers' &&
             node.left.object.property.name === 'common' &&
-            node.left.property.name === 'TOKEN_NAME';
+            node.left.property.name === 'TOKEN_NAME'
+        );
     }
     return false;
 };
 
-const isRightNodeLogicalExpressionWithLiteral = (node) => {
-    return node.right.type === 'LogicalExpression' && (node.right.left.type === 'Literal' || node.right.right.type === 'Literal');
-};
-
-const isRightNodeDirectAssignment = (node) => {
-    return node.right.type === 'Literal';
-};
-
-const isMockToken = (node) => {
+const isMockToken = node => {
     return node.right.type === 'Literal' && node.right.value === MOCK_UQL_TOKEN;
 };
 
-module.exports = (context) => (
-    {
-        AssignmentExpression: (node) => {
-            if (isLeftNodeApiToken(node)) {
-                if (isRightNodeLogicalExpressionWithLiteral(node)) {
-                    context.report(node, ERROR_MESSAGE_CONDITIONAL);
-                } else if (isRightNodeDirectAssignment(node) && !isMockToken(node)) {
-                    context.report(node, ERROR_MESSAGE_DIRECT_ASSIGNMENT);
-                }
+module.exports = context => ({
+    AssignmentExpression: node => {
+        if (isLeftNodeApiToken(node)) {
+            if (
+                node.right.type === 'LogicalExpression' &&
+                (node.right.left.type === 'Literal' ||
+                    node.right.right.type === 'Literal')
+            ) {
+                context.report(node, ERROR_MESSAGE_CONDITIONAL);
+            } else if (node.right.type === 'Literal' && !isMockToken(node)) {
+                context.report(node, ERROR_MESSAGE_DIRECT_ASSIGNMENT);
             }
         }
     }
-);
+});
