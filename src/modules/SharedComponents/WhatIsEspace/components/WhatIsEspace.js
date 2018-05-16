03df2ea77867c5f1ff86639d16710a7e2fdfeeab
jannakha <j_malikova@yahoo.com>
2018-05-16 17:43:40 +1000

    internal links

--- src/modules/SharedComponents/WhatIsEspace/components/WhatIsEspace.js
+++ src/modules/SharedComponents/WhatIsEspace/components/WhatIsEspace.js
@@ -2,6 +2,8 @@ import React, {PureComponent} from 'react';
 
 import {StandardCard} from 'modules/SharedComponents/Toolbox/StandardCard';
 import {ExternalLink} from 'modules/SharedComponents/ExternalLink';
+import {Link} from 'react-router-dom';
+
 import {locale} from 'locale';
 
 export default class WhatIsEspace extends PureComponent {
@@ -10,10 +12,16 @@ export default class WhatIsEspace extends PureComponent {
         return (
             <StandardCard title={txt.title} className="whatIsEspace primaryHeader">
                 {txt.text}
-                <ExternalLink href={txt.readMoreLink} title={txt.readMoreTitle} aria-label={txt.readMoreTitle}
-                    target={'_self'} openInNewIcon={false}>
-                    {txt.readMoreLabel}
-                </ExternalLink>
+                {
+                    txt.readMoreLink.indexOf('http') === -1 &&
+                    <Link to={txt.readMoreLink}>{txt.readMoreLabel}</Link>
+                }
+                {
+                    txt.readMoreLink.indexOf('http') >= 0 &&
+                    <ExternalLink href={txt.readMoreLink} title={txt.readMoreTitle} aria-label={txt.readMoreTitle}>
+                        {txt.readMoreLabel}
+                    </ExternalLink>
+                }
             </StandardCard>
         );
     }
