44803175d32ad6f129f3286a0e6fa005ff4866da
Cliff Lien <uqclien@gmail.com>
2018-05-18 17:32:02 +1000

    update trending api route [#157684937]

--- src/repositories/routes.js
+++ src/repositories/routes.js
@@ -69,7 +69,7 @@ export const AUTHOR_ORCID_DETAILS_API = ({userId, params}) => ({apiUrl: `orcid/$
 
 // academic stats apis
 export const ACADEMIC_STATS_PUBLICATION_HINDEX_API = ({userId}) => ({apiUrl: `academic/${userId}/hindex`});
-export const ACADEMIC_STATS_PUBLICATIONS_TRENDING_API = () => ({apiUrl: 'records/search', options: {params: {rule: 'trending'}}});
+export const ACADEMIC_STATS_PUBLICATIONS_TRENDING_API = () => ({apiUrl: 'records/trending', options: {params: {filter: 'author'}}});
 
 // lookup apis
 export const GET_ACML_QUICK_TEMPLATES_API = () => ({apiUrl: 'acml/quick-templates'});
