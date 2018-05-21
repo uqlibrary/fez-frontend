aa904e66c01662e3fdac6d6d993d369ea8bc446c
Vishal Asai <asai_vishal@yahoo.com>
2018-05-21 16:17:11 +1000

    adding test [#157649279]

--- src/modules/SearchRecords/components/SearchRecords.test.js
+++ src/modules/SearchRecords/components/SearchRecords.test.js
@@ -289,4 +289,30 @@ describe('SearchRecords page', () => {
             }
         });
     });
+
+    it('should correctly parse search query string from location search (published year)', () => {
+        const wrapper = setup({});
+
+        const result = wrapper.instance().parseSearchQueryStringFromUrl('page=1&pageSize=20&sortBy=published_date&sortDirection=Desc&activeFacets%5Branges%5D%5BYear+published%5D%5Bfrom%5D=2008&activeFacets%5Branges%5D%5BYear+published%5D%5Bto%5D=2023&activeFacets%5BshowOpenAccessOnly%5D=false&searchQueryParams%5Btitle%5D=some+test+data');
+
+        expect(result).toEqual({
+            page: '1',
+            pageSize: 20,
+            sortBy: 'published_date',
+            sortDirection: 'Desc',
+            searchQueryParams: {
+                title: 'some test data'
+            },
+            activeFacets: {
+                filters: {},
+                ranges: {
+                    'Year published': {
+                        from: '2008',
+                        to: '2023'
+                    }
+                },
+                showOpenAccessOnly: false
+            }
+        });
+    });
 });
