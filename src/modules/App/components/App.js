865fedd7c3121c1c346ba6b4b00c1793efb409fe
Ky Lane <k.lane@uq.edu.au>
2018-05-22 11:41:34 +1000

    [PT#157759629] Cleaned up locale for logo and added link props

--- src/modules/App/components/App.js
+++ src/modules/App/components/App.js
@@ -212,8 +212,9 @@ export default class App extends PureComponent {
                         drawerOpen={this.state.docked || this.state.menuDrawerOpen}
                         docked={this.state.docked}
                         history={this.props.history}
-                        logoImage={locale.global.logo}
-                        logoText={locale.global.title}
+                        logoImage={locale.global.logo.image}
+                        logoText={locale.global.logo.label}
+                        logoLink={locale.global.logo.link}
                         onToggleDrawer={this.toggleDrawer}
                         isMobile={this.state.isMobile}
                         locale={{
