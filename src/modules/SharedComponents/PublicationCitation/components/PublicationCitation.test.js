403df6d9b5dea323fad999cf8c4fcf616ba8a115
Vishal Asai <asai_vishal@yahoo.com>
2018-06-01 14:28:26 +1000

    removed test [#157240965]

--- src/modules/SharedComponents/PublicationCitation/components/PublicationCitation.test.js
+++ src/modules/SharedComponents/PublicationCitation/components/PublicationCitation.test.js
@@ -34,11 +34,6 @@ describe('PublicationCitation ', () => {
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
-    it('should render component with default list hiding the citation content', () => {
-        const wrapper = setup({hideCitationContent: true});
-        expect(toJson(wrapper)).toMatchSnapshot();
-    });
-
     it('should render component with default actions', () => {
         const wrapper = setup({showDefaultActions: true});
         expect(toJson(wrapper)).toMatchSnapshot();
