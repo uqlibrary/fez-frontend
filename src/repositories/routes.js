f09563b1d73c353f17c6ef32b02af030c3e00d28
Ky Lane <k.lane@uq.edu.au>
2018-06-08 15:22:12 +1000

    Change to author trending API endpoint per CK

--- src/repositories/routes.js
+++ src/repositories/routes.js
@@ -70,7 +70,7 @@ export const AUTHOR_ORCID_DETAILS_API = ({userId, params}) => ({apiUrl: `orcid/$
 
 // academic stats apis
 export const ACADEMIC_STATS_PUBLICATION_HINDEX_API = ({userId}) => ({apiUrl: `academic/${userId}/hindex`});
-export const ACADEMIC_STATS_PUBLICATIONS_TRENDING_API = () => ({apiUrl: 'records/trending', options: {params: {filter: 'author'}}});
+export const ACADEMIC_STATS_PUBLICATIONS_TRENDING_API = () => ({apiUrl: 'records/my-trending'});
 
 // lookup apis
 export const GET_ACML_QUICK_TEMPLATES_API = () => ({apiUrl: 'acml/quick-templates'});
