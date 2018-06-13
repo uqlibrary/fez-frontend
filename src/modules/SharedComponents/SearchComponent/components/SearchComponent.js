6baf84ac9390dfb71822ec978dcdefefe55dbfb1
Vishal Asai <asai_vishal@yahoo.com>
2018-06-13 14:01:01 +1000

    fixed snackbar [#157241591]

--- src/modules/SharedComponents/SearchComponent/components/SearchComponent.js
+++ src/modules/SharedComponents/SearchComponent/components/SearchComponent.js
@@ -129,7 +129,8 @@ export default class SearchComponent extends PureComponent {
         this.setState({
             simpleSearch: {
                 searchText: value
-            }
+            },
+            snackbarOpen: false
         });
     };
 
