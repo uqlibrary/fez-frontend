1da5a97459e2cb374310f072fb0fd978ee5e8622
Lea de Groot <l.degroot@library.uq.edu.au>
2018-06-20 10:34:51 +1000

    correct token name [PT #155964092]

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -199,7 +199,7 @@ let webpackConfig = {
 // if you need to run this locally, create .sentryclirc and add the variables from the codeship env variables
 // per https://docs.sentry.io/learn/cli/configuration/#configuration-file
 // and comment out the wrapping if
-if (!!process.env.SENTRY_SOURCEMAP_AUTH_TOKEN) {
+if (!!process.env.SENTRY_AUTH_TOKEN) {
     const SentryCliPlugin = require('@sentry/webpack-plugin');
 
     webpackConfig.plugins.push(new SentryCliPlugin({
