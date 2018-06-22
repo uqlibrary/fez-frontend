dfffeff679bcd8904b2edba92b5f30316b9a8129
Vishal Asai <asai_vishal@yahoo.com>
2018-06-22 15:33:47 +1000

    added simple test for regex [#157419220]

--- src/modules/SharedComponents/LookupFields/containers/FieldOfResearchField.js
+++ src/modules/SharedComponents/LookupFields/containers/FieldOfResearchField.js
@@ -3,7 +3,7 @@ import {FieldOfResearchVocabId} from 'config/general';
 import {connect} from 'react-redux';
 import * as actions from 'actions';
 
-const escapeRegExp = (text) => {
+export const escapeRegExp = (text) => {
     return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 };
 
