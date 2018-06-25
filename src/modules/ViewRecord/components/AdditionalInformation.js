c1390bea841395e08c8af251fe24d975a243e733
jannakha <j_malikova@yahoo.com>
2018-06-25 17:57:20 +1000

    added google map api key [#158582687]

--- src/modules/ViewRecord/components/AdditionalInformation.js
+++ src/modules/ViewRecord/components/AdditionalInformation.js
@@ -162,7 +162,6 @@ export default class AdditionalInformation extends Component {
         }
         return (
             <PublicationMap
-                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
                 loadingElement={<div style={{height: '100%'}}/>}
                 containerElement={<div style={{height: '400px'}}/>}
                 mapElement={<div style={{height: '100%'}}/>}
