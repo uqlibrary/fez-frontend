1adfceb51e0e6b32bbb21b950ddd6ef56956306a
Ky Lane <k.lane@uq.edu.au>
2018-06-25 16:05:55 +1000

    [PT#158277543] Attempting to prevent pasting images at all into CKeditor via config

--- custom_modules/ckeditor/config.js
+++ custom_modules/ckeditor/config.js
@@ -1,8 +1,12 @@
 /* config.js */
 
 window.CKEDITOR.editorConfig = function() {
-    // console.log(config);
+    console.log(CKEDITOR.config);
     // Define changes to default configuration here.
     // For complete reference see:
     // http://docs.ckeditor.com/#!/api/CKEDITOR.config
+    CKEDITOR.config.pasteFromWord_inlineImages = false;
+    CKEDITOR.config.clipboard_defaultContentType = 'text';
+    CKEDITOR.config.pasteFromWordPromptCleanup = true;
+    CKEDITOR.config.removePlugins = 'image';
 };
