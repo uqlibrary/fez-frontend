bd6de9fd46bfdffc52e0cb9653d14546e294784a
Ky Lane <k.lane@uq.edu.au>
2018-06-26 09:49:15 +1000

    [PT#158277543] allowing content for ckeditor to paste and clean test in edge

--- custom_modules/ckeditor/config.js
+++ custom_modules/ckeditor/config.js
@@ -1,12 +1,11 @@
 /* config.js */
 
 window.CKEDITOR.editorConfig = function() {
-    console.log(CKEDITOR.config);
+    // console.log(CKEDITOR.config);
     // Define changes to default configuration here.
     // For complete reference see:
     // http://docs.ckeditor.com/#!/api/CKEDITOR.config
     CKEDITOR.config.pasteFromWord_inlineImages = false;
-    CKEDITOR.config.clipboard_defaultContentType = 'text';
-    CKEDITOR.config.pasteFromWordPromptCleanup = true;
     CKEDITOR.config.removePlugins = 'image';
+    config.allowedContent = true;
 };
