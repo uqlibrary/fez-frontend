cef99c7319f4c99517a6a003d9b7b792afa26c47
Ky Lane <k.lane@uq.edu.au>
2018-06-26 15:58:42 +1000

    Testing using prefilter to specific tags only

--- src/modules/SharedComponents/RichEditor/components/RichEditor.js
+++ src/modules/SharedComponents/RichEditor/components/RichEditor.js
@@ -26,7 +26,7 @@ export default class RichEditor extends PureComponent {
                 {
                     removeButtons: 'Cut,Copy,Paste,Undo,Redo,Anchor',
                     height: this.props.height,
-                    allowedContent: true
+                    pasteFilter: 'p, b, strong, i, em, u, sup, sub, ul, ol, li, s, br'
                 },
                 this.props.value
             );
