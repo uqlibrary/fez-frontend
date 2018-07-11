c08eb67896a17f72b0623060a1b5a0588dd5f0fd
Vishal Asai <asai_vishal@yahoo.com>
2018-07-11 14:04:40 +1000

    [#157270394] mock test data

--- src/middleware/saveReducerOnSessionExpired.test.js
+++ src/middleware/saveReducerOnSessionExpired.test.js
@@ -3,6 +3,7 @@ import {
     CURRENT_ACCOUNT_SESSION_EXPIRED
 } from 'actions/actionTypes';
 import Immutable from 'immutable';
+import {thesisFormData} from 'mock/data/testing/localStorageData';
 
 describe('middleware saveReducerOnSessionExpired', () => {
     it('should save given reducer (form) in action payload to a local storage', () => {
@@ -13,125 +14,7 @@ describe('middleware saveReducerOnSessionExpired', () => {
 
         const next = jest.fn();
 
-        const formData = Immutable.Map({
-            ThesisSubmission: {
-                values: {
-                    currentAuthor: [{
-                        nameAsPublished: 'HDR Student, N',
-                        authorId: 44444
-                    }],
-                    fez_record_search_key_ismemberof: [{
-                        rek_ismemberof: 'UQ:152694'
-                    }],
-                    supervisors: [{
-                        nameAsPublished: 'test',
-                        disabled: false
-                    }],
-                    fieldOfResearch: [{
-                        rek_value: {
-                            key: 451814,
-                            value: '0102 Applied Mathematics'
-                        },
-                        rek_order: 1
-                    }],
-                    thesisTitle: {
-                        htmlText: '<p>test</p>',
-                        plainText: 'test'
-                    },
-                    thesisAbstract: {
-                        htmlText: '<p>test</p>',
-                        plainText: 'test'
-                    },
-                    fez_record_search_key_org_name: {
-                        rek_org_name: 'The University of Queensland'
-                    },
-                    rek_object_type: 3,
-                    rek_date: '2018-7-11',
-                    fez_record_search_key_keywords: [{
-                        rek_keywords: 'test',
-                        rek_keywords_order: 1
-                    }],
-                    files: {
-                        queue: [],
-                        isValid: true
-                    },
-                    rek_status: 2,
-                    fileAccessId: 3,
-                    rek_genre_type: 'PhD Thesis',
-                    rek_display_type: 187,
-                    fez_record_search_key_org_unit_name: {
-                        rek_org_unit_name: 'School of Economics'
-                    }
-                },
-                initial: {
-                    rek_date: '2018-7-11',
-                    currentAuthor: [{
-                        nameAsPublished: 'HDR Student, N',
-                        authorId: 44444
-                    }],
-                    fez_record_search_key_org_name: {
-                        rek_org_name: 'The University of Queensland'
-                    },
-                    rek_object_type: 3,
-                    rek_status: 2,
-                    fez_record_search_key_ismemberof: [{
-                        rek_ismemberof: 'UQ:152694'
-                    }],
-                    rek_display_type: 187,
-                    fileAccessId: 3
-                },
-                registeredFields: {
-                    'currentAuthor.0.nameAsPublished': {
-                        name: 'currentAuthor.0.nameAsPublished',
-                        type: 'Field',
-                        count: 1
-                    },
-                    supervisors: {
-                        name: 'supervisors',
-                        type: 'Field',
-                        count: 1
-                    },
-                    fieldOfResearch: {
-                        name: 'fieldOfResearch',
-                        type: 'Field',
-                        count: 1
-                    },
-                    thesisTitle: {
-                        name: 'thesisTitle',
-                        type: 'Field',
-                        count: 1
-                    },
-                    thesisAbstract: {
-                        name: 'thesisAbstract',
-                        type: 'Field',
-                        count: 1
-                    },
-                    'fez_record_search_key_org_unit_name.rek_org_unit_name': {
-                        name: 'fez_record_search_key_org_unit_name.rek_org_unit_name',
-                        type: 'Field',
-                        count: 1
-                    },
-                    fez_record_search_key_keywords: {
-                        name: 'fez_record_search_key_keywords',
-                        type: 'Field',
-                        count: 1
-                    },
-                    files: {
-                        name: 'files',
-                        type: 'Field',
-                        count: 1
-                    },
-                    rek_genre_type: {
-                        name: 'rek_genre_type',
-                        type: 'Field',
-                        count: 1
-                    }
-                },
-                syncErrors: {
-                    files: 'Add at least one file to upload'
-                }
-            }
-        });
+        const formData = Immutable.Map(thesisFormData);
 
         const store = {
             getState: () => ({
