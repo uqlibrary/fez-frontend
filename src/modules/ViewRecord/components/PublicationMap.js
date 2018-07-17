da3094f8af0d0449e6697354c41346cfe7ba57aa
Ky Lane <k.lane@uq.edu.au>
2018-07-17 12:47:16 +1000

    [PT#157238975] Updated many tests. Much wow.

--- src/modules/ViewRecord/components/PublicationMap.js
+++ src/modules/ViewRecord/components/PublicationMap.js
@@ -24,7 +24,6 @@ const PublicationMap = withScriptjs(withGoogleMap((props) => {
     const pointZoom = 7;
     const polygonZoom = 13;
     const bounds = new window.google.maps.LatLngBounds();
-    console.log('bounds', bounds);
     geoCoords.map((coord) => {
         bounds.extend(new window.google.maps.LatLng(coord.lat, coord.lng));
     });
--- src/modules/ViewRecord/components/PublicationMap.test.js
+++ src/modules/ViewRecord/components/PublicationMap.test.js
@@ -10,32 +10,20 @@ function setup(testProps, isShallow = false){
     return getElement(PublicationMap, props, isShallow);
 }
 
-// describe('Publication\'s map coordinates', () => {
-//     it('should render component with a selected area', () => {
-//         const wrapper = setup({coordinates: '153.021781,-27.489337 152.988274,-27.489337 152.988274,-27.509529 153.021781,-27.509529 153.021781,-27.489337'});
-//         expect(toJson(wrapper)).toMatchSnapshot();
-//     });
-//
-//     it('should render component with empty coordinates', () => {
-//         const wrapper = setup({coordinates: ''});
-//         expect(toJson(wrapper)).toMatchSnapshot();
-//     });
-//
-//     it('should render component with a marker', () => {
-//         const wrapper = setup({coordinates: '153.021781,-27.489337' });
-//         expect(toJson(wrapper)).toMatchSnapshot();
-//     });
-// });
+describe('Publication\'s map coordinates', () => {
+    it('should render component with a selected area', () => {
+        const wrapper = setup({coordinates: '153.021781,-27.489337 152.988274,-27.489337 152.988274,-27.509529 153.021781,-27.509529 153.021781,-27.489337'});
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
 
-describe('Publication\'s google maps api', () => {
-    it('should call api', () => {
-        console.log(googleapi);
-        return googleapi.geocode({
-            address: 'Parc expo, 30000 Nimes, France'
-        })
-            .then((response) => {
-                expect(response.json.results[0].formatted_address)
-                    .toEqual('827 Chemin du Mas de Vignolles, 30900 Nîmes, France');
-            });
+    it('should render component with empty coordinates', () => {
+        const wrapper = setup({coordinates: ''});
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render component with a marker', () => {
+        const wrapper = setup({coordinates: '153.021781,-27.489337' });
+        expect(toJson(wrapper)).toMatchSnapshot();
     });
 });
+
