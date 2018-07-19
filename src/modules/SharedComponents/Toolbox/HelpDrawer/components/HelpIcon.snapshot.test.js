4012574c0df772f6603150a9196275afa8e5a615
Ky Lane <k.lane@uq.edu.au>
2018-07-19 13:20:55 +1000

    [PT#157238975] Cleanups

--- src/modules/SharedComponents/Toolbox/HelpDrawer/components/HelpIcon.snapshot.test.js
+++ src/modules/SharedComponents/Toolbox/HelpDrawer/components/HelpIcon.snapshot.test.js
@@ -3,17 +3,18 @@ import HelpIcon from './HelpIcon';
 function setup(testProps, isShallow = true) {
     const props = {
         ...testProps,
-        title: testProps.title,
-        text: testProps.text,
-        buttonLabel: testProps.buttonLabel
+        title: testProps.title || 'This is the title',
+        text: testProps.text || 'This is some text',
+        buttonLabel: testProps.buttonLabel || 'This is a button',
+        tooltip: testProps.tooltip || 'This is a tooltip',
+        onClick: testProps.onClick || jest.fn()
     };
     return getElement(HelpIcon, props, isShallow);
 }
 
 describe('HelpIcon snapshots tests', () => {
-    it('renders menu', () => {
-        const hdText = 'Integer mattis rutrum velit nec posuere. Quisque rhoncus quam elit, eu tincidunt diam feugiat eu. Quisque luctus luctus mauris faucibus ornare. Ut eu metus vitae est euismod gravida ac vel augue. Duis sapien massa, tempor id vulputate nec, auctor nec augue. Donec ullamcorper dignissim metus at vulputate. Mauris non magna enim. Quisque gravida libero augue, efficitur vestibulum leo porttitor id. Etiam quis nisi vehicula, eleifend turpis ut, accumsan nisi. Nam nec nibh auctor, placerat ex vel, rhoncus risus. In fermentum ex sit amet augue molestie sodales. Sed eleifend convallis dui id euismod. Nam eu turpis non mi facilisis euismod. Morbi congue et lectus sed tempus. Interdum et malesuada fames ac ante ipsum primis in faucibus.';
-        const wrapper = setup({title:'HelpDrawer Title', text: hdText, buttonLabel: 'Close'});
+    it('renders help icon', () => {
+        const wrapper = setup({});
         const tree = toJson(wrapper);
         expect(tree).toMatchSnapshot();
     });
--- src/modules/SharedComponents/Toolbox/NavigationPrompt/components/NavigationPrompt.test.js
+++ src/modules/SharedComponents/Toolbox/NavigationPrompt/components/NavigationPrompt.test.js
@@ -8,12 +8,7 @@ function setup(testProps, isShallow = true) {
 describe('NavigationPrompt component', () => {
     it('should render', () => {
         const testFunction = jest.fn();
-
-        const history = {
-            block: testFunction
-        };
-
-        const wrapper = setup({when: true, history: history, children: jest.fn()}, false);
+        const wrapper = setup({when: true, history: {block: testFunction}, children: jest.fn()}, false);
         const smallWrapper = wrapper.find('NavigationPrompt');
         expect(toJson(smallWrapper)).toMatchSnapshot();
         expect(testFunction).toBeCalled();
