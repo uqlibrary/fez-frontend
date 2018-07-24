1891444faade130a50d1a59e3b641f6663a4c6b7
Cliff Lien <uqclien@gmail.com>
2018-07-24 14:08:45 +1000

    remove citation quote [#159258961]

--- src/modules/SharedComponents/PublicationCitation/components/citations/BookCitation.js
+++ src/modules/SharedComponents/PublicationCitation/components/citations/BookCitation.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import FontIcon from 'material-ui/FontIcon';
 import * as Partials from './partials';
 
 export default class BookCitation extends Component {
@@ -37,10 +36,6 @@ export default class BookCitation extends Component {
 
         return (
             <div className="citationContent citationBook">
-                <FontIcon className="material-icons citationIcon" data-place="left">
-                    format_quote
-                </FontIcon>
-
                 {/* {Author} */}
                 {
                     !record.isEditedBook &&
