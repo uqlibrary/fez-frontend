2ad87f3eba163a8f4374f13c3fbc0bad059287bf
Vishal Asai <asai_vishal@yahoo.com>
2018-07-26 12:15:54 +1000

    [#158898491] added AuthorIdField for auto-complete

---
+++ src/modules/SharedComponents/SearchComponent/components/AutoCompleteFields/AuthorIdField.js
@@ -0,0 +1,29 @@
+import {AdvancedSearchAutoComplete} from '../AdvancedSearchAutoComplete';
+import {connect} from 'react-redux';
+import * as actions from 'actions';
+
+const mapStateToProps = (state, props) => {
+    const category = 'author';
+    console.log(props);
+    return {
+        category: category,
+        itemsList: state.get('searchKeysReducer') && state.get('searchKeysReducer')[category]
+            ? state.get('searchKeysReducer')[category].itemsList : [],
+        onChange: (value) => {
+            props.onChange({}, value.id, value.value);
+        },
+        async: true,
+        dataSourceConfig: {text: 'value', value: 'id'},
+        errorText: props.errorText,
+        selectedValue: !!props.label && props.label || !!props.value && props.value || ''
+    };
+};
+
+const mapDispatchToProps = (dispatch) => (
+    {
+        loadSuggestions: (searchKey, searchQuery = ' ') => dispatch(actions.loadSearchKeyList(searchKey, searchQuery))
+    }
+);
+
+export const AuthorIdField = connect(mapStateToProps, mapDispatchToProps)(AdvancedSearchAutoComplete);
+
