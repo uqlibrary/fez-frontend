8b6665ca9bc750478b4bdef903799986b8f08110
Vishal Asai <asai_vishal@yahoo.com>
2018-07-31 17:06:12 +1000

    re-add file

---
+++ src/modules/SharedComponents/SearchComponent/components/Fields/SubTypeField.js
@@ -0,0 +1,11 @@
+import React, {PureComponent} from 'react';
+import {publicationSubtypes} from 'config/general';
+import AdvancedSearchSelectInput from '../AdvancedSearchSelectInput';
+
+export default class SubTypeField extends PureComponent {
+    render() {
+        return (
+            <AdvancedSearchSelectInput {...this.props} options={publicationSubtypes} className="subtype menuitem" />
+        );
+    }
+}
