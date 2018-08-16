6260eaf48c639cdaeed0bd99cf6fce805eca3ba9
Vishal Asai <asai_vishal@yahoo.com>
2018-08-16 13:47:35 +1000

    [#159721618] added separate component for file upload status

---
+++ src/modules/SharedComponents/Toolbox/FileUploader/components/FileUploadRowStatus.test.js
@@ -0,0 +1,35 @@
+import {FileUploadRowStatus} from './FileUploadRowStatus';
+
+function setup(testProps, isShallow = true) {
+    const props = {
+        progress: 0,
+        isUploadInProgress: false,
+        onDelete: jest.fn(),
+        ...testProps
+    };
+
+    return getElement(FileUploadRowStatus, props, isShallow);
+}
+
+describe('Component FileUploadRowStatus', () => {
+    it('should render default view', () => {
+        const wrapper = setup({});
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render circular progress if upload is in progress', () => {
+        const wrapper = setup({
+            progress: 50,
+            isUploadInProgress: true
+        });
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render done icon if upload is finished', () => {
+        const wrapper = setup({
+            progress: 100,
+            isUploadInProgress: true
+        });
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
