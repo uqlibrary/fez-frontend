7eb3702eaf62fcba78c28b3587157d88e1c7de86
Vishal Asai <asai_vishal@yahoo.com>
2018-08-20 13:42:01 +1000

    [#159721618] JSS for dropzone

--- src/modules/SharedComponents/Toolbox/FileUploader/components/FileUploadDropzoneStaticContent.js
+++ src/modules/SharedComponents/Toolbox/FileUploader/components/FileUploadDropzoneStaticContent.js
@@ -2,17 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Grid} from '@material-ui/core';
 import {CloudUpload} from '@material-ui/icons';
+import {withStyles} from '@material-ui/core/styles';
 
-const FileUploadDropzoneStaticContent = ({locale}) => (
-    <Grid container className="file-instructions">
-        <Grid item xs={12} sm={6}>
+const FileUploadDropzoneStaticContent = ({locale, classes}) => (
+    <Grid container className={classes.dropzone}>
+        <Grid item xs sm={6}>
             {locale.fileUploadRestrictionHeading}
             {locale.fileUploadRestrictions}
         </Grid>
-        <Grid item xs={12} sm={6} className="upload-instructions">
-            <Grid container direction="column" alignItems="center">
+        <Grid item container xs sm={6}>
+            <Grid container direction="column" alignItems="center" justify="center" className={classes.instructions}>
                 <Grid item>
-                    <CloudUpload/>
+                    <CloudUpload className={classes.cloudIcon}/>
                 </Grid>
                 <Grid item>
                     {locale.fileUploadInstruction}
@@ -23,7 +24,25 @@ const FileUploadDropzoneStaticContent = ({locale}) => (
 );
 
 FileUploadDropzoneStaticContent.propTypes = {
-    locale: PropTypes.object
+    locale: PropTypes.object,
+    classes: PropTypes.object
 };
 
-export default FileUploadDropzoneStaticContent;
+const styles = () => ({
+    dropzone: {
+        marginTop: 16,
+        padding: '16px 32px 16px 32px',
+        backgroundColor: '#f2f2f2',
+        border: '2px dashed #8c8c8c',
+        fontSize: '12px',
+        lineHeight: '16px'
+    },
+    instructions: {
+        height: '100%'
+    },
+    cloudIcon: {
+        fontSize: 36
+    }
+});
+
+export default withStyles(styles)(FileUploadDropzoneStaticContent);
