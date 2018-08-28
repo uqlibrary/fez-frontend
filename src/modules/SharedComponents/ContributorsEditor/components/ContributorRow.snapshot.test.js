08b74e30bdabec06eaf260de4137f5979c6bd9fd
Vishal Asai <asai_vishal@yahoo.com>
2018-08-28 14:47:12 +1000

    [#159721785] updated contributor row component

--- src/modules/SharedComponents/ContributorsEditor/components/ContributorRow.snapshot.test.js
+++ src/modules/SharedComponents/ContributorsEditor/components/ContributorRow.snapshot.test.js
@@ -1,26 +1,31 @@
-import ContributorRow from './ContributorRow';
+import {ContributorRow} from './ContributorRow';
 
 import {authorsSearch} from 'mock/data';
 
 function setup(testProps, isShallow = true) {
     // build full props list requied by the component
     const props = {
-        index: testProps.index || 0,
-        contributor: testProps.contributor || { nameAsPublished: 'A. Smith'},
-        canMoveUp: testProps.canMoveUp || false,
-        canMoveDown: testProps.canMoveDown || false,
-        showIdentifierLookup: testProps.showIdentifierLookup || false,
-        showContributorAssignment: testProps.showContributorAssignment || false,
-        disabledContributorAssignment: testProps.disabledContributorAssignment || false,
-        disabled: testProps.disabled || false,
+        index: 0,
+        contributor: { nameAsPublished: 'A. Smith'},
+        canMoveUp: false,
+        canMoveDown: false,
+        showIdentifierLookup: false,
+        showContributorAssignment: false,
+        disabledContributorAssignment: false,
+        disabled: false,
+        classes: {
+            listItem: 'listItem',
+            selected: 'selected',
+            hideIcon: 'hideIcon',
+            primary: 'primary',
+            identifierName: 'identifierName',
+            identifierSubtitle: 'identifierSubtitle'
+        },
+        width: 'md',
         ...testProps,
     };
     return getElement(ContributorRow, props, isShallow);
 }
-
-beforeAll(() => {
-    
-});
 
 describe('Component ContributorRow ', () => {
 
@@ -29,8 +34,18 @@ describe('Component ContributorRow ', () => {
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
+    it('a row with index and contributor set, renders only name and delete button for mobile view', () => {
+        const wrapper = setup({index: 0, width: 'xs'});
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
     it('a row with index and contributor with author details set and set as selected', () => {
-        const wrapper = setup({nameAsPublished: "J. Smith", ...authorsSearch.data[0], index: 0, showIdentifierLookup: true, contributor: {selected: true}});
+        const wrapper = setup({...authorsSearch.data[0], index: 0, showIdentifierLookup: true, contributor: {nameAsPublished: "J. Smith", selected: true}});
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('a row with index and contributor with author details set and set as selected for mobile view', () => {
+        const wrapper = setup({...authorsSearch.data[0], index: 0, showIdentifierLookup: true, contributor: {nameAsPublished: "J. Smith", selected: true}});
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
@@ -55,63 +70,64 @@ describe('Component ContributorRow ', () => {
     it('a row with index and contributor set calls move up function', () => {
         const testFunction = jest.fn();
         const contributor = {
-                    nameAsPublished: "J. Smith",
-                    ...authorsSearch.data[0]
-                };
-        const wrapper = setup({contributor, index: 0, canMoveUp: true, onMoveUp: testFunction, isShallow: false});
+            nameAsPublished: "J. Smith",
+            ...authorsSearch.data[0]
+        };
+        const wrapper = setup({contributor, index: 0, canMoveUp: true, onMoveUp: testFunction}, false);
 
-        const button = wrapper.find('IconButton .reorderUp');
+        const button = wrapper.find('KeyboardArrowUp');
         expect(button.length).toBe(1);
 
-        const buttonDown = wrapper.find('IconButton .reorderDown');
+        const buttonDown = wrapper.find('KeyboardArrowDown');
         expect(buttonDown.length).toBe(0);
 
-        wrapper.instance()._onMoveUp();
+        wrapper.find('KeyboardArrowUp').simulate('click');
         expect(testFunction).toBeCalled();
     });
 
     it('a row with index and contributor set calls move down function', () => {
         const testFunction = jest.fn();
-        const wrapper = setup({index: 0, canMoveDown: true, onMoveDown: testFunction, isShallow: false});
+        const wrapper = setup({index: 0, canMoveDown: true, onMoveDown: testFunction}, false);
 
-        const button = wrapper.find('IconButton .reorderDown');
+        const button = wrapper.find('KeyboardArrowDown');
         expect(button.length).toBe(1);
 
-        wrapper.instance()._onMoveDown();
+        wrapper.find('KeyboardArrowDown').simulate('click');
         expect(testFunction).toBeCalled;
 
-        const buttonUp = wrapper.find('IconButton .reorderUp');
+        const buttonUp = wrapper.find('KeyboardArrowUp');
         expect(buttonUp.length).toBe(0);
         testFunction.mockReset();
     });
 
     it('a row with index and contributor set calls assignment function', () => {
         const testFunction = jest.fn();
-        const wrapper = setup({index: 0, showContributorAssignment: true, onContributorAssigned: testFunction, isShallow: false});
-        wrapper.instance()._onContributorAssigned();
+        const wrapper = setup({index: 0, showContributorAssignment: true, onContributorAssigned: testFunction}, false);
+        wrapper.find('ListItem').simulate('click');
         expect(testFunction).toBeCalled;
     });
 
     it('a row with index and contributor set calls delete function', () => {
         const testFunction = jest.fn();
-        const wrapper = setup({index: 0, onDelete: testFunction, isShallow: false});
-        wrapper.instance()._deleteRecord();
-        const button = wrapper.find('IconButton .contributorDelete');
+        const wrapper = setup({index: 0, onDelete: testFunction}, false);
+        const button = wrapper.find('Delete');
         expect(button.length).toBe(1);
+        wrapper.find('Delete').simulate('click');
         expect(testFunction).toBeCalled;
     });
+
     it('should add the contributor when it is not yet selected', () => {
         const testFunction = jest.fn();
 
-        const wrapper = setup({nameAsPublished: "J. Smith", index: 0, disabled: false, contributor: {selected: false}, onContributorAssigned: testFunction});
+        const wrapper = setup({index: 0, disabled: false, contributor: {selected: false, nameAsPublished: "J. Smith"}, onContributorAssigned: testFunction});
         wrapper.instance()._assignContributor();
-        expect(testFunction).toBeCalledWith({selected: false}, 0);
+        expect(testFunction).toBeCalledWith({selected: false, nameAsPublished: "J. Smith"}, 0);
     });
 
     it('should remove the contributor assigned when it is already selected', () => {
         const testFunction = jest.fn();
 
-        const wrapper = setup({nameAsPublished: "J. Smith", index: 0, disabled: false, contributor: {selected: true}, onContributorAssigned: testFunction});
+        const wrapper = setup({index: 0, disabled: false, contributor: {selected: true, nameAsPublished: "J. Smith"}, onContributorAssigned: testFunction});
         wrapper.instance()._assignContributor();
         expect(testFunction).toBeCalledWith(null, null);
     });
@@ -152,7 +168,7 @@ describe('Component ContributorRow ', () => {
         const wrapper = setup({contributor: {nameAsPublished: "J. Smith"}});
         wrapper.instance().shouldComponentUpdate = testFunction;
         wrapper.setProps({contributor: {nameAsPublished: "K. Lane"}});
-        expect(testFunction).toBeCalledWith({"canMoveDown": false, "canMoveUp": false, "contributor": {"nameAsPublished": "K. Lane"}, "disabled": false, "disabledContributorAssignment": false, "index": 0, "locale": {"deleteHint": "Remove this record", "deleteRecordConfirmation": {"cancelButtonLabel": "No", "confirmButtonLabel": "Yes", "confirmationMessage": "Are you sure you want to delete this record?", "confirmationTitle": "Delete record"}, "moveDownHint": "Move record down the order", "moveUpHint": "Move record up the order", "ordinalData": ["First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth"], "selectHint": "Select this record ([name]) to assign it to you", "suffix": " listed contributor"}, "showContributorAssignment": false, "showIdentifierLookup": false}, null, {});
+        expect(testFunction).toBeCalledWith({"canMoveDown": false, "canMoveUp": false, "classes": {"hideIcon": "hideIcon", "identifierName": "identifierName", "identifierSubtitle": "identifierSubtitle", "listItem": "listItem", "primary": "primary", "selected": "selected"}, "contributor": {"nameAsPublished": "K. Lane"}, "disabled": false, "disabledContributorAssignment": false, "index": 0, "locale": {"deleteHint": "Remove this record", "deleteRecordConfirmation": {"cancelButtonLabel": "No", "confirmButtonLabel": "Yes", "confirmationMessage": "Are you sure you want to delete this record?", "confirmationTitle": "Delete record"}, "moveDownHint": "Move record down the order", "moveUpHint": "Move record up the order", "ordinalData": ["First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth"], "selectHint": "Select this record ([name]) to assign it to you", "suffix": " listed contributor"}, "showContributorAssignment": false, "showIdentifierLookup": false, "width": "md"}, null, {});
     });
 
     it('triggers the confirmation box', () => {
@@ -162,6 +178,4 @@ describe('Component ContributorRow ', () => {
         wrapper.instance()._showConfirmation();
         expect(testFunction).toBeCalled();
     });
-
-
 });
