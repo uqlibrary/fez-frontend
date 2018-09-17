6594644a34acb37ddeddaec5f10604246234cb7c
Vishal Asai <asai_vishal@yahoo.com>
2018-09-17 14:34:25 +1000

    [#159721899] added tests for facet filter nested items

---
+++ src/modules/SharedComponents/PublicationsList/components/FacetsFilter/FacetFilterNestedListItem.test.js
@@ -0,0 +1,35 @@
+import FacetFilterNestedListItem from './FacetFilterNestedListItem';
+
+function setup(testProps, isShallow = true) {
+    const props = {
+        index: 0,
+        primaryText: 'Test facet filter',
+        disabled: false,
+        isActive: false,
+        classes: {
+            listItemGutters: 'listItemGutters',
+            inset: 'inset',
+            selectedFacet: 'selectedFacet'
+        },
+        onFacetClick: jest.fn(),
+        ...testProps
+    };
+    return getElement(FacetFilterNestedListItem, props, isShallow);
+}
+
+describe('Facet filter nested list item ', () => {
+    it('should render default filter view', () => {
+        const wrapper = setup({primaryText: 'Test filter'});
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render active filter view', () => {
+        const wrapper = setup({primaryText: 'Test filter', isActive: true});
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render disabled filter view', () => {
+        const wrapper = setup({primaryText: 'Test filter', isActive: true, disabled: true});
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
