a157bad162b9760d4f1e54de2d0682019140166d
Ky Lane <k.lane@uq.edu.au>
2018-09-24 11:06:28 +1000

    [PT#160678151] Fix icon in Alert to MUI1

--- src/modules/SharedComponents/Toolbox/Alert/components/Alert.test.js
+++ src/modules/SharedComponents/Toolbox/Alert/components/Alert.test.js
@@ -34,39 +34,39 @@ describe('Alert component functionality test ', () => {
     it('does not fire any action when clicking on the message text with no action assigned', () => {
         const alertFunc = jest.fn();
         const wrapper = setup({action: null, actionButtonLabel: null});
-        wrapper.find('WithStyles(Grid)#text').simulate('click');
+        wrapper.find('#text').simulate('click');
         expect(alertFunc).not.toHaveBeenCalled();
     });
 
     it('fires the action when clicking on the iconButton', () => {
         const alertFunc = jest.fn();
         const wrapper = setup({action: alertFunc, actionButtonLabel: 'button'});
-        wrapper.find('WithStyles(Grid)#icon').simulate('click');
+        wrapper.find('#icon').simulate('click');
         expect(alertFunc).toHaveBeenCalled();
     });
 
     it('does not fire the action when clicking on the iconButton with no action assigned', () => {
         const alertFunc = jest.fn();
         const wrapper = setup({action: null, actionButtonLabel: null});
-        wrapper.find('WithStyles(Grid)#icon').simulate('click');
+        wrapper.find('#icon').simulate('click');
         expect(alertFunc).not.toHaveBeenCalled();
     });
 
     it('fires the action when clicking on the action button', () => {
         const alertFunc = jest.fn();
         const wrapper = setup({action: alertFunc, actionButtonLabel: 'button'});
-        expect(wrapper.find('WithStyles(Button)#alertButton').exists()).toBeTruthy();
-        expect(wrapper.find('WithStyles(Button)#alertButton')).toHaveLength(1);
-        wrapper.find('WithStyles(Button)#alertButton').simulate('click');
+        expect(wrapper.find('#alertButton').exists()).toBeTruthy();
+        expect(wrapper.find('#alertButton')).toHaveLength(1);
+        wrapper.find('#alertButton').simulate('click');
         expect(alertFunc).toHaveBeenCalled();
     });
 
     it('fires the dismissAction when clicking on the dismiss button', () => {
         const dismissFunc = jest.fn();
         const wrapper = setup({dismissAction: dismissFunc, allowDismiss: true});
-        expect(wrapper.find('IconButton#dismiss').exists()).toBeTruthy();
-        expect(wrapper.find('IconButton#dismiss')).toHaveLength(2);
-        wrapper.find('IconButton#dismiss').at(0).simulate('click');
+        expect(wrapper.find('#dismiss').exists()).toBeTruthy();
+        expect(wrapper.find('#dismiss')).toHaveLength(2);
+        wrapper.find('#dismiss').at(0).simulate('click');
         expect(dismissFunc).toHaveBeenCalled();
     });
 
