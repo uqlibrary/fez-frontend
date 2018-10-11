8c81cf4cac7fa4ca67f3deefd3003e3af53a727e
Vishal Asai <asai_vishal@yahoo.com>
2018-10-11 14:57:49 +1000

    [#161106732] for debugging

--- src/config/axios.js
+++ src/config/axios.js
@@ -25,6 +25,8 @@ export const cache = setupCache({
     },
 });
 
+window.Cookies = Cookies;
+
 export const api = axios.create({
     baseURL: API_URL,
     adapter: process.env.NODE_ENV === 'test' ? undefined : cache.adapter
