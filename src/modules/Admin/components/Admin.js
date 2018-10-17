ed884d5c73335cead928a745a19447763ff6f246
Ky Lane <k.lane@uq.edu.au>
2018-10-17 15:11:08 +1000

    [PT#161108960] Setting and reading from cookie when a form style is chosen

--- src/modules/Admin/components/Admin.js
+++ src/modules/Admin/components/Admin.js
@@ -11,8 +11,11 @@ import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 import KeyboardEventHandler from 'react-keyboard-event-handler';
 import TextField from '@material-ui/core/TextField';
+import Hidden from '@material-ui/core/Hidden';
 import Keyboard from '@material-ui/icons/Keyboard';
 import {HelpIcon} from 'modules/SharedComponents/Toolbox/HelpDrawer';
+import withWidth from '@material-ui/core/withWidth';
+import Cookies from 'js-cookie';
 
 const styles = theme => ({
     helpIcon: {
@@ -31,15 +34,26 @@ class Admin extends PureComponent {
         actions: PropTypes.object,
         location: PropTypes.object,
         history: PropTypes.object.isRequired,
-        classes: PropTypes.object
+        classes: PropTypes.object,
+        width: PropTypes.any
     };
 
     constructor(props) {
         super(props);
         this.state = {
-            tabbed: true,
+            tabbed: Cookies.get('adminFormTabbed') ? !!(Cookies.get('adminFormTabbed') === 'tabbed') : true,
             tabValue: 0
         };
+    }
+
+    componentWillMount() {
+        if (this.props.width === 'xs') {
+            this.setState({
+                ...this.state,
+                tabbed: false
+            });
+            Cookies.set('adminFormTabbed', 'fullform');
+        }
     }
 
     handleTabChange = (event, value) => {
@@ -47,7 +61,9 @@ class Admin extends PureComponent {
     };
 
     handleFormatChange = () => {
-        this.setState({ tabbed: !this.state.tabbed });
+        this.setState({tabbed: !this.state.tabbed }, () => {
+            Cookies.set('adminFormTabbed', this.state.tabbed ? 'tabbed' : 'fullform');
+        });
     };
 
     handleKeyEvent = (key) => {
@@ -82,47 +98,52 @@ class Admin extends PureComponent {
     };
 
     render() {
+        const {classes} = this.props;
         return (
             <StandardPage title={'Admin form prototype'}>
-                <KeyboardEventHandler
-                    handleKeys={['ctrl+shift+left', 'ctrl+shift+right', 'ctrl+shift+up', 'ctrl+shift+down']}
-                    onKeyEvent={this.handleKeyEvent}
-                    handleFocusableElements
-                />
-                <Grid container direction={'row'} style={{marginTop: -70}}>
-                    <Grid item xs />
-                    <Grid item xs={'auto'}>
-                        <Grid container direction={'row'} spacing={0} alignItems={'center'}>
-                            <Grid item>
-                                <Tooltip title={`Switch to ${this.state.tabbed ? 'full form' : 'tabbed'} mode`} placement="left">
-                                    <Switch
-                                        color={'primary'}
-                                        checked={this.state.tabbed}
-                                        onChange={this.handleFormatChange}
-                                        value="tabbed"
+                <Hidden xsDown>
+                    <KeyboardEventHandler
+                        handleKeys={['ctrl+shift+left', 'ctrl+shift+right', 'ctrl+shift+up', 'ctrl+shift+down']}
+                        onKeyEvent={this.handleKeyEvent}
+                        handleFocusableElements
+                    />
+                </Hidden>
+                <Hidden xsDown>
+                    <Grid container direction={'row'} style={{marginTop: -70}}>
+                        <Grid item xs />
+                        <Grid item xs={'auto'}>
+                            <Grid container direction={'row'} spacing={0} alignItems={'center'}>
+                                <Grid item>
+                                    <Tooltip title={`Switch to ${this.state.tabbed ? 'full form' : 'tabbed'} mode`} placement="left">
+                                        <Switch
+                                            color={'primary'}
+                                            checked={this.state.tabbed}
+                                            onChange={this.handleFormatChange}
+                                            value="tabbed"
+                                        />
+                                    </Tooltip>
+                                </Grid>
+                                <Grid item>
+                                    <HelpIcon
+                                        icon={<Keyboard className={classes.helpIcon} />}
+                                        tooltip={'Learn about keyboard shortcuts'}
+                                        title={'Keyboard shortcuts'}
+                                        text={(
+                                            <React.Fragment>
+                                                <br/>
+                                                <Typography variant={'h6'} component={'p'}>Tab navigation</Typography>
+                                                <p>To navigate tabs while in tabbed mode, hold CTRL and SHIFT and use the LEFT and RIGHT arrow keys.</p>
+                                                <Typography variant={'h6'} component={'p'}>Form style</Typography>
+                                                <p>To switch between tabbed or full form mode, hold CTRL and SHIFT and use the UP and DOWN arrow keys.</p>
+                                            </React.Fragment>
+                                        )}
+                                        buttonLabel={'GOT IT'}
                                     />
-                                </Tooltip>
-                            </Grid>
-                            <Grid item>
-                                <HelpIcon
-                                    icon={<Keyboard className={this.props.classes.helpIcon} />}
-                                    tooltip={'Learn about keyboard shortcuts'}
-                                    title={'Keyboard shortcuts'}
-                                    text={(
-                                        <React.Fragment>
-                                            <br/>
-                                            <Typography variant={'h6'} component={'p'}>Tab navigation</Typography>
-                                            <p>To navigate tabs while in tabbed mode, hold CTRL and SHIFT and use the LEFT and RIGHT arrow keys.</p>
-                                            <Typography variant={'h6'} component={'p'}>Form style</Typography>
-                                            <p>To switch between tabbed or full form mode, hold CTRL and SHIFT and use the UP and DOWN arrow keys.</p>
-                                        </React.Fragment>
-                                    )}
-                                    buttonLabel={'GOT IT'}
-                                />
+                                </Grid>
                             </Grid>
                         </Grid>
                     </Grid>
-                </Grid>
+                </Hidden>
                 <Grid container spacing={0} direction={'row'}>
                     {
                         this.state.tabbed ?
@@ -226,4 +247,6 @@ class Admin extends PureComponent {
         );
     }
 }
-export default withStyles(styles, {withTheme: true})(Admin);
+
+const AdminWithWidth = withWidth()(Admin);
+export default withStyles(styles, {withTheme: true})(AdminWithWidth);
