a8ff93d26ce834e5f6d2a82772e3df10631308f9
Ky Lane <k.lane@uq.edu.au>
2019-01-25 10:13:13 +1000

    Small fix for ntro details in view page

--- src/modules/ViewRecord/components/NtroDetails.js
+++ src/modules/ViewRecord/components/NtroDetails.js
@@ -76,7 +76,7 @@ export class NtroDetails extends PureComponent {
                                         key={index}
                                         heading={locale.viewRecord.headings.NTRO.impactStatement}
                                         subheading={`(${publication.fez_record_search_key_author[index].rek_author})`}
-                                        data={item.rek_creator_contribution_statement.trim().length !== 0 && ReactHtmlParser(item.rek_creator_contribution_statement) || null}
+                                        data={item.rek_creator_contribution_statement && item.rek_creator_contribution_statement.trim().length !== 0 && ReactHtmlParser(item.rek_creator_contribution_statement) || null}
                                     />
                                 );
                             } else {
