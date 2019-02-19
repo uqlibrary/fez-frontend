cd4d31efd7f5d4183eacd07b57b97e9987343688
Vishal Asai <asai_vishal@yahoo.com>
2019-02-19 11:16:13 +1000

    [#163354589] Test coverage for Masquerade component

---
+++ src/modules/Masquerade/components/Masquerade.test.js
@@ -0,0 +1,68 @@
+import Masquerade from './Masquerade';
+
+function setup(testProps, isShallow = true) {
+    const props = {
+        author: testProps.author || null,
+        actions: testProps.actions || {},
+        history: testProps.history || {
+            push: jest.fn()
+        },
+        account: testProps.account || {},
+        ...testProps
+    };
+    return getElement(Masquerade, props, isShallow);
+}
+
+describe('Component Masquerade', () => {
+    it('should correctly set state on username change', () => {
+       const wrapper = setup({});
+
+       wrapper.instance()._usernameChanged({
+           target: {
+               value: 'uqtest'
+           }
+       });
+
+       wrapper.update();
+       expect(wrapper.state('userName')).toEqual('uqtest');
+    });
+
+    it('should not masquerade if Esc key is pressed', () => {
+        const wrapper = setup({});
+        wrapper.instance()._usernameChanged({
+            target: {
+                value: 'uqtest'
+            }
+        });
+        wrapper.update();
+
+        wrapper.instance()._masqueradeAs({
+            key: 'Esc'
+        });
+
+        expect(wrapper.state('loading')).toBeFalsy();
+    });
+
+    it('should not masquerade if Enter is pressed but username length is 0', () => {
+        const wrapper = setup({});
+        wrapper.instance()._masqueradeAs({
+            key: 'Enter'
+        });
+        expect(wrapper.state('loading')).toBeFalsy();
+    });
+
+    it('should masquerade if Enter is pressed and username is set', () => {
+        const wrapper = setup({});
+        wrapper.instance()._usernameChanged({
+            target: {
+                value: 'uqtest'
+            }
+        });
+        wrapper.update();
+
+        wrapper.instance()._masqueradeAs({
+            key: 'Enter'
+        });
+        expect(wrapper.state('loading')).toBeTruthy();
+    });
+});
