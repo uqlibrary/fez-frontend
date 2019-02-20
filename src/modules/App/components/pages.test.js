7abce6a528c5cc3797a6bf0dd68e2437bb85a6e7
Ky Lane <k.lane@uq.edu.au>
2019-02-20 16:53:01 +1000

    [PT#163354589] Updated tests for App

---
+++ src/modules/App/components/pages.test.js
@@ -0,0 +1,21 @@
+import * as pages from './pages';
+
+describe('Pages component', () => {
+
+    it('FixRecord', () => {
+        const expectation = '{\"key\":null,\"ref\":null,\"props\":{\"load\":{}},\"_owner\":null,\"_store\":{}}';
+        
+        expect(JSON.stringify(pages.FixRecord())).toEqual(expectation);
+        expect(JSON.stringify(pages.ClaimRecord())).toEqual(expectation);
+        expect(JSON.stringify(pages.PossiblyMyRecords())).toEqual(expectation);
+        expect(JSON.stringify(pages.Dashboard())).toEqual(expectation);
+        expect(JSON.stringify(pages.Orcid())).toEqual(expectation);
+        expect(JSON.stringify(pages.GoogleScholar())).toEqual(expectation);
+        expect(JSON.stringify(pages.ThesisSubmission())).toEqual(expectation);
+        expect(JSON.stringify(pages.SbsSubmission())).toEqual(expectation);
+        expect(JSON.stringify(pages.ViewRecord())).toEqual(expectation);
+        expect(JSON.stringify(pages.AddDataCollection())).toEqual(expectation);
+        expect(JSON.stringify(pages.ThirdPartyLookupTool())).toEqual(expectation);
+    });
+
+});
