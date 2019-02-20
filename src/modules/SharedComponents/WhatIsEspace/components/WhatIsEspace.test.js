5caf3d0c1a135445864aaf20f9c1f5b6b141e384
Vishal Asai <asai_vishal@yahoo.com>
2019-02-20 16:34:24 +1000

    [#163354589] updated WhatIsEspace tests and component

--- src/modules/SharedComponents/WhatIsEspace/components/WhatIsEspace.test.js
+++ src/modules/SharedComponents/WhatIsEspace/components/WhatIsEspace.test.js
@@ -10,17 +10,4 @@ describe('Component WhatIsEspace', () => {
         const wrapper = setup({});
         expect(toJson(wrapper)).toMatchSnapshot();
     });
-
-    it('should render external link', () => {
-        const wrapper = setup({
-            details: {
-                readMoreLink: 'https://example.com/',
-                readMoreTitle: 'mouse over and see me',
-                readMoreLabel: 'click me to learn more',
-                title: 'what is espace title',
-                text: 'what is espace intro'
-            }
-        });
-        expect(toJson(wrapper)).toMatchSnapshot();
-    });
 });
