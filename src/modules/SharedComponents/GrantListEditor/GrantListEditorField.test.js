722a325fc8ef030937cb6b8b93a29a08f173172e
Vishal Asai <asai_vishal@yahoo.com>
2019-02-21 16:26:43 +1000

    [#163354589] GrantListEditorField coverage, removed unused file

---
+++ src/modules/SharedComponents/GrantListEditor/GrantListEditorField.test.js
@@ -0,0 +1,16 @@
+import GrantListEditorField from './GrantListEditorField';
+
+function setup(testProps, isShallow = true) {
+    const props = {
+        ...testProps
+    };
+
+    return getElement(GrantListEditorField, props, isShallow);
+}
+
+describe('GrantListEditorField', () => {
+    it('should render default component', () => {
+        const wrapper = setup({input: {onChange: jest.fn()}});
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
