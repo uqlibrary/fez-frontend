12e540ea189c06cc92b0768a0c123f1c83001f0f
Vishal Asai <asai_vishal@yahoo.com>
2019-02-21 15:40:26 +1000

    [#163354589] QualityIndicatorField coverage

--- src/modules/SharedComponents/Toolbox/QualityIndicatorField/QualityIndicatorField.js
+++ src/modules/SharedComponents/Toolbox/QualityIndicatorField/QualityIndicatorField.js
@@ -2,17 +2,15 @@ import React from 'react';
 import {GenericSelectField} from 'modules/SharedComponents/GenericSelectField';
 import {QUALITY_INDICATORS} from 'config/general';
 
-export default function QualityIndicatorField(fieldProps) {
-    return (
-        <GenericSelectField
-            itemsList={QUALITY_INDICATORS}
-            hideLabel={false}
-            locale={{label: fieldProps.label, placeholder: fieldProps.placeholder}}
-            selectedValue={fieldProps.input.value || []}
-            onChange={!!fieldProps.input && fieldProps.input.onChange}
-            errorText={!!fieldProps.meta && fieldProps.meta.error || ''}
-            error={!!fieldProps.meta && !!fieldProps.meta.error || false}
-            { ...fieldProps }
-        />
-    );
-}
+export default (fieldProps) => (
+    <GenericSelectField
+        itemsList={QUALITY_INDICATORS}
+        hideLabel={false}
+        locale={{label: fieldProps.label, placeholder: fieldProps.placeholder}}
+        selectedValue={!!fieldProps.input && fieldProps.input.value || []}
+        onChange={!!fieldProps.input && fieldProps.input.onChange || undefined}
+        errorText={!!fieldProps.meta && fieldProps.meta.error || ''}
+        error={!!fieldProps.meta && !!fieldProps.meta.error || false}
+        { ...fieldProps }
+    />
+);
