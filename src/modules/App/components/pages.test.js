2603a14e0031d77870855b8a7d35785ccdd71200
Vishal Asai <asai_vishal@yahoo.com>
2019-02-25 10:05:28 +1000

    [#163354589] updated packages and tests

--- src/modules/App/components/pages.test.js
+++ src/modules/App/components/pages.test.js
@@ -3,8 +3,8 @@ import * as pages from './pages';
 describe('Pages component', () => {
 
     it('FixRecord', () => {
-        const expectation = '{\"key\":null,\"ref\":null,\"props\":{\"load\":{}},\"_owner\":null,\"_store\":{}}';
-        
+        const expectation = '{\"key\":null,\"ref\":null,\"props\":{\"load\":{\"_c\":[],\"_s\":0,\"_d\":false,\"_h\":0,\"_n\":false}},\"_owner\":null,\"_store\":{}}';
+
         expect(JSON.stringify(pages.FixRecord())).toEqual(expectation);
         expect(JSON.stringify(pages.ClaimRecord())).toEqual(expectation);
         expect(JSON.stringify(pages.PossiblyMyRecords())).toEqual(expectation);
