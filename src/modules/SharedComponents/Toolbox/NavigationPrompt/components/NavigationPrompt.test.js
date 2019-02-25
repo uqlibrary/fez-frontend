c011f6afa945479347bed6c564e92a18de9f7fe7
Vishal Asai <asai_vishal@yahoo.com>
2019-02-26 09:48:46 +1000

    [#163354589] NavigationPrompt coverage

--- src/modules/SharedComponents/Toolbox/NavigationPrompt/components/NavigationPrompt.test.js
+++ src/modules/SharedComponents/Toolbox/NavigationPrompt/components/NavigationPrompt.test.js
@@ -19,4 +19,105 @@ describe('NavigationPrompt component', () => {
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
+    it('should call componentWillUnmount and unblock method', () => {
+        const unblockFn = jest.fn();
+        const wrapper = setup({
+            when: true,
+            history: {block: jest.fn(() => unblockFn)},
+            children: jest.fn()
+        });
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+
+        const componentWillUnmount = jest.spyOn(wrapper.instance(), 'componentWillUnmount');
+
+        wrapper.unmount();
+        expect(componentWillUnmount).toHaveBeenCalled();
+        expect(unblockFn).toHaveBeenCalled();
+    });
+
+    it('should block navigation', () => {
+        const wrapper = setup({
+            when: true,
+            history: {block: jest.fn()},
+            children: jest.fn()
+        });
+
+        const showConfirmationFn = jest.fn();
+        wrapper.instance().confirmationBox = {
+            showConfirmation: showConfirmationFn
+        };
+
+        const result = wrapper.instance().blockNavigation('/test');
+
+        expect(wrapper.state().nextLocation).toEqual('/test');
+        expect(showConfirmationFn).toHaveBeenCalled();
+        expect(result).toBeFalsy();
+    });
+
+    it('should not block navigation', () => {
+        const wrapper = setup({
+            when: false,
+            history: {block: jest.fn()},
+            children: jest.fn()
+        });
+        const showConfirmationFn = jest.fn();
+        wrapper.instance().confirmationBox = {
+            showConfirmation: showConfirmationFn
+        };
+
+        const result = wrapper.instance().blockNavigation('/test');
+
+        expect(wrapper.state().nextLocation).toBeNull()
+        expect(showConfirmationFn).toHaveBeenCalledTimes(0);
+        expect(result).toBeFalsy();
+    });
+
+    it('should cancel navigation to next location', () => {
+        let onCancelFn = jest.fn();
+        const wrapper = setup({
+            when: true,
+            history: {block: jest.fn()},
+            children: jest.fn((setNavigationConfirmation, onConfirm, onCancel) => {
+                onCancelFn = onCancel
+                return {
+                    cancel: onCancel
+                }
+            })
+        });
+
+        wrapper.setState({
+            nextLocation: '/test'
+        });
+        wrapper.props().children.cancel();
+        expect(wrapper.state().nextLocation).toBeNull();
+    });
+
+    it('should navigation to next location on confirm', () => {
+        let pushFn = jest.fn();
+
+        const wrapper = setup({
+            when: true,
+            history: {
+                block: jest.fn((unblock) => {
+                    return () => unblock({pathname: '/test'});
+                }),
+                push: pushFn
+            },
+            children: jest.fn((setNavigationConfirmation, onConfirm, onCancel) => {
+                return {
+                    confirm: onConfirm
+                }
+            })
+        });
+
+        const showConfirmationFn = jest.fn();
+        wrapper.instance().confirmationBox = {
+            showConfirmation: showConfirmationFn
+        };
+
+        wrapper.update();
+        wrapper.props().children.confirm();
+        expect(pushFn).toHaveBeenCalledWith('/test');
+    });
 });
