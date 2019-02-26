af218217851f38a3b42fcd6c9560efd39b73a42d
Lea de Groot <l.degroot@library.uq.edu.au>
2019-02-26 14:26:29 +1000

    larger coverage for actions/records [PT #163354589]
    The coverage ignore else from line 204 on actions/records.js could be removed in favour of removing the check on the item - but it feels safer to leave it; if future specs change and those items arent available, lack of a check could be a problem

--- src/actions/records.test.js
+++ src/actions/records.test.js
@@ -386,6 +386,204 @@ describe('Record action creators', () => {
             expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
         });
 
+        it('correctly creates a record that needs no data deletions', async () => {
+            const testInput = {
+                "rek_title": "test",
+                "rek_display_type": 179,
+                "supervisors": [],
+                "fieldOfResearch": "",
+                "fez_record_search_key_journal_name": {
+                    "rek_journal_name": "test"
+                },
+                "rek_date": "2017-01-01",
+                "rek_subtype": "Article (original research)"
+            };
+            const pidRequest = {pid: 'UQ:396321'};
+
+            mockApi
+                .onPost(repositories.routes.NEW_RECORD_API().apiUrl)
+                .reply(200, {data: {...record}})
+                .onPatch(repositories.routes.EXISTING_RECORD_API(pidRequest).apiUrl)
+                .reply(200, {data: {...record}});
+
+
+            const expectedActions = [
+                actions.CREATE_RECORD_SAVING,
+                actions.CREATE_RECORD_SUCCESS
+            ];
+
+            await mockActionsStore.dispatch(recordActions.createNewRecord(testInput));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+
+        it('dispatches expected actions when authors list is not provided (just author)', async () => {
+            const testInput = {
+                "currentAuthor": [
+                    {
+                        "nameAsPublished": "Researcher, J",
+                        "authorId": 410
+                    }
+                ],
+                "rek_title": "test",
+                "rek_display_type": 179,
+                "editors": [],
+                "files": [],
+                "supervisors": [],
+                "fieldOfResearch": "",
+                "fez_record_search_key_journal_name": {
+                    "rek_journal_name": "test"
+                },
+                "rek_date": "2017-01-01",
+                "rek_subtype": "Article (original research)"
+            };
+            const pidRequest = {pid: 'UQ:396321'};
+
+            mockApi
+                .onPost(repositories.routes.NEW_RECORD_API().apiUrl)
+                .reply(200, {data: {...record}})
+                .onPatch(repositories.routes.EXISTING_RECORD_API(pidRequest).apiUrl)
+                .reply(200, {data: {...record}});
+
+
+            const expectedActions = [
+                actions.CREATE_RECORD_SAVING,
+                actions.CREATE_RECORD_SUCCESS
+            ];
+
+            await mockActionsStore.dispatch(recordActions.createNewRecord(testInput));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+
+        it('dispatches expected actions on successful save of an NTRO record', async () => {
+            const testInput = {
+                "currentAuthor": [
+                    {
+                        "nameAsPublished": "Researcher, J",
+                        "authorId": 410
+                    }
+                ],
+                "rek_title": "test",
+                "rek_display_type": 179,
+                "authors": [
+                    {
+                        "nameAsPublished": "test",
+                        "disabled": false,
+                        "selected": true,
+                        "authorId": 410
+                    }
+                ],
+                "editors": [],
+                "files": [],
+                "supervisors": [],
+                "fieldOfResearch": "",
+                "fez_record_search_key_journal_name": {
+                    "rek_journal_name": "test"
+                },
+                "rek_date": "2017-01-01",
+                "rek_subtype": "Article (original research)",
+                "isNtro": true,
+                "ntroAbstract": {
+                    "rek_description": "blah blah blah",
+                    "rek_formatted_abstract": "<p>blah blah blah</p>"
+                },
+                "grants": [{
+                    "rek_grant_agency_type": 7,
+                    "rek_grant_agency_type_order": 1
+                }],
+                "languages": [{
+                    "rek_language": "english",
+                    "rek_language_order": 1
+                }]
+
+            };
+            const pidRequest = {pid: 'UQ:396321'};
+
+            mockApi
+                .onPost(repositories.routes.NEW_RECORD_API().apiUrl)
+                .reply(200, {data: {...record}})
+                .onPatch(repositories.routes.EXISTING_RECORD_API(pidRequest).apiUrl)
+                .reply(200, {data: {...record}});
+
+
+            const expectedActions = [
+                actions.CREATE_RECORD_SAVING,
+                actions.CREATE_RECORD_SUCCESS
+            ];
+
+            await mockActionsStore.dispatch(recordActions.createNewRecord(testInput));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+
+        it('dispatches expected actions on successful save of record with various obscure fields', async () => {
+            const testInput = {
+                "currentAuthor": [
+                    {
+                        "nameAsPublished": "Researcher, J",
+                        "authorId": 410
+                    }
+                ],
+                "rek_title": "test",
+                "rek_display_type": 179,
+                "authors": [
+                    {
+                        "nameAsPublished": "test",
+                        "disabled": false,
+                        "selected": true,
+                        "authorId": 410
+                    }
+                ],
+                "editors": [],
+                "files": [],
+                "supervisors": [],
+                "fez_record_search_key_journal_name": {
+                    "rek_journal_name": "test"
+                },
+                "rek_date": "2017-01-01",
+                "rek_subtype": "Article (original research)",
+                "isNtro": true,
+                "contact": {
+                    "contactName": "Test Contact",
+                    "contactNameId": {
+                        "id": 121212,
+                        "value": "Test, Contact"
+                    },
+                    "contactEmail": "test@example.com"
+                },
+                "geographicArea": "lat long string for some place somewhere",
+                "significance": "454026",
+                "impactStatement": {
+                    "htmlText": "<p>more blah</p>"
+                },
+                "fieldOfResearch": [{
+                    "rek_value": {
+                        "key": 7
+                    },
+                    "rek_order": 1
+                }],
+                "qualityIndicators": [{
+                    "rek_quality_indicator": 454035,
+                    "rek_quality_indicator_order": 1
+                }],
+
+            };
+            const pidRequest = {pid: 'UQ:396321'};
+
+            mockApi
+                .onPost(repositories.routes.NEW_RECORD_API().apiUrl)
+                .reply(200, {data: {...record}})
+                .onPatch(repositories.routes.EXISTING_RECORD_API(pidRequest).apiUrl)
+                .reply(200, {data: {...record}});
+
+
+            const expectedActions = [
+                actions.CREATE_RECORD_SAVING,
+                actions.CREATE_RECORD_SUCCESS
+            ];
+
+            await mockActionsStore.dispatch(recordActions.createNewRecord(testInput));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+
     });
 
     describe('submitThesis()', () => {
@@ -813,6 +1011,50 @@ describe('Record action creators', () => {
             expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
         });
 
+        it('dispatches expected actions for action with only the basic fields', async () => {
+            const testInput = {
+                "thesisTitle": {
+                    planText: 'Title',
+                    htmlText: 'Title'
+                },
+                "thesisAbstract": {
+                    plainText: 'Abstract',
+                    htmlText: 'Abstract'
+                },
+                "rek_title": "test",
+                "rek_display_type": 179,
+                "editors": [],
+                "supervisors": [],
+                "files": [],
+                "fez_record_search_key_journal_name": {
+                    "rek_journal_name": "test"
+                },
+                "rek_date": "2017-01-01",
+                "rek_subtype": "Article (original research)",
+                "fieldOfResearch": [{
+                    "rek_value": {
+                        "key": 7
+                    },
+                    "rek_order": 1
+                }],
+            };
+            const pidRequest = {pid: 'UQ:396321'};
+
+            mockApi
+                .onPost(repositories.routes.NEW_RECORD_API().apiUrl)
+                .reply(200, {data: {...record}})
+                .onPatch(repositories.routes.EXISTING_RECORD_API(pidRequest).apiUrl)
+                .reply(200, {data: {...record}});
+
+
+            const expectedActions = [
+                actions.CREATE_RECORD_SAVING,
+                actions.CREATE_RECORD_SUCCESS
+            ];
+
+            await mockActionsStore.dispatch(recordActions.submitThesis(testInput));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
     });
 
     describe('clearNewRecord()', () => {
