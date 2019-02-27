5e48f3c664d18e84dc6a094a375bb7cbe2d3e2f0
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-02-27 14:24:11 +1000

    Switch to rendering unwrapped component to test proper rendering

--- src/modules/SharedComponents/Toolbox/StandardRighthandCard/components/StandardRighthandCard.snapshot.test.js
+++ src/modules/SharedComponents/Toolbox/StandardRighthandCard/components/StandardRighthandCard.snapshot.test.js
@@ -1,31 +1,44 @@
-import StandardRighthandCard from './StandardRighthandCard';
+import {RighthandCard} from './StandardRighthandCard';
 
 function setup(testProps, isShallow = true) {
     // build full props list required by the component
     const props = {
-        ...testProps,
-        title: testProps.title,
-        help: testProps.help,
-        children: testProps.children
+        classes: {},
+        ...testProps
     };
-    return getElement(StandardRighthandCard, props, isShallow);
+    return getElement(RighthandCard, props, isShallow);
 }
 
-describe('Snapshot tests for StandardRighthandCard component', () => {
-    it('renders StandardRighthandCard with title and no help icon', () => {
+describe('Snapshot tests for RighthandCard component', () => {
+    it('renders with title and no help icon', () => {
         const wrapper = setup({title: 'card title'});
         const tree = toJson(wrapper);
         expect(tree).toMatchSnapshot();
     });
 
-    it('renders StandardRighthandCard with title and help button', () => {
-        const wrapper = setup({title: 'Title', help: {title:'Help text', text:'Some help text', buttonLabel:'OK'}});
+    it('renders with title and help button', () => {
+        const wrapper = setup({
+            title: 'Title',
+            help: {
+                title:'Help text',
+                text:'Some help text',
+                buttonLabel:'OK'
+            }
+        });
         const tree = toJson(wrapper);
         expect(tree).toMatchSnapshot();
     });
 
-    it('renders StandardRighthandCard with title and help button and some content', () => {
-        const wrapper = setup({title: 'Title', help: {title:'Help text', text:'Some help text', buttonLabel:'OK'}, children: 'Some content'});
+    it('renders with title and help button and some content', () => {
+        const wrapper = setup({
+            title: 'Title',
+            help: {
+                title:'Help text',
+                text:'Some help text',
+                buttonLabel:'OK'
+            },
+            children: 'Some content'
+        });
         const tree = toJson(wrapper);
         expect(tree).toMatchSnapshot();
     });
