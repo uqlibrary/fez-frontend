360d2ebf4474b9545be6230c1315443974248469
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-02-28 10:29:34 +1000

    Complete branch coverage for ScrollToTop [#163354589]

--- src/modules/SharedComponents/Toolbox/ScrollToTop/components/ScrollToTop.test.js
+++ src/modules/SharedComponents/Toolbox/ScrollToTop/components/ScrollToTop.test.js
@@ -3,7 +3,7 @@ jest.mock('react-router-dom');
 
 function setup(testProps, isShallow = true) {
     const props = {
-        children: 'Childrens',
+        children: 'Children',
         location: {},
         ...testProps
     };
@@ -11,18 +11,28 @@ function setup(testProps, isShallow = true) {
     return getElement(ScrollToTop, props, isShallow);
 }
 describe('ScrollToTop component', () => {
-    it('should render given childrens', () => {
+    it('should render given children', () => {
         const wrapper = setup({});
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
     it('should scrollTo to if location is changed', () => {
+        const testLocation = {}; // {} !== {}
         const scrollToFn = jest.fn();
         global.window.scrollTo = scrollToFn;
         const wrapper = setup({});
+
+        // test if changed
         wrapper.setProps({
-            location: 'test'
+            location: testLocation
         });
         expect(scrollToFn).toHaveBeenCalledWith(0, 0);
+
+        // test else
+        scrollToFn.mockClear();
+        wrapper.setProps({
+            location: testLocation
+        });
+        expect(scrollToFn).toHaveBeenCalledTimes(0);
     });
 });
