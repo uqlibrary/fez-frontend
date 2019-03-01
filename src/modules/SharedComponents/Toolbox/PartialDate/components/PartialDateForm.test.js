2646d5f7799fe81a07ef2167ec2032c042ecf299
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-03-01 10:07:28 +1000

    Complete coverage for PartialDateForm [#163354589]

---
+++ src/modules/SharedComponents/Toolbox/PartialDate/components/PartialDateForm.test.js
@@ -0,0 +1,21 @@
+import {PartialDateForm} from './PartialDateForm';
+
+function setup(testProps, isShallow = true) {
+    const props = {
+        ...testProps,
+        classes: testProps.classes || {
+            hideLabel: 'hidden'
+        },
+    };
+    return getElement(PartialDateForm, props, isShallow);
+}
+
+describe('PartialDateForm component', () => {
+    it('should not call _setDate if props.onChange is not defined', () => {
+        const wrapper = setup({});
+        wrapper.instance()._setDate = jest.fn();
+        wrapper.instance().componentWillUpdate();
+        expect(wrapper.instance()._setDate).toHaveBeenCalledTimes(0);
+    });
+});
+
