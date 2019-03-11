cc686ed6f929685be1ab2d4f6ce2b6929a7ec1d6
Lea de Groot <l.degroot@library.uq.edu.au>
2019-03-12 07:58:05 +1000

    nvm downgrade not needed for upgraded jest [PT #164479570]

--- bin/codeship-setup.sh
+++ bin/codeship-setup.sh
@@ -3,13 +3,22 @@
 # exit if command returns error (non-zero status)
 set -e
 
+echo "npm version"
 npm -v
+
+echo "node version"
 node -v
+
+echo "jest version"
 jest --version
 
+#ls -ls ~/.nvm/
+
+#chmod a+x ~/.nvm/
+
 # v11.11.0 currently causing TypeError: Cannot assign to read only property 'Symbol(Symbol.toStringTag)' of object '#<process>' in tests
-~/.nvm/nvm.sh install v11.10.1
-node -v
+#~/.nvm/nvm.sh install v11.10.1
+#node -v
 
 npm cache clear -f
 npm ci
