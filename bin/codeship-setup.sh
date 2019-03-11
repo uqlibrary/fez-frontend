dd27d4a7fb693104873686ea6ddd59e50984cca1
Lea de Groot <l.degroot@library.uq.edu.au>
2019-03-12 07:54:39 +1000

    invoke nvm directly [PT #164479570]

--- bin/codeship-setup.sh
+++ bin/codeship-setup.sh
@@ -8,7 +8,7 @@ node -v
 jest --version
 
 # v11.11.0 currently causing TypeError: Cannot assign to read only property 'Symbol(Symbol.toStringTag)' of object '#<process>' in tests
-nvm install v11.10.1
+~/.nvm/nvm.sh install v11.10.1
 node -v
 
 npm cache clear -f
