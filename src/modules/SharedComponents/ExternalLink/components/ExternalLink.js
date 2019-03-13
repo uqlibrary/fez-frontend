843280193bfc1bfbe719b05f76ab04c49e588ce0
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-03-13 16:44:20 +1000

    Complete coverage for `ExternalLink` [#163354589]

--- src/modules/SharedComponents/ExternalLink/components/ExternalLink.js
+++ src/modules/SharedComponents/ExternalLink/components/ExternalLink.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import {locale} from 'locale';
+import { locale } from 'locale';
 import OpenInNew from '@material-ui/icons/OpenInNew';
-import {withStyles} from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
     externalLink: {
@@ -25,34 +25,48 @@ const styles = {
     }
 };
 
-const ExternalLink = (props) => {
-    const {className, children, openInNewIcon, classes, ...rest} = props;
-    return (
-        <a {...rest}
-            tabIndex="0"
-            title={rest.title || (openInNewIcon && locale.global.linkWillOpenInNewWindow.replace('[destination]', rest.href)) || undefined}
-            className={`${className} ${classes.externalLink}`}
-        >
-            {openInNewIcon &&
-            <OpenInNew className={classes.externalLinkIcon} />
-            }
-            {!!children && children}
-        </a>
-    );
-};
+export class ExternalLink extends PureComponent {
+    static propTypes = {
+        className: PropTypes.string,
+        openInNewIcon: PropTypes.bool,
+        children: PropTypes.any,
+        classes: PropTypes.object.isRequired
+    };
 
-ExternalLink.propTypes = {
-    className: PropTypes.string,
-    openInNewIcon: PropTypes.bool,
-    children: PropTypes.any,
-    classes: PropTypes.object
-};
+    static defaultProps = {
+        className: '',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+        openInNewIcon: true,
+        classes: {}
+    };
 
-ExternalLink.defaultProps = {
-    className: '',
-    target: '_blank',
-    rel: 'noopener noreferrer',
-    openInNewIcon: true
-};
+    constructor(props) {
+        super(props);
+    }
 
-export default withStyles(styles, {withTheme: true})(ExternalLink);
+    render() {
+        const { className, children, openInNewIcon, classes, ...rest } = this.props;
+        return (
+            <a {...rest}
+                tabIndex="0"
+                title={
+                    rest.title ||
+                    (
+                        openInNewIcon &&
+                        locale.global.linkWillOpenInNewWindow.replace('[destination]', rest.href)
+                    ) ||
+                    undefined
+                }
+                className={`${className} ${classes.externalLink}`}
+            >
+                {openInNewIcon &&
+                    <OpenInNew className={classes.externalLinkIcon} />
+                }
+                {!!children && children}
+            </a>
+        );
+    }
+}
+
+export default withStyles(styles, { withTheme: true })(ExternalLink);
