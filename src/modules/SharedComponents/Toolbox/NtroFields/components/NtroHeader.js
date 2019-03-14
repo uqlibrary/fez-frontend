b71dcf854c33b94ffd8af14af5386834ab8145d9
Lea de Groot <l.degroot@library.uq.edu.au>
2019-03-14 11:40:25 +1000

    move from a class to a functional component to skip lifecycle methods [PT #164453756]

--- src/modules/SharedComponents/Toolbox/NtroFields/components/NtroHeader.js
+++ src/modules/SharedComponents/Toolbox/NtroFields/components/NtroHeader.js
@@ -3,15 +3,15 @@ import {default as componentLocale} from 'locale/components';
 import {StandardCard} from 'modules/SharedComponents/Toolbox/StandardCard';
 import Grid from '@material-ui/core/Grid';
 
-export default class NtroHeader extends React.PureComponent {
-    render() {
-        const txt = componentLocale.components.ntroFields.header;
-        return (
-            <Grid item xs={12}>
-                <StandardCard title={txt.title}>
-                    {txt.body}
-                </StandardCard>
-            </Grid>
-        );
-    }
-}
+const NtroHeader = ({}) => {
+    const txt = componentLocale.components.ntroFields.header;
+    return (
+        <Grid item xs={12}>
+            <StandardCard title={txt.title}>
+                {txt.body}
+            </StandardCard>
+        </Grid>
+    );
+};
+
+export default NtroHeader;
