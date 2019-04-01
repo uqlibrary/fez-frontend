8b66ee314feb143f80cb739fc52a14ac07d712bb
Vishal Asai <asai_vishal@yahoo.com>
2019-04-01 14:59:02 +1000

    [#164749865] fixed error

--- src/modules/Admin/components/InheritedSecurityDetails.js
+++ src/modules/Admin/components/InheritedSecurityDetails.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
-import {renderPolicyDesc} from './SecurityCard';
+import {PolicyDescription} from './SecurityCard';
 
 export const InheritedSecurityDetails = () => (
     <Grid item xs={12} style={{
@@ -18,8 +18,8 @@ export const InheritedSecurityDetails = () => (
             <Grid item xs={5}>UQ:12345</Grid>
             <Grid item xs={5}>UQ:67890</Grid>
             <Grid item xs={2}><b>Policy:</b></Grid>
-            <Grid item xs={5}>{renderPolicyDesc(2)}</Grid>
-            <Grid item xs={5}>{renderPolicyDesc(3)}</Grid>
+            <Grid item xs={5}><PolicyDescription selectedPolicyKey={2} /></Grid>
+            <Grid item xs={5}><PolicyDescription selectedPolicyKey={3} /></Grid>
         </Grid>
     </Grid>
 );
