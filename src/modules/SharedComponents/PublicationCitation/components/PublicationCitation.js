0ecbf07b8d4854b558aea2aad6ad6b54a285f9ef
Lea de Groot <l.degroot@library.uq.edu.au>
2019-06-06 07:59:52 +1000

    Put a space after the 'content indicator:' label [PT #166146095]

--- src/modules/SharedComponents/PublicationCitation/components/PublicationCitation.js
+++ src/modules/SharedComponents/PublicationCitation/components/PublicationCitation.js
@@ -309,7 +309,7 @@ export class PublicationCitation extends PureComponent {
                                 this.props.publication.fez_record_search_key_content_indicator &&
                                 this.props.publication.fez_record_search_key_content_indicator.length > 0 &&
                                 <Grid item xs={12} className={classes.contentIndicators}>
-                                    <Typography gutterBottom variant="caption">{locale.components.contentIndicators.label}:
+                                    <Typography gutterBottom variant="caption">{locale.components.contentIndicators.label}:&nbsp;
                                         {
                                             this.props.publication.fez_record_search_key_content_indicator.map((item, index) => {
                                                 return (
