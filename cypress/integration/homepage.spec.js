e25166a17defb7bd0e92b0f46497889ce8211fef
Ky Lane <k.lane@uq.edu.au>
2019-06-18 12:14:31 +1000

    [PT#166370980] Working around material ui animations and snapshotting

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,9 +1,11 @@
 context('Homepage', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000', {timeout: 5000});
-        cy.get('button').contains('Close this message', {timeout: 5000}).click({
-            force: true,
-        });
+        cy.get('button')
+            .contains('Close this message', {timeout: 5000})
+            .click({
+                force: true,
+            });
         cy.wait(1000);
     });
 
@@ -12,43 +14,42 @@ context('Homepage', () => {
             .get('span')
             .contains('Trending on Scopus')
             .click();
-        cy.get('.content-container').scrollTo('top');
         cy.get('.content-container', {timeout: 1000}).toMatchSnapshot();
+
         cy.get('button', {timeout: 1000})
             .get('span')
             .contains('Trending on Web of science')
             .click();
-        cy.get('.content-container').scrollTo('top');
         cy.get('.content-container', {timeout: 1000}).toMatchSnapshot();
+
         cy.get('button', {timeout: 1000})
             .get('span')
             .contains('Trending on Altmetric')
             .click();
-        cy.get('.content-container').scrollTo('top');
         cy.get('.content-container', {timeout: 1000}).toMatchSnapshot();
     });
 
     it('Doing a basic search', () => {
         cy.get('#simpleSearchField').type('cats and dogs{enter}', {delay: 20});
-        cy.get('.content-container').scrollTo('top');
+
         cy.get('#showAdvancedSearchButton').click();
-        cy.get('.content-container').scrollTo('top');
+
         cy.get(
             '[aria-label="Check to search for publications with are only open access / full text"]',
         ).click();
-        cy.get('.content-container').scrollTo('top');
+
         cy.get('.content-container', {timeout: 1000}).toMatchSnapshot();
 
         cy.get('[aria-label="Click to add another advanced search field"]', {
             delay: 1000,
         }).click();
-        cy.get('.content-container').scrollTo('top');
+
         cy.get(
             '[aria-label="Click to select a field to search from the list - Select a field currently selected"]',
         ).click();
-        cy.get('.content-container').scrollTo('top');
+
         cy.get('[data-value="rek_author"]').click();
-        cy.get('.content-container').scrollTo('top');
+
         cy.get('.content-container', {timeout: 1000}).toMatchSnapshot();
         cy.get('[placeholder="Add an author name"]').type('Ky Lane{enter}', {
             delay: 100,
@@ -70,7 +71,7 @@ context('Homepage', () => {
         cy.get('div[id="menu-"]')
             .get('div[aria-hidden="true"]')
             .click({force: true, multiple: true}); // This will close any select field modal popup by force
-        cy.get('.content-container').scrollTo('top');
+
         cy.get('.content-container', {timeout: 1000}).toMatchSnapshot();
     });
 });
