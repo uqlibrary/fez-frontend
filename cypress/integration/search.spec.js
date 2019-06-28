16d92f258f291e84e086d9a5636182fa5882c9d3
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-06-28 12:14:38 +1000

    Add additional checks to search e2e tests [#166370980]

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -1,4 +1,9 @@
+// import { default as componentsLocale } from '../../src/locale/components';
+
 context('Search', () => {
+    // const searchLocale = componentsLocale.components.searchComponent;
+    const cleanExtraSpaces = $string => $string.replace(/\s+/g, ' ').trim();
+
     beforeEach(() => {
         cy.visit('/records/search');
         cy.get('#unsupportedBrowser.card button').then($button => {
@@ -10,50 +15,110 @@ context('Search', () => {
     });
     it('Doing a basic search to advanced search', () => {
         // Perform a basic search
-        cy.get('#simpleSearchField').type('cats and dogs{enter}', {
-            delay: 20,
-        });
-        cy.get('.StandardCard').should('contain', 'Displaying works 1 to 6 of 6 total records.');
-        cy.get('h6').should('contain', 'Refine results');
-        cy.get('#showAdvancedSearchButton').should('be.visible');
+        cy.get('#simpleSearchField')
+            .should(
+                'have.attr',
+                'aria-label',
+                // searchLocale.ariaInputLabel
+                'Enter your search query to search eSpace and then press Enter'
+            )
+            .closest('[class*="MuiFormControl-root"]')
+            // .contains('label', searchLocale.searchBoxPlaceholder);
+            .contains('label', 'Search eSpace');
+        cy.get('#simpleSearchField')
+            .type('cats and dogs{enter}');
+        cy.get('.StandardPage > div > div > div:nth-of-type(2) .StandardCard')
+            .should('contain', 'Displaying works 1 to 6 of 6 total records.');
+        cy.get('.StandardPage > div > div > div:nth-of-type(3) h6')
+            .should('contain', 'Refine results');
+
         // Click through to advanced search UI
-        cy.get('#showAdvancedSearchButton').click();
-        cy.get('h5').should('contain', 'Advanced search');
-        cy.get('input[name="searchField0"]').should('have.value', 'cats and dogs');
-        // Check the open access checkbox, then click "away"
-        cy.get('[aria-label="Check to search for publications with are only open access / full text"]').click();
-        cy.get('body').click();
-        // Add another advanced search field
-        cy.get('[aria-label="Click to add another advanced search field"]').click();
-        cy.get('.content-container').scrollTo('top');
-        cy.get('div[aria-label="Click to select a field to search from the list - Select a field currently selected"]')
-            .get('p')
-            .contains('Please select a field to search')
-            .should('be.visible');
-        // Select author from the field dropdown
-        cy.get(
-            '[aria-label="Click to select a field to search from the list - Select a field currently selected"]',
-        ).click();
-        cy.get('[data-value="rek_author"]').click();
-        cy.get('button#advancedSearchButton').should('to.have.attr', 'disabled');
-        cy.get('[placeholder="Add an author name"]').type('Ky Lane{enter}', {
-            delay: 100,
-        });
-        cy.get('button#advancedSearchButton').should('not.to.have.attr', 'disabled');
-        // Add a set of collections to search from
-        cy.get('[aria-label="Click to add another advanced search field"]').click();
-        cy.get(
-            '[aria-label="Click to select a field to search from the list - Select a field currently selected"]',
-        ).click();
-        cy.get('[data-value="rek_ismemberof"]').click();
-        cy.get('button#advancedSearchButton').should('to.have.attr', 'disabled');
-        cy.get('div')
-            .contains('Select collections')
+        cy.get('button#showAdvancedSearchButton')
+            .should('contain', 'Advanced search')
+            .should(
+                'have.attr',
+                'aria-label',
+                'Click to switch to Advanced search'
+            )
             .click();
-        cy.get('[data-value="UQ:131735"]').click();
-        cy.get('[data-value="UQ:131375"]').click();
-        cy.get('[data-value="UQ:292807"]').click();
-        cy.get('div[id="menu-"]').click(10, 10);
-        cy.get('button#advancedSearchButton').should('not.to.have.attr', 'disabled');
+        cy.get('#advancedSearchForm h5')
+            .should('contain', 'Advanced search');
+        cy.get('input[name="searchField0"]')
+            .should('have.value', 'cats and dogs')
+            .type('{home}it\'s raining ');
+        cy.contains('label', 'Open access')
+            .find('[class*="MuiCheckbox"]')
+            .should(
+                'have.attr',
+                'aria-label',
+                'Check to search for publications with are only open access / full text'
+            );
+        cy.contains('label', 'Open access')
+            .click();
+        cy.contains('button', 'Add another field')
+            .should(
+                'have.attr',
+                'aria-label',
+                'Click to add another advanced search field'
+            )
+            .click();
+        cy.get('.content-container').scrollTo('top');
+        cy.contains('Select a field')
+            .closest('[class*="MuiInput-root"]')
+            .should(
+                'have.attr',
+                'aria-label',
+                'Click to select a field to search from the list - Select a field currently selected'
+            )
+            .siblings('p')
+            .contains('Please select a field to search');
+        cy.contains('Select a field')
+            .click();
+        // Select author from the field dropdown
+        cy.contains('#menu- li', 'Author Name')
+            .click();
+        cy.get('button#advancedSearchButton')
+            .should('be.disabled')
+            .should('have.text', 'Search');
+        cy.get('[placeholder="Add an author name"]')
+            .type('Ky Lane{enter}');
+        cy.get('button#advancedSearchButton')
+            .should('not.be.disabled');
+        // Add a set of collections to search from
+        cy.contains('button', 'Add another field')
+            .click();
+        cy.contains('Select a field')
+            .click();
+        cy.contains('#menu- li', 'Collection')
+            .click();
+        cy.get('button#advancedSearchButton')
+            .should('be.disabled');
+        cy.contains('Select collections')
+            .click();
+        cy.contains('#menu- li', '5th Australasian Congress on Applied Mechanics')
+            .click();
+        cy.contains('#menu- li', 'Adaptive Interactive Profit Expectations')
+            .click();
+        cy.contains('#menu- li', 'Admin only - CHRC')
+            .click();
+        cy.get('#menu-')
+            .click(10, 10);
+        cy.get('#advancedSearchForm .searchQueryCaption')
+            .should($caption => {
+                expect(
+                    cleanExtraSpaces(
+                        $caption.text()
+                    )
+                ).to.equal(
+                    'Any field contains it\'s raining cats and dogs AND Author Name contains Ky Lane AND Collection' +
+                    ' is one of UQ:131735, UQ:131375 or UQ:292807 AND is open access/full text'
+                );
+            });
+        cy.get('button#advancedSearchButton')
+            .should('not.be.disabled')
+            .click();
+        cy.get('.StandardPage > div > div > div:nth-of-type(2)')
+            .should('contain', 'Searching for works')
+            .contains('Displaying works 1 to 6 of 6 total records.');
     });
 });
