26a3c02e567f4f587702a2249725416b58a9a488
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-07-02 08:51:25 +1000

    [#166370980] Cleanup

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,8 +24,6 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-import 'cypress-file-upload';
-
 // Allows the targeting of CKEditors
 // CKeditor dynamically names instances as "editor1", "editor2" etc.
 // USAGE : cy.type_ckeditor('editor1', '<p>This is some text</p>');
