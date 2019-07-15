555a00f89f3c775a3debd851cc7791e44229002d
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-07-15 10:01:28 +1000

    Speed up and simplify homepage e2e

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,9 +1,17 @@
 context('Homepage', () => {
+    const checkMenuItemCount = expectedCount => {
+        cy.closeUnsupported();
+        cy.get('button[aria-label="Click to open the main navigation"]').click();
+        cy.get('nav#mainMenu')
+            .get('div[role="button"]')
+            .should('have.length', expectedCount);
+    };
+
     it('Renders the tabbed panes as expected', () => {
         cy.visit('/');
         cy.closeUnsupported();
-        cy.wait(3000);
-        cy.get('button', { timeout: 30000 })
+
+        cy.get('button[role="tab"]')
             .contains('Trending on Scopus')
             .click();
         cy.get('h6')
@@ -11,7 +19,7 @@ context('Homepage', () => {
             .should('not.contain', 'Web of Science citation count')
             .should('not.contain', 'Altmetric score');
 
-        cy.get('button', { timeout: 30000 })
+        cy.get('button[role="tab"]')
             .contains('Trending on Web of science')
             .click();
         cy.get('h6')
@@ -19,7 +27,7 @@ context('Homepage', () => {
             .should('contain', 'Web of Science citation count')
             .should('not.contain', 'Altmetric score');
 
-        cy.get('button', { timeout: 30000 })
+        cy.get('button[role="tab"]')
             .contains('Trending on Altmetric')
             .click();
         cy.get('h6')
@@ -27,56 +35,31 @@ context('Homepage', () => {
             .should('not.contain', 'Web of Science citation count')
             .should('contain', 'Altmetric score');
 
-        cy.get('h3').should('contain', 'What is eSpace?');
+        cy.get('.StandardPage > div > div > div:nth-of-type(2) h3').should('contain', 'What is eSpace?');
     });
 
     it('Has expected menu items for a public user', () => {
         cy.visit('/?user=uqexpired');
-        cy.closeUnsupported();
-        cy.wait(3000);
-        cy.get('button[aria-label="Click to open the main navigation"]').click();
-        cy.get('nav#mainMenu')
-            .get('div[role="button"]')
-            .should('have.length', 4);
+        checkMenuItemCount(4);
     });
 
     it('Has expected menu items for a researcher', () => {
         cy.visit('/?user=uqresearcher');
-        cy.closeUnsupported();
-        cy.wait(3000);
-        cy.get('button[aria-label="Click to open the main navigation"]').click();
-        cy.get('nav#mainMenu')
-            .get('div[role="button"]')
-            .should('have.length', 12);
+        checkMenuItemCount(12);
     });
 
     it('Has expected menu items for an admin', () => {
         cy.visit('/?user=uqstaff');
-        cy.closeUnsupported();
-        cy.wait(3000);
-        cy.get('button[aria-label="Click to open the main navigation"]').click();
-        cy.get('nav#mainMenu')
-            .get('div[role="button"]')
-            .should('have.length', 16);
+        checkMenuItemCount(16);
     });
 
     it('Has expected menu items for a student', () => {
         cy.visit('/?user=s1111111');
-        cy.closeUnsupported();
-        cy.wait(3000);
-        cy.get('button[aria-label="Click to open the main navigation"]').click();
-        cy.get('nav#mainMenu')
-            .get('div[role="button"]')
-            .should('have.length', 12);
+        checkMenuItemCount(12);
     });
 
     it('Has expected menu items for a RHD student', () => {
         cy.visit('/?user=s2222222');
-        cy.closeUnsupported();
-        cy.wait(3000);
-        cy.get('button[aria-label="Click to open the main navigation"]').click();
-        cy.get('nav#mainMenu')
-            .get('div[role="button"]')
-            .should('have.length', 12);
+        checkMenuItemCount(12);
     });
 });
