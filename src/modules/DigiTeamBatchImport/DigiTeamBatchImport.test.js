a023ed97e6f8b5bf42fa1231360e6b969da9df50
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-07-19 13:48:33 +1000

    Update tests & snapshots

--- src/modules/DigiTeamBatchImport/DigiTeamBatchImport.test.js
+++ src/modules/DigiTeamBatchImport/DigiTeamBatchImport.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Immutable from 'immutable';
 import { rtlRender } from 'test-utils';
 import { Provider } from 'react-redux';
 import { setupStoreForMount } from 'test.setup';
 
-import DigiTeamBatchImport, { getMenuObjectsFromCollectionsArray } from './components/DigiTeamBatchImport';
+import DigiTeamBatchImport from './components/DigiTeamBatchImport';
 
 function setup(testProps) {
     return rtlRender(
@@ -17,29 +16,5 @@ function setup(testProps) {
 describe('DigiTeamBatchImport Component', () => {
     it('renders page', () => {
         setup({});
-    });
-
-    it('renders after a community is selected', () => {
-        setup({
-            formValues: Immutable.Map({
-                communityID: 'UQ:342708',
-            }),
-        });
-    });
-});
-
-describe('DigiTeamBatchImport helpers', () => {
-    it('can convert collections array to menu object array', () => {
-        const test = [
-            { rek_title: 'test1', rek_pid: 'UQ:123456' },
-            { rek_title: 'test3', rek_pid: 'UQ:123457' },
-            { rek_title: 'test2', rek_pid: 'UQ:123458' },
-        ];
-        const expected = [
-            { text: 'test1', value: 'UQ:123456', index: 0 },
-            { text: 'test3', value: 'UQ:123457', index: 1 },
-            { text: 'test2', value: 'UQ:123458', index: 2 },
-        ];
-        expect(getMenuObjectsFromCollectionsArray(test)).toEqual(expected);
     });
 });
