12317f81fa1e61996f95a1a924f683f7e7e9e062
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-08-09 16:52:06 +1000

    Fix sub-process issue

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 printf "\n--- \e[1mRUNNING CODE STYLE CHECKS\e[0m ---\n"
-printf "\n$ npm run codestyles:files\n"
-FILES=$(npm run codestyles:files)
+printf "\n$ prettier-eslint '**/*.js' --list-different -l silent\n"
+FILES=$(prettier-eslint '**/*.js' --list-different -l silent)
 
 if [[ "$?" == 0 ]]; then
     printf "\n\e[92mLooks good! Well done.\e[0m\n\n"
