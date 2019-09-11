1a9bcb771c41d9495a7f94ba95c82262a4c61df8
Lea de Groot <l.degroot@library.uq.edu.au>
2019-09-11 10:13:55 +1000

    re-arrange slightly [PT #167126529]

--- src/config/admin/fields/audioFields.js
+++ src/config/admin/fields/audioFields.js
@@ -45,16 +45,16 @@ export default {
             title: 'Bibliographic',
             groups: [
                 ['fez_record_search_key_place_of_publication', 'fez_record_search_key_publisher'],
-                ['fez_record_search_key_series', 'rek_date'],
-                ['fez_record_search_key_date_available', 'fez_record_search_key_date_recorded'],
-                ['fez_record_search_key_acknowledgements'],
+                ['rek_date', 'fez_record_search_key_date_available', 'fez_record_search_key_date_recorded'],
                 ['rek_description'],
                 ['fez_record_search_key_length', 'fez_record_search_key_original_format'],
+                ['fez_record_search_key_series'],
                 ['fez_record_search_key_rights'],
                 ['fez_record_search_key_source'],
                 ['fez_record_search_key_license'],
                 ['fez_record_search_key_transcript'],
                 ['fez_record_search_key_refereed_source'],
+                ['fez_record_search_key_acknowledgements'],
             ],
         },
         {
@@ -88,9 +88,8 @@ export default {
         {
             groups: [
                 ['additionalNotes'],
-                ['fez_record_search_key_institutional_status'],
+                ['fez_record_search_key_institutional_status', 'fez_record_search_key_oa_status'],
                 ['contentIndicators'],
-                ['fez_record_search_key_oa_status'],
             ],
         },
     ],
