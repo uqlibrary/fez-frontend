ef0b9d4afac16f1be6df5f1a60bb060a75bbf6a7
Lea de Groot <l.degroot@library.uq.edu.au>
2019-09-12 14:03:42 +1000

    add journal form [PT #167126840]

---
+++ src/config/admin/fields/journalFields.js
@@ -0,0 +1,96 @@
+import commonFields from './commonFields';
+
+export default {
+    ...commonFields,
+    admin: () => [
+        {
+            groups: [['internalNotes'], ['rek_herdc_notes']],
+        },
+    ],
+    identifiers: () => [
+        {
+            title: 'Manage identifiers',
+            groups: [
+                ['fez_record_search_key_isi_loc', 'rek_wok_doc_type'],
+                ['fez_record_search_key_scopus_id', 'rek_scopus_doc_type'],
+                ['rek_pubmed_doc_type'],
+            ],
+        },
+        {
+            title: 'Manage links',
+            groups: [['links']],
+        },
+    ],
+    bibliographic: (isLote = false) => [
+        {
+            title: 'Book title',
+            groups: [['rek_title'], ...(isLote ? [['fez_record_search_key_translated_title']] : [])],
+        },
+        {
+            title: 'Language of work',
+            groups: [['languages']],
+        },
+        {
+            title: 'ISBN',
+            groups: [['fez_record_search_key_isbn']],
+        },
+        {
+            title: 'ISSN',
+            groups: [['fez_record_search_key_issn']],
+        },
+        {
+            title: 'Bibliographic',
+            groups: [
+                ['fez_record_search_key_place_of_publication', 'fez_record_search_key_publisher'],
+                [
+                    'fez_record_search_key_volume_number',
+                    'fez_record_search_key_issue_number',
+                    'fez_record_search_key_article_number',
+                ],
+                ['fez_record_search_key_series'],
+                [
+                    'fez_record_search_key_start_page',
+                    'fez_record_search_key_end_page',
+                    'fez_record_search_key_total_pages',
+                ],
+                ['rek_date'],
+                ['rek_description'],
+                ['fez_record_search_key_refereed_source'],
+            ],
+        },
+        {
+            title: 'Keyword(s)',
+            groups: [['fez_record_search_key_keywords']],
+        },
+        {
+            title: 'Subject',
+            groups: [['subjects']],
+        },
+    ],
+    authors: () => [
+        {
+            title: 'Creators',
+            groups: [['authors']],
+        },
+        {
+            title: 'Contributors',
+            groups: [['editors']],
+        },
+    ],
+    additionalInformation: () => [
+        {
+            title: 'Member of collections',
+            groups: [['collections']],
+        },
+        {
+            title: 'Additional information',
+            groups: [
+                ['rek_subtype'],
+                ['fez_record_search_key_herdc_code', 'fez_record_search_key_herdc_status'],
+                ['fez_record_search_key_institutional_status', 'fez_record_search_key_oa_status', 'contentIndicators'],
+                ['additionalNotes'],
+            ],
+        },
+    ],
+    ntro: () => [],
+};
