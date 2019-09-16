f41f9010e84565519e83f778adfde964065d9fb1
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-09-16 13:19:22 +1000

    [#167126855] Add patent fields config

---
+++ src/config/admin/fields/patentFields.js
@@ -0,0 +1,10 @@
+import commonFields from './commonFields';
+
+export default {
+    ...commonFields,
+    identifiers: () => [],
+    bibliographic: () => [],
+    authors: () => [],
+    additionalInformation: () => [],
+    ntro: () => [],
+};
