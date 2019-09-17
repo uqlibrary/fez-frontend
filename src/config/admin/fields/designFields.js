78ea0e915318c5da3ea9c4681d2e01570cb456ba
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-09-18 09:11:47 +1000

    [#167126798] Add first-cut field config for Design

---
+++ src/config/admin/fields/designFields.js
@@ -0,0 +1,79 @@
+import commonFields from './commonFields';
+
+export default {
+    ...commonFields,
+    bibliographic: (isLote = false) => [
+        {
+            title: 'Title',
+            groups: [['rek_title'], ...(isLote ? [['fez_record_search_key_translated_title']] : [])],
+        },
+        {
+            title: 'Language of work',
+            groups: [['languages']],
+        },
+        {
+            title: 'Bibliographic',
+            groups: [
+                ['fez_record_search_key_place_of_publication', 'fez_record_search_key_publisher'],
+                ['fez_record_search_key_series'],
+                [
+                    'fez_record_search_key_start_page',
+                    'fez_record_search_key_end_page',
+                    'fez_record_search_key_total_pages',
+                ],
+                ['rek_date'],
+                ['rek_description'],
+                ['fez_record_search_key_rights'],
+                ['fez_record_search_key_source'],
+                ['fez_record_search_key_acknowledgements'],
+            ],
+        },
+        {
+            title: 'Geographic area',
+            groups: [['geoCoordinates']],
+        },
+        {
+            title: 'Keyword(s)',
+            groups: [['fez_record_search_key_keywords']],
+        },
+        {
+            title: 'Subject',
+            groups: [['subjects']],
+        },
+    ],
+    authors: () => [
+        {
+            title: 'Designers',
+            groups: [['authors']],
+        },
+        {
+            title: 'Editors',
+            groups: [['editors']],
+        },
+    ],
+    additionalInformation: () => [
+        {
+            title: 'Member of collections',
+            groups: [['collections']],
+        },
+        {
+            title: 'Additional information',
+            groups: [
+                ['rek_subtype'],
+                ['fez_record_search_key_herdc_code', 'fez_record_search_key_herdc_status'],
+                ['fez_record_search_key_institutional_status', 'fez_record_search_key_oa_status', 'contentIndicators'],
+                ['additionalNotes'],
+            ],
+        },
+    ],
+    ntro: () => [
+        {
+            title: 'Scale/Significance of work & Creator research statement',
+            groups: [['significanceAndContributionStatement']],
+        },
+        {
+            title: 'Quality indicators',
+            groups: [['qualityIndicators']],
+        },
+    ],
+};
