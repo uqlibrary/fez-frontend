a3b44be1c6677798e84ac718e4ac7a9ac77fb760
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-09-17 14:08:08 +1000

    [#167126779] Add admin edit config for Creative Work

---
+++ src/config/admin/fields/creativeWorkFields.js
@@ -0,0 +1,95 @@
+import commonFields from './commonFields';
+
+export default {
+    ...commonFields,
+    bibliographic: (isLote = false) => [
+        {
+            title: 'Title',
+            groups: [['rek_title'], ...(isLote ? [['fez_record_search_key_translated_title']] : [])],
+        },
+        {
+            title: 'Language of work',
+            groups: [['languages']],
+        },
+        {
+            title: 'ISBN',
+            groups: [['fez_record_search_key_isbn']],
+        },
+        {
+            title: 'ISSN',
+            groups: [['fez_record_search_key_issn']],
+        },
+        {
+            title: 'Bibliographic',
+            groups: [
+                ['fez_record_search_key_place_of_publication', 'fez_record_search_key_publisher'],
+                [
+                    'fez_record_search_key_edition',
+                    'fez_record_search_key_volume_number',
+                    'fez_record_search_key_issue_number',
+                ],
+                ['fez_record_search_key_series'],
+                [
+                    'fez_record_search_key_start_page',
+                    'fez_record_search_key_end_page',
+                    'fez_record_search_key_total_pages',
+                ],
+                ['rek_date', 'fez_record_search_key_date_available'],
+                ['rek_description'],
+                ['fez_record_search_key_original_format'],
+                ['fez_record_search_key_refereed_source'],
+            ],
+        },
+        {
+            title: 'Keyword(s)',
+            groups: [['fez_record_search_key_keywords']],
+        },
+        {
+            title: 'Subject',
+            groups: [['subjects']],
+        },
+        {
+            title: 'Related publications', // Succeeds
+            groups: [['fez_record_search_key_isderivationof']],
+        },
+    ],
+    authors: () => [
+        {
+            title: 'Authors',
+            groups: [['authors']],
+        },
+        {
+            title: 'Editors',
+            groups: [['editors']],
+        },
+    ],
+    additionalInformation: () => [
+        {
+            title: 'Member of collections',
+            groups: [['collections']],
+        },
+        {
+            title: 'Additional information',
+            groups: [
+                ['rek_subtype'],
+                ['fez_record_search_key_herdc_code', 'fez_record_search_key_herdc_status'],
+                ['fez_record_search_key_institutional_status', 'fez_record_search_key_oa_status', 'contentIndicators'],
+                ['additionalNotes'],
+            ],
+        },
+    ],
+    ntro: () => [
+        {
+            title: 'Scale/Significance of work & Creator research statement',
+            groups: [['significanceAndContributionStatement']],
+        },
+        {
+            title: 'ISMN',
+            groups: [['fez_record_search_key_ismn']],
+        },
+        {
+            title: 'Quality indicators',
+            groups: [['qualityIndicators']],
+        },
+    ],
+};
