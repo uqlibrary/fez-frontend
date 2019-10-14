3c2e33cc5d827836f957ab0eeeac82f1d20311b0
Lea de Groot <l.degroot@library.uq.edu.au>
2019-10-14 11:29:15 +1000

    video admin edit cypress test [PT #168828096]

---
+++ cypress/integration/admin-edit/video.spec.js
@@ -0,0 +1,55 @@
+import { default as recordList } from '../../../src/mock/data/records/publicationTypeListVideo';
+
+context('Video admin edit', () => {
+    // const baseUrl = Cypress.config('baseUrl');
+    const record = recordList.data[0];
+
+    beforeEach(() => {
+        cy.visit(`/admin/edit/${record.rek_pid}?user=uqstaff`);
+        cy.closeUnsupported();
+        cy.wait(1000); // Wait for data load
+    });
+
+    afterEach(() => {
+        cy.window()
+            .then(win => (win.onbeforeunload = undefined));
+    });
+
+    it('should load with specifed elements', () => {
+        cy.get('h2')
+            .should('have.length', 1)
+            .should('have.text', `Edit ${record.rek_display_type_lookup} - ${record.rek_title}: ${record.rek_pid}`);
+
+        cy.get('input[value=tabbed]')
+            .should('be.not.checked');
+
+        cy.get('button[title="Learn about keyboard shortcuts"]')
+            .should('exist');
+
+        cy.get('.StandardPage form > div > div > div.StandardCard > div > div > h3')
+            .as('cards')
+            .should('have.length', 8);
+    });
+
+    it('should render Video specific fields on the Bibliographic tab', () => {
+        cy.get('.StandardPage form > div > div:nth-child(3)')
+            .within(() => {
+                cy.root()
+                    .children('.StandardCard')
+                    .children('div')
+                    .children('div')
+                    .children('h3')
+                    .should('have.text', 'Bibliographic');
+
+                cy.get('div:nth-child(5) > .StandardCard')
+                    .within(() => {
+                        cy.get('h3')
+                            .should('have.text', 'Bibliographic');
+
+                        // Video record includes the owner's Rights
+                        cy.get('#Rights')
+                            .should('have.value', record.fez_record_search_key_rights.rek_rights);
+                    });
+            });
+    });
+});
