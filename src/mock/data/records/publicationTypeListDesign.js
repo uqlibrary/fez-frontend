73b496d89ed7c70a5b99c00e8b5a5bb4e5d47286
Lea de Groot <l.degroot@library.uq.edu.au>
2019-10-16 09:03:06 +1000

    Fix oversights [PT #168828096]
    - add floating label to Series
    - correct data passed through for Scale
    - correct data passed through for Job number

--- src/mock/data/records/publicationTypeListDesign.js
+++ src/mock/data/records/publicationTypeListDesign.js
@@ -428,5 +428,367 @@ export default {
             "rek_status_lookup": "Published",
             "rek_wok_doc_type_lookup": null
         },
+        {
+            "rek_pid": "UQ:00222bb",
+            "rek_title_xsdmf_id": null,
+            "rek_title": "Flats for Mr. and Mrs. A Franchin, Wavell Heights, ground floor and basement plan blueprints, Nov 1960",
+            "rek_description_xsdmf_id": null,
+            "rek_description": null,
+            "rek_display_type_xsdmf_id": null,
+            "rek_display_type": 316,
+            "rek_status_xsdmf_id": null,
+            "rek_status": 2,
+            "rek_date_xsdmf_id": null,
+            "rek_date": "1960-01-01T00:00:00Z",
+            "rek_object_type_xsdmf_id": null,
+            "rek_object_type": 3,
+            "rek_depositor_xsdmf_id": null,
+            "rek_depositor": 41783,
+            "rek_created_date_xsdmf_id": null,
+            "rek_created_date": "2019-09-09T03:38:26Z",
+            "rek_updated_date_xsdmf_id": null,
+            "rek_updated_date": "2019-09-09T03:38:27Z",
+            "rek_file_downloads": 0,
+            "rek_citation": "",
+            "rek_genre_xsdmf_id": null,
+            "rek_genre": "image\/tiff",
+            "rek_genre_type_xsdmf_id": null,
+            "rek_genre_type": null,
+            "rek_formatted_title_xsdmf_id": null,
+            "rek_formatted_title": null,
+            "rek_formatted_abstract_xsdmf_id": null,
+            "rek_formatted_abstract": "The Karl Langer collection consists of architectural drawings, financial records, correspondence, biographical and personal notes, articles, speeches, job photos, subject files, diaries, journal and newspaper cuttings. Buildings which drawings and files relate to include St. Peters Lutheran College, Brisbane; Ipswich Girls Grammar School; Ipswich Boys Grammar School; Lennons Hotel, Brisbane and Lennons Hotel, Broadbeach.<br\/><br\/>Karl Langer graduated in Vienna as an architect from the master class of Peter Behrens at the Akademie der Bildenden K\u00fcnste in Wien (Vienna Academy of Fine Arts). He graduated in 1928 and was employed by Behrens to head his architectural atelier in Vienna. Langer went on to receive qualification of zivilarchitekt (1931) from Technische Hochschule (later Technical University of Vienna and at the University of Vienna for his doctorate in art history (1933). He migrated to Australia in 1939 with his wife Gertrude (n\u00e9e Fr\u00f6schel) Langer, and settled in Brisbane. He became a lecturer at the University of Queensland, and later also at what is now called the Queensland University of Technology. He was fellow of the Royal Australian Institute of Architects and of the Australian Planning Institute. He was also the first president of the Institute of Landscape Architecture, and an executive member of the National Trust. He designed many of Queensland's best-known buildings, including the chapel at St. Peter's Lutheran College in Brisbane and Lennons Hotel (1956) at Broadbeach, Gold Coast.",
+            "rek_depositor_affiliation_xsdmf_id": null,
+            "rek_depositor_affiliation": null,
+            "rek_thomson_citation_count": null,
+            "rek_thomson_citation_count_xsdmf_id": null,
+            "rek_subtype_xsdmf_id": null,
+            "rek_subtype": null,
+            "rek_scopus_citation_count": null,
+            "rek_herdc_notes_xsdmf_id": null,
+            "rek_herdc_notes": null,
+            "rek_scopus_doc_type_xsdmf_id": null,
+            "rek_scopus_doc_type": null,
+            "rek_wok_doc_type_xsdmf_id": null,
+            "rek_wok_doc_type": null,
+            "rek_pubmed_doc_type_xsdmf_id": null,
+            "rek_pubmed_doc_type": null,
+            "rek_security_inherited": 1,
+            "rek_altmetric_score": null,
+            "rek_altmetric_score_xsdmf_id": null,
+            "rek_altmetric_id": null,
+            "rek_altmetric_id_xsdmf_id": null,
+            "rek_copyright_xsdmf_id": null,
+            "rek_copyright": null,
+            "rek_security_policy": 1,
+            "rek_datastream_policy": null,
+            "fez_record_search_key_advisory_statement": null,
+            "fez_record_search_key_acknowledgements": {
+                "rek_acknowledgements_id": 31572,
+                "rek_acknowledgements_pid": "UQ:00222bb",
+                "rek_acknowledgements_xsdmf_id": null,
+                "rek_acknowledgements": "Digitisation made possible from donations to the UQ Library."
+            },
+            "fez_record_search_key_additional_notes": null,
+            "fez_record_search_key_article_number": null,
+            "fez_record_search_key_assigned_group_id": [],
+            "fez_record_search_key_assigned_user_id": [],
+            "fez_record_search_key_author": [{
+                "rek_author_id": 32381260,
+                "rek_author_pid": "UQ:00222bb",
+                "rek_author_xsdmf_id": null,
+                "rek_author": "Trebilcock, Ivan",
+                "rek_author_order": 1
+            }],
+            "fez_record_search_key_author_affiliation_country": [],
+            "fez_record_search_key_author_affiliation_full_address": [],
+            "fez_record_search_key_author_affiliation_id": [],
+            "fez_record_search_key_author_affiliation_name": [],
+            "fez_record_search_key_author_affiliation_type": [],
+            "fez_record_search_key_author_id": [{
+                "rek_author_id_id": 32460843,
+                "rek_author_id_pid": "UQ:00222bb",
+                "rek_author_id_xsdmf_id": null,
+                "rek_author_id": 0,
+                "rek_author_id_order": 1
+            }],
+            "fez_record_search_key_content_indicator": [],
+            "fez_record_search_key_contributor": [{
+                "rek_contributor_id": 3237402,
+                "rek_contributor_pid": "UQ:00222bb",
+                "rek_contributor_xsdmf_id": null,
+                "rek_contributor": "Langer, Karl, 1903-1969",
+                "rek_contributor_order": 1
+            }],
+            "fez_record_search_key_contributor_id": [],
+            "fez_record_search_key_corresponding_country": [],
+            "fez_record_search_key_corresponding_email": [],
+            "fez_record_search_key_corresponding_name": [],
+            "fez_record_search_key_corresponding_organisation": [],
+            "fez_record_search_key_creator_contribution_statement": [],
+            "fez_record_search_key_creator_id": [],
+            "fez_record_search_key_creator_name": [],
+            "fez_record_search_key_datastream_policy": null,
+            "fez_record_search_key_date_available": {
+                "rek_date_available_id": 1090813,
+                "rek_date_available_pid": "UQ:00222bb",
+                "rek_date_available_xsdmf_id": null,
+                "rek_date_available": "1960-01-01T00:00:00Z"
+            },
+            "fez_record_search_key_doi": null,
+            "fez_record_search_key_end_page": null,
+            "fez_record_search_key_file_attachment_access_condition": [],
+            "fez_record_search_key_file_attachment_embargo_date": [],
+            "fez_record_search_key_file_attachment_name": [{
+                "rek_file_attachment_name_id": 4568612,
+                "rek_file_attachment_name_pid": "UQ:00222bb",
+                "rek_file_attachment_name_xsdmf_id": null,
+                "rek_file_attachment_name": "UQFL158_1143_3_07.tif",
+                "rek_file_attachment_name_order": 1
+            }, {
+                "rek_file_attachment_name_id": 4569410,
+                "rek_file_attachment_name_pid": "UQ:00222bb",
+                "rek_file_attachment_name_xsdmf_id": null,
+                "rek_file_attachment_name": "thumbnail_UQFL158_1143_3_07_t.jpg",
+                "rek_file_attachment_name_order": 2
+            }, {
+                "rek_file_attachment_name_id": 4569416,
+                "rek_file_attachment_name_pid": "UQ:00222bb",
+                "rek_file_attachment_name_xsdmf_id": null,
+                "rek_file_attachment_name": "web_UQFL158_1143_3_07_t.jpg",
+                "rek_file_attachment_name_order": 3
+            }, {
+                "rek_file_attachment_name_id": 4569445,
+                "rek_file_attachment_name_pid": "UQ:00222bb",
+                "rek_file_attachment_name_xsdmf_id": null,
+                "rek_file_attachment_name": "preview_UQFL158_1143_3_07_t.jpg",
+                "rek_file_attachment_name_order": 4
+            }],
+            "fez_record_search_key_geographic_area": [{
+                "rek_geographic_area_id": 30043,
+                "rek_geographic_area_pid": "UQ:00222bb",
+                "rek_geographic_area_xsdmf_id": null,
+                "rek_geographic_area": "153.050925,-27.396682",
+                "rek_geographic_area_order": 1
+            }],
+            "fez_record_search_key_grant_acronym": [],
+            "fez_record_search_key_grant_agency": [],
+            "fez_record_search_key_grant_agency_id": [],
+            "fez_record_search_key_grant_agency_type": [],
+            "fez_record_search_key_grant_id": [],
+            "fez_record_search_key_grant_text": [],
+            "fez_record_search_key_grant_type": [],
+            "fez_record_search_key_herdc_code": null,
+            "fez_record_search_key_herdc_status": null,
+            "fez_record_search_key_institutional_status": null,
+            "fez_record_search_key_isderivationof": [],
+            "fez_record_search_key_ismemberof": [{
+                "rek_ismemberof_id": 12487712,
+                "rek_ismemberof_pid": "UQ:00222bb",
+                "rek_ismemberof_xsdmf_id": null,
+                "rek_ismemberof": "UQ:734699",
+                "rek_ismemberof_order": 1,
+                "parent": {
+                    "rek_pid": "UQ:734699",
+                    "rek_security_policy": 5,
+                    "rek_datastream_policy": 5
+                },
+                "rek_ismemberof_lookup": "Dulcie Stewart - Batch Import Testing -\u00a0Open access"
+            }],
+            "fez_record_search_key_job_number": {
+                "rek_job_number_id": 614,
+                "rek_job_number_pid": "UQ:00222bb",
+                "rek_job_number_xsdmf_id": null,
+                "rek_job_number": "UQFL158, job no. 1127-2"
+            },
+            "fez_record_search_key_keywords": [{
+                "rek_keywords_id": 30926751,
+                "rek_keywords_pid": "UQ:00222bb",
+                "rek_keywords_xsdmf_id": null,
+                "rek_keywords": "Langer, Karl, 1903-1969",
+                "rek_keywords_order": 1
+            }, {
+                "rek_keywords_id": 30926752,
+                "rek_keywords_pid": "UQ:00222bb",
+                "rek_keywords_xsdmf_id": null,
+                "rek_keywords": "Architectural drawings",
+                "rek_keywords_order": 2
+            }, {
+                "rek_keywords_id": 30926753,
+                "rek_keywords_pid": "UQ:00222bb",
+                "rek_keywords_xsdmf_id": null,
+                "rek_keywords": "Architecture -- Queensland -- Design and plans",
+                "rek_keywords_order": 3
+            }, {
+                "rek_keywords_id": 30926754,
+                "rek_keywords_pid": "UQ:00222bb",
+                "rek_keywords_xsdmf_id": null,
+                "rek_keywords": "Architecture, domestic -- Queensland -- Designs and plans",
+                "rek_keywords_order": 4
+            }, {
+                "rek_keywords_id": 30926755,
+                "rek_keywords_pid": "UQ:00222bb",
+                "rek_keywords_xsdmf_id": null,
+                "rek_keywords": "Apartment houses -- Designs and plans",
+                "rek_keywords_order": 5
+            }, {
+                "rek_keywords_id": 30926756,
+                "rek_keywords_pid": "UQ:00222bb",
+                "rek_keywords_xsdmf_id": null,
+                "rek_keywords": "Wavell Heights (Qld.)",
+                "rek_keywords_order": 6
+            }, {
+                "rek_keywords_id": 30926757,
+                "rek_keywords_pid": "UQ:00222bb",
+                "rek_keywords_xsdmf_id": null,
+                "rek_keywords": "Rode Road (Wavell Heights Qld.)",
+                "rek_keywords_order": 7
+            }, {
+                "rek_keywords_id": 30926758,
+                "rek_keywords_pid": "UQ:00222bb",
+                "rek_keywords_xsdmf_id": null,
+                "rek_keywords": "Franchin family",
+                "rek_keywords_order": 8
+            }, {
+                "rek_keywords_id": 30926759,
+                "rek_keywords_pid": "UQ:00222bb",
+                "rek_keywords_xsdmf_id": null,
+                "rek_keywords": "Blueprints",
+                "rek_keywords_order": 9
+            }],
+            "fez_record_search_key_language": [],
+            "fez_record_search_key_license": null,
+            "fez_record_search_key_link": [],
+            "fez_record_search_key_link_description": [],
+            "fez_record_search_key_location": [{
+                "rek_location_id": 80256,
+                "rek_location_pid": "UQ:00222bb",
+                "rek_location_xsdmf_id": null,
+                "rek_location": "126 Rode Road, Wavell Heights, Brisbane",
+                "rek_location_order": 1
+            }],
+            "fez_record_search_key_notes": {
+                "rek_notes_id": 1190622,
+                "rek_notes_pid": "UQ:00222bb",
+                "rek_notes_xsdmf_id": null,
+                "rek_notes": "One of two copies of job no. 1127-3, original held in collection (job no. 1127-2, item 5). This copy is blueprint, the other copy is hand coloured. <br\/>Drawings in collection arranged by architect\u2019s numbering system and individual architectural drawing item numbers assigned by library staff.<br\/>Job number written on plan: 1127-3."
+            },
+            "fez_record_search_key_oa_status": {
+                "rek_oa_status_id": 738878,
+                "rek_oa_status_pid": "UQ:00222bb",
+                "rek_oa_status_xsdmf_id": null,
+                "rek_oa_status": 453695,
+                "rek_oa_status_lookup": "File (Publisher version)"
+            },
+            "fez_record_search_key_original_format": {
+                "rek_original_format_id": 30523,
+                "rek_original_format_pid": "UQ:00222bb",
+                "rek_original_format_xsdmf_id": null,
+                "rek_original_format": "Architectural drawing; blueprint; 49 x 73 cm."
+            },
+            "fez_record_search_key_place_of_publication": null,
+            "fez_record_search_key_possible_author_id": [],
+            "fez_record_search_key_project_name": null,
+            "fez_record_search_key_project_description": null,
+            "fez_record_search_key_project_start_date": null,
+            "fez_record_search_key_publisher": null,
+            "fez_record_search_key_quality_indicator": [],
+            "fez_record_search_key_refereed": null,
+            "fez_record_search_key_refereed_source": null,
+            "fez_record_search_key_rights": {
+                "rek_rights_id": 134885,
+                "rek_rights_pid": "UQ:00222bb",
+                "rek_rights_xsdmf_id": null,
+                "rek_rights": "This work is made available by the UQ Library for your personal research or study. For further information and access please contact the Fryer Library, The University of Queensland Library."
+            },
+            "fez_record_search_key_scale": {
+                "rek_scale_id": 566,
+                "rek_scale_pid": "UQ:00222bb",
+                "rek_scale_xsdmf_id": null,
+                "rek_scale": "1\" To 8'"
+            },
+            "fez_record_search_key_scopus_id": null,
+            "fez_record_search_key_series": {
+                "rek_series_id": 229011,
+                "rek_series_pid": "UQ:00222bb",
+                "rek_series_xsdmf_id": null,
+                "rek_series": "Karl Langer Collection, UQFL158"
+            },
+            "fez_record_search_key_significance": [],
+            "fez_record_search_key_source": {
+                "rek_source_id": 80422,
+                "rek_source_pid": "UQ:00222bb",
+                "rek_source_xsdmf_id": null,
+                "rek_source": "Karl Langer Collection, UQFL158, job no. 1127-2, item 7"
+            },
+            "fez_record_search_key_start_page": null,
+            "fez_record_search_key_subject": [],
+            "fez_record_search_key_total_pages": null,
+            "fez_record_search_key_translated_title": null,
+            "fez_record_search_key_issn": [],
+            "fez_record_search_key_isi_loc": null,
+            "fez_datastream_info": [{
+                "dsi_pid": "UQ:00222bb",
+                "dsi_dsid": "preview_UQFL158_1143_3_07_t.jpg",
+                "dsi_embargo_date": null,
+                "dsi_open_access": null,
+                "dsi_label": null,
+                "dsi_mimetype": "image\/jpeg",
+                "dsi_copyright": null,
+                "dsi_state": "A",
+                "dsi_size": 9416,
+                "dsi_security_policy": 1,
+                "dsi_security_inherited": 0
+            }, {
+                "dsi_pid": "UQ:00222bb",
+                "dsi_dsid": "thumbnail_UQFL158_1143_3_07_t.jpg",
+                "dsi_embargo_date": null,
+                "dsi_open_access": null,
+                "dsi_label": null,
+                "dsi_mimetype": "image\/jpeg",
+                "dsi_copyright": null,
+                "dsi_state": "A",
+                "dsi_size": 1555,
+                "dsi_security_policy": 1,
+                "dsi_security_inherited": 0
+            }, {
+                "dsi_pid": "UQ:00222bb",
+                "dsi_dsid": "UQFL158_1143_3_07.tif",
+                "dsi_embargo_date": null,
+                "dsi_open_access": null,
+                "dsi_label": null,
+                "dsi_mimetype": "image\/tiff",
+                "dsi_copyright": null,
+                "dsi_state": "A",
+                "dsi_size": 810004992,
+                "dsi_security_policy": 5,
+                "dsi_security_inherited": 1
+            }, {
+                "dsi_pid": "UQ:00222bb",
+                "dsi_dsid": "web_UQFL158_1143_3_07_t.jpg",
+                "dsi_embargo_date": null,
+                "dsi_open_access": null,
+                "dsi_label": null,
+                "dsi_mimetype": "image\/jpeg",
+                "dsi_copyright": null,
+                "dsi_state": "A",
+                "dsi_size": 45282,
+                "dsi_security_policy": 1,
+                "dsi_security_inherited": 0
+            }],
+            "fez_matched_journals": [],
+            "rek_display_type_lookup": "Design",
+            "rek_pubmed_doc_type_lookup": null,
+            "rek_object_type_lookup": "Record",
+            "rek_scopus_doc_type_lookup": null,
+            "rek_status_lookup": "Published",
+            "rek_wok_doc_type_lookup": null,
+            "fez_record_search_key_security_policy": [{
+                "rek_security_policy_id": 385210,
+                "rek_security_policy_pid": "UQ:00222bb",
+                "rek_security_policy": 5,
+                "rek_security_policy_order": 1
+            }]
+        }
     ],
 }
