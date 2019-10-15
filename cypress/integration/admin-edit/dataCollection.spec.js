4eb7516d6fcc6151a894b1bbb82d840562daa830
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-10-15 16:39:33 +1000

    [#168828096] Stub for Data collection admin edit E2E tests

---
+++ cypress/integration/admin-edit/dataCollection.spec.js
@@ -0,0 +1,38 @@
+import { default as recordList } from '../../../src/mock/data/records/publicationTypeListDataCollection';
+
+context('Book chapter admin edit', () => {
+    const record = recordList.data[0];
+
+    beforeEach(() => {
+        cy.visit(`/admin/edit/${record.rek_pid}?user=uqstaff`);
+        cy.closeUnsupported();
+        cy.wait(1000);
+    });
+
+    afterEach(() => {
+        cy.window()
+            .then(win => (win.onbeforeunload = undefined));
+    });
+
+    it('should load expected tabs', () => {
+        cy.get('.StandardPage form > div > div > div.StandardCard > div > div > h3')
+            .as('cards')
+            .should('have.length', 8);
+
+        cy.get('input[value=tabbed]')
+            .click()
+            .should('be.checked');
+
+        cy.get('@cards')
+            .should('have.length', 1)
+            .should('have.text', 'Bibliographic');
+    });
+
+    it('should render Bibliographic tab', () => {
+        cy.get('.StandardPage form > div > div:nth-child(3)')
+            .within(() => {
+                cy.get('h3')
+                    .should('have.text', 'Dataset name');
+            });
+    });
+});
