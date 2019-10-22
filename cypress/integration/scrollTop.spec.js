8c2cc374bb5278a9ea017e7fee48e686cd712795
Ky Lane <k.lane@uq.edu.au>
2019-10-22 10:42:12 +1000

    [#169195942] Cypress tests

--- cypress/integration/scrollTop.spec.js
+++ cypress/integration/scrollTop.spec.js
@@ -10,15 +10,18 @@ context('Scroll to top module', () => {
 
     it('Scroll to the bottom of the homepage, then click the scroll to top and the tabs should be visible', () => {
         // Have to work out how to return the current Y scroll value for a div container
-        cy.get('h3')
-            .contains('What is eSpace?')
-            .should('be.visible');
+        cy.get('button#scrolltopbtn')
+            .should('have.css', 'opacity', '0');
         cy.wait(1000);
         cy.get('#content-container')
             .scrollTo('bottom');
+        cy.get('button#scrolltopbtn')
+            .should('have.css', 'opacity', '0.5');
         cy.wait(1000);
         cy.get('button#scrolltopbtn')
             .click();
         cy.wait(1000);
+        cy.get('button#scrolltopbtn')
+            .should('have.css', 'opacity', '0');
     });
 });
