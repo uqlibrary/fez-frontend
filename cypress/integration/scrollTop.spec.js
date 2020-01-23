cf13367a95b0ac5f06dc9edc5308bc8f5ba1e5c6
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2020-01-23 18:12:10 +1000

    Removing wait()s

--- cypress/integration/scrollTop.spec.js
+++ cypress/integration/scrollTop.spec.js
@@ -8,19 +8,19 @@ context('Scroll to top module', () => {
     });
 
     it('Scroll to the bottom of the homepage, then click the scroll to top and the tabs should be visible', () => {
-        // Have to work out how to return the current Y scroll value for a div container
-        cy.get('button#scrolltopbtn')
+        cy.contains('Long-Term Outcomes')
+            .should('exist');
+        cy.get('.StandardPage')
+            .should('exist'); // This forces a scroll to the top.
+        cy.get('#scrolltopbtn')
             .should('have.css', 'opacity', '0');
-        cy.wait(1000);
-        cy.get('#content-container')
-            .scrollTo('bottom');
-        cy.get('button#scrolltopbtn')
-            .should('have.css', 'opacity', '0.5');
-        cy.wait(1000);
-        cy.get('button#scrolltopbtn')
+        cy.contains('Genome-wide association')
+            .scrollIntoView();
+        cy.get('#scrolltopbtn')
+            .should('have.css', 'opacity', '0.9');
+        cy.get('#scrolltopbtn')
             .click();
-        cy.wait(1000);
-        cy.get('button#scrolltopbtn')
+        cy.get('#scrolltopbtn')
             .should('have.css', 'opacity', '0');
     });
 });
