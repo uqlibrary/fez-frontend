de9cae48f6004ef962bdc2efee3ccd5bae306623
Lea de Groot <l.degroot@library.uq.edu.au>
2020-02-03 10:03:24 +1000

    refactor herdc code selector [PT #168744466]

--- src/modules/SharedComponents/Toolbox/HerdcCodeField/HerdcCodeField.js
+++ src/modules/SharedComponents/Toolbox/HerdcCodeField/HerdcCodeField.js
@@ -3,33 +3,29 @@ import { GenericSelectField } from 'modules/SharedComponents/GenericSelectField'
 import { DEPRECATED_HERDC_CODES, HERDC_CODES } from 'config/general';
 import Immutable from 'immutable';
 
+/**
+ * Supply the list of herdc codes to display as options in the drop down.
+ * If the input value is not in the current list, but is in the deprecated list, add it to the list to display.
+ * @param input - the herdc code to be preselected in the list
+ * @returns []
+ * @private
+ */
+const _herdcCodes = input => {
+    const herdcCodes = HERDC_CODES;
+    if (!HERDC_CODES.find(herdc => herdc.value === input)) {
+        const foundDeprecated = DEPRECATED_HERDC_CODES.filter(obj => {
+            // just comparing obj.value === input works in tests but not in actual form :(
+            return obj.value.toString(10) === input.toString(10);
+        });
+        if (foundDeprecated.length > 0) {
+            herdcCodes.push(foundDeprecated[0]);
+        }
+    }
+    return herdcCodes;
+};
+
 export default function HerdcCodeField(fieldProps) {
     const preselected = !!fieldProps.input && fieldProps.input.value;
-
-    /**
-     * Supply the list of herdc codes to display as options in the drop down.
-     * If the input value is not in the current list, but is in the deprecated list, add it to the list to display.
-     * @param input - the herdc code to be preselected in the list
-     * @returns []
-     * @private
-     */
-    const _herdcCodes = input => {
-        const herdcCodes = HERDC_CODES;
-        const herdcList = [];
-        herdcCodes.forEach(obj => {
-            herdcList[obj.value] = obj.text;
-        });
-        /* istanbul ignore else */
-        if (herdcList.indexOf(input) === -1) {
-            const foundDeprecated = DEPRECATED_HERDC_CODES.filter(obj => {
-                return obj.value.toString(10) === input.toString(10);
-            });
-            if (foundDeprecated.length > 0) {
-                herdcCodes.push(foundDeprecated[0]);
-            }
-        }
-        return herdcCodes;
-    };
 
     return (
         <GenericSelectField
