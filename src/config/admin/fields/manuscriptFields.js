fc11ff5731f435b06eb053749600377d660f8887
Vishal Asai <asai_vishal@yahoo.com>
2020-02-13 16:54:39 +1000

    [#171175362] removed subtypes from manuscript and seminar paper

--- src/config/admin/fields/manuscriptFields.js
+++ src/config/admin/fields/manuscriptFields.js
@@ -55,7 +55,7 @@ export default {
         },
         {
             title: 'Additional information',
-            groups: [['rek_subtype'], ['fez_record_search_key_oa_status']],
+            groups: [['fez_record_search_key_oa_status']],
         },
     ],
     ntro: () => [],
