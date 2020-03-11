397195cc83faab3ec699b98002bd0718133b147e
Vishal <asai_vishal@yahoo.com>
2020-03-11 12:36:57 +1000

    [#171225238] Updating OrgNameField with new auto complete async field

--- src/modules/SharedComponents/LookupFields/containers/OrgNameField.js
+++ src/modules/SharedComponents/LookupFields/containers/OrgNameField.js
@@ -1,4 +1,4 @@
-import { AutoCompleteAsyncField } from 'modules/SharedComponents/Toolbox/AutoSuggestField';
+import { AutoCompleteAsynchronousField } from 'modules/SharedComponents/Toolbox/AutoSuggestField';
 import { connect } from 'react-redux';
 import * as actions from 'actions';
 
@@ -11,31 +11,35 @@ const mapStateToProps = (state, props) => {
                 ? state.get('searchKeysReducer')[category].itemsList
                 : [],
         allowFreeText: true,
-        async: true,
-        errorText: (!!props.meta && props.meta.error) || (props.error && !!props.errorText && props.errorText) || null,
-        error: props.meta ? !!props.meta.error : (props.error && !!props.errorText) || null,
-        selectedValue:
-            (!!props.input && !!props.input.value && { value: props.input.value }) ||
-            (!!props.value && { value: props.value }) ||
-            '',
-        itemToString: item => (!!item && String(item.value)) || '',
+        getOptionLabel: item => (!!item && String(item.value)) || '',
+        ...(!!((props || {}).meta || {}).form // If form key is set in props.meta object then it's a redux-form Field
+            ? {
+                defaultValue: (!!props.input.value && { value: props.input.value }) || null,
+                error: !!props.meta.error,
+                errorText: props.meta.error || '',
+            }
+            : {
+                defaultValue: (!!props.value && { value: props.value }) || '',
+                error: props.error,
+                errorText: props.errorText || '',
+            }),
     };
 };
 
 const mapDispatchToProps = (dispatch, props) => ({
     loadSuggestions: (searchKey, searchQuery = ' ') => dispatch(actions.loadSearchKeyList(searchKey, searchQuery)),
-    onChange: value => {
-        if (!!props.input) {
-            props.input.onChange(value.value);
-        } else if (typeof value === 'string') {
-            props.onChange({ value });
-        } else {
-            props.onChange(value);
+    ...(!!((props || {}).meta || {}).form // If form key is set in props.meta object then it's a redux-form Field
+        ? {
+            onChange: item => props.input.onChange(item),
+            onClear: () => props.input.onChange(null),
         }
-    },
+        : {
+            onChange: item => props.onChange(item),
+            onClear: () => props.onChange(null),
+        }),
 });
 
 export const OrgNameField = connect(
     mapStateToProps,
     mapDispatchToProps,
-)(AutoCompleteAsyncField);
+)(AutoCompleteAsynchronousField);
