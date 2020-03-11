40932ee21c34d22ff958c3bc11ef837ce641b620
Vishal <asai_vishal@yahoo.com>
2020-03-11 11:39:20 +1000

    [#171225238] updated JournalNameField to use new auto complete async component

--- src/modules/SharedComponents/LookupFields/containers/JournalNameField.js
+++ src/modules/SharedComponents/LookupFields/containers/JournalNameField.js
@@ -1,30 +1,30 @@
-import { AutoCompleteAsyncField } from 'modules/SharedComponents/Toolbox/AutoSuggestField';
+import { AutoCompleteAsynchronousField } from 'modules/SharedComponents/Toolbox/AutoSuggestField';
 import { connect } from 'react-redux';
 import * as actions from 'actions';
+import matchSorter from 'match-sorter';
 
 const mapStateToProps = (state, props) => {
     const category = 'journal_name';
     return {
-        category: category,
         itemsList:
             state.get('searchKeysReducer') && state.get('searchKeysReducer')[category]
                 ? state.get('searchKeysReducer')[category].itemsList
                 : [],
         allowFreeText: true,
-        onChange: item => props.input.onChange(item.value),
-        async: true,
         errorText: props.meta ? props.meta.error : null,
         error: props.meta ? !!props.meta.error : null,
-        selectedValue: !!props.input && !!props.input.value ? { value: props.input.value } : null,
-        itemToString: item => (!!item && String(item.value)) || '',
+        getOptionLabel: item => (!!item && String(item.value)) || '',
+        filterOptions: (options, { inputValue }) => matchSorter(options, inputValue, { keys: ['value'] }),
     };
 };
 
-const mapDispatchToProps = dispatch => ({
-    loadSuggestions: (searchKey, searchQuery = ' ') => dispatch(actions.loadSearchKeyList(searchKey, searchQuery)),
+const mapDispatchToProps = (dispatch, props) => ({
+    loadSuggestions: (searchQuery = ' ') => dispatch(actions.loadSearchKeyList('journal_name', searchQuery)),
+    onChange: item => props.input.onChange(item.value),
+    onClear: () => props.input.onChange(null),
 });
 
 export const JournalNameField = connect(
     mapStateToProps,
     mapDispatchToProps,
-)(AutoCompleteAsyncField);
+)(AutoCompleteAsynchronousField);
