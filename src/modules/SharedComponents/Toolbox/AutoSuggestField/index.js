8a48c5aedccb6d595aa92320174b8126dba12332
Vishal <asai_vishal@yahoo.com>
2020-03-16 14:39:40 +1000

    [#171225238] removing reduntant components

--- src/modules/SharedComponents/Toolbox/AutoSuggestField/index.js
+++ src/modules/SharedComponents/Toolbox/AutoSuggestField/index.js
@@ -1,4 +1,3 @@
-export { default as AutoCompleteAsyncField } from './components/AutoCompleteAsyncField';
 export { default as AutoCompleteAsynchronousField } from './components/AutoCompleteAsynchronousField';
 export { default as AutoCompleteMultiSelectField } from './components/AutoCompleteMultiSelectField';
 export { default as AutoCompleteSelectField } from './components/AutoCompleteSelectField';
