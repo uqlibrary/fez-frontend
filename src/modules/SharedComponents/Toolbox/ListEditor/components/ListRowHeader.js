368e8a39746c7ce614d4c158a5b54f38cbd20d1a
Vishal <asai_vishal@yahoo.com>
2020-03-20 13:37:04 +1000

    [#171225238] updated listrowheader and tests

--- src/modules/SharedComponents/Toolbox/ListEditor/components/ListRowHeader.js
+++ src/modules/SharedComponents/Toolbox/ListEditor/components/ListRowHeader.js
@@ -1,80 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { ConfirmDialogBox } from '../../ConfirmDialogBox';
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 import DeleteForever from '@material-ui/icons/DeleteForever';
-import { withStyles } from '@material-ui/core/styles';
+import { ConfirmationBox, useConfirmationState } from 'modules/SharedComponents/Toolbox/ConfirmDialogBox';
 
-export class ListRowHeader extends Component {
-    static propTypes = {
-        onDeleteAll: PropTypes.func.isRequired,
-        locale: PropTypes.object,
-        disabled: PropTypes.bool,
-        hideReorder: PropTypes.bool,
-        classes: PropTypes.object,
-    };
-
-    static defaultProps = {
-        locale: {
-            nameColumn: 'Name',
-            reorderColumn: 'Reorder items',
-            deleteAll: 'Remove all items',
-            deleteAllConfirmation: {
-                confirmationTitle: 'Delete all',
-                confirmationMessage: 'Are you sure you want to delete all items?',
-                cancelButtonLabel: 'No',
-                confirmButtonLabel: 'Yes',
-            },
-        },
-    };
-
-    constructor(props) {
-        super(props);
-    }
-
-    showConfirmation = () => {
-        this.confirmationBox.showConfirmation();
-    };
-
-    setConfirmationRef = ref => (this.confirmationBox = ref);
-
-    render() {
-        const { nameColumn, reorderColumn, deleteAll, deleteAllConfirmation } = this.props.locale;
-        const { disabled, hideReorder, classes } = this.props;
-
-        return (
-            <div style={{ flexGrow: 1, padding: 8 }}>
-                <ConfirmDialogBox
-                    onRef={this.setConfirmationRef}
-                    onAction={this.props.onDeleteAll}
-                    locale={deleteAllConfirmation}
-                />
-                <Grid container alignItems="center" spacing={2} className={classes.header}>
-                    <Grid item xs={hideReorder ? 10 : 5} sm={hideReorder ? 11 : 6}>
-                        <Typography variant="caption">{nameColumn}</Typography>
-                    </Grid>
-                    {!hideReorder && (
-                        <Grid item xs={5} sm={5} className={classes.right}>
-                            <Typography variant="caption">{reorderColumn}</Typography>
-                        </Grid>
-                    )}
-                    <Grid item xs={2} sm={1} className={classes.center}>
-                        <Tooltip title={deleteAll}>
-                            <IconButton onClick={this.showConfirmation} disabled={disabled}>
-                                <DeleteForever />
-                            </IconButton>
-                        </Tooltip>
-                    </Grid>
-                </Grid>
-            </div>
-        );
-    }
-}
-
-const styles = () => ({
+const useStyles = makeStyles(() => ({
     right: {
         textAlign: 'right',
     },
@@ -84,6 +18,61 @@ const styles = () => ({
     header: {
         borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
     },
-});
+}));
 
-export default withStyles(styles)(ListRowHeader);
+export const ListRowHeader = ({ onDeleteAll, locale, disabled, hideReorder }) => {
+    const classes = useStyles();
+    const [isOpen, showConfirmation, hideConfirmation] = useConfirmationState();
+    const { nameColumn, reorderColumn, deleteAll, deleteAllConfirmation } = locale;
+
+    return (
+        <div style={{ flexGrow: 1, padding: 8 }}>
+            <ConfirmationBox
+                onAction={onDeleteAll}
+                onClose={hideConfirmation}
+                isOpen={isOpen}
+                locale={deleteAllConfirmation}
+            />
+            <Grid container alignItems="center" spacing={2} className={classes.header}>
+                <Grid item xs={hideReorder ? 10 : 5} sm={hideReorder ? 11 : 6}>
+                    <Typography variant="caption">{nameColumn}</Typography>
+                </Grid>
+                {!hideReorder && (
+                    <Grid item xs={5} sm={5} className={classes.right}>
+                        <Typography variant="caption">{reorderColumn}</Typography>
+                    </Grid>
+                )}
+                <Grid item xs={2} sm={1} className={classes.center}>
+                    <Tooltip title={deleteAll}>
+                        <IconButton onClick={showConfirmation} disabled={disabled} id="delete-all-items">
+                            <DeleteForever />
+                        </IconButton>
+                    </Tooltip>
+                </Grid>
+            </Grid>
+        </div>
+    );
+};
+
+ListRowHeader.propTypes = {
+    onDeleteAll: PropTypes.func.isRequired,
+    locale: PropTypes.object,
+    disabled: PropTypes.bool,
+    hideReorder: PropTypes.bool,
+};
+
+ListRowHeader.defaultProps = {
+    locale: {
+        nameColumn: 'Name',
+        reorderColumn: 'Reorder items',
+        deleteAll: 'Remove all items',
+        deleteAllConfirmation: {
+            confirmationTitle: 'Delete all',
+            confirmationMessage: 'Are you sure you want to delete all items?',
+            cancelButtonLabel: 'No',
+            confirmButtonLabel: 'Yes',
+        },
+    },
+};
+
+export default ListRowHeader;
