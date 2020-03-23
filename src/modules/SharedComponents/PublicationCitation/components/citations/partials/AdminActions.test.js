66dbc128f6bab129bdd6190e78ac61a4412cb4b5
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2020-03-23 13:59:13 +1000

    Update testing libs, update tests.
    - The signature of `fireEvent()` has changed.
    - `waitForElement()` is deprecated.

--- src/modules/SharedComponents/PublicationCitation/components/citations/partials/AdminActions.test.js
+++ src/modules/SharedComponents/PublicationCitation/components/citations/partials/AdminActions.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AdminActions, { navigateToUrl } from './AdminActions';
-import { rtlRender, fireEvent, cleanup, waitForElement } from 'test-utils';
+import { rtlRender, fireEvent, cleanup } from 'test-utils';
 import { RECORD_ACTION_URLS as options } from 'config/general';
 import { APP_URL } from '../../../../../../config';
 
@@ -17,7 +17,7 @@ function setup(testProps = {}) {
 describe('AdminActions component', () => {
     afterEach(() => cleanup);
 
-    it('should handle admin actions menu', async() => {
+    it('should handle admin actions menu', () => {
         global.window.open = jest.fn((url, target) => {
             expect(url).toBe(
                 'https://fez-staging.library.uq.edu.au/admin/edit/UQ:111111?tab=security&navigatedFrom=test',
@@ -31,12 +31,12 @@ describe('AdminActions component', () => {
 
         fireEvent.click(getByTestId('admin-actions-button'));
 
-        const menu = await waitForElement(() => getByTestId('admin-actions-menu'));
+        const menu = getByTestId('admin-actions-menu');
 
         fireEvent.click(getByText(/edit security for selected record/i, menu));
     });
 
-    it('should handle admin actions in a new window ', async() => {
+    it('should handle admin actions in a new window ', () => {
         global.window.open = jest.fn((url, target) => {
             expect(url).toBe('https://fez-staging.library.uq.edu.au/admin/edit/UQ:111111');
             expect(target).toBe('_blank');
@@ -53,7 +53,7 @@ describe('AdminActions component', () => {
 
         fireEvent.click(getByTestId('admin-actions-button'));
 
-        const menu = await waitForElement(() => getByTestId('admin-actions-menu'));
+        const menu = getByTestId('admin-actions-menu');
 
         fireEvent.click(getByText(/edit selected record/i, menu));
     });
