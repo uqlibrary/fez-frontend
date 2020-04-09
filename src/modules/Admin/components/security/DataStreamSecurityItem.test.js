c58f26361b094997df6e6d2c083334d5e2600b1d
Vishal <asai_vishal@yahoo.com>
2020-04-09 10:58:06 +1000

    [#171225238] Fixed DataStreamSecurityItem tests

--- src/modules/Admin/components/security/DataStreamSecurityItem.test.js
+++ src/modules/Admin/components/security/DataStreamSecurityItem.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import DataStreamSecurityItem, { isSame } from './DataStreamSecurityItem';
-import { rtlRender, fireEvent, cleanup } from 'test-utils';
+import { rtlRender, fireEvent, cleanup, waitForElement } from 'test-utils';
 
 function setup(testProps = {}) {
     const props = {
@@ -46,8 +46,8 @@ describe('DataStreamSecurityItem component', () => {
         expect(asFragment()).toMatchSnapshot();
     });
 
-    it('should change security value for the file', () => {
-        const { asFragment, getByText, getByTestId } = setup({
+    it('should change security value for the file', async() => {
+        const { getByText, getByTestId } = setup({
             dataStream: {
                 dsi_dsid: 'test.jpg',
                 dsi_security_policy: 1,
@@ -55,17 +55,17 @@ describe('DataStreamSecurityItem component', () => {
             },
         });
 
-        let fragment = asFragment();
-        fireEvent.click(getByText(/Administrator/i));
-        expect(fragment).toMatchDiffSnapshot((fragment = asFragment()));
-        const menu = getByTestId('menu-test.jpg');
+        fireEvent.mouseDown(getByText(/Administrator/i));
 
-        fireEvent.click(getByText(/public/i, menu));
-        expect(fragment).toMatchDiffSnapshot(asFragment());
+        const menu = await waitForElement(() => getByTestId('menu-test.jpg'));
+
+        fireEvent.mouseDown(getByText(/public/i, menu));
+
+        expect(getByText('Public')).toBeInTheDocument();
     });
 
-    it('should change security value for the file back to inherited', () => {
-        const { asFragment, getByText, getByTestId } = setup({
+    it('should change security value for the file back to inherited', async() => {
+        const { getByText, getByTestId } = setup({
             dataStream: {
                 dsi_dsid: 'test.jpg',
                 dsi_security_policy: 1,
@@ -73,13 +73,13 @@ describe('DataStreamSecurityItem component', () => {
             },
         });
 
-        let fragment = asFragment();
-        fireEvent.click(getByText(/administrator/i));
-        expect(fragment).toMatchDiffSnapshot((fragment = asFragment()));
-        const menu = getByTestId('menu-test.jpg');
+        fireEvent.mouseDown(getByText(/administrator/i));
 
-        fireEvent.click(getByText(/Theses Assessors/i, menu));
-        expect(fragment).toMatchDiffSnapshot(asFragment());
+        const menu = await waitForElement(() => getByTestId('menu-test.jpg'));
+
+        fireEvent.mouseDown(getByText(/Theses Assessors/i, menu));
+
+        expect(getByText('Theses Assessors')).toBeInTheDocument();
     });
 
     describe('isSame callback function', () => {
