dc1e9aa16176795d4404d77eec58cac3ce9ee392
Vishal Asai <asai_vishal@yahoo.com>
2020-05-15 14:14:59 +1000

    [#172595221] bring back the changes object

--- src/modules/Admin/submitHandler/index.js
+++ src/modules/Admin/submitHandler/index.js
@@ -1,14 +1,18 @@
 import { SubmissionError } from 'redux-form/immutable';
 import { adminUpdate, adminCreate, updateCollection, updateCommunity } from 'actions';
+import { detailedDiff } from 'deep-object-diff';
 
-export const onSubmit = (values, dispatch, { match }) => {
+export const onSubmit = (values, dispatch, { initialValues, match }) => {
     const data = (values && values.toJS()) || null;
+    const initialData = (initialValues && initialValues.toJS()) || null;
+    const changes = detailedDiff(initialData, data);
 
     const isEdit = !!data.publication && !!data.publication.rek_pid && data.publication.rek_pid === match.params.pid;
 
     let action = null;
     let requestObject = isEdit
         ? {
+            ...changes,
             // This is ignored for regular records.
             pid: data.publication.rek_pid,
             // The fallback to rek_created_date for rek_date may only be used for
