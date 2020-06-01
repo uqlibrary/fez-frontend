74f9f3e1d34836b654b277b9e76147c47e392781
Ky <k.lane@uq.edu.au>
2020-06-01 14:29:16 +1000

    Remove a replaceAll call for CKEditor

--- custom_modules/ckeditor/index.js
+++ custom_modules/ckeditor/index.js
@@ -2,10 +2,3 @@
 
 import './loader.js';
 import './ckeditor_build/ckeditor';
-
-// You can replace this with you own init script, e.g.:
-// - jQuery(document).ready()
-window.onload = function() {
-    console.log('Window loaded');
-    // window.CKEDITOR.replaceAll();
-};
