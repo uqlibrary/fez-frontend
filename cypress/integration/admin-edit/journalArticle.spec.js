0fdfc2ec0c566e6c15e66097c8685fd09ff328f8
Vishal Asai <asai_vishal@yahoo.com>
2020-06-08 14:33:42 +1000

    [#173121575] Fixing cypress test

--- cypress/integration/admin-edit/journalArticle.spec.js
+++ cypress/integration/admin-edit/journalArticle.spec.js
@@ -321,18 +321,13 @@ context('Journal Article admin edit', () => {
                         const authorUsernames = record.fez_record_search_key_author_id.map(
                             item => item.author.aut_org_username,
                         );
-                        const authorUQ = record.fez_record_search_key_author_id.map(item =>
-                            item.rek_author_id_id > 0 ? 'UQ' : 'nonUQ',
-                        );
                         const authorNames = record.fez_record_search_key_author_id.map(
                             item => item.rek_author_id_lookup,
                         );
                         const authorIDs = record.fez_record_search_key_author_id.map(item => item.rek_author_id);
-                        const authorAffs = record.fez_record_search_key_author_affiliation_name.map((item, index) => {
-                            return authorUQ[index] === 'UQ'
-                                ? 'The University of Queensland'
-                                : item.rek_author_affiliation_name;
-                        });
+                        const authorAffs = record.fez_record_search_key_author_affiliation_name.map(
+                            item => item.rek_author_affiliation_name,
+                        );
                         authors.forEach((author, index) => {
                             cy.get('p')
                                 .eq(2 * index)
