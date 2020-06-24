8f14e3a9551ff092587c7d64df7300c5ef860e50
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2020-06-25 00:19:56 +1000

    [#173391105] PUT the record on DOI button click

--- src/actions/doi.test.js
+++ src/actions/doi.test.js
@@ -5,10 +5,13 @@ import {
     RECORD_DOI_UPDATE_FAILED,
 } from './actionTypes';
 import { updateDoi } from 'actions/doi';
-import { DOI_API } from 'repositories/routes';
+import { EXISTING_RECORD_API } from 'repositories/routes';
 
 describe('DOI actions', () => {
-    const pid = 'UQ:1234567';
+    const record = {
+        rek_pid: 'UQ:1234567',
+    };
+    const escapeRegExp = input => input.replace('.\\*', '.*').replace(/[\-\[\]\{\}\(\)\+\?\\\^\$\|]/g, '\\$&');
 
     beforeEach(() => {
         mockActionsStore = setupStoreForActions();
@@ -20,21 +23,21 @@ describe('DOI actions', () => {
     });
 
     it('should dispatch actions for successful update', async () => {
-        mockApi.onGet(DOI_API({ pid }).apiUrl).reply(200, {});
+        mockApi.onPut(new RegExp(escapeRegExp(EXISTING_RECORD_API({ pid: '.*' }).apiUrl))).reply(200, { data: record });
 
         const expectedActions = [RECORD_DOI_UPDATE_REQUESTING, RECORD_DOI_UPDATE_SUCCEEDED];
 
-        await mockActionsStore.dispatch(updateDoi(pid));
+        await mockActionsStore.dispatch(updateDoi(record));
         expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
     });
 
     it('should dispatch actions for failed update', async () => {
-        mockApi.onGet(DOI_API({ pid }).apiUrl).reply(500, {});
+        mockApi.onPut(new RegExp(escapeRegExp(EXISTING_RECORD_API({ pid: '.*' }).apiUrl))).reply(500, {});
 
         const expectedActions = [RECORD_DOI_UPDATE_REQUESTING, APP_ALERT_SHOW, RECORD_DOI_UPDATE_FAILED];
 
         try {
-            await mockActionsStore.dispatch(updateDoi(pid));
+            await mockActionsStore.dispatch(updateDoi(record));
         } catch (e) {
             expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
         }
