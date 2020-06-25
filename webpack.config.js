4ef4f657c4892fe92d59074672f10a5603ce7f34
Ky Lane <k.lane@uq.edu.au>
2020-06-25 22:57:33 +1000

    Trying cypress-plugin-retries

--- webpack.config.js
+++ webpack.config.js
@@ -151,6 +151,7 @@ module.exports = {
             'process.env.BRANCH': JSON.stringify('development'),
             'process.env.GIT_SHA': JSON.stringify(process.env.CI_COMMIT_ID),
             'process.env.SESSION_COOKIE_NAME': JSON.stringify(process.env.SESSION_COOKIE_NAME),
+            'process.env.CYPRESS_RETRIES': 5,
         }),
         new Dotenv(),
     ],
