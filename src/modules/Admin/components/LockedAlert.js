4e9919a359c4568822c03d8faa67f45631769b69
Vishal Asai <asai_vishal@yahoo.com>
2020-07-07 08:40:02 +1000

    [#167084106] added info alert for the admin user currently editing the record

--- src/modules/Admin/components/LockedAlert.js
+++ src/modules/Admin/components/LockedAlert.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { useRecordContext } from 'context';
+import { useRecordContext, useAccountContext } from 'context';
 import Grid from '@material-ui/core/Grid';
 import { Alert } from 'modules/SharedComponents/Toolbox/Alert';
 import pageLocale from 'locale/pages';
 import { unlockRecordToView } from 'actions';
 
-export const LockedAlert = () => {
+export const LockedAlert = ({ handleCancel }) => {
     const { record } = useRecordContext();
+    const { account } = useAccountContext();
     const dispatch = useDispatch();
+    const selfLocked = React.useRef(account.id === record.rek_editing_user);
+    const alert = React.useRef(
+        selfLocked.current ? pageLocale.pages.edit.alerts.selfLockedAlert : pageLocale.pages.edit.alerts.lockedAlert,
+    );
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
     const handleAction = React.useCallback(() => dispatch(unlockRecordToView()), []);
@@ -17,9 +23,9 @@ export const LockedAlert = () => {
         <Grid container style={{ marginTop: 12, marginBottom: 12 }}>
             <Grid item xs={12}>
                 <Alert
-                    {...pageLocale.pages.edit.alerts.lockedAlert}
-                    action={handleAction}
-                    message={pageLocale.pages.edit.alerts.lockedAlert.message
+                    {...alert.current}
+                    action={selfLocked.current ? handleCancel : handleAction}
+                    message={alert.current.message
                         .replace('[username]', record.rek_editing_user)
                         .replace('[name]', record.rek_editing_user_lookup)}
                     wiggle
@@ -27,6 +33,10 @@ export const LockedAlert = () => {
             </Grid>
         </Grid>
     );
+};
+
+LockedAlert.propTypes = {
+    handleCancel: PropTypes.func,
 };
 
 export default LockedAlert;
