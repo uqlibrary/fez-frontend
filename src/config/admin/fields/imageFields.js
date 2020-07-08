758880fe8346e7cecba0cde74e6b74d879b13658
Lea de Groot <l.degroot@uq.edu.au>
2020-07-09 09:54:16 +1000

    fix duplicated admin key [PT #173237294]

--- src/config/admin/fields/imageFields.js
+++ src/config/admin/fields/imageFields.js
@@ -63,7 +63,7 @@ export default {
             title: 'Additional information',
             groups: [
                 ['fez_record_search_key_institutional_status', 'fez_record_search_key_refereed_source'],
-                ['fez_record_search_key_oa_status', 'fez_record_search_key_oa_status'],
+                ['fez_record_search_key_oa_status', 'fez_record_search_key_oa_status_type'],
                 ['additionalNotes'],
             ],
         },
