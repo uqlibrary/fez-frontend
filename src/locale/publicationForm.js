1ecdea70ae624c92fd7925591e5e98794d4fdfc1
Lea de Groot <l.degroot@uq.edu.au>
2020-07-21 09:55:50 +1000

    tweak wording [PT #173370538]

--- src/locale/publicationForm.js
+++ src/locale/publicationForm.js
@@ -518,7 +518,7 @@ export default {
         information: {
             title: 'Thesis information',
             hdrRedirectActionButtonLabel: 'Visit now',
-            nonHdrWarningMessage: 'If you are trying to submit your UQ HDR thesis, please use the HDR submission page',
+            nonHdrWarningMessage: 'If you are trying to submit your UQ HDR thesis, please use the HDR submission form',
             hdrRedirectMessage:
                 "HDR theses cannot be submitted via 'Add a missing work' - use the official submission link to upload your thesis",
             hdrRedirectAlertTitle: 'Redirect',
