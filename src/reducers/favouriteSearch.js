ff38a2d5f0e8720e42a5016e2e47e6a22637979a
Vishal Asai <asai_vishal@yahoo.com>
2020-08-04 16:04:27 +1000

    [#173955964] Updated reducer for updating/deleting actions

--- src/reducers/favouriteSearch.js
+++ src/reducers/favouriteSearch.js
@@ -4,32 +4,77 @@ export const initialState = {
     favouriteSearchListLoading: true,
     favouriteSearchList: null,
     favouriteSearchListError: null,
+    favouriteSearchListItemUpdating: false,
+    favouriteSearchListItemUpdateError: null,
+    favouriteSearchListItemDeleting: false,
+    favouriteSearchListItemDeleteError: null,
 };
 
 const handlers = {
-    [actions.FAVOURITE_SEARCH_LIST_LOADING]: state => {
+    [actions.FAVOURITE_SEARCH_LIST_LOADING]: state => ({
+        ...state,
+        favouriteSearchListLoading: true,
+    }),
+
+    [actions.FAVOURITE_SEARCH_LIST_LOADED]: (state, action) => ({
+        ...state,
+        favouriteSearchListLoading: false,
+        favouriteSearchList: action.payload,
+    }),
+
+    [actions.FAVOURITE_SEARCH_LIST_FAILED]: (state, action) => ({
+        ...state,
+        favouriteSearchListLoading: false,
+        favouriteSearchList: null,
+        favouriteSearchListError: action.payload,
+    }),
+
+    [actions.FAVOURITE_SEARCH_ITEM_UPDATING]: state => ({
+        ...state,
+        favouriteSearchListItemUpdating: true,
+    }),
+
+    [actions.FAVOURITE_SEARCH_ITEM_UPDATE_SUCCESS]: (state, action) => {
+        const index = state.favouriteSearchList.indexOf(action.oldData);
         return {
             ...state,
-            favouriteSearchListLoading: true,
+            favouriteSearchListItemUpdating: false,
+            favouriteSearchList: [
+                ...state.favouriteSearchList.slice(0, index),
+                action.payload,
+                ...state.favouriteSearchList.slice(index + 1),
+            ],
         };
     },
 
-    [actions.FAVOURITE_SEARCH_LIST_LOADED]: (state, action) => {
+    [actions.FAVOURITE_SEARCH_ITEM_UPDATE_FAILED]: (state, action) => ({
+        ...state,
+        favouriteSearchListItemUpdating: false,
+        favouriteSearchListItemUpdateError: action.payload,
+    }),
+
+    [actions.FAVOURITE_SEARCH_ITEM_DELETING]: state => ({
+        ...state,
+        favouriteSearchListItemDeleting: true,
+    }),
+
+    [actions.FAVOURITE_SEARCH_ITEM_DELETE_SUCCESS]: (state, action) => {
+        const index = state.favouriteSearchList.indexOf(action.payload);
         return {
             ...state,
-            favouriteSearchListLoading: false,
-            favouriteSearchList: action.payload,
+            favouriteSearchListItemDeleting: false,
+            favouriteSearchList: [
+                ...state.favouriteSearchList.slice(0, index),
+                ...state.favouriteSearchList.slice(index + 1),
+            ],
         };
     },
 
-    [actions.FAVOURITE_SEARCH_LIST_FAILED]: (state, action) => {
-        return {
-            ...state,
-            favouriteSearchListLoading: false,
-            favouriteSearchList: null,
-            favouriteSearchListError: action.payload,
-        };
-    },
+    [actions.FAVOURITE_SEARCH_ITEM_DELETE_FAILED]: (state, action) => ({
+        ...state,
+        favouriteSearchListItemDeleting: false,
+        favouriteSearchListItemDeleteError: action.payload,
+    }),
 };
 
 export default function exportPublicationsReducer(state = initialState, action) {
