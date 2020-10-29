5b004305de5d53966801d9f862f2fe2d9e9fabe2
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2020-10-29 14:43:34 +1000

    Remove redundant b wrapper, fix spacing

--- src/modules/SharedComponents/Toolbox/NtroFields/components/NtroFields.js
+++ src/modules/SharedComponents/Toolbox/NtroFields/components/NtroFields.js
@@ -93,16 +93,14 @@ export default class NtroFields extends React.PureComponent {
                                 Creator research statement
                             </Typography>
                             <p>
-                                For more information about the research statement, click
-                                <b>
-                                    <a
-                                        style={{ fontWeight: 700 }}
-                                        target="_blank"
-                                        href="https://guides.library.uq.edu.au/for-researchers/uqespace-publications-datasets/ntro-submission-requirements#s-lg-box-20836548"
-                                    >
-                                        here
-                                    </a>
-                                </b>
+                                For more information about the research statement, click{' '}
+                                <a
+                                    style={{ fontWeight: 700 }}
+                                    target="_blank"
+                                    href="https://guides.library.uq.edu.au/for-researchers/uqespace-publications-datasets/ntro-submission-requirements#s-lg-box-20836548"
+                                >
+                                    here
+                                </a>
                             </p>
                         </React.Fragment>
                     ),
