6404e8db41c2b0458f34cfbc95bc8645a2d3fdf7
Vishal Asai <asai_vishal@yahoo.com>
2020-11-19 09:07:49 +1000

    Revert "Revert "[#175678395] Fixed authors citation view""
    This reverts commit e84c8a48fd8ccb5eb7f8eabd754f40f45cfdebd1.

--- src/modules/SharedComponents/PublicationCitation/components/citations/partials/AuthorsCitationView.test.js
+++ src/modules/SharedComponents/PublicationCitation/components/citations/partials/AuthorsCitationView.test.js
@@ -470,7 +470,7 @@ describe('AuthorsCitationView test ', () => {
             { isShallow: true },
         );
         expect(toJson(wrapper)).toMatchSnapshot();
-        expect(wrapper.state().authors.length).toEqual(4);
+        expect(wrapper.state().authors.length).toEqual(3);
         expect(wrapper.find('.citationAuthor').length).toEqual(3);
     });
 
@@ -556,7 +556,7 @@ describe('AuthorsCitationView test ', () => {
         const wrapper = setup(
             {
                 publication: testObject,
-                maxAuthorDisplayNumber: 3,
+                maxAuthorDisplayNumber: 2,
                 citationStyle: 'header',
             },
             { isShallow: true },
