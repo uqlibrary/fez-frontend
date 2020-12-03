97580b77d680d52fd9d8755cced44dc7a2ee8954
Vishal Asai <asai_vishal@yahoo.com>
2020-12-03 22:45:06 +1000

    [#175966187] Added mock data

---
+++ src/mock/data/myEditorialAppointments/myEditorialAppointmentItem.js
@@ -0,0 +1,12 @@
+export default {
+    "data": {
+        "eap_id": 1,
+        "eap_journal_name": 'Test journal name',
+        "eap_jnl_id": '1234',
+        "eap_role_cvo_id": '454145',
+        "eap_role_name": "Guest Editor",
+        "eap_start_year": '2010',
+        "eap_end_year": '2020',
+        "eap_aut_id": 11123
+    }
+}
---
+++ src/mock/data/myEditorialAppointments/myEditorialAppointmentsList.js
@@ -0,0 +1,31 @@
+export default {
+    "total": 3,
+    "data": [{
+        "eap_id": 1,
+        "eap_journal_name": 'Test journal name',
+        "eap_jnl_id": '1234',
+        "eap_role_cvo_id": '454145',
+        "eap_role_name": "Guest Editor",
+        "eap_start_year": '2010',
+        "eap_end_year": '2020',
+        "eap_aut_id": 11123
+    }, {
+        "eap_id": 2,
+        "eap_journal_name": 'Test journal name testing',
+        "eap_jnl_id": '1235',
+        "eap_role_cvo_id": '454142',
+        "eap_role_name": "Editor",
+        "eap_start_year": '2010',
+        "eap_end_year": '2020',
+        "eap_aut_id": 11123
+    }, {
+        "eap_id": 3,
+        "eap_journal_name": 'Test journal name test',
+        "eap_jnl_id": '1236',
+        "eap_role_cvo_id": '454143',
+        "eap_role_name": "Editor-in-chief",
+        "eap_start_year": '2010',
+        "eap_end_year": '2020',
+        "eap_aut_id": 11123
+    }]
+}
