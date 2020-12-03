81e339654711a0bd429761656233a3de7547899b
Vishal Asai <asai_vishal@yahoo.com>
2020-12-03 15:50:41 +1000

    [#175966187] Added actions, route and tests

---
+++ src/mock/data/testing/myEditorialAppointments.js
@@ -0,0 +1,22 @@
+export const myEditorialAppointmentsList = {
+    "total": 1,
+    "data": [{
+        "eap_id": 1,
+        "eap_journal_name": 'test',
+        "eap_jnl_id": 1234,
+        "eap_role_cvo_id": '123456',
+        "eap_start_year": "2006",
+        "eap_end_year": "2026",
+        "eap_role_name": 'Guest Editor',
+    }]
+};
+
+export const myEditorialAppointmentsListItem = {
+    "eap_id": 1,
+    "eap_journal_name": 'test_updated',
+    "eap_jnl_id": 1234,
+    "eap_role_cvo_id": '123456',
+    "eap_start_year": "2006",
+    "eap_end_year": "2026",
+    "eap_role_name": 'Editor',
+};
