3a307e15dca8644d0be219ea510fe00820450834
Vishal Asai <asai_vishal@yahoo.com>
2021-01-11 16:03:44 +1000

    [#175985102] More test coverage for keywords and issn forms

--- src/modules/SharedComponents/Toolbox/ListEditor/components/IssnForm.test.js
+++ src/modules/SharedComponents/Toolbox/ListEditor/components/IssnForm.test.js
@@ -38,4 +38,77 @@ describe('IssnForm behaviour tests', () => {
             indexFinder,
         );
     });
+
+    it('should submit correct form of issn for NewListEditor', () => {
+        const onSubmitFn = jest.fn();
+        const { getByTestId } = rtlRender(
+            <IssnForm
+                onSubmit={onSubmitFn}
+                locale={{
+                    inputFieldLabel: 'Item name',
+                    inputFieldHint: 'Please type the item name',
+                    addButtonLabel: 'Add',
+                }}
+                listEditorId="issn"
+                normalize={jest.fn(value => value)}
+                mode="add"
+            />,
+        );
+
+        expect(getByTestId('issn-add')).toHaveAttribute('disabled', '');
+
+        fireEvent.change(getByTestId('issn-input'), { target: { value: '0000-0000' } });
+
+        expect(getByTestId('issn-add')).not.toHaveAttribute('disabled', '');
+
+        fireEvent.click(getByTestId('issn-add'));
+
+        expect(onSubmitFn).toHaveBeenCalledWith(
+            {
+                key: '0000-0000',
+                value: {
+                    sherpaRomeo: { link: false },
+                    ulrichs: { link: false, linkText: '' },
+                },
+            },
+            indexFinder,
+        );
+    });
+
+    it('should submit update selected item to update', () => {
+        const onSubmitFn = jest.fn();
+        const { getByTestId } = rtlRender(
+            <IssnForm
+                onSubmit={onSubmitFn}
+                locale={{
+                    inputFieldLabel: 'Item name',
+                    inputFieldHint: 'Please type the item name',
+                    addButtonLabel: 'Update',
+                }}
+                listEditorId="issn"
+                normalize={jest.fn(value => value)}
+                mode="update"
+                itemSelectedToEdit={{ key: '1111-2222' }}
+            />,
+        );
+
+        fireEvent.change(getByTestId('issn-input'), { target: { value: '0000-0000' } });
+        fireEvent.click(getByTestId('issn-update'));
+
+        expect(onSubmitFn).toHaveBeenCalledWith(
+            {
+                key: '0000-0000',
+                value: {
+                    sherpaRomeo: { link: false },
+                    ulrichs: { link: false, linkText: '' },
+                },
+            },
+            indexFinder,
+        );
+    });
+
+    it('should correctly handle filtering for the given list and the item', () => {
+        expect(indexFinder([{ key: '1111-2222', value: {} }], { key: '1111-2222' })).toBe(0);
+        expect(indexFinder([{ key: '1111-2222', value: {} }], { key: '1111-1111' })).toBe(-1);
+    });
 });
