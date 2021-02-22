4e8d172cce25774ee59b66dfb1ceb1f7fe1f4856
Vishal Asai <asai_vishal@yahoo.com>
2021-02-22 16:28:08 +1000

    [#167390803] update account reducer

--- src/reducers/account.js
+++ src/reducers/account.js
@@ -6,7 +6,7 @@ export const initialState = {
     authorDetails: null,
     accountLoading: false,
     accountAuthorLoading: false,
-    accountAuthorDetailsLoading: true,
+    accountAuthorDetailsLoading: false,
     isSessionExpired: null,
 };
 
