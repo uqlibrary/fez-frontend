e0691f39c3240a78a40a95636f53e7b2a0261fc0
Vishal Asai <asai_vishal@yahoo.com>
2021-03-04 21:21:58 +1000

    [#177089838] Added actions and reducer for authors list management

---
+++ src/actions/authorsList.js
@@ -0,0 +1,111 @@
+import {
+    AUTHOR_ADDING,
+    AUTHOR_ADD_SUCCESS,
+    AUTHOR_ADD_FAILED,
+    AUTHOR_LIST_LOADING,
+    AUTHOR_LIST_LOADED,
+    AUTHOR_LIST_FAILED,
+    AUTHOR_ITEM_UPDATING,
+    AUTHOR_ITEM_UPDATE_SUCCESS,
+    AUTHOR_ITEM_UPDATE_FAILED,
+    AUTHOR_ITEM_DELETING,
+    AUTHOR_ITEM_DELETE_SUCCESS,
+    AUTHOR_ITEM_DELETE_FAILED,
+} from './actionTypes';
+import { get, put, destroy, post } from 'repositories/generic';
+import { CURRENT_AUTHOR_API } from 'repositories/routes';
+
+export function loadAuthorList() {
+    return async dispatch => {
+        dispatch({ type: AUTHOR_LIST_LOADING });
+
+        try {
+            const response = await get(CURRENT_AUTHOR_API());
+            dispatch({
+                type: AUTHOR_LIST_LOADED,
+                payload: response.data,
+            });
+
+            return Promise.resolve(response.data);
+        } catch (e) {
+            dispatch({
+                type: AUTHOR_LIST_FAILED,
+                payload: e,
+            });
+
+            return Promise.reject(e);
+        }
+    };
+}
+
+export function updateAuthorListItem(newData, oldData) {
+    return async dispatch => {
+        try {
+            dispatch({ type: AUTHOR_ITEM_UPDATING });
+
+            const response = await put(CURRENT_AUTHOR_API({ id: newData.aut_id }), newData);
+
+            dispatch({
+                type: AUTHOR_ITEM_UPDATE_SUCCESS,
+                payload: response.data,
+                oldData,
+            });
+
+            return Promise.resolve(response.data);
+        } catch (e) {
+            dispatch({
+                type: AUTHOR_ITEM_UPDATE_FAILED,
+                payload: e,
+                oldData,
+            });
+
+            return Promise.reject(e);
+        }
+    };
+}
+
+export function deleteAuthorListItem(oldData) {
+    return async dispatch => {
+        dispatch({ type: AUTHOR_ITEM_DELETING });
+
+        try {
+            const response = await destroy(CURRENT_AUTHOR_API({ id: oldData.aut_id }));
+            dispatch({
+                type: AUTHOR_ITEM_DELETE_SUCCESS,
+                payload: oldData,
+            });
+
+            return Promise.resolve(response.data);
+        } catch (e) {
+            dispatch({
+                type: AUTHOR_ITEM_DELETE_FAILED,
+                payload: e,
+            });
+
+            return Promise.reject(e);
+        }
+    };
+}
+
+export function addAuthor(data) {
+    return async dispatch => {
+        dispatch({ type: AUTHOR_ADDING });
+
+        try {
+            const response = await post(CURRENT_AUTHOR_API(), data);
+            dispatch({
+                type: AUTHOR_ADD_SUCCESS,
+                payload: response.data,
+            });
+
+            return Promise.resolve(response.data);
+        } catch (e) {
+            dispatch({
+                type: AUTHOR_ADD_FAILED,
+                payload: e,
+            });
+
+            return Promise.reject(e);
+        }
+    };
+}
---
+++ src/actions/authorsList.test.js
@@ -0,0 +1,162 @@
+import { deleteAuthorListItem, loadAuthorList, updateAuthorListItem, addAuthor } from './authorsList';
+import * as actions from './actionTypes';
+import * as repositories from 'repositories';
+import * as mockData from 'mock/data/testing/authorsList';
+
+describe('author list actions', () => {
+    beforeEach(() => {
+        mockActionsStore = setupStoreForActions();
+        mockApi = setupMockAdapter();
+    });
+
+    afterEach(() => {
+        mockApi.reset();
+    });
+
+    describe('loadAuthorList action', () => {
+        it('should dispatch correct number of actions on loading author list', async () => {
+            mockApi
+                .onGet(repositories.routes.CURRENT_AUTHOR_API().apiUrl)
+                .reply(200, { data: { ...mockData.authorList } });
+
+            const expectedActions = [actions.AUTHOR_LIST_LOADING, actions.AUTHOR_LIST_LOADED];
+
+            await mockActionsStore.dispatch(loadAuthorList());
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+
+        it('should dispatch correct number of actions on failed to load author list', async () => {
+            mockApi.onGet(repositories.routes.CURRENT_AUTHOR_API().apiUrl).reply(500);
+
+            const expectedActions = [actions.AUTHOR_LIST_LOADING, actions.APP_ALERT_SHOW, actions.AUTHOR_LIST_FAILED];
+
+            await expect(mockActionsStore.dispatch(loadAuthorList())).rejects.toEqual({
+                status: 500,
+                message:
+                    'Error has occurred during request and request cannot be processed. Please contact eSpace administrators or try again later.',
+            });
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+    });
+
+    describe('updateAuthorListItem action', () => {
+        it('should dispatch correct number of actions on author list item successfully updated', async () => {
+            mockApi
+                .onPut(repositories.routes.CURRENT_AUTHOR_API({ id: 1 }).apiUrl)
+                .reply(200, { data: { ...mockData.authorListItem } });
+
+            const expectedActions = [actions.AUTHOR_ITEM_UPDATING, actions.AUTHOR_ITEM_UPDATE_SUCCESS];
+
+            await mockActionsStore.dispatch(updateAuthorListItem({ aut_id: 1 }, { aut_id: 1 }));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+
+        it('should dispatch correct number of actions on author list item update failed', async () => {
+            mockApi.onPut(repositories.routes.CURRENT_AUTHOR_API({ id: 1 }).apiUrl).reply(500);
+
+            const expectedActions = [
+                actions.AUTHOR_ITEM_UPDATING,
+                actions.APP_ALERT_SHOW,
+                actions.AUTHOR_ITEM_UPDATE_FAILED,
+            ];
+
+            await expect(mockActionsStore.dispatch(updateAuthorListItem({ aut_id: 1 }, { aut_id: 1 }))).rejects.toEqual(
+                {
+                    status: 500,
+                    message:
+                        'Error has occurred during request and request cannot be processed. Please contact eSpace administrators or try again later.',
+                },
+            );
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+
+        it('should dispatch correct of number of actions for existing alias not found while updating', async () => {
+            mockApi
+                .onPut(repositories.routes.CURRENT_AUTHOR_API({ id: 1 }).apiUrl)
+                .reply(200, { data: { ...mockData.authorListItem } });
+
+            const expectedActions = [actions.AUTHOR_ITEM_UPDATING, actions.AUTHOR_ITEM_UPDATE_SUCCESS];
+
+            await mockActionsStore.dispatch(
+                updateAuthorListItem(
+                    { aut_id: 1, aut_org_username: 'test' },
+                    { aut_id: 1, aut_org_username: 'testing' },
+                ),
+            );
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+    });
+
+    describe('deleteAuthorListItem action', () => {
+        it('should dispatch correct number of actions on author list item successfully deleted', async () => {
+            mockApi
+                .onDelete(repositories.routes.CURRENT_AUTHOR_API({ id: 1 }).apiUrl)
+                .reply(200, { data: { ...mockData.authorListItem } });
+
+            const expectedActions = [actions.AUTHOR_ITEM_DELETING, actions.AUTHOR_ITEM_DELETE_SUCCESS];
+
+            await mockActionsStore.dispatch(deleteAuthorListItem({ aut_id: 1 }));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+
+        it('should dispatch correct number of actions on author list item delete failed', async () => {
+            mockApi.onDelete(repositories.routes.CURRENT_AUTHOR_API({ id: 1 }).apiUrl).reply(500);
+
+            const expectedActions = [
+                actions.AUTHOR_ITEM_DELETING,
+                actions.APP_ALERT_SHOW,
+                actions.AUTHOR_ITEM_DELETE_FAILED,
+            ];
+
+            await expect(mockActionsStore.dispatch(deleteAuthorListItem({ aut_id: 1 }))).rejects.toEqual({
+                status: 500,
+                message:
+                    'Error has occurred during request and request cannot be processed. Please contact eSpace administrators or try again later.',
+            });
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+    });
+
+    describe('addAuthor action', () => {
+        it('should dispatch correct number of actions on author successfully added', async () => {
+            mockApi
+                .onPost(repositories.routes.CURRENT_AUTHOR_API().apiUrl)
+                .reply(200, { data: { ...mockData.authorListItem } });
+
+            const expectedActions = [actions.AUTHOR_ADDING, actions.AUTHOR_ADD_SUCCESS];
+
+            await mockActionsStore.dispatch(
+                addAuthor({
+                    aut_student_username: 'test',
+                    aut_org_staff_id: '1234',
+                    aut_org_username: 'test',
+                    aut_fname: 'Test',
+                }),
+            );
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+
+        it('should dispatch correct number of actions on author add failed', async () => {
+            mockApi.onPost(repositories.routes.CURRENT_AUTHOR_API().apiUrl).reply(500);
+
+            const expectedActions = [actions.AUTHOR_ADDING, actions.APP_ALERT_SHOW, actions.AUTHOR_ADD_FAILED];
+
+            await expect(
+                mockActionsStore.dispatch(
+                    addAuthor({
+                        aut_student_username: 'test',
+                        aut_org_staff_id: '1234',
+                        aut_org_username: 'test',
+                        aut_fname: 'Test',
+                    }),
+                ),
+            ).rejects.toEqual({
+                status: 500,
+                message:
+                    'Error has occurred during request and request cannot be processed. Please contact eSpace administrators or try again later.',
+            });
+
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+    });
+});
