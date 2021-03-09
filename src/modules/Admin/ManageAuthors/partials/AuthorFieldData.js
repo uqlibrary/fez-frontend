26887bfbd17c3dbd8f2afd6c037441efa1364ccc
Vishal Asai <asai_vishal@yahoo.com>
2021-03-09 13:20:39 +1000

    [#177089838] added dashed underline for text fields

--- src/modules/Admin/ManageAuthors/partials/AuthorFieldData.js
+++ src/modules/Admin/ManageAuthors/partials/AuthorFieldData.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Grid from '@material-ui/core/Grid';
 import { TextField } from 'modules/SharedComponents/Toolbox/TextField';
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles(() => ({
     root: {
-        '&$before': {
-            borderBottom: '1px solid red',
+        '&::before': {
+            borderBottom: '1px dashed rgba(0, 0, 0, 0.13)',
         },
     },
     underline: {},
@@ -16,50 +15,29 @@ const useStyles = makeStyles(() => ({
 export const AuthorFieldData = ({ authorFieldDataId, data, title }) => {
     const classes = useStyles();
     return (
-        <Grid container>
-            <Grid item xs={12}>
-                {/* <Typography
-                    className={classes.cellEditable}
-                    variant="body2"
-                    {...(!!authorFieldDataId
-                        ? {
-                              'data-testid': authorFieldDataId,
-                              id: authorFieldDataId,
-                          }
-                        : {})}
-                >
-                    {data || '-'}
-                </Typography> */}
-                <TextField
-                    textFieldId={authorFieldDataId}
-                    label={title}
-                    InputProps={{
-                        style: {
-                            fontSize: 14,
-                            fontWeight: 400,
-                        },
-                        readOnly: true,
-                        classes: {
-                            root: classes.root,
-                            underline: classes.underline,
-                        },
-                    }}
-                    InputLabelProps={{
-                        style: {
-                            fontSize: '0.8rem',
-                        },
-                        shrink: true,
-                        disableAnimation: true,
-                    }}
-                    value={data}
-                />
-            </Grid>
-            {/* <Grid item>
-                <Typography variant="caption" color="secondary" className={classes.fieldTitle}>
-                    {title}
-                </Typography>
-            </Grid> */}
-        </Grid>
+        <TextField
+            textFieldId={authorFieldDataId}
+            label={title}
+            InputProps={{
+                style: {
+                    fontSize: 14,
+                    fontWeight: 400,
+                },
+                readOnly: true,
+                classes: {
+                    root: classes.root,
+                    underline: classes.underline,
+                },
+            }}
+            InputLabelProps={{
+                style: {
+                    fontSize: '0.8rem',
+                },
+                shrink: true,
+                disableAnimation: true,
+            }}
+            value={data}
+        />
     );
 };
 
