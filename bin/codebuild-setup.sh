542039fd1e1dc0b9046172c547c836f87cbdd0c5
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2021-04-23 12:53:07 +1000

    Cleanups and version upgrades
    - Update node and npm version specifications
    - Generate lock file for package.json in subdir
    - Cleanup unused Codeship scripts
    - Change tab width to 4 spaces to make diffs easier

--- bin/codebuild-setup.sh
+++ bin/codebuild-setup.sh
@@ -6,9 +6,9 @@ set -e
 printf "Node "; node -v;
 printf "(Codeship default) npm v"; npm -v
 
-printf "\n\n--- GET LATEST VERSION OF NPM 6 ---\n"
-echo "$ npm install -g npm@6"
-npm install -g npm@6
+printf "\n\n--- GET LATEST VERSION OF NPM 7 ---\n"
+echo "$ npm install -g npm@7"
+npm install -g npm@7
 
 printf "\nNow running npm v"; npm -v
 
