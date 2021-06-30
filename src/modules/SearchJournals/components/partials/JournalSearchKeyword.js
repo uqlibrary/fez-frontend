b6e4c6e761bae526a79748bb9336f576113219ed
Vishal Asai <asai_vishal@yahoo.com>
2021-06-30 12:37:48 +1000

    [#178615118] Added partial for journal search keyword

---
+++ src/modules/SearchJournals/components/partials/JournalSearchKeyword.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/makeStyles';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        color: theme.palette.accent.main,
+        cursor: 'pointer',
+    },
+    default: {},
+    addable: {
+        '&::before': {
+            content: '"+"',
+            marginRight: '4px',
+        },
+    },
+}));
+
+export const JournalSearchKeyword = ({ keyword, onClickKeyword, variant }) => {
+    const classes = useStyles();
+
+    return (
+        <Typography
+            variant="body2"
+            classes={{ root: classes.root }}
+            className={classes[variant || 'default']}
+            onClick={onClickKeyword}
+        >
+            {keyword}
+        </Typography>
+    );
+};
+
+JournalSearchKeyword.propTypes = {
+    keyword: PropTypes.string.isRequired,
+    onClickKeyword: PropTypes.func.isRequired,
+    variant: PropTypes.oneOf(['default', 'addable']),
+};
+
+export default React.memo(JournalSearchKeyword);
