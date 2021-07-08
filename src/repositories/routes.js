fce4ef4740fdea6523533c675f59ebe97529822a
Vishal Asai <asai_vishal@yahoo.com>
2021-07-08 16:16:47 +1000

    [#178615118] added api route for searching keywords

--- src/repositories/routes.js
+++ src/repositories/routes.js
@@ -428,3 +428,7 @@ export const USER_API = ({ userId, userIds } = { userId: undefined, userIds: und
 
     return { apiUrl: 'fez-users' };
 };
+
+export const JOURNAL_KEYWORDS_LOOKUP_API = ({ query }) => ({
+    apiUrl: `journals/search?rule=keywords&query=${query}`,
+});
