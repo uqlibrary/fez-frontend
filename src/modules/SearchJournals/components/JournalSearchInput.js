12db301cdc4f6fa4c6b7b69d9ab63b9ab2dc47ef
Vishal Asai <asai_vishal@yahoo.com>
2021-07-12 23:25:40 +1000

    [#178615118] implemented throttling for journal search input

--- src/modules/SearchJournals/components/JournalSearchInput.js
+++ src/modules/SearchJournals/components/JournalSearchInput.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { throttle } from 'throttle-debounce';
+
+import InputAdornment from '@material-ui/core/InputAdornment';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { TextField } from 'modules/SharedComponents/Toolbox/TextField';
 import { loadJournalSearchKeywords } from 'actions';
@@ -10,19 +14,27 @@ import locale from 'locale/components';
 export const JournalSearchInput = () => {
     const txt = locale.components.journalSearch;
     const dispatch = useDispatch();
+    const [journalSearchInput, setJournalSearchInput] = React.useState(null);
 
-    // const loadingJournalSearchKeywords = useSelector(
-    //     state => (state.get('journalSearchReducer') || {}).loadingJournalSearchKeywords,
-    // );
+    const loadingJournalSearchKeywords = useSelector(
+        state => (state.get('journalReducer') || {}).journalSearchKeywordsLoading,
+    );
 
+    const throttledLoadSuggestions = React.useRef(
+        throttle(1000, newValue => dispatch(loadJournalSearchKeywords(newValue))),
+    );
     /**
      * Journal search input handler
      *
      * @param {object} event keypress event object
-     * @returns {Promise}
-     * @todo Implement throttling
      */
-    const handleJournalSearchInput = event => dispatch(loadJournalSearchKeywords(event.target.value));
+    const handleJournalSearchInput = React.useCallback(event => setJournalSearchInput(event.target.value.trim()), []);
+
+    React.useEffect(() => {
+        if (journalSearchInput && throttledLoadSuggestions) {
+            throttledLoadSuggestions.current(journalSearchInput);
+        }
+    }, [journalSearchInput]);
 
     return (
         <TextField
@@ -30,7 +42,24 @@ export const JournalSearchInput = () => {
             placeholder={txt.input.placeholder}
             name="journal-search-keywords-input"
             onChange={handleJournalSearchInput}
+            value={journalSearchInput}
             fullWidth
+            InputProps={{
+                ...((!!loadingJournalSearchKeywords && {
+                    endAdornment: (
+                        <InputAdornment position="end">
+                            <CircularProgress
+                                size={18}
+                                thickness={2}
+                                color="primary"
+                                id="loading-journal-search-keywords"
+                                data-testid="loading-journal-search-keywords"
+                            />
+                        </InputAdornment>
+                    ),
+                }) ||
+                    {}),
+            }}
         />
     );
 };
