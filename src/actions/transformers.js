94015baf50917d76023f0d7490b8bce0575104ed
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2021-07-26 12:11:20 +1000

    [#178876508] Avoid sending file attachment access condition on file upload if inheriting

--- src/actions/transformers.js
+++ src/actions/transformers.js
@@ -4,6 +4,7 @@ import { CONTENT_INDICATORS } from 'config/general';
 import {
     FILE_ACCESS_CONDITION_CLOSED,
     FILE_ACCESS_CONDITION_OPEN,
+    FILE_ACCESS_CONDITION_INHERIT,
 } from 'modules/SharedComponents/Toolbox/FileUploader';
 
 const moment = require('moment');
@@ -108,6 +109,9 @@ export const getRecordFileAttachmentSearchKey = (files, record) => {
         .map((item, index) => {
             if (!item.hasOwnProperty('access_condition_id')) return null;
             let accessCondition = item.access_condition_id;
+            if (accessCondition === FILE_ACCESS_CONDITION_INHERIT) {
+                return null;
+            }
             if (accessCondition === FILE_ACCESS_CONDITION_OPEN && item.date && moment(item.date).isAfter()) {
                 accessCondition = FILE_ACCESS_CONDITION_CLOSED;
             }
