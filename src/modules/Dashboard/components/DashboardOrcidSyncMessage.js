ab755a99b575c530f5afb9ce76963ef16d7048bb
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2021-08-18 10:11:35 +1000

    Best practice imports for MUI

--- src/modules/Dashboard/components/DashboardOrcidSyncMessage.js
+++ src/modules/Dashboard/components/DashboardOrcidSyncMessage.js
@@ -1,5 +1,5 @@
 import Grid from '@material-ui/core/Grid';
-import { Button } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 import React from 'react';
 import PropTypes from 'prop-types';
 import DoneIcon from '@material-ui/icons/Done';
