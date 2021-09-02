92076843924868163c653777853a1b447364af2d
uqmmoise <m.moises@library.uq.edu.au>
2021-09-03 09:38:20 +1000

    - improving form messages - tests [#177408548]

--- src/modules/SharedComponents/PublicationsList/components/BulkUpdatesForms/CreateOrUpdateDoiForm.js
+++ src/modules/SharedComponents/PublicationsList/components/BulkUpdatesForms/CreateOrUpdateDoiForm.js
@@ -29,7 +29,10 @@ export const CreateOrUpdateDoiForm = ({
     const txt = locale.components.bulkUpdates.bulkUpdatesForms;
     const hasCollectionsAmongSelectedRecords =
         Object.values(recordsSelected).filter(
-            record => !!record && record.rek_object_type_lookup.toLowerCase() === RECORD_TYPE_COLLECTION,
+            record =>
+                !!record &&
+                !!record.rek_object_type_lookup &&
+                record.rek_object_type_lookup.toLowerCase() === RECORD_TYPE_COLLECTION,
         ).length > 0;
 
     React.useEffect(() => {
