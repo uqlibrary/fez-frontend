95edd7958b75453e92c1d386275d33756b15e2d1
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2021-11-22 15:28:39 +1000

    Adjusted Unit tests [#180286920]
    Removed JournalSearchResult.test.js
    Renamed JournalSearchResults.test.js to JournalSearchResult.test.js
    Cleaned up and added sorting into test file.

--- src/modules/SearchJournals/components/JournalSearchResult.test.js
+++ src/modules/SearchJournals/components/JournalSearchResult.test.js
@@ -1,152 +1,36 @@
 import React from 'react';
-import { render, WithRouter, WithReduxStore, act, fireEvent } from 'test-utils';
-import { MuiThemeProvider } from '@material-ui/core';
+import { render, WithRouter, WithReduxStore, fireEvent, act } from 'test-utils';
+
 import { pathConfig } from 'config';
 import { createMemoryHistory } from 'history';
 import Immutable from 'immutable';
 import locale from 'locale/components';
-import JournalSearchResult, { getSearchResultSortingParams } from './JournalSearchResult';
-import { mockData, mockDataEmpty } from '../../../mock/data/testing/journals/journalSearchResults';
-import { createMuiTheme } from '@material-ui/core';
 import deparam from 'can-deparam';
+
+import JournalSearchResult, { getSearchResultSortingParams } from './JournalSearchResult';
+import { mockDataEmpty, mockData } from '../../../mock/data/testing/journals/journalSearchResults';
+
+import { PublicationsListSorting } from '../../SharedComponents/PublicationsList';
 
 const setup = ({
     state = {},
     testHistory = createMemoryHistory({ initialEntries: ['/'] }),
     onSearchFn = jest.fn(),
-    theme = createMuiTheme({ props: { MuiWithWidth: { initialWidth: 'sm' } } }),
 }) => {
     return render(
         <WithRouter history={testHistory}>
             <WithReduxStore initialState={Immutable.Map({ searchJournalsReducer: state })}>
-                <MuiThemeProvider theme={theme}>
-                    <JournalSearchResult onSearch={onSearchFn} />
-                </MuiThemeProvider>
+                <JournalSearchResult onSearch={onSearchFn} />
             </WithReduxStore>
         </WithRouter>,
     );
 };
-const mockDataPaged = {
-    ...mockData,
-    total: 20,
-    took: 10,
-    per_page: 10,
-    current_page: 1,
-    from: 1,
-    to: 10,
-};
-const testQueryPart = 'keywords%5BTitle-Medicine';
 
-const path = `/espace/feature-strategic-publishing/#${pathConfig.journals.search}`;
-const testHistory = createMemoryHistory({ initialEntries: [path] });
-describe('Search Journals Paging', () => {
-    testHistory.push({
-        path,
-        search: testQueryPart,
-        state: {
-            source: 'code',
-        },
-    });
-
-    it('should have previous and next paging areas where paging occurs', () => {
-        const journalsList = mockDataPaged;
-
-        const { getByTestId } = setup({
-            state: { journalsListLoaded: true, journalsList },
-            testHistory,
-        });
-
-        expect(getByTestId('search-journals-paging-top')).toBeInTheDocument();
-        expect(getByTestId('search-journals-paging-bottom')).toBeInTheDocument();
-    });
-    it('should have page buttons when results span multiple pages', () => {
-        const journalsList = mockDataPaged;
-
-        const { getByTestId } = setup({
-            state: { journalsListLoaded: true, journalsList },
-            testHistory,
-        });
-
-        expect(getByTestId('search-journals-paging-top-select-page-1')).toBeInTheDocument();
-        expect(getByTestId('search-journals-paging-top-select-page-2')).toBeInTheDocument();
-    });
-
-    it('should not have paging data when results span multiple pages', () => {
-        const journalsList = mockData;
-
-        const { queryByTestId } = setup({
-            state: { journalsListLoaded: true, journalsList },
-            testHistory,
-        });
-
-        expect(queryByTestId('search-journals-paging-top-select-page-1')).not.toBeInTheDocument();
-        expect(queryByTestId('search-journals-paging-top-select-page-2')).not.toBeInTheDocument();
-    });
-
-    it('should not have paging buttons on xs and below screens if paging present', () => {
-        const journalsList = mockDataPaged;
-
-        const { queryByTestId } = setup({
-            state: { journalsListLoaded: true, journalsList },
-            testHistory,
-            theme: createMuiTheme({ props: { MuiWithWidth: { initialWidth: 'xs' } } }),
-        });
-
-        expect(queryByTestId('search-journals-paging-top-select-page-1')).not.toBeInTheDocument();
-        expect(queryByTestId('search-journals-paging-top-select-page-2')).not.toBeInTheDocument();
-    });
-    it('should have paging buttons on sm and above screens if paging present', () => {
-        const journalsList = mockDataPaged;
-
-        const { queryByTestId } = setup({
-            state: { journalsListLoaded: true, journalsList },
-            testHistory,
-            theme: createMuiTheme({ props: { MuiWithWidth: { initialWidth: 'sm' } } }),
-        });
-
-        expect(queryByTestId('search-journals-paging-top-select-page-1')).toBeInTheDocument();
-        expect(queryByTestId('search-journals-paging-top-select-page-2')).toBeInTheDocument();
-    });
-
-    testHistory.push({
-        path,
-        search: testQueryPart,
-        state: {
-            source: 'code',
-        },
-    });
-    it('should use defined default sort order when explicitly provided', () => {
-        const testQueryPart = 'keywords%5BTitle-Medicine&sortDirection=Desc&sortBy=cite_score&pageSize=10&page=1';
-        const journalSearchQueryParams = deparam(testQueryPart);
-        const journalsList = mockData;
-        const { sortDirection, sortBy, pageSize } = getSearchResultSortingParams(
-            journalSearchQueryParams,
-            journalsList.per_page,
-            locale.components.searchJournals.sortingDefaults,
-        );
-        expect(sortDirection).toEqual('Desc');
-        expect(sortBy).toEqual('cite_score');
-        expect(pageSize).toEqual(10);
-    });
-
-    it('should use hardcoded default sorting values when no other defaults are provided', () => {
-        const { sortBy, sortDirection, pageSize } = getSearchResultSortingParams({}, undefined, {});
-        expect(sortDirection).toEqual('Desc');
-        expect(sortBy).toEqual('score');
-        expect(pageSize).toEqual(20);
-    });
-});
-
-describe('Search Sort Journals UI', () => {
-    testHistory.push({
-        path,
-        search: testQueryPart,
-        state: {
-            source: 'code',
-        },
-    });
-
-    it('should update the URL when the Journal comparsion button is clicked', () => {
+describe('Search Journals Results component', () => {
+    it("should show 'No journals found' when no results are present", () => {
+        const testQueryPart =
+            'keywords%5BTitle-Astrobiology%5D%5Btype%5D=Title&keywords%5BTitle-Astrobiology%5D%5Btext%5D=Astrobiology&keywords%5BTitle-Astrobiology%5D%5Bid%5D=Title-Astrobiology';
+        const path = `/espace/feature-strategic-publishing/#${pathConfig.journals.search}`;
         const testHistory = createMemoryHistory({ initialEntries: [path] });
         testHistory.push({
             path,
@@ -155,34 +39,7 @@ describe('Search Sort Journals UI', () => {
                 source: 'code',
             },
         });
-        const journalsList = mockData;
 
-        const { queryByTestId } = setup({
-            state: { journalsListLoaded: true, journalsList },
-            testHistory,
-        });
-
-        expect(queryByTestId('journal-comparison-button')).toBeInTheDocument();
-        expect(queryByTestId('journal-comparison-button')).toBeDisabled();
-
-        expect(queryByTestId('journal-list-data-col-1-checkbox-0')).toBeInTheDocument();
-        expect(queryByTestId('journal-list-data-col-1-checkbox-1')).toBeInTheDocument();
-
-        act(() => {
-            fireEvent.click(queryByTestId('journal-list-data-col-1-checkbox-0'));
-            fireEvent.click(queryByTestId('journal-list-data-col-1-checkbox-1'));
-        });
-
-        expect(queryByTestId('journal-comparison-button')).not.toBeDisabled();
-
-        act(() => {
-            fireEvent.click(queryByTestId('journal-comparison-button'));
-        });
-
-        expect(testHistory.location.pathname).toEqual(pathConfig.journals.compare);
-    });
-
-    it("should show 'No journals found' when no results are present", () => {
         const journalsList = mockDataEmpty;
 
         const { getByText } = setup({
@@ -192,8 +49,94 @@ describe('Search Sort Journals UI', () => {
 
         expect(getByText('No journals found')).toBeInTheDocument();
     });
+    it("should show 'No journals found' when result object is missing", () => {
+        const testQueryPart =
+            'keywords%5BTitle-Astrobiology%5D%5Btype%5D=Title&keywords%5BTitle-Astrobiology%5D%5Btext%5D=Astrobiology&keywords%5BTitle-Astrobiology%5D%5Bid%5D=Title-Astrobiology';
+        const path = `/espace/feature-strategic-publishing/#${pathConfig.journals.search}`;
+        const testHistory = createMemoryHistory({ initialEntries: [path] });
+        testHistory.push({
+            path,
+            search: testQueryPart,
+            state: {
+                source: 'code',
+            },
+        });
 
+        const { getByText } = setup({
+            state: { journalsListLoaded: true, undefined },
+            testHistory,
+        });
+
+        expect(getByText('No journals found')).toBeInTheDocument();
+    });
+    it('should show an empty document when no keywords are present', () => {
+        const testQueryPart = '';
+        const path = `/espace/feature-strategic-publishing/#${pathConfig.journals.search}`;
+        const testHistory = createMemoryHistory({ initialEntries: [path] });
+        testHistory.push({
+            path,
+            search: testQueryPart,
+            state: {
+                source: 'code',
+            },
+        });
+
+        const journalsList = mockDataEmpty;
+
+        const { queryByTestId } = setup({
+            state: { journalsListLoaded: true, journalsList },
+            testHistory,
+        });
+
+        expect(queryByTestId('journal-search-results-no-keywords')).toBeInTheDocument();
+    });
+    it('should show an empty document when an empty keyword is present', () => {
+        const testQueryPart = '?keywords%5B';
+        const path = `/espace/feature-strategic-publishing/#${pathConfig.journals.search}`;
+        const testHistory = createMemoryHistory({ initialEntries: [path] });
+        testHistory.push({
+            path,
+            search: testQueryPart,
+            state: {
+                source: 'code',
+            },
+        });
+
+        const journalsList = mockDataEmpty;
+
+        const { queryByTestId } = setup({
+            state: { journalsListLoaded: true, journalsList },
+            testHistory,
+        });
+
+        expect(queryByTestId('journal-search-results-no-keywords')).toBeInTheDocument();
+    });
+
+    it('should show an empty document when results are not loaded', () => {
+        const testQueryPart = '';
+        const path = `/espace/feature-strategic-publishing/#${pathConfig.journals.search}`;
+        const testHistory = createMemoryHistory({ initialEntries: [path] });
+        testHistory.push({
+            path,
+            search: testQueryPart,
+            state: {
+                source: 'code',
+            },
+        });
+
+        const journalsList = mockDataEmpty;
+
+        const { queryByTestId } = setup({
+            state: { journalsListLoaded: false, journalsList },
+            testHistory,
+        });
+
+        expect(queryByTestId('journal-search-results-no-keywords')).toBeInTheDocument();
+    });
     it('should show a message when results are loading', () => {
+        const testQueryPart =
+            'keywords%5BTitle-Astrobiology%5D%5Btype%5D=Title&keywords%5BTitle-Astrobiology%5D%5Btext%5D=Astrobiology&keywords%5BTitle-Astrobiology%5D%5Bid%5D=Title-Astrobiology';
+        const path = `/espace/feature-strategic-publishing/#${pathConfig.journals.search}`;
         const testHistory = createMemoryHistory({ initialEntries: [path] });
         testHistory.push({
             path,
@@ -215,21 +158,18 @@ describe('Search Sort Journals UI', () => {
 
         expect(getByText('Loading journals list')).toBeInTheDocument();
     });
-
     it('should show an error message when a loading error occurs', () => {
-        const setup = ({
-            state = {},
-            testHistory = createMemoryHistory({ initialEntries: ['/'] }),
-            onSearchFn = jest.fn(),
-        }) => {
-            return render(
-                <WithRouter history={testHistory}>
-                    <WithReduxStore initialState={Immutable.Map({ searchJournalsReducer: state })}>
-                        <JournalSearchResult onSearch={onSearchFn} />
-                    </WithReduxStore>
-                </WithRouter>,
-            );
-        };
+        const testQueryPart =
+            'keywords%5BTitle-Astrobiology%5D%5Btype%5D=Title&keywords%5BTitle-Astrobiology%5D%5Btext%5D=Astrobiology&keywords%5BTitle-Astrobiology%5D%5Bid%5D=Title-Astrobiology';
+        const path = `/espace/feature-strategic-publishing/#${pathConfig.journals.search}`;
+        const testHistory = createMemoryHistory({ initialEntries: [path] });
+        testHistory.push({
+            path,
+            search: testQueryPart,
+            state: {
+                source: 'code',
+            },
+        });
 
         const journalsList = mockData;
         const { getByText } = setup({
@@ -237,11 +177,112 @@ describe('Search Sort Journals UI', () => {
                 journalsListLoaded: true,
                 journalsListLoading: false,
                 journalsList,
-                journalsListError: { message: 'Unable to load Journals' },
+                journalsListError: { message: 'Test error message' },
             },
             testHistory,
         });
 
-        expect(getByText('Unable to load Journals')).toBeInTheDocument();
+        expect(getByText('Test error message')).toBeInTheDocument();
+    });
+    it('should use defined default sorting values when none are explicitly provided', () => {
+        const testQueryPart =
+            'keywords%5BTitle-Astrobiology%5D%5Btype%5D=Title&keywords%5BTitle-Astrobiology%5D%5Btext%5D=Astrobiology&keywords%5BTitle-Astrobiology%5D%5Bid%5D=Title-Astrobiology';
+        const journalSearchQueryParams = deparam(testQueryPart);
+        const journalsList = mockDataEmpty;
+        const { sortBy, sortDirection, pageSize } = getSearchResultSortingParams(
+            journalSearchQueryParams,
+            journalsList.per_page,
+            locale.components.searchJournals.sortingDefaults,
+        );
+        expect(sortDirection).toEqual('Asc');
+        expect(sortBy).toEqual('highest_quartile');
+        expect(pageSize).toEqual(10);
+    });
+    it('should use hardcoded default sorting values when no other defaults are provided', () => {
+        const { sortBy, sortDirection, pageSize } = getSearchResultSortingParams({}, undefined, {});
+        expect(sortDirection).toEqual('Desc');
+        expect(sortBy).toEqual('score');
+        expect(pageSize).toEqual(20);
+    });
+    it('should use provided sorting values instead of defaults', () => {
+        const testQueryPart =
+            'keywords%5BTitle-Astrobiology%5D%5Btype%5D=Title&keywords%5BTitle-Astrobiology%5D%5Btext%5D=Astrobiology&keywords%5BTitle-Astrobiology%5D%5Bid%5D=Title-Astrobiology&sortBy=cite_score&sortDirection=Desc&pageSize=50&page=1';
+        const journalSearchQueryParams = deparam(testQueryPart);
+        const journalsList = mockDataEmpty;
+        const { sortBy, sortDirection, pageSize } = getSearchResultSortingParams(
+            journalSearchQueryParams,
+            journalsList.per_page,
+            locale.components.searchJournals.sortingDefaults ?? {},
+        );
+        expect(sortDirection).toEqual('Desc');
+        expect(sortBy).toEqual('cite_score');
+        expect(pageSize).toEqual(50);
+    });
+
+    it('should update the URL when the Journal comparsion button is clicked', () => {
+        const testQueryPart =
+            'keywords%5BTitle-Astrobiology%5D%5Btype%5D=Title&keywords%5BTitle-Astrobiology%5D%5Btext%5D=Astrobiology&keywords%5BTitle-Astrobiology%5D%5Bid%5D=Title-Astrobiology';
+        const path = `/espace/feature-strategic-publishing/#${pathConfig.journals.search}`;
+        const testHistory = createMemoryHistory({ initialEntries: [path] });
+        testHistory.push({
+            path,
+            search: testQueryPart,
+            state: {
+                source: 'code',
+            },
+        });
+        const journalsList = mockData;
+
+        const { queryByTestId } = setup({
+            state: { journalsListLoaded: true, journalsList },
+            testHistory,
+        });
+
+        expect(queryByTestId('journal-comparison-button')).toBeInTheDocument();
+        expect(queryByTestId('journal-comparison-button')).toBeDisabled();
+
+        // should be at least two journals to select for comparison
+        expect(queryByTestId('journal-list-data-col-1-checkbox-0')).toBeInTheDocument();
+        expect(queryByTestId('journal-list-data-col-1-checkbox-1')).toBeInTheDocument();
+
+        act(() => {
+            fireEvent.click(queryByTestId('journal-list-data-col-1-checkbox-0'));
+            fireEvent.click(queryByTestId('journal-list-data-col-1-checkbox-1'));
+        });
+
+        expect(queryByTestId('journal-comparison-button')).not.toBeDisabled();
+
+        act(() => {
+            fireEvent.click(queryByTestId('journal-comparison-button'));
+        });
+
+        // compare button should update the URL path
+        expect(testHistory.location.pathname).toEqual(pathConfig.journals.compare);
+    });
+
+    it('should show custom values and sort order in sort dropdown if provided in props', () => {
+        const sortingProps = {
+            sortingData: {
+                sortBy: [
+                    { value: 'test_1', label: 'Test Sort 1' },
+                    { value: 'test_2', label: 'Test Sort 2' },
+                    { value: 'test_3', label: 'Test Sort 3' },
+                ],
+            },
+            pagingData: {
+                total: 20,
+            },
+            sortDirection: 'Asc',
+            sortBy: 'test_2',
+            pageSize: 10,
+        };
+        function setup(sortingProps) {
+            return getElement(PublicationsListSorting, sortingProps);
+        }
+        const wrapper = setup(sortingProps);
+        // Default sort set in props to be test_2
+        expect(wrapper.find('[data-testid="publication-list-sorting-sort-order"]').props().value).toEqual('Asc');
+        expect(wrapper.find('#sortBy').props().value).toEqual('test_2');
+        expect(wrapper.find('#sortBy').children().length).toEqual(3);
     });
 });
