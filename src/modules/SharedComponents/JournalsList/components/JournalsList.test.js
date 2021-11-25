b1bbf7c68ba475852a5cda24ec88556d5de57eb4
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2021-11-25 13:52:10 +1000

    Updated working branch [#180339792]
    Update test case on working branch

--- src/modules/SharedComponents/JournalsList/components/JournalsList.test.js
+++ src/modules/SharedComponents/JournalsList/components/JournalsList.test.js
@@ -85,4 +85,66 @@ describe('Journal Search Results list', () => {
                 : expect(queryByText(item.label)).not.toBeInTheDocument();
         });
     });
+    it('should show the correct information in the table, minimised', () => {
+        const { getByTestId } = setup({
+            ...testData,
+        });
+
+        mockData.data.map((dataItem, index) => {
+            // Make sure the title of the Journal is in the document as per the data.
+            const titlesElement = getByTestId(`${dataItem.jnl_jid}-${dataItem.jnl_title}-link`);
+            expect(titlesElement).toBeInTheDocument();
+            expect(titlesElement).toHaveTextContent(dataItem.jnl_title);
+
+            const dataElement = getByTestId(`journal-list-data-col-2-min-${index}`);
+            // Only using the first few items in the map for minified view.
+            JournalFieldsMap.slice(0, 3).map(fieldMap => {
+                switch (fieldMap.key) {
+                    case 'highest_quartile':
+                        // data appended with Q
+                        expect(dataElement).toHaveTextContent(`Q${fieldMap.translateFn(dataItem)}`);
+                        break;
+                    case 'fez_journal_doaj':
+                        // expect tooltip to match supplied data.
+                        expect(dataElement.querySelector('p').title).toEqual(fieldMap.toolTipLabel(dataItem));
+                        break;
+                    default:
+                        break;
+                }
+            });
+        });
+    });
+    it('should show the correct information in the table, maximised', () => {
+        Cookies.set('minimalView', false);
+        const { getByTestId } = setup({
+            ...testData,
+        });
+        mockData.data.map((dataItem, index) => {
+            // Make sure the title of the Journal is in the document as per the data.
+            const titlesElement = getByTestId(`${dataItem.jnl_jid}-${dataItem.jnl_title}-link`);
+            expect(titlesElement).toBeInTheDocument();
+
+            const dataElement = getByTestId(`journal-list-data-col-2-full-${index}`);
+            JournalFieldsMap.slice(1).map(fieldMap => {
+                switch (fieldMap.key) {
+                    case 'highest_quartile':
+                        // data appended with Q
+                        expect(dataElement).toHaveTextContent(`Q${fieldMap.translateFn(dataItem)}`);
+                        break;
+                    case 'fez_journal_doaj':
+                        // expect tooltip to match supplied data.
+                        expect(dataElement.querySelector('p').title).toEqual(fieldMap.toolTipLabel(dataItem));
+                        break;
+                    case 'fez_journal_cite_score':
+                        // Normalising spaces in this string, which appears to happen in the component.
+                        expect(dataElement).toHaveTextContent(fieldMap.translateFn(dataItem).replace(/\s\s+/g, ' '));
+                        break;
+                    default:
+                        // expect data to be as returned from function
+                        expect(dataElement).toHaveTextContent(fieldMap.translateFn(dataItem));
+                        break;
+                }
+            });
+        });
+    });
 });
