a615abd951e7940513ce7f9c5ac99a9723dea6cf
Cliff Lien <uqclien@gmail.com>
2022-01-17 08:18:16 +1000

    Integrate sentry and react route [#180881872]

--- src/AppErrorBoundary.js
+++ src/AppErrorBoundary.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
 
 class AppErrorBoundary extends React.Component {
     static propTypes = {
         children: PropTypes.node,
     };
-
-    componentDidMount() {
-        if (process.env.ENABLE_LOG) {
-            Sentry.init({
-                dsn: 'https://2e8809106d66495ba3023139b1bcfbe5@sentry.io/301681',
-                integrations: [new Integrations.BrowserTracing()],
-                environment: process.env.BRANCH,
-                release: process.env.GIT_SHA,
-                allowUrls: [/library\.uq\.edu\.au/],
-                ignoreErrors: ['Object Not Found Matching Id'],
-            });
-        }
-    }
 
     componentDidCatch(error, errorInfo) {
         if (process.env.ENABLE_LOG) {
