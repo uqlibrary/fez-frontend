6a85bfcd6138afd971d295a784008fa0d450da99
Lea de Groot <l.degroot@uq.edu.au>
2022-02-01 16:31:04 +1000

    update publicationTypeListManuscript mock [PT #178822299]

--- src/mock/data/records/publicationTypeListManuscript.js
+++ src/mock/data/records/publicationTypeListManuscript.js
@@ -1,4 +1,6 @@
-export default {
+import { hydrateMockSearchList } from '../../../helpers/general';
+
+const publicationTypeListManuscript = {
     "total": 2,
     "took": 1,
     "per_page": 999,
@@ -8,142 +10,36 @@ export default {
     "data": [
         {
             "rek_pid": "UQ:364367",
-            "rek_title_xsdmf_id": null,
             "rek_title": "Henry Fryer, [3rd Australian Auxiliary Hospital], Dartford, [England] to Liz, 23 Jul 1917",
-            "rek_description_xsdmf_id": null,
             "rek_description": "John Denis (Jack) Fryer was born in 1895 at Springsure, Queensland, son of Charles George Fryer and his wife Rosina, née Richards. Charles and Rosina had seven children: Elizabeth Stuart (later Gilmour), William Thomas, Charles George, Henry Hardy, John Denis, Richard Alexander James and Walter Ponsonby. Jack won a scholarship to the University of Queensland and commenced study in 1915. By the end of first term, Jack decided to volunteer for military service in the first Australian Imperial Force. He was commissioned in 1916 and went to France where he was gassed in early 1917. After hospitalisation in England, Jack returned to France for the final push to Amiens, in August 1918. There he was again wounded, by a stick bomb. After the war he re-enrolled at the University of Queensland for the first term of 1920 to complete his English honours examinations. His health failed in 1922 and he died in February 1923. In 1926, as a memorial to their former member and vice-president, members of the University Dramatic Society donated £10 to establish a collection of works in Australian literature. The Fryer collection was maintained in the English Department until the 1950s when it became part of the University of Queensland Library.",
-            "rek_display_type_xsdmf_id": null,
             "rek_display_type": 374,
-            "rek_status_xsdmf_id": null,
             "rek_status": 2,
-            "rek_date_xsdmf_id": null,
             "rek_date": "1917-07-23T00:00:00Z",
-            "rek_object_type_xsdmf_id": null,
             "rek_object_type": 3,
-            "rek_depositor_xsdmf_id": null,
             "rek_depositor": 3946,
-            "rek_created_date_xsdmf_id": null,
             "rek_created_date": "2015-07-08T13:56:45Z",
-            "rek_updated_date_xsdmf_id": null,
             "rek_updated_date": "2020-07-24T07:42:57Z",
-            "rek_file_downloads": 0,
             "rek_citation": "<a class=\"citation_title\" title=\"Click to view Manuscript: Henry Fryer, [3rd Australian Auxiliary Hospital], Dartford, [England] to Liz, 23 Jul 1917\" href=\"/view/UQ:364367\">Henry Fryer, [3rd Australian Auxiliary Hospital], Dartford, [England] to Liz, 23 Jul 1917</a>",
-            "rek_genre_xsdmf_id": null,
             "rek_genre": "text/pdf",
-            "rek_genre_type_xsdmf_id": null,
-            "rek_genre_type": null,
-            "rek_formatted_title_xsdmf_id": null,
-            "rek_formatted_title": null,
-            "rek_formatted_abstract_xsdmf_id": null,
-            "rek_formatted_abstract": null,
-            "rek_depositor_affiliation_xsdmf_id": null,
             "rek_depositor_affiliation": 789,
-            "rek_thomson_citation_count": null,
-            "rek_thomson_citation_count_xsdmf_id": null,
-            "rek_subtype_xsdmf_id": null,
-            "rek_subtype": null,
-            "rek_scopus_citation_count": null,
-            "rek_herdc_notes_xsdmf_id": null,
-            "rek_scopus_doc_type_xsdmf_id": null,
-            "rek_scopus_doc_type": null,
-            "rek_wok_doc_type_xsdmf_id": null,
-            "rek_wok_doc_type": null,
-            "rek_pubmed_doc_type_xsdmf_id": null,
-            "rek_pubmed_doc_type": null,
             "rek_security_inherited": 1,
-            "rek_altmetric_score": null,
-            "rek_altmetric_score_xsdmf_id": null,
-            "rek_altmetric_id": null,
-            "rek_altmetric_id_xsdmf_id": null,
-            "rek_copyright_xsdmf_id": null,
             "rek_copyright": "on",
             "rek_security_policy": 1,
             "rek_datastream_policy": null,
-            "fez_record_search_key_acknowledgements": {
-                "rek_acknowledgements_id": 25415,
-                "rek_acknowledgements_pid": "UQ:364367",
-                "rek_acknowledgements_xsdmf_id": null,
-                "rek_acknowledgements": "This project supported by the Queensland State Government."
-            },
-            "fez_record_search_key_additional_notes": {
-                "rek_additional_notes_id": 550,
-                "rek_additional_notes_pid": "UQ:364367",
-                "rek_additional_notes_xsdmf_id": null,
-                "rek_additional_notes": "Written on letterhead from the Australian Branch B.R.C.S. [British Red Cross Society]. Letterhead at top of page bears the Red Cross emblem."
-            },
-            "fez_record_search_key_advisory_statement": null,
-            "fez_record_search_key_assigned_group_id": [],
-            "fez_record_search_key_assigned_user_id": [
-                {
-                    "rek_assigned_user_id_id": 907647,
-                    "rek_assigned_user_id_pid": "UQ:364367",
-                    "rek_assigned_user_id_xsdmf_id": null,
-                    "rek_assigned_user_id": 3946,
-                    "rek_assigned_user_id_order": 1
-                }
-            ],
-            "fez_record_search_key_author": [
-                {
-                    "rek_author_id": 28817405,
-                    "rek_author_pid": "UQ:364367",
-                    "rek_author_xsdmf_id": null,
-                    "rek_author": "Fryer, John Denis, 1895-1923",
-                    "rek_author_order": 1
-                }
-            ],
-            "fez_record_search_key_author_id": [],
-            "fez_record_search_key_contributor": [
-                {
-                    "rek_contributor_id": 3081119,
-                    "rek_contributor_pid": "UQ:364367",
-                    "rek_contributor_xsdmf_id": null,
-                    "rek_contributor": "Fryer, Henry Hardy",
-                    "rek_contributor_order": 1
-                }
-            ],
-            "fez_record_search_key_contributor_id": [],
-            "fez_record_search_key_datastream_policy": null,
-            "fez_record_search_key_date_available": {
-                "rek_date_available_id": 911585,
-                "rek_date_available_pid": "UQ:364367",
-                "rek_date_available_xsdmf_id": null,
-                "rek_date_available": "2015-01-01T00:00:00Z"
-            },
-            "fez_record_search_key_file_attachment_access_condition": [],
-            "fez_record_search_key_file_attachment_embargo_date": [],
-            "fez_record_search_key_file_attachment_name": [
-                {
-                    "rek_file_attachment_name_id": 3847381,
-                    "rek_file_attachment_name_pid": "UQ:364367",
-                    "rek_file_attachment_name_xsdmf_id": null,
-                    "rek_file_attachment_name": "UQFL23_b1f03_0013a_p01_02.pdf",
-                    "rek_file_attachment_name_order": 1
-                },
-                {
-                    "rek_file_attachment_name_id": 3847382,
-                    "rek_file_attachment_name_pid": "UQ:364367",
-                    "rek_file_attachment_name_xsdmf_id": null,
-                    "rek_file_attachment_name": "presmd_UQFL23_b1f03_0013a_p01_02.xml",
-                    "rek_file_attachment_name_order": 2
-                }
-            ],
+            "fez_record_search_key_acknowledgements": "This project supported by the Queensland State Government.",
+            "fez_record_search_key_additional_notes":
+                "Written on letterhead from the Australian Branch B.R.C.S. [British Red Cross Society]. Letterhead at top of page bears the Red Cross emblem.",
+            "fez_record_search_key_assigned_user_id": [3946],
+            "fez_record_search_key_author": ["Fryer, John Denis, 1895-1923"],
+            "fez_record_search_key_contributor": ["Fryer, Henry Hardy"],
+            "fez_record_search_key_date_available": "2015-01-01T00:00:00Z",
+            "fez_record_search_key_file_attachment_name": ["UQFL23_b1f03_0013a_p01_02.pdf", "presmd_UQFL23_b1f03_0013a_p01_02.xml"],
             "fez_record_search_key_geographic_area": [
-                {
-                    "rek_geographic_area_id": 12218,
-                    "rek_geographic_area_pid": "UQ:364367",
-                    "rek_geographic_area_xsdmf_id": null,
-                    "rek_geographic_area": "0.281572,51.482959 0.149134,51.482959 0.149134,51.406804 0.281572,51.406804 0.281572,51.482959",
-                    "rek_geographic_area_order": 1
-                }
+                "0.281572,51.482959 0.149134,51.482959 0.149134,51.406804 0.281572,51.406804 0.281572,51.482959",
             ],
-            "fez_record_search_key_isderivationof": [],
             "fez_record_search_key_ismemberof": [
                 {
-                    "rek_ismemberof_id": 11490839,
-                    "rek_ismemberof_pid": "UQ:364367",
-                    "rek_ismemberof_xsdmf_id": null,
                     "rek_ismemberof": "UQ:278419",
-                    "rek_ismemberof_order": 1,
                     "parent": {
                         "rek_pid": "UQ:278419",
                         "rek_security_policy": 5,
@@ -153,142 +49,29 @@ export default {
                 }
             ],
             "fez_record_search_key_keywords": [
-                {
-                    "rek_keywords_id": 29105813,
-                    "rek_keywords_pid": "UQ:364367",
-                    "rek_keywords_xsdmf_id": null,
-                    "rek_keywords": "World War, 1914-1918",
-                    "rek_keywords_order": 1
-                },
-                {
-                    "rek_keywords_id": 29105814,
-                    "rek_keywords_pid": "UQ:364367",
-                    "rek_keywords_xsdmf_id": null,
-                    "rek_keywords": "Fryer Library",
-                    "rek_keywords_order": 2
-                },
-                {
-                    "rek_keywords_id": 29105815,
-                    "rek_keywords_pid": "UQ:364367",
-                    "rek_keywords_xsdmf_id": null,
-                    "rek_keywords": "Fryer, Henry Hardy",
-                    "rek_keywords_order": 3
-                },
-                {
-                    "rek_keywords_id": 29105816,
-                    "rek_keywords_pid": "UQ:364367",
-                    "rek_keywords_xsdmf_id": null,
-                    "rek_keywords": "Australian Army. Australian Imperial Force (1914-1921)",
-                    "rek_keywords_order": 4
-                },
-                {
-                    "rek_keywords_id": 29105817,
-                    "rek_keywords_pid": "UQ:364367",
-                    "rek_keywords_xsdmf_id": null,
-                    "rek_keywords": "World War, 1914-1918 -- Australia -- Correspondence",
-                    "rek_keywords_order": 5
-                },
-                {
-                    "rek_keywords_id": 29105818,
-                    "rek_keywords_pid": "UQ:364367",
-                    "rek_keywords_xsdmf_id": null,
-                    "rek_keywords": "World War, 1914-1918 -- Personal narratives, Australian",
-                    "rek_keywords_order": 6
-                },
-                {
-                    "rek_keywords_id": 29105819,
-                    "rek_keywords_pid": "UQ:364367",
-                    "rek_keywords_xsdmf_id": null,
-                    "rek_keywords": "Gilmour, Elizabeth Stuart",
-                    "rek_keywords_order": 7
-                },
-                {
-                    "rek_keywords_id": 29105820,
-                    "rek_keywords_pid": "UQ:364367",
-                    "rek_keywords_xsdmf_id": null,
-                    "rek_keywords": "Soldiers -- Wounds and injuries",
-                    "rek_keywords_order": 8
-                },
-                {
-                    "rek_keywords_id": 29105821,
-                    "rek_keywords_pid": "UQ:364367",
-                    "rek_keywords_xsdmf_id": null,
-                    "rek_keywords": "World War, 1914-1918 -- Hospitals -- England",
-                    "rek_keywords_order": 9
-                },
-                {
-                    "rek_keywords_id": 29105822,
-                    "rek_keywords_pid": "UQ:364367",
-                    "rek_keywords_xsdmf_id": null,
-                    "rek_keywords": "Australia. Army. Battalion, 47th",
-                    "rek_keywords_order": 10
-                },
-                {
-                    "rek_keywords_id": 29105823,
-                    "rek_keywords_pid": "UQ:364367",
-                    "rek_keywords_xsdmf_id": null,
-                    "rek_keywords": "3rd Australian Auxiliary Hospital (Dartford, England)",
-                    "rek_keywords_order": 11
-                }
+                "World War, 1914-1918",
+                "Fryer Library",
+                "Fryer, Henry Hardy",
+                "Australian Army. Australian Imperial Force (1914-1921)",
+                "World War, 1914-1918 -- Australia -- Correspondence",
+                "World War, 1914-1918 -- Personal narratives, Australian",
+                "Gilmour, Elizabeth Stuart",
+                "Soldiers -- Wounds and injuries",
+                "World War, 1914-1918 -- Hospitals -- England",
+                "Australia. Army. Battalion, 47th",
+                "3rd Australian Auxiliary Hospital (Dartford, England)",
             ],
-            "fez_record_search_key_language": [
-                {
-                    "rek_language_id": 5211960,
-                    "rek_language_pid": "UQ:364367",
-                    "rek_language_xsdmf_id": null,
-                    "rek_language": "eng",
-                    "rek_language_order": 1
-                }
-            ],
-            "fez_record_search_key_license": null,
-            "fez_record_search_key_link": [],
-            "fez_record_search_key_link_description": [],
-            "fez_record_search_key_notes": null,
+            "fez_record_search_key_language": ["eng"],
             "fez_record_search_key_oa_status": {
-                "rek_oa_status_id": 305299,
-                "rek_oa_status_pid": "UQ:364367",
-                "rek_oa_status_xsdmf_id": null,
                 "rek_oa_status": 453697,
                 "rek_oa_status_lookup": "Other"
             },
-            "fez_record_search_key_oa_status_type": null,
-            "fez_record_search_key_original_format": {
-                "rek_original_format_id": 10088,
-                "rek_original_format_pid": "UQ:364367",
-                "rek_original_format_xsdmf_id": null,
-                "rek_original_format": "Letter, 3p ; 18 x 23 cm."
-            },
-            "fez_record_search_key_possible_author_id": [],
-            "fez_record_search_key_rights": {
-                "rek_rights_id": 111422,
-                "rek_rights_pid": "UQ:364367",
-                "rek_rights_xsdmf_id": null,
-                "rek_rights": "For all enquiries about this work, please contact the Fryer Library, The University of Queensland Library"
-            },
-            "fez_record_search_key_security_policy": [
-                {
-                    "rek_security_policy_id": 585708,
-                    "rek_security_policy_pid": "UQ:364367",
-                    "rek_security_policy": 5,
-                    "rek_security_policy_order": 1
-                }
-            ],
-            "fez_record_search_key_series": {
-                "rek_series_id": 191119,
-                "rek_series_pid": "UQ:364367",
-                "rek_series_xsdmf_id": null,
-                "rek_series": "John Denis Fryer Collection, UQFL23"
-            },
-            "fez_record_search_key_source": {
-                "rek_source_id": 59761,
-                "rek_source_pid": "UQ:364367",
-                "rek_source_xsdmf_id": null,
-                "rek_source": "John Denis Fryer Collection, UQFL23, Box 1, Folder 3, item 13"
-            },
-            "fez_record_search_key_subject": [],
-            "fez_record_search_key_transcript": null,
-            "fez_record_search_key_translated_title": null,
-            "fez_record_search_key_type_of_data": [],
+            "fez_record_search_key_original_format": "Letter, 3p ; 18 x 23 cm.",
+            "fez_record_search_key_rights":
+                "For all enquiries about this work, please contact the Fryer Library, The University of Queensland Library",
+            "fez_record_search_key_security_policy": [5],
+            "fez_record_search_key_series": "John Denis Fryer Collection, UQFL23",
+            "fez_record_search_key_source": "John Denis Fryer Collection, UQFL23, Box 1, Folder 3, item 13",
             "fez_datastream_info": [
                 {
                     "dsi_id": 2742258,
@@ -336,21 +119,12 @@ export default {
                     "dsi_security_inherited": 1
                 }
             ],
-            "fez_matched_journals": null,
-            "fez_record_search_key_issn": [],
-            "fez_record_search_key_doi": null,
-            "fez_record_search_key_isi_loc": null,
-            "fez_record_search_key_scopus_id": null,
-            "fez_record_search_key_isdatasetof": [],
-            "fez_record_search_key_has_related_datasets": [],
-            "fez_record_search_key_has_derivations": [],
             "rek_author_count": 1,
             "rek_display_type_lookup": "Manuscript",
-            "rek_pubmed_doc_type_lookup": null,
             "rek_object_type_lookup": "Record",
-            "rek_scopus_doc_type_lookup": null,
             "rek_status_lookup": "Published",
             "rek_wok_doc_type_lookup": "Abstract of Published Item"
         },
     ]
 }
+export default hydrateMockSearchList(publicationTypeListManuscript);
