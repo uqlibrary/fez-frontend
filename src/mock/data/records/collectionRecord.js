5e012c953d7b574b18fa2660c93476a638c66a6c
Lea de Groot <l.degroot@uq.edu.au>
2022-02-02 13:40:15 +1000

    put extra data back in collection [PT #178822299]

--- src/mock/data/records/collectionRecord.js
+++ src/mock/data/records/collectionRecord.js
@@ -9,6 +9,22 @@ const collectionRecord = {
     rek_status_lookup: 'Published',
     rek_object_type: 2,
     rek_object_type_lookup: 'Collection',
+    "rek_citation": "<a class=\"citation_collections\" title=\"Click to list records in Collection Aboriginal+and+Torres+Strait+Islander+Studies+Unit+Publications\" href=\"/records/search?searchQueryParams%5Brek_ismemberof%5D%5Bvalue%5D%5B%5D=UQ%3A11398&searchMode=advanced\">Aboriginal+and+Torres+Strait+Islander+Studies+Unit+Publications</a>",
+    "fez_record_search_key_ismemberof": [
+        {
+            "rek_ismemberof": "UQ:12096",
+            "parent": {
+                "rek_pid": "UQ:12096",
+                "rek_security_policy": 1,
+                "rek_datastream_policy": null
+            },
+            "rek_ismemberof_lookup": "Aboriginal and Torres Strait Islander Studies Unit"
+        }
+    ],
+    "fez_record_search_key_keywords": [
+        "Torres Strait Islanders",
+        "Aborigines",
+    ],
     fez_record_search_key_xsd_display_option: [263],
     rek_wok_doc_type_lookup: 'Abstract of Published Item',
 };
