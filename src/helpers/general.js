4019e34090a52aec2923db39924c3e55da96f338
Lea de Groot <l.degroot@uq.edu.au>
2022-02-03 14:54:45 +1000

    test for bad hydration data properly [PT #178822299]

--- src/helpers/general.js
+++ src/helpers/general.js
@@ -15,7 +15,6 @@ export const stripHtml = html => {
 };
 
 export function hydrateMock(truncatedData) {
-    /* istanbul ignore next */
     if (!truncatedData.rek_pid) {
         throw Error('missing PID in data ' + JSON.stringify(truncatedData));
     }
