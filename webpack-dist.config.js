eb7a4d6566612f1e6b02b1a05133b652b750db67
uqmmoise <m.moises@library.uq.edu.au>
2022-02-10 15:02:52 +1000

    - enabling profiler for SP branch [#180240462]

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -227,6 +227,7 @@ const webpackConfig = {
         alias: {
             '@material-ui/styles': resolve(__dirname, 'node_modules', '@material-ui/styles'),
             'react-dom$': resolve(__dirname, 'node_modules', 'react-dom/profiling'),
+            'scheduler/tracing': resolve(__dirname, 'node_modules', 'scheduler/tracing-profiling'),
         },
     },
     performance: {
@@ -254,8 +255,9 @@ if (!!process.env.SENTRY_AUTH_TOKEN) {
 
 // enable profiler
 if (!!process.env.ENABLE_PROFILING) {
-    console.log('ENABLE_PROFILING=true, enabling profiling');
-    webpackConfig.resolve.alias['react-dom$'] = resolve(__dirname, 'node_modules', 'react-dom/profiling');
+    // webpackConfig.resolve.alias['react-dom$'] = resolve(__dirname, 'node_modules', 'react-dom/profiling');
+    // eslint-disable-next-line max-len
+    // webpackConfig.resolve.alias['scheduler/tracing'] = resolve(__dirname, 'node_modules', 'scheduler/tracing-profiling');
 }
 
 module.exports = webpackConfig;
