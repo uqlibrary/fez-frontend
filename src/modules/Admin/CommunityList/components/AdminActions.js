633de24ffc9ff412eceace172a4fdbb91350e064
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2022-02-14 13:32:25 +1000

    data table adjustments

---
+++ src/modules/Admin/CommunityList/components/AdminActions.js
@@ -0,0 +1,131 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { debounce } from 'throttle-debounce';
+
+// import { PUBLICATION_TYPES_WITH_DOI, RECORD_ACTION_URLS as defaultActions,
+// RECORD_TYPE_RECORD } from 'config/general';
+// import { DOI_CROSSREF_PREFIX, DOI_DATACITE_PREFIX } from 'config/general';
+
+export const navigateToUrl = (uri, target, navigatedFrom, options) => {
+    let fullUri = uri;
+    if (navigatedFrom) {
+        const queryStringGlue = uri.indexOf('?') > -1 ? '&' : '?';
+        fullUri = `${uri}${queryStringGlue}navigatedFrom=${encodeURIComponent(navigatedFrom)}`;
+    }
+    window.open(fullUri, target, options);
+};
+
+export const AdminActions = ({
+    adminActions = [
+        {
+            label: 'Edit selected record',
+            url: pid => `/adminx/editx/${pid}`,
+            inApp: true,
+            showInDeleted: true,
+            options: null,
+            isRecordEdit: true,
+        },
+        {
+            label: 'Change security for record',
+            url: pid => `/adminx/editx/${pid}`,
+            inApp: true,
+            showInDeleted: true,
+            options: null,
+            isRecordEdit: true,
+        },
+        {
+            label: 'Other Actions',
+            url: pid => `/adminx/editx/${pid}`,
+            inApp: true,
+            showInDeleted: true,
+            options: null,
+            isRecordEdit: true,
+        },
+    ],
+    record,
+}) => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const pid = record;
+    // const displayType = publication.rek_display_type;
+    // const recordType = (publication.rek_object_type_lookup
+    // && publication.rek_object_type_lookup.toLowerCase()) || '';
+    // const isTypeRecord = recordType === RECORD_TYPE_RECORD;
+    // const doi = !!publication.fez_record_search_key_doi && publication.fez_record_search_key_doi.rek_doi;
+    // const hasUQDoi = !!doi && (doi.indexOf(DOI_CROSSREF_PREFIX) === 0 || doi.indexOf(DOI_DATACITE_PREFIX) === 0);
+
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    /* istanbul ignore next */
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    // Remove actions which should not be shown for deleted records, if specified
+    // let filteredActions = isRecordDeleted ? adminActions.filter(action => action.showInDeleted) : adminActions;
+
+    // 'change display type' only applies to Record types
+    // filteredActions = filteredActions.filter(action => {
+    //    return !action.isChangeDisplayMenu || isTypeRecord;
+    // });
+
+    // Restrict DOI option to restricted types
+    // const isDoiType = isTypeRecord && PUBLICATION_TYPES_WITH_DOI.includes(displayType);
+    // filteredActions = filteredActions.filter(action => !action.isDoi || (isDoiType && (!doi || hasUQDoi)));
+
+    const menuOptions = adminActions.map(action => {
+        const linkTarget = action.inApp ? '_self' : '_blank';
+        const options = action.options || null;
+        const url = action.url(pid);
+        const clickHandler = (forceNewTab = false) =>
+            debounce(300, true, event => {
+                navigateToUrl(
+                    url,
+                    event.ctrlKey || forceNewTab ? '_blank' : linkTarget,
+                    !!action.isRecordEdit,
+                    options,
+                );
+            });
+        const label = action.label;
+        return {
+            label,
+            clickHandler,
+        };
+    });
+
+    return (
+        <React.Fragment>
+            <IconButton id="admin-actions-button" aria-label="More" aria-haspopup="true" onClick={handleClick}>
+                <MoreVertIcon fontSize="small" />
+            </IconButton>
+            <Menu id="admin-actions-menu" anchorEl={anchorEl} open={open} onClose={handleClose}>
+                {menuOptions.map((option, index) => (
+                    <MenuItem
+                        key={index}
+                        onClick={option.clickHandler()}
+                        onContextMenu={option.clickHandler(true)}
+                        onAuxClick={option.clickHandler(true)}
+                    >
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </Menu>
+        </React.Fragment>
+    );
+};
+
+AdminActions.propTypes = {
+    adminActions: PropTypes.array,
+    isRecordDeleted: PropTypes.bool,
+    navigatedFrom: PropTypes.string,
+    record: PropTypes.number,
+};
+
+export default React.memo(AdminActions);
---
+++ src/modules/Admin/CommunityList/components/CommunityList.js
@@ -0,0 +1,268 @@
+import React from 'react';
+import { StandardPage } from 'modules/SharedComponents/Toolbox/StandardPage';
+import MUIDataTable from 'mui-datatables';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from '@material-ui/core/Typography';
+import AdminActions from './AdminActions';
+import { communitySearchList } from 'mock/data';
+const theme = createTheme();
+
+const communityList = communitySearchList.data.filter(object => object.rek_display_type === 11);
+
+const columns = [
+    {
+        name: 'rek_pid',
+        label: 'ID',
+        options: {
+            filter: false,
+            sort: false,
+            display: 'excluded',
+        },
+    },
+    {
+        name: 'rek_title',
+        label: 'Title',
+        options: {
+            filter: true,
+            sort: true,
+            customBodyRender: (value, tableMeta) => {
+                return !!tableMeta.rowData[2] ? (
+                    <div>
+                        <Typography variant="body2">
+                            <a href="#">{value}</a>
+                        </Typography>
+                        <Typography variant="caption">{tableMeta.rowData[2]}</Typography>
+                    </div>
+                ) : (
+                    <Typography variant="body2">
+                        <a href={`/view/${tableMeta.rowData[0]}`}>{value}</a>
+                    </Typography>
+                );
+            },
+        },
+    },
+
+    {
+        name: 'rek_description',
+        label: 'Description',
+        options: {
+            display: 'excluded',
+            filter: false,
+        },
+    },
+
+    {
+        name: 'rek_created_date',
+        label: 'Created Date',
+        options: {
+            filter: false,
+            sort: true,
+        },
+    },
+    {
+        name: 'rek_updated_date',
+        label: 'Updated Date',
+        options: {
+            filter: false,
+            sort: true,
+            customBodyRender: value => {
+                return <AdminActions record={rowArrayData.rowData[0]} />;
+            },
+        },
+    },
+    {
+        name: 'actions',
+        label: 'Actions',
+        options: {
+            filter: false,
+            sort: false,
+            customBodyRender: (value, rowArrayData) => {
+                return <AdminActions record={rowArrayData.rowData[0]} />;
+            },
+        },
+    },
+    {
+        name: 'citationCount',
+        label: 'Citation Count',
+        options: {
+            display: 'excluded',
+            filter: true,
+            sort: false,
+        },
+    },
+];
+
+// const data = [
+//     { name: 'Joe James', company: 'Test Corp', city: 'Yonkers', state: 'NY' },
+//     { name: 'John Walsh', company: 'Test Corp', city: 'Hartford', state: 'CT' },
+//     { name: 'Bob Herm', company: 'Test Corp', city: 'Tampa', state: 'FL' },
+//     { name: 'James Houston', company: 'Test Corp', city: 'Dallas', state: 'TX' },
+// ];
+
+// const rows = [
+//     {
+//         id: 1,
+//         title: 'Aboriginal and Torres Strait Islander Studies Unit',
+//         createdDate: '1/1/2002',
+//         updatedDate: '1/2/2015',
+//         citationCount: 5,
+//         scopusCitationCount: 1,
+//         altmetricScore: 10,
+//     },
+//     {
+//         id: 2,
+//         title: 'Advanced Computational Modelling Centre',
+//         createdDate: '1/1/2002',
+//         updatedDate: '1/2/2015',
+//         citationCount: 2,
+//         scopusCitationCount: 5,
+//         altmetricScore: 14,
+//     },
+//     {
+//         id: 3,
+//         title: 'AustLit - The Resource for Australian Literature',
+//         createdDate: '1/1/2002',
+//         updatedDate: '1/2/2015',
+//         citationCount: 5,
+//         scopusCitationCount: 8,
+//         altmetricScore: 40,
+//     },
+//     {
+//         id: 4,
+//         title: 'Australian Centre for Complementary Medicine, Education and Research',
+//         subTitle: 'testing sub title',
+//         createdDate: '1/1/2002',
+//         updatedDate: '1/2/2015',
+//         citationCount: 6,
+//         scopusCitationCount: 5,
+//         altmetricScore: 18,
+//     },
+//     {
+//         id: 5,
+//         title: 'Australian Institute for Bioengineering and Nanotechnology',
+//         createdDate: '1/1/2002',
+//         updatedDate: '1/2/2015',
+//         citationCount: 7,
+//         scopusCitationCount: 5,
+//         altmetricScore: 19,
+//     },
+//     {
+//         id: 6,
+//         title: 'Conferences',
+//         createdDate: '1/1/2002',
+//         updatedDate: '1/2/2015',
+//         citationCount: 8,
+//         scopusCitationCount: 5,
+//         altmetricScore: 6,
+//     },
+//     {
+//         id: 7,
+//         title: 'Digilib: Architecture Image Library',
+//         createdDate: '1/1/2002',
+//         updatedDate: '1/2/2015',
+//         citationCount: 9,
+//         scopusCitationCount: 5,
+//         altmetricScore: 16,
+//     },
+//     {
+//         id: 8,
+//         title: 'ERA',
+//         createdDate: '1/1/2002',
+//         updatedDate: '1/2/2015',
+//         citationCount: 5,
+//         scopusCitationCount: 1,
+//         altmetricScore: 4,
+//     },
+//     {
+//         id: 9,
+//         title: 'ERA Non-Traditional Works - Portfolios',
+//         createdDate: '1/1/2002',
+//         updatedDate: '1/2/2015',
+//         citationCount: 3,
+//         scopusCitationCount: 2,
+//         altmetricScore: 22,
+//     },
+//     {
+//         id: 10,
+//         title: 'ERA Other Items',
+//         createdDate: '1/1/2002',
+//         updatedDate: '1/2/2015',
+//         citationCount: 5,
+//         scopusCitationCount: 3,
+//         altmetricScore: 4,
+//     },
+//     {
+//         id: 11,
+//         title: 'Faculty of Business, Economics and Law',
+//         createdDate: '1/1/2002',
+//         updatedDate: '1/2/2015',
+//         citationCount: 6,
+//         scopusCitationCount: 4,
+//         altmetricScore: 5,
+//     },
+//     {
+//         id: 12,
+//         title: 'Faculty of Engineering, Architecture and Information Technology',
+//         createdDate: '1/1/2002',
+//         updatedDate: '1/2/2015',
+//         citationCount: 7,
+//         scopusCitationCount: 6,
+//         altmetricScore: 8,
+//     },
+//     {
+//         id: 13,
+//         title: 'Faculty of Health and Behavioural Sciences',
+//         createdDate: '1/1/2002',
+//         updatedDate: '1/2/2015',
+//         citationCount: 9,
+//         scopusCitationCount: 8,
+//         altmetricScore: 10,
+//     },
+//     {
+//         id: 14,
+//         title: 'Faculty of Humanities and Social Sciences',
+//         createdDate: '1/1/2002',
+//         updatedDate: '1/2/2015',
+//         citationCount: 15,
+//         scopusCitationCount: 11,
+//         altmetricScore: 12,
+//     },
+//     {
+//         id: 15,
+//         title: 'Faculty of Medicine',
+//         createdDate: '1/1/2002',
+//         updatedDate: '1/2/2015',
+//         citationCount: 35,
+//         scopusCitationCount: 5,
+//         altmetricScore: 11,
+//     },
+// ];
+const options = {
+    filterType: 'checkbox',
+    rowsPerPageOptions: [10, 20, 50, 100],
+    serverSide: false,
+    onTableChange: (action, tableState) => {
+        console.log(action, tableState);
+    },
+};
+export const CommunityList = () => {
+    const txt = {
+        title: 'List of Communities',
+        headings: 'none',
+    };
+    return (
+        <StandardPage title={txt.title}>
+            <ThemeProvider theme={theme}>
+                <MUIDataTable
+                    title={txt.title}
+                    data={communityList}
+                    columns={columns}
+                    options={options}
+                    rowsPerPageOptions={[10, 20, 50, 100]}
+                />
+            </ThemeProvider>
+        </StandardPage>
+    );
+};
+
+export default React.memo(CommunityList);
