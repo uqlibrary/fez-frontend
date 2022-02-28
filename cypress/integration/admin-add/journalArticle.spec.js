d6c54971dc94528b6fd5950be5048a4433dd1493
Cliff Lien <uqclien@gmail.com>
2022-02-28 12:35:53 +1000

    Added a e2e test for journal lookup in admin add form [#181396291]

--- cypress/integration/admin-add/journalArticle.spec.js
+++ cypress/integration/admin-add/journalArticle.spec.js
@@ -1,5 +1,5 @@
 context('As an admin,', () => {
-    it('I can add a video', () => {
+    it('I can add a journal article', () => {
         cy.visit('/admin/add?user=uqstaff');
 
         // Choose a collection
@@ -9,7 +9,13 @@ context('As an admin,', () => {
         // Choose display type
         cy.get('[data-testid=rek-display-type-select]').click();
         cy.get('[data-testid=rek-display-type-options]')
-            .contains('li', 'Video')
+            .contains('li', 'Journal Article')
+            .click();
+
+        // Choose sub type
+        cy.get('[data-testid=rek-subtype-select]').click();
+        cy.get('[data-testid=rek-subtype-options]')
+            .contains('li', 'Article (original research)')
             .click();
 
         // Apply selections
@@ -27,11 +33,14 @@ context('As an admin,', () => {
         // Fill required fields
         cy.typeCKEditor('rek-title', 'Test title');
         cy.get('[data-testid=rek-date-year-input]').type('2020');
-        cy.get('[data-testid=rek-rights-input]').type('All rights reserved');
         cy.get('[data-testid=rek-author-add]').click();
         cy.get('[data-testid=rek-author-input]').type('Test author');
         cy.get('[data-testid=rek-author-add-save]').click();
         cy.get('[data-testid=rek-copyright-input]').click();
+
+        // Lookup journal
+        cy.get('[data-testid=fez-matched-journals-input]').type('acta');
+        cy.clickAutoSuggestion('fez-matched-journals', 0);
 
         // Submit form
         cy.get('#admin-work-submit')
