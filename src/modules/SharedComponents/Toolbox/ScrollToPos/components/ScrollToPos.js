0ba41966441e4dd5f478b5fca4f3ff34b82d8bc5
Lee Sibbald <lsibbald100+git@gmail.com>
2022-03-24 14:30:42 +1000

    [#181442454] modified scrollTo calls to gracefully fail if method is unavailable (to avoid test fails)

--- src/modules/SharedComponents/Toolbox/ScrollToPos/components/ScrollToPos.js
+++ src/modules/SharedComponents/Toolbox/ScrollToPos/components/ScrollToPos.js
@@ -5,7 +5,7 @@ export default function ScrollToPos({ selector = window, x = 0, y = 0 } = {}) {
     const { pathname } = useLocation();
 
     useEffect(() => {
-        typeof selector === 'string' ? document.querySelector(selector).scrollTo(x, y) : selector.scrollTo(x, y);
+        typeof selector === 'string' ? document.querySelector(selector)?.scrollTo(x, y) : selector?.scrollTo(x, y);
     }, [pathname, selector, x, y]);
 
     return null;
