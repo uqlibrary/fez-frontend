d371f5f8eacc492ba3ac3c78707611d5ccc10e0d
Lee Sibbald <lsibbald100+git@gmail.com>
2022-04-01 15:31:35 +1000

    [#181692639] Moved link formatter function to helpers/general

--- src/modules/ViewJournal/components/partials/LinkTemplate.js
+++ src/modules/ViewJournal/components/partials/LinkTemplate.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import DefaultTemplate from './DefaultTemplate';
 import { ExternalLink } from 'modules/SharedComponents/ExternalLink';
 import { makeStyles } from '@material-ui/core/styles';
-import ReactHtmlParser from 'react-html-parser';
+import { formatUrlTextWithWbrTags as formatUrlText } from 'helpers/general';
 
 export const useStyles = makeStyles(() => ({
     wrappableExternalLink: {
@@ -20,32 +20,6 @@ export const useStyles = makeStyles(() => ({
         cursor: 'pointer',
     },
 }));
-
-/**
- * Insert line break opportunities into a URL
- */
-const formatUrlText = url => {
-    // Split the URL into an array to distinguish double slashes from single slashes
-    const doubleSlash = url.split('//');
-
-    // Format the strings on either side of double slashes separately
-    const formatted = doubleSlash
-        .map(
-            str =>
-                // Insert a word break opportunity after a colon
-                str
-                    .replace(/(?<after>:)/giu, '$1<wbr>')
-                    // Before a single slash, tilde, period, comma, hyphen,
-                    // underline, question mark, number sign, or percent symbol
-                    .replace(/(?<before>[/~.,\-_?#%])/giu, '<wbr>$1')
-                    // Before and after an equals sign or ampersand
-                    .replace(/(?<beforeAndAfter>[=&])/giu, '<wbr>$1<wbr>'),
-            // Reconnect the strings with word break opportunities after double slashes
-        )
-        .join('//<wbr>');
-
-    return ReactHtmlParser(formatted);
-};
 
 export const LinkTemplate = ({ data, templateProps, fieldId }) => {
     const classes = useStyles();
