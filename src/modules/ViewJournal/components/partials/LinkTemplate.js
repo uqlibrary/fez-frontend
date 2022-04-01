6690e2009ca0beb660a675f533ca811b81995159
Lee Sibbald <lsibbald100+git@gmail.com>
2022-04-01 15:14:18 +1000

    [#181692639] - restored original styles for the ExternalLink component, and overrode them in the calling component so that certain links can wrap to a new line without affecting other links. Also added function to add <wbr> tags within the link text to better inform the browser where it can split the link in the case of it wrapping to a new line.

--- src/modules/ViewJournal/components/partials/LinkTemplate.js
+++ src/modules/ViewJournal/components/partials/LinkTemplate.js
@@ -3,15 +3,66 @@ import PropTypes from 'prop-types';
 
 import DefaultTemplate from './DefaultTemplate';
 import { ExternalLink } from 'modules/SharedComponents/ExternalLink';
+import { makeStyles } from '@material-ui/core/styles';
+import ReactHtmlParser from 'react-html-parser';
+
+export const useStyles = makeStyles(() => ({
+    wrappableExternalLink: {
+        overflow: 'visible !important',
+        textOverflow: 'clip !important',
+        overflowWrap: 'break-word',
+        wordBreak: 'break-word !important',
+        whiteSpace: 'normal !important',
+        maxWidth: '100% !important',
+        minWidth: 0,
+        display: 'inline-block',
+        verticalAlign: 'bottom',
+        cursor: 'pointer',
+    },
+}));
+
+/**
+ * Insert line break opportunities into a URL
+ */
+const formatUrlText = url => {
+    // Split the URL into an array to distinguish double slashes from single slashes
+    const doubleSlash = url.split('//');
+
+    // Format the strings on either side of double slashes separately
+    const formatted = doubleSlash
+        .map(
+            str =>
+                // Insert a word break opportunity after a colon
+                str
+                    .replace(/(?<after>:)/giu, '$1<wbr>')
+                    // Before a single slash, tilde, period, comma, hyphen,
+                    // underline, question mark, number sign, or percent symbol
+                    .replace(/(?<before>[/~.,\-_?#%])/giu, '<wbr>$1')
+                    // Before and after an equals sign or ampersand
+                    .replace(/(?<beforeAndAfter>[=&])/giu, '<wbr>$1<wbr>'),
+            // Reconnect the strings with word break opportunities after double slashes
+        )
+        .join('//<wbr>');
+
+    return ReactHtmlParser(formatted);
+};
 
 export const LinkTemplate = ({ data, templateProps, fieldId }) => {
-    const { href, title, text } = templateProps;
+    const classes = useStyles();
+    const { href, title, text, format = false } = templateProps;
+    const derivedText = text(data);
+    const finalLinkText = format ? formatUrlText(derivedText) : derivedText;
     return (
         <DefaultTemplate
             fieldId={fieldId}
             data={
-                <ExternalLink href={href(data)} title={title} id={`${fieldId}-lookup`}>
-                    {text(data)}
+                <ExternalLink
+                    href={href(data)}
+                    title={title}
+                    id={`${fieldId}-lookup`}
+                    className={classes.wrappableExternalLink}
+                >
+                    {finalLinkText}
                 </ExternalLink>
             }
         />
