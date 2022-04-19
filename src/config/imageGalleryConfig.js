529fd9c8b96fe9a4ecd95400ee33562d41638e36
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2022-04-19 11:51:45 +1000

    [#181815371] config  merge

---
+++ src/config/imageGalleryConfig.js
@@ -0,0 +1,12 @@
+export default {
+    thumbnailImage: {
+        defaultImageName: process.env.GALLERY_IMAGE_PATH_PREPEND
+            ? `${process.env.GALLERY_IMAGE_PATH_PREPEND}uqlogo.svg`
+            : 'assets/29fa0b621d784c4afef5b774a9a6adf5.svg', // TODO - fix this bodge this so it works across installs,
+        defaultImageMimeType: 'image/svg+xml',
+        defaultWidth: 150,
+        defaultHeight: 150,
+        defaultItemsPerRow: 5,
+        defaultLazyLoading: true,
+    },
+};
