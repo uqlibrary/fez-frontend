6e59c6e7356741e2430328fcc69b1e6250cdcd23
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2022-05-03 14:45:21 +1000

    Update ImageGalleryItemImage.js

--- src/modules/SharedComponents/ImageGallery/ImageGalleryItemImage.js
+++ src/modules/SharedComponents/ImageGallery/ImageGalleryItemImage.js
@@ -5,46 +5,62 @@ import PropTypes from 'prop-types';
 import { default as config } from 'config/imageGalleryConfig';
 import { getThumbnail, getUrl } from './Utils';
 import { makeStyles } from '@material-ui/core/styles';
-import Lock from '@material-ui/icons/Lock';
 
 const useStyles = makeStyles(() => ({
     imageGalleryItemImage: {
         objectFit: 'cover',
     },
     lockIcon: {
-        width: '100%',
-        height: 'auto',
-        backgroundColor: '#51247a',
+        position: 'absolute',
+        top: '5px',
+        left: '5px',
+        width: '25px',
+        height: '25px',
+        filter: 'drop-shadow(0px 0px 5px rgba(0,0,0,0.5))',
     },
 }));
 
-const ImageGalleryItemImage = ({ item, security, className, optional, ...rest }) => {
+const ImageGalleryItemImage = ({ item, security, className, optional, setRestricted, setAdvisory, ...rest }) => {
     const classes = useStyles();
 
     const fileData = getThumbnail(item, security.isAdmin, security.isAuthor);
+    const thumbnailRestricted = !!fileData?.thumbnailFileName && (!fileData?.securityStatus || !fileData.isWhiteListed);
+    const thumbnailAdvisory =
+        (!security.isAdmin &&
+            !!fileData?.thumbnailFileName &&
+            // eslint-disable-next-line camelcase
+            item.fez_record_search_key_advisory_statement?.rek_advisory_statement) ??
+        false;
 
-    // const isWhiteListed = config.allowedTypes.indexOf(item.rek_display_type_lookup) >= 0;
+    React.useEffect(() => {
+        if (thumbnailRestricted && !!setRestricted) setRestricted(true);
+        if (thumbnailAdvisory && !!setAdvisory) setAdvisory(true);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-    // console.log('IS THIS WHITELISTED', isWhiteListed);
-    // no thumbnail available but optional is true
-    if (!!!fileData?.thumbnailFileName && optional) return <></>;
-    if (!!fileData?.thumbnailFileName && !fileData?.securityStatus) {
-        return <Lock color={'secondary'} className={classes.lockIcon} />; // thumbnail available but security denied
-    }
+    if (!!!fileData?.thumbnailFileName && optional) return <></>; // no thumbnail available but optional is true
+
     // at this stage fileData could still be null, which is fine as below will fall back to default image
 
     return (
         <img
             id={`imageGalleryItemImage-${item.rek_pid}`}
             data-testid={`imageGalleryItemImage-${item.rek_pid}`}
-            src={`${getUrl(item.rek_pid, fileData?.thumbnailFileName, fileData?.checksums?.thumbnail)}`}
-            onError={e => {
-                e.target.onerror = null;
-                // env vars from root .env file e.g. GALLERY_IMAGE_PATH_PREPEND='/images/thumbs/'
-                // TODO - need a proper fallback image and guaranteed location on server
-                e.target.src = config.thumbnailImage.defaultImageName;
-            }}
-            alt=""
+            src={
+                !thumbnailRestricted && !thumbnailAdvisory
+                    ? `${getUrl(item.rek_pid, fileData?.thumbnailFileName, fileData?.checksums?.thumbnail)}`
+                    : config.thumbnailImage.defaultImageName
+            }
+            onError={
+                /* istanbul ignore next */ e => {
+                    /* istanbul ignore next */
+                    e.target.onerror = null;
+                    // env vars from root .env file e.g. GALLERY_IMAGE_PATH_PREPEND='/images/thumbs/'
+                    // TODO - need a proper fallback image and guaranteed location on server
+                    /* istanbul ignore next */
+                    e.target.src = config.thumbnailImage.defaultImageName;
+                }
+            }
             className={`${classes.imageGalleryItemImage} ${className} image-gallery-item-image`}
             {...rest}
         />
@@ -56,6 +72,8 @@ ImageGalleryItemImage.propTypes = {
     security: PropTypes.object,
     className: PropTypes.string,
     optional: PropTypes.bool,
+    setRestricted: PropTypes.func,
+    setAdvisory: PropTypes.func,
 };
 
 ImageGalleryItemImage.defaultProps = {
