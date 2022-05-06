37f2c54f0fd7b72b9621607923eaa67d8f497441
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2022-05-06 14:42:52 +1000

    Apply Notes fix from LS ticket.

--- src/modules/Admin/CollectionForm/containers/CollectionForm.js
+++ src/modules/Admin/CollectionForm/containers/CollectionForm.js
@@ -4,12 +4,18 @@ import Immutable from 'immutable';
 import CollectionForm from '../components/CollectionForm';
 import { createCollection, checkSession, clearSessionExpiredFlag } from 'actions';
 import { bindActionCreators } from 'redux';
+import { getNotesSectionSearchKeys } from 'actions/transformers';
 
 import queryString from 'query-string';
 
 const FORM_NAME = 'Collection';
 
 const onSubmit = (values, dispatch, props) => {
+    const dataJS = { ...values.toJS() };
+    const data = { ...dataJS, ...getNotesSectionSearchKeys(dataJS) };
+
+    delete data.internalNotes; // transformed above to fez_internal_notes: {ain_detail}
+
     const queryStringObject = queryString.parse(
         location && ((location.hash && location.hash.replace('?', '&').replace('#', '?')) || location.search),
         { ignoreQueryPrefix: true },
@@ -22,9 +28,8 @@ const onSubmit = (values, dispatch, props) => {
             fez_record_search_key_ismemberof: queryStringObject.pid,
         };
     }
-    return dispatch(
-        createCollection({ ...values.toJS(), ...parentPID }, (currentAuthor && currentAuthor.aut_id) || null),
-    ).catch(error => {
+    // eslint-disable-next-line camelcase
+    return dispatch(createCollection(data, { ...parentPID }, currentAuthor?.aut_id || null)).catch(error => {
         throw new SubmissionError({ _error: error });
     });
 };
