b7962851ce8e4e6b352924007839f9f1847723e2
Lee Sibbald <lsibbald100+git@gmail.com>
2022-05-09 14:07:08 +1000

    Tweak to onclick of lock/info icons to prevent parent click event firing on mobile devices if user taps the icons to see the tooltip

--- src/modules/SharedComponents/ImageGallery/ImageGalleryItem.js
+++ src/modules/SharedComponents/ImageGallery/ImageGalleryItem.js
@@ -74,9 +74,11 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-export const viewRecord = (history, url) => {
+function viewRecord(history, url) {
+    if (this.target?.tagName?.toLowerCase() === 'svg') return;
+
     history.push(url);
-};
+}
 
 const ImageGalleryItem = ({
     item,
@@ -95,7 +97,13 @@ const ImageGalleryItem = ({
     const [advisory, setAdvisory] = React.useState(false);
     const historyObject = history ?? useHistory();
 
-    const clickLink = !!url && url.length > 0 ? { onClick: () => viewRecord(historyObject, url), role: 'button' } : {};
+    const clickLink =
+        !!url && url.length > 0
+            ? {
+                  onClick: e => viewRecord.call(e, historyObject, url),
+                  role: 'button',
+              }
+            : {};
 
     return (
         <ImageListItem
@@ -139,7 +147,7 @@ const ImageGalleryItem = ({
                     title={item.title}
                     position="top"
                     actionIcon={
-                        <Tooltip title={txt.components.imageGallery.tooltip.restricted}>
+                        <Tooltip title={txt.components.imageGallery.tooltip.restricted} enterTouchDelay={0}>
                             <LockOutlinedIcon className={internalClasses.icon} size="small" />
                         </Tooltip>
                     }
@@ -152,7 +160,7 @@ const ImageGalleryItem = ({
                     title={item.title}
                     position="top"
                     actionIcon={
-                        <Tooltip title={txt.components.imageGallery.tooltip.advisory}>
+                        <Tooltip title={txt.components.imageGallery.tooltip.advisory} enterTouchDelay={0}>
                             <ErrorOutlineOutlinedIcon className={internalClasses.icon} size="small" />
                         </Tooltip>
                     }
