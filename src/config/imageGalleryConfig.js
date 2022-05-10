a96ec778fa29b99fc07c4a0f7bc8a5dda14c8e66
Lee Sibbald <lsibbald100+git@gmail.com>
2022-05-10 14:36:47 +1000

    Fix to placeholder filename bodge so it works on staging as well as localhost and dev

--- src/config/imageGalleryConfig.js
+++ src/config/imageGalleryConfig.js
@@ -1,7 +1,10 @@
 export default {
     thumbnailImage: {
+        // eslint-disable-next-line no-nested-ternary
         defaultImageName: process.env.GALLERY_IMAGE_PATH_PREPEND
             ? `${process.env.GALLERY_IMAGE_PATH_PREPEND}uqlogo.svg`
+            : location.hostname.indexOf('fez-staging') > -1
+            ? '/assets/29fa0b621d784c4afef5b774a9a6adf5.svg'
             : 'assets/29fa0b621d784c4afef5b774a9a6adf5.svg', // TODO - fix this bodge this so it works across installs,
         defaultImageMimeType: 'image/svg+xml',
         defaultWidth: 150,
