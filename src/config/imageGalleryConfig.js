ea3a3ea0e830b9b3d2bd210b481fababfea64013
Lee Sibbald <lsibbald100+git@gmail.com>
2022-05-25 14:35:40 +1000

    Fix to default image path Requires a full path and prefix to work on staging (but not dev), and probably Prod

--- src/config/imageGalleryConfig.js
+++ src/config/imageGalleryConfig.js
@@ -1,10 +1,11 @@
+import { APP_URL, PATH_PREFIX } from 'config/general';
 export const placeholderImage = require('../../public/images/thumbs/image_unavailable.svg');
 
 export default {
     thumbnailImage: {
         defaultImageName: process.env.GALLERY_IMAGE_PATH_PREPEND
             ? `${process.env.GALLERY_IMAGE_PATH_PREPEND}image_unavailable.svg`
-            : placeholderImage,
+            : `${APP_URL}${PATH_PREFIX}${placeholderImage}`,
         defaultImageMimeType: 'image/svg+xml',
         defaultWidth: 150,
         defaultHeight: 150,
