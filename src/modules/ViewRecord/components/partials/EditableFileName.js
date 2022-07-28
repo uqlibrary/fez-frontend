674bb560492eb160b322b1b8140ab00c341c5dba
Lee Sibbald <lsibbald100+git@gmail.com>
2022-07-28 17:04:09 +1000

    [#182281002] Added accept and reset buttons

--- src/modules/ViewRecord/components/partials/EditableFileName.js
+++ src/modules/ViewRecord/components/partials/EditableFileName.js
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types';
 
 import Grid from '@material-ui/core/Grid';
 import Input from '@material-ui/core/Input';
+import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import ClearIcon from '@material-ui/icons/Clear';
 import EditIcon from '@material-ui/icons/Edit';
+import CheckIcon from '@material-ui/icons/Check';
+import ReplayIcon from '@material-ui/icons/Replay';
 
 import FileName from './FileName';
 import { FileNameProps } from './FileName';
@@ -17,27 +20,37 @@ const EditableFileName = ({ onFileNameChange, onFileNameBlur, handleCancelEdit, 
     const [isInvalid, setIsInvalid] = useState(false);
     const isEdited = useRef(false);
     const originalFilenameRef = useRef(null);
+    const editingFilenameRef = useRef(null);
 
     const onFilenameChangeProxy = event => {
-        event.target.value !== originalFilenameRef.current && (isEdited.current = true);
+        isEdited.current = event.target.value !== originalFilenameRef.current;
         onFileNameChange(event.target.value);
     };
 
     const handleFileCancelEdit = () => {
         setIsEditing(false);
         setIsInvalid(false);
-        onFileNameChange(originalFilenameRef.current, true);
+        onFileNameChange(editingFilenameRef.current ?? originalFilenameRef.current, true);
         handleCancelEdit?.();
     };
 
     const handleFileEditFilename = () => {
-        originalFilenameRef.current = props.fileName;
+        !!!originalFilenameRef.current && (originalFilenameRef.current = props.fileName);
+        editingFilenameRef.current = props.fileName;
         setIsEditing(true);
     };
 
-    const onFileNameBlurProxy = () => {
-        setIsInvalid(!new RegExp(filenameRestrictions, 'gi').test(props.fileName));
+    const handleFileSaveFilename = () => {
+        const isInvalid = !new RegExp(filenameRestrictions, 'gi').test(props.fileName);
+        setIsInvalid(isInvalid);
+        !isInvalid && setIsEditing(false);
         onFileNameBlur?.();
+    };
+
+    const handleFileRestoreFilename = () => {
+        onFileNameChange(originalFilenameRef.current);
+        isEdited.current = false;
+        setIsInvalid(false);
     };
 
     return (
@@ -46,18 +59,37 @@ const EditableFileName = ({ onFileNameChange, onFileNameBlur, handleCancelEdit, 
                 <>
                     <Hidden smDown>
                         <Grid container alignItems={'center'} wrap="nowrap">
-                            <Grid item xs={10} style={{ display: 'flex', alignItems: 'center' }}>
-                                <FileName {...props} />
+                            <Grid item xs={8} style={{ display: 'flex', alignItems: 'center' }}>
+                                {!!!isEdited.current && <FileName {...props} />}
+                                {!!isEdited.current && (
+                                    <Typography variant="body2" color="textPrimary">
+                                        {props.fileName}
+                                    </Typography>
+                                )}
                             </Grid>
                             <Grid item xs>
                                 <IconButton aria-label="rename file" onClick={handleFileEditFilename} size={'small'}>
                                     <EditIcon />
                                 </IconButton>
+                                {!!isEdited.current && (
+                                    <IconButton
+                                        aria-label="rename file"
+                                        onClick={handleFileRestoreFilename}
+                                        size={'small'}
+                                    >
+                                        <ReplayIcon />
+                                    </IconButton>
+                                )}
                             </Grid>
                         </Grid>
                     </Hidden>
                     <Hidden mdUp>
-                        <FileName {...props} />
+                        {!!!isEdited.current && <FileName {...props} />}
+                        {!!isEdited.current && (
+                            <Typography variant="body2" color="textPrimary">
+                                {props.fileName}
+                            </Typography>
+                        )}
                     </Hidden>
                 </>
             ) : (
@@ -68,15 +100,19 @@ const EditableFileName = ({ onFileNameChange, onFileNameBlur, handleCancelEdit, 
                     type={'text'}
                     value={props.fileName}
                     onChange={onFilenameChangeProxy}
-                    onBlur={onFileNameBlurProxy}
                     id={`${props.id}-editing`}
                     data-testid={`${props.id}-editing`}
                     endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton aria-label="cancel rename" onClick={handleFileCancelEdit} size={'small'}>
-                                <ClearIcon />
-                            </IconButton>
-                        </InputAdornment>
+                        <>
+                            <InputAdornment position="end">
+                                <IconButton aria-label="save rename" onClick={handleFileSaveFilename} size={'small'}>
+                                    <CheckIcon />
+                                </IconButton>
+                                <IconButton aria-label="cancel rename" onClick={handleFileCancelEdit} size={'small'}>
+                                    <ClearIcon />
+                                </IconButton>
+                            </InputAdornment>
+                        </>
                     }
                 />
             )}
