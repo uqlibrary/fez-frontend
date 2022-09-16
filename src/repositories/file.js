e9274d04bd0ae5a3358e98acfa6b0874dabd4332
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2022-09-16 15:32:39 +1000

    [#182519486] - handle null or empty description
    Handle null or empty description
    Adjust tests

--- src/repositories/file.js
+++ src/repositories/file.js
@@ -14,7 +14,7 @@ import locale from 'locale/global';
 const moment = require('moment');
 
 const sanitiseDescription = description =>
-    description
+    (description || '')
         .replace(/[[\u2011\u2012\u2013\u2014\u2015\u2017]/g, '-')
         .replace(/[\u2018\u2019\u201A\u201B\u2032\u2035\u2039\u203A]/g, "'")
         .replace(/[\u201C\u201D\u201E\u201F\u00AB\u00BB]/g, '"');
@@ -43,7 +43,9 @@ export const getFileUploadMetadata = (file, collections) => {
     const metadata = {
         dsi_security_inherited: securityInherited ? 1 : 0,
         dsi_security_policy: securityPolicy,
-        dsi_label: sanitiseDescription(file.description),
+        ...(file.description && file.description.length > 0
+            ? { dsi_label: sanitiseDescription(file.description) }
+            : {}),
         ...((file.access_condition_id === FILE_ACCESS_CONDITION_OPEN ||
             file.security_policy === FILE_SECURITY_POLICY_PUBLIC) &&
         !moment(file.date).isSame(moment(), 'day')
