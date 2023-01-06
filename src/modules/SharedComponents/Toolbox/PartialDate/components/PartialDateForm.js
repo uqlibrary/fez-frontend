db46f8825246cf6163c9534f859ac51895f66761
Lee Sibbald <lsibbald100+git@gmail.com>
2023-01-06 11:16:50 +1000

    [#184157317] Fixed class warning in console

--- src/modules/SharedComponents/Toolbox/PartialDate/components/PartialDateForm.js
+++ src/modules/SharedComponents/Toolbox/PartialDate/components/PartialDateForm.js
@@ -120,11 +120,10 @@ export class PartialDateForm extends PureComponent {
     }
 
     static getDerivedStateFromProps(props, state) {
-        console.log('getDerivedStateFromProps');
         if (props.onChange) {
-            console.log('getDerivedStateFromProps inside IF');
             props.onChange(state.setDate(state));
         }
+        return state;
     }
 
     /**
