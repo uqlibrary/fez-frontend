67bcd3296f1a2150e7b771f7c6747eaf2b9141ab
uqmmoise <m.moises@library.uq.edu.au>
2023-01-24 11:01:14 +1000

    - fixing non-related failing cypress tests [#183358715]

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -19,6 +19,7 @@ import 'cypress-plugin-tab';
 import '@cypress/code-coverage/support';
 import 'cypress-wait-until';
 import 'cypress-axe';
+import 'cypress-real-events';
 
 // Custom
 import './adminEdit';
