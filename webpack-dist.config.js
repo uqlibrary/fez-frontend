8dae3102e68e8289c369de209d1c3e885bf0ad83
uqmmoise <m.moises@library.uq.edu.au>
2023-01-31 12:53:33 +1000

    - reverting tuning --max-old-space-size node's option - removing @mui/icons-material to check if this is the culprit of mem increase [#183358715]

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -2,7 +2,7 @@
 
 const { resolve, join } = require('path');
 const webpack = require('webpack');
-// const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -163,12 +163,12 @@ const webpackConfig = {
                 },
             },
         },
-        // minimizer: [
-        //     new TerserPlugin({
-        //         sourceMap: true,
-        //         parallel: true,
-        //     }),
-        // ],
+        minimizer: [
+            new TerserPlugin({
+                sourceMap: false,
+                parallel: true,
+            }),
+        ],
     },
     module: {
         rules: [
