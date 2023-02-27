342dd0f50f9df444c37506aaa1c9c06d8efebe71
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2023-02-27 12:46:55 +1000

    [#183182328] cypress test additions and adjustments

--- cypress/integration/admin-edit/userList.spec.js
+++ cypress/integration/admin-edit/userList.spec.js
@@ -1,5 +1,6 @@
 context('Manage User List', () => {
     beforeEach(() => {
+        cy.viewport(1200, 1000);
         cy.visit('/admin/users?user=uqstaff');
     });
 
@@ -10,4 +11,13 @@ context('Manage User List', () => {
         cy.get('#usr-id-0').should('exist');
         cy.get('#usr-id-1').should('exist');
     });
+    it('Handles bulk delete', () => {
+        cy.get('[data-testid="select-user-1"]').click();
+        cy.get('[data-testid="users-delete-selected-users"]').click();
+        cy.get('[data-testid="confirm-bulk-delete-users-confirmation"]').click();
+        cy.wait(1000);
+        cy.get('#usr-id-1').should('exist');
+        cy.get('#usr-id-0').should('not.exist');
+    });
+    it('Handles row delete', () => {});
 });
