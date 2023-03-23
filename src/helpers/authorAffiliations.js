98634dd2f0bcab9b6c7ebec59ea000f58bbf8316
Lee Sibbald <lsibbald100+git@gmail.com>
2023-03-23 16:43:51 +1000

    [#184647919] Auth affil problem function cleanup and abstraction

--- src/helpers/authorAffiliations.js
+++ src/helpers/authorAffiliations.js
@@ -1,33 +1,41 @@
-const MAX_TOTAL = 100000;
+export const TOTAL = 100;
+export const PRECISION = 1000;
+export const MAX_TOTAL = TOTAL * PRECISION;
 
-export const composeAuthorAffiliationProblems = record => {
-    const uniqueAffiliations = record.fez_author_affiliation?.reduce(
+export const hasValidOrgAffiliations = (author, affiliations) =>
+    affiliations?.filter(item => item.af_author_id === author.rek_author_id)?.every(item => !!item.fez_org_structure) ??
+    false;
+
+export const has100pcAffiliations = (author, affiliations, total = MAX_TOTAL) =>
+    affiliations
+        ?.filter(item => item.af_author_id === author.rek_author_id)
+        .reduce((accumulated, current) => accumulated + current.af_percent_affiliation, 0) >= total ?? false;
+
+export const getUniqueAffiliations = affiliations =>
+    affiliations?.reduce(
         (accumulated, current) =>
             accumulated.includes(current.af_author_id) ? accumulated : [...accumulated, current.af_author_id],
         [],
-    );
+    ) ?? [];
 
-    return record.fez_record_search_key_author_id
-        .map((author, index) => {
-            const hasAffiliations = uniqueAffiliations.includes(author.rek_author_id);
-            const hasOrgAffiliations =
-                hasAffiliations &&
-                record.fez_author_affiliation
-                    ?.filter(item => item.af_author_id === author.rek_author_id)
-                    ?.every(item => !!item.fez_org_structure);
-            return {
-                rek_author_id: author.rek_author_id,
-                rek_author:
-                    record.fez_record_search_key_author[index]?.rek_author ??
-                    author.rek_author_id_lookup ??
-                    'Unknown Author',
-                hasOrgAffiliations,
-                has100pcAffiliations:
-                    hasAffiliations &&
-                    record.fez_author_affiliation
-                        .filter(item => item.af_author_id === author.rek_author_id)
-                        .reduce((accumulated, current) => accumulated + current.af_percent_affiliation, 0) >= MAX_TOTAL,
-            };
-        })
-        .filter(item => item.rek_author_id !== 0 && (!item.hasOrgAffiliations || !item.has100pcAffiliations));
+export const composeAuthorAffiliationProblems = record => {
+    const uniqueAffiliations = getUniqueAffiliations(record.fez_author_affiliation);
+
+    return (
+        record.fez_record_search_key_author_id
+            ?.map((author, index) => {
+                const hasAffiliations = uniqueAffiliations.includes(author.rek_author_id);
+                const hasOrgAffiliations =
+                    hasAffiliations && hasValidOrgAffiliations(author, record.fez_author_affiliation);
+                return {
+                    rek_author_id: author.rek_author_id,
+                    rek_author:
+                        record.fez_record_search_key_author?.[index]?.rek_author ?? author.rek_author_id_lookup ?? '',
+                    hasOrgAffiliations,
+                    has100pcAffiliations:
+                        hasAffiliations && has100pcAffiliations(author, record.fez_author_affiliation),
+                };
+            })
+            .filter(item => item.rek_author_id !== 0 && (!item.hasOrgAffiliations || !item.has100pcAffiliations)) ?? []
+    );
 };
