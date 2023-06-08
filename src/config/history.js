e99b854de4238b3c7ad1b4e532e0963d97eec975
Cliff Lien <uqclien@gmail.com>
2023-06-08 12:50:09 +1000

    Removed debugging code [#185197444]

--- src/config/history.js
+++ src/config/history.js
@@ -1,7 +1,5 @@
 import { createBrowserHistory, createHashHistory } from 'history';
 
-console.log(process.env.NODE_ENV);
-
 export const history =
     process.env.USE_MOCK ||
     process.env.BRANCH === 'production' ||
