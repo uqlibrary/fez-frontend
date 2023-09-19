fb4c0715a86cbf76e8cb506345fa6a38a9ea8e08
Lee Sibbald <lsibbald100+git@gmail.com>
2023-09-19 16:06:26 +1000

    [#184156753] Help Drawer

--- src/modules/SharedComponents/Toolbox/HelpDrawer/components/HelpDrawer.js
+++ src/modules/SharedComponents/Toolbox/HelpDrawer/components/HelpDrawer.js
@@ -2,34 +2,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
-import withStyles from '@mui/styles/withStyles';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-
-const styles = theme => ({
-    paper: {
-        width: 320,
-        padding: theme.spacing(3),
-        [theme.breakpoints.up('sm')]: {
-            padding: theme.spacing(5),
-        },
-        paddingBottom: 0,
-        maxHeight: '100%',
-        boxSizing: 'border-box',
-    },
-    title: {
-        color: theme.palette.primary.main,
-    },
-    button: {
-        float: 'right',
-        marginBottom: 60,
-    },
-});
 
 export class HelpDrawer extends Component {
     static propTypes = {
         buttonLabel: PropTypes.string,
-        classes: PropTypes.object,
         hide: PropTypes.func.isRequired,
         open: PropTypes.bool.isRequired,
         text: PropTypes.any.isRequired,
@@ -40,7 +18,7 @@ export class HelpDrawer extends Component {
     };
 
     render() {
-        const { buttonLabel, classes, hide, open, text, title } = this.props;
+        const { buttonLabel, hide, open, text, title } = this.props;
         let indexedText = null;
         if (this.props.text && this.props.text.props && this.props.text.props.children) {
             indexedText = React.Children.map(this.props.text.props.children, (child, index) => {
@@ -52,11 +30,24 @@ export class HelpDrawer extends Component {
             });
         }
         return (
-            <Drawer classes={{ paper: classes.paper }} open={open} anchor="right" onClose={hide}>
+            <Drawer
+                sx={{
+                    '& .MuiDrawer-paper': {
+                        width: '320px',
+                        padding: { xs: 3, sm: 5 },
+                        paddingBottom: 0,
+                        maxHeight: '100%',
+                        boxSizing: 'border-box',
+                    },
+                }}
+                open={open}
+                anchor="right"
+                onClose={hide}
+            >
                 <Grid container spacing={5} id="help-drawer">
                     <Grid item xs={12}>
                         <Typography
-                            className={classes.title}
+                            color={'primary.main'}
                             component={'h3'}
                             data-testid="help-drawer-title"
                             id="help-drawer-title"
@@ -78,7 +69,7 @@ export class HelpDrawer extends Component {
                     <Grid item xs={12} id="help-drawer-button">
                         <Button
                             children={buttonLabel}
-                            className={classes.button}
+                            sx={{ float: 'right', marginBottom: '60px' }}
                             color="primary"
                             data-analyticsid="help-drawer-close"
                             data-testid="help-drawer-close"
@@ -92,4 +83,4 @@ export class HelpDrawer extends Component {
     }
 }
 
-export default withStyles(styles, { withTheme: true })(HelpDrawer);
+export default HelpDrawer;
