fc04cc8acb18f0d082752bd47862592ce27eb78f
Lee Sibbald <lsibbald100+git@gmail.com>
2023-09-21 13:53:20 +1000

    [#184156753]  Fixed failing tests due to invalid theme import, and tweaked the MuiClassNameSetup file to replace instances of Mui with the espace prefix

--- src/MuiClassNameSetup.js
+++ src/MuiClassNameSetup.js
@@ -1,4 +1,5 @@
 // Replaces Mui 4's deprecated createGenerateClassName
+// https://github.com/mui/material-ui/issues/30011#issuecomment-988049123
 import { unstable_ClassNameGenerator as ClassNameGenerator } from '@mui/material/className';
 
 ClassNameGenerator.configure(componentName => componentName.replace('Mui', 'uq-espace-'));
