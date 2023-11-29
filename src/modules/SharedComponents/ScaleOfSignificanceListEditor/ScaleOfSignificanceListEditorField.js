c2fa7e0044e58ec6a588e3c521b0f9a25e1af0ad
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2023-11-29 21:07:15 +1000

    Removal of redundant selector

--- src/modules/SharedComponents/ScaleOfSignificanceListEditor/ScaleOfSignificanceListEditorField.js
+++ src/modules/SharedComponents/ScaleOfSignificanceListEditor/ScaleOfSignificanceListEditorField.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ScaleOfSignificanceListEditor from './ScaleOfSignificanceListEditor';
-import { useSelector } from 'react-redux';
 
 export default function ScaleOfSignificanceListEditorField(fieldProps) {
-    console.log('Field Props', fieldProps);
-    const contributors = useSelector(state => state.get('adminAuthorsReducer'));
-    console.log('USE SELECTOR CONTRIBS', contributors);
     return (
         <ScaleOfSignificanceListEditor
             listEditorId="rek-significance"
