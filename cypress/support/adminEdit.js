ce5aa048b02adc51683deed39a25a3cc3f947829
Lea de Groot <l.degroot@glibrary.uq.edu.au>
2023-12-14 07:59:06 +1000

    tweak tests [PT #186546619]

--- cypress/support/adminEdit.js
+++ cypress/support/adminEdit.js
@@ -8,6 +8,17 @@ const tabHeadingSelector = '.StandardPage form > div > div div.StandardCard > di
 
 Cypress.Commands.add('loadRecordForAdminEdit', pid => {
     cy.visit(`/admin/edit/${pid}?user=uqstaff`);
+    cy.waitUntil(
+        () =>
+            cy
+                .get('h2')
+                .should('exist')
+                .should('be.visible'),
+        {
+            timeout: 10000,
+        },
+    );
+
     cy.get('h2').should('contain.text', pid);
 });
 
