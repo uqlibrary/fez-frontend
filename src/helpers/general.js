e77f2e3a3f037f6525eb1bdde5ffc4b08aec0e18
uqmmoise <m.moises@library.uq.edu.au>
2023-12-16 07:05:59 +1000

    cc

--- src/helpers/general.js
+++ src/helpers/general.js
@@ -5,6 +5,7 @@ global.dd = (...args) => args.forEach(arg => console.dir(arg, { depth: null }));
 global.dc = (...args) => args.forEach(arg => console.log(arg));
 global.dj = (...args) => args.forEach(arg => console.log(JSON.stringify(arg)));
 
+/* istanbul ignore next */
 const tryCatch = (callback, _default = undefined) => {
     try {
         return callback();
