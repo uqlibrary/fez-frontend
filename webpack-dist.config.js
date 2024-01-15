d410bf789a1384e23764892aa7d267000de693e9
Cliff Lien <uqclien@gmail.com>
2024-01-15 20:09:30 +1000

    Fixed webpack config [#184156816]

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -213,7 +213,6 @@ const webpackConfig = {
                     loader: 'babel-loader',
                     options: {
                         plugins: [
-                            '@babel/plugin-proposal-export-namespace-from',
                             '@babel/plugin-proposal-export-default-from',
                             ['@babel/plugin-transform-spread', { loose: true }],
                         ],
