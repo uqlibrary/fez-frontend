7f0a6d9c7ab334b0a9d76cf647a601d7d0623815
John Zeng <john.zeng@uq.edu.au>
2024-01-19 13:10:23 +1000

    Remove start/end which isn't needed [#186670689]

--- src/config/controlledVocabConfig.js
+++ src/config/controlledVocabConfig.js
@@ -4,8 +4,7 @@ export const controlledVocabConfig = {
     collapseSwitchText: 'Auto-close other expanded',
     viewControlledVocabTitle: 'Explore',
     viewControlledVocabText: 'View',
-    vocabCountTitle: (start, end, total) =>
-        `Displaying controlled vocabularies ${start} to ${end} of ${total} total controlled vocabularies`,
+    vocabCountTitle: total => `Displaying ${total} total controlled vocabularies`,
     collectionCountTitle: (start, end, total, community) =>
         `Displaying ${start} to ${end} of ${total} child controlled vocabularies for '${community}'`,
     formatCreationDate: date => `Created: ${date}`,
--- src/modules/Admin/ControlledVocabularies/ControlledVocabularies.js
+++ src/modules/Admin/ControlledVocabularies/ControlledVocabularies.js
@@ -22,14 +22,6 @@ export const ControlledVocabularies = () => {
     const totalRecords = useSelector(state => state.get('viewVocabReducer').totalRecords);
     const loadingVocabError = useSelector(state => state.get('viewVocabReducer').loadingVocabError);
 
-    const startRecord = useSelector(state => state.get('viewVocabReducer').startRecord);
-    const endRecord = useSelector(state => state.get('viewVocabReducer').endRecord);
-    // when API return undefined but total has value, use those instead.
-    if (totalRecords && totalRecords > 0) {
-        if (!startRecord) startRecord = 1;
-        if (!endRecord) endRecord = totalRecords;
-    }
-
     React.useEffect(() => {
         dispatch(actions.loadControlledVocabList());
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -51,7 +43,7 @@ export const ControlledVocabularies = () => {
                                 id="total-vocab"
                                 data-testid="total-vocab"
                             >
-                                {controlledVocabConfig.vocabCountTitle(startRecord, endRecord, totalRecords)}
+                                {controlledVocabConfig.vocabCountTitle(totalRecords)}
                             </Typography>
                         )}
 
