333c49765467188e9b71c052849c1b42861a8e8d
John Zeng <john.zeng@uq.edu.au>
2024-01-24 13:08:15 +1000

    child table border [#186847782]

---
+++ src/modules/Admin/ControlledVocabularies/components/ChildVocabDataRow.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Grid from '@mui/material/Grid';
+import ChildVocabTable from './ChildVocabTable';
+// import { useSelector, useDispatch } from 'react-redux';
+// import * as actions from 'actions';
+
+export const ChildVocabDataRow = ({ row }) => {
+    // const dispatch = useDispatch();
+
+    // const vocabOpened = useSelector(state => state.get('viewVocabReducer').vocabOpened);
+    console.log('row=', row);
+    const open = false;
+    // const open = vocabOpened.indexOf(row.cvo_id) > -1;
+    // const triggerChildren = openState => {
+    //     // alert('here');
+    //     // dispatch(actions.clearCCCollectionsList());
+    //     console.log('openState=', openState);
+    //     console.log('dispatch=', dispatch, 'actions=', typeof actions.setOpenedVocab);
+    //     dispatch(actions.setOpenedVocab({ id: row.cvo_id, open: openState }));
+    //     // if (openState) {
+    //     //     dispatch(
+    //     //         actions.loadChildVocabList({
+    //     //             pid: row.cvo_id,
+    //     //         }),
+    //     //     );
+    //     // }
+    // };
+
+    return (
+        <Grid
+            container
+            key={row.cvo_id}
+            data-testid={`row-${row.cvo_id}`}
+            sx={{ boxSizing: 'border-box', boxShadow: '0 -1px 0 #eaeaea', padding: '15px 0px 0px' }}
+        >
+            <React.Fragment key={row.cvo_id}>
+                <Grid container sx={{ paddingBottom: '10px' }}>
+                    <Grid item xs={1} sm={1} md={1}>
+                        <Box sx={{ float: 'left', width: '24px' }}>
+                            <IconButton
+                                sx={{ paddingTop: '5px' }}
+                                aria-label="expand row"
+                                size="small"
+                                id={`expand-row-${row.cvo_id}`}
+                                data-analyticsid={`expand-row-${row.cvo_id}`}
+                                data-testid={`expand-row-${row.cvo_id}`}
+                                // onClick={() => {
+                                //     triggerChildren(!open);
+                                // }}
+                            >
+                                {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                            </IconButton>
+                        </Box>
+                    </Grid>
+                    <Grid item md={8} sm={6} xs={6}>
+                        <Box>{row.cvo_title}</Box>
+                    </Grid>
+                    <Grid item md={1} xs={2} sm={2}>
+                        <Box>{/* row.cvo_image_filename*/}</Box>
+                    </Grid>
+                    <Grid item md={1} xs={2} sm={2}>
+                        <Box>{row.cvo_external_id}</Box>
+                    </Grid>
+                    <Grid item md={1} xs={1} sm={1}>
+                        {''}
+                    </Grid>
+                </Grid>
+                {!!open && (
+                    <Grid container>
+                        <Grid item md={12}>
+                            <ChildVocabTable parentRow={row} />
+                        </Grid>
+                    </Grid>
+                )}
+            </React.Fragment>
+        </Grid>
+    );
+};
+ChildVocabDataRow.propTypes = {
+    conf: PropTypes.object,
+    row: PropTypes.object,
+    labels: PropTypes.object,
+};
+export default ChildVocabDataRow;
--- src/modules/Admin/ControlledVocabularies/components/ChildVocabTable.js
+++ src/modules/Admin/ControlledVocabularies/components/ChildVocabTable.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 import locale from 'locale/components';
-import VocabDataRow from './VocabDataRow';
+import ChildVocabDataRow from './ChildVocabDataRow';
 import { useSelector, useDispatch } from 'react-redux';
 import * as actions from 'actions';
 
@@ -34,38 +35,50 @@ export const ChildVocabTable = ({ parentRow }) => {
     console.log('loadingChildVocabError=', loadingChildVocabError);
 
     return (
-        <Grid container spacing={0}>
-            <Grid item md={12}>
-                Title: {parentRow.cvo_title}
-            </Grid>
-            <Grid item md={12}>
-                Key: {parentRow.cvo_id}
-            </Grid>
-            {/* Header Row */}
-            <Grid container spacing={0} sx={{ fontWeight: 400 }} data-testid="vocab-child-header">
-                <Grid item md={1}>
-                    {''}
+        <Box
+            sx={{
+                backgroundColor: '#eee',
+                padding: '20px',
+                boxShadow: 'inset 0px 2px 4px 0px rgba(0,0,0,0.2)',
+            }}
+            data-testid={`vocab-table-${parentRow.cvo_id}`}
+            id={`vocab-table-${parentRow.cvo_id}`}
+        >
+            <Box sx={{ minHeight: 200, backgroundColor: '#FFF', padding: '10px' }}>
+                <Grid container spacing={0}>
+                    <Grid item md={12}>
+                        Title: {parentRow.cvo_title}
+                    </Grid>
+                    <Grid item md={12}>
+                        Key: {parentRow.cvo_id}
+                    </Grid>
+                    {/* Header Row */}
+                    <Grid container spacing={0} sx={{ fontWeight: 400 }} data-testid="vocab-child-header">
+                        <Grid item md={1}>
+                            {''}
+                        </Grid>
+                        <Grid item md={8} sm={6} xs={6}>
+                            {labels.title}
+                        </Grid>
+                        <Grid item md={1} xs={2} sm={2}>
+                            {labels.license}
+                        </Grid>
+                        <Grid item md={1} xs={2} sm={2}>
+                            {labels.external_id}
+                        </Grid>
+                        <Grid item md={1}>
+                            {labels.actions}
+                        </Grid>
+                    </Grid>
+                    {/* Data Row */}
+                    <Grid container sx={{ paddingTop: '10px' }} data-testid="vocab-child-body">
+                        {vocabList.map(row => (
+                            <ChildVocabDataRow key={row.cvo_id} row={row} />
+                        ))}
+                    </Grid>
                 </Grid>
-                <Grid item md={8} sm={6} xs={6}>
-                    {labels.title}
-                </Grid>
-                <Grid item md={1} xs={2} sm={2}>
-                    {labels.license}
-                </Grid>
-                <Grid item md={1} xs={2} sm={2}>
-                    {labels.external_id}
-                </Grid>
-                <Grid item md={1}>
-                    {labels.actions}
-                </Grid>
-            </Grid>
-            {/* Data Row */}
-            <Grid container sx={{ paddingTop: '10px' }} data-testid="vocab-child-body">
-                {vocabList.map(row => (
-                    <VocabDataRow key={row.cvo_id} row={row} />
-                ))}
-            </Grid>
-        </Grid>
+            </Box>
+        </Box>
     );
 };
 ChildVocabTable.propTypes = {
