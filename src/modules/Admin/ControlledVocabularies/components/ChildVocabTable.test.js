ba6b6efea155430bcdf5037903c58512dd45b0e5
John Zeng <john.zeng@uq.edu.au>
2024-01-29 15:08:29 +1000

    ChildVocabTable test [#186847782]

---
+++ src/modules/Admin/ControlledVocabularies/components/ChildVocabTable.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { render, WithReduxStore } from 'test-utils';
+
+import * as mockData from 'mock/data';
+
+import ChildVocabTable from './ChildVocabTable';
+import locale from 'locale/components';
+import Immutable from 'immutable';
+
+const parentRow = mockData.vocabList.data[0];
+const labels = locale.components.controlledVocabulary.columns.labels;
+
+function setup(testProps = {}, state={}) {
+    return render(
+        <WithReduxStore initialState={Immutable.Map(state)}>
+            <ChildVocabTable {...testProps} />
+        </WithReduxStore>
+    );
+}
+
+describe('ControlledVocabularies ChildVocabTable', () => {
+    it('should render the child table', async () => {
+        const { getByText } = setup({ parentRow: parentRow});
+        // expect(getByText('AIATSIS codes')).toBeInTheDocument();
+        expect(getByText('Description')).toBeInTheDocument();
+    });
+});
