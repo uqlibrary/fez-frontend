0f9a4451ab3e1e0f728e411aab69f989fd73ca08
John Zeng <john.zeng@uq.edu.au>
2024-01-31 16:39:37 +1000

    remove experiments

--- src/modules/Admin/ControlledVocabularies/components/ChildVocabTable.test.js
+++ src/modules/Admin/ControlledVocabularies/components/ChildVocabTable.test.js
@@ -35,32 +35,13 @@ describe('ControlledVocabularies ChildVocabTable', () => {
     it('should render the child table', async () => {
         mockApi.onGet(repositories.routes.CHILD_VOCAB_LIST_API(453669).apiUrl)
         .reply(200, mockData.childVocabList[453669]);
-        // console.log('url=', repositories.routes.CHILD_VOCAB_LIST_API(453669).apiUrl);
-        //work1
-        // mockApi.onGet('vocabularies/453669').reply(200, mockData.childVocabList[453669]);
-        //work2
-        // mockApi.onAny().reply(
-        //     (config)=>{
-        //         console.log('config=', config);
-        //         return [200, mockData.childVocabList[453669]];
-        //     }
-        // );
 
-        // const initState={viewChildVocabReducer: {vocabList: mockData.childVocabList['453669'].data}}; 
         const initState={};
-        // const initState={viewChildVocabReducer: mockData.childVocabList['453669']}; 
         const { getByText, getByTestId } = setup({ parentRow: parentRow}, initState);
         //ztodo: when loading one children, it updated the other expanded children.
-        // await waitFor(
-        //     ()=>{getByText('Description').toBeInTheDocument()}
-        // );
         expect(getByText('Description')).toBeInTheDocument();
         await waitFor(() => 
         {
-            //get text not working
-            // expect(getByText('Yukulta')).toBeInTheDocument();
-            //getByTestId works
-            // expect(getByTestId('child-vocab-title-453670')).toBeDefined();
             expect(getByTestId('child-vocab-title-453670')).toBeInTheDocument();
         }
         );
