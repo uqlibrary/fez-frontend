c2c82e685fa8fa1be34fd010713c564aaf89c593
John Zeng <john.zeng@uq.edu.au>
2024-02-16 15:04:46 +1000

    [#186847782] fix cy test

--- cypress/e2e/controlledVocabulalries.spec.js
+++ cypress/e2e/controlledVocabulalries.spec.js
@@ -7,13 +7,13 @@ context('Controlled vocabularies', () => {
     it('Renders the top level controlled vocabulary screen', () => {
         cy.get('[data-testid="page-title"]').should('contain', 'Controlled Vocabulary');
         cy.get('[data-testid="total-vocab"]').should('contain', 'Displaying 42 controlled vocabularies');
-        cy.get('[data-testid="row-451780"]').should('contain', 'Fields of Research');
+        cy.get('[data-testid="row-em-451780"]').should('contain', 'Fields of Research');
         dismissPopover();
     });
 
     it('Renders the child level controlled vocabulary screen', () => {
         cy.get('[data-testid="expand-row-453669"]').click();
-        cy.get('[data-testid="row-453670"]').should('contain', 'Yukulta / Ganggalidda language G34');
+        cy.get('[data-testid="row-em-453670"]').should('contain', 'Yukulta / Ganggalidda language G34');
         dismissPopover();
     });
 });
