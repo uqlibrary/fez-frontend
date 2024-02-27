5276d287fb4b17d026d8363bcaf9f9f25a9143cf
John Zeng <john.zeng@uq.edu.au>
2024-02-27 11:20:50 +1000

    [#186969388] move findCurrentChild into reducer

--- src/reducers/viewChildVocabReducer.js
+++ src/reducers/viewChildVocabReducer.js
@@ -1,5 +1,38 @@
 import * as actions from 'actions/actionTypes';
-import { findCurrentChild } from './fnVocab';
+
+export function findCurrentChild(lists, parentId, currentPath = []) {
+    if (parentId === 0) return [lists, currentPath];
+    if (lists && lists.length) {
+        // current level
+        if (lists[0].cvr_parent_cvo_id === parentId) {
+            return [lists, currentPath];
+        } else {
+            // child level
+            for (let i = 0; i < lists.length; i++) {
+                const em = lists[i];
+                if (em.cvr_child_cvo_id === parentId) {
+                    const path = [
+                        ...currentPath,
+                        { id: em.controlled_vocab.cvo_id, title: em.controlled_vocab.cvo_title },
+                    ];
+                    return [em.controlled_vocab.controlled_vocab_children, path];
+                }
+            }
+            // further level
+            for (let i = 0; i < lists.length; i++) {
+                const em = lists[i];
+                const path = [...currentPath, { id: em.controlled_vocab.cvo_id, title: em.controlled_vocab.cvo_title }];
+                const [currentList, newPath] = findCurrentChild(
+                    em.controlled_vocab.controlled_vocab_children,
+                    parentId,
+                    path,
+                );
+                if (currentList) return [currentList, newPath];
+            }
+        }
+    }
+    return [[], [...currentPath]];
+}
 
 export const initialState = {
     childData: {},
--- src/reducers/viewChildVocabReducer.test.js
+++ src/reducers/viewChildVocabReducer.test.js
@@ -1,6 +1,7 @@
 import { VIEW_CHILD_VOCAB_LOADING, VIEW_CHILD_VOCAB_LOADED, VIEW_CHILD_VOCAB_LOAD_FAILED } from 'actions/actionTypes';
 
-import viewChildVocabReducer from './viewChildVocabReducer';
+import viewChildVocabReducer, { findCurrentChild } from './viewChildVocabReducer';
+import { vocabsFieldResearch } from 'mock/data/vocabsFieldResearch.js';
 
 const initialState = {
     childData: {},
@@ -86,5 +87,54 @@ describe('viewChildVocab reducer', () => {
             parentId: 451799,
         });
         expect(test).toEqual(expected);
+    });
+});
+
+describe('Test findCurrentChild', () => {
+    it('returns the top level when specifying 0', () => {
+        const [list, path] = findCurrentChild(vocabsFieldResearch.data, 0, []);
+        expect(list[0].cvr_parent_cvo_id).toEqual(451780);
+        expect(list[0]).toHaveProperty('controlled_vocab');
+        expect(path).toEqual([]);
+    });
+
+    it('returns the top level', () => {
+        const [list, path] = findCurrentChild(vocabsFieldResearch.data, 451780);
+        expect(list[0].cvr_parent_cvo_id).toEqual(451780);
+        expect(list[0]).toHaveProperty('controlled_vocab');
+        expect(path).toEqual([]);
+    });
+
+    it('returns the child level', () => {
+        const [list, path] = findCurrentChild(vocabsFieldResearch.data, 451799);
+        expect(list[0].cvr_parent_cvo_id).toEqual(451799);
+        expect(list[0]).toHaveProperty('controlled_vocab');
+        expect(path).toEqual([{ id: 451799, title: '01 Mathematical Sciences' }]);
+    });
+
+    it('returns the 3rd level', () => {
+        const [list, path] = findCurrentChild(vocabsFieldResearch.data, 451800, []);
+        expect(list[0].cvr_parent_cvo_id).toEqual(451800);
+        expect(list[0]).toHaveProperty('controlled_vocab');
+        expect(path).toEqual([
+            { id: 451799, title: '01 Mathematical Sciences' },
+            { id: 451800, title: '0101 Pure Mathematics' },
+        ]);
+    });
+
+    it('returns the 4th level', () => {
+        const [list, path] = findCurrentChild(vocabsFieldResearch.data, 451801, []);
+        expect(list).toEqual([]);
+        expect(path).toEqual([
+            { id: 451799, title: '01 Mathematical Sciences' },
+            { id: 451800, title: '0101 Pure Mathematics' },
+            { id: 451801, title: '010101 Algebra and Number Theory' },
+        ]);
+    });
+
+    it('returns []', () => {
+        const [list, path] = findCurrentChild([], 451801, []);
+        expect(list).toEqual([]);
+        expect(path).toEqual([]);
     });
 });
