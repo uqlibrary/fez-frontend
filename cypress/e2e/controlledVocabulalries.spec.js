156f85f2eb4276e36f332cff323ea0009feea24e
John Zeng <john.zeng@uq.edu.au>
2024-02-29 11:52:57 +1000

    [#186969388]cypress test cover 100

--- cypress/e2e/controlledVocabulalries.spec.js
+++ cypress/e2e/controlledVocabulalries.spec.js
@@ -38,4 +38,15 @@ context('Controlled vocabularies', () => {
 
         dismissPopover();
     });
+
+    it('Expand two rows', () => {
+        cy.get('[data-testid="expand-row-451780"]').click();
+        cy.get('[data-testid="child-row-em-451799"]').should('contain', '01 Mathematical Sciences');
+
+        cy.get('[data-testid="expand-row-453669"]').click();
+        console.log('get=', cy.get('[data-testid="nav-451780"]'));
+        cy.get('[data-testid="nav-451780"]').should('have.length', 1);
+
+        dismissPopover();
+    });
 });
