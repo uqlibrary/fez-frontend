162cbff72e9bf829ceee81edfed5fb0affda538f
Lee S <lsibbald100+git@gmail.com>
2024-03-26 09:31:29 +1000

    [#187283348,#187217071] Related Publications component

--- src/modules/ViewRecord/components/RelatedPublications.js
+++ src/modules/ViewRecord/components/RelatedPublications.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import locale from 'locale/viewRecord';
 import { pathConfig } from 'config/pathConfig';
@@ -8,48 +8,13 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Unstable_Grid2';
 
-export class RelatedPublications extends PureComponent {
-    static propTypes = {
-        publication: PropTypes.object.isRequired,
-        title: PropTypes.string,
-        parentSearchKey: PropTypes.object,
-        childrenSearchKey: PropTypes.object.isRequired,
-        showPublicationTitle: PropTypes.bool,
+const RelatedPublications = ({ publication, title, parentSearchKey, childrenSearchKey, showPublicationTitle }) => {
+    const renderTitle = (item, searchKey) => {
+        const pid = item[searchKey.pid];
+        return <Link to={pathConfig.records.view(pid)}>{item[searchKey.title]}</Link>;
     };
 
-    static defaultProps = {
-        title: locale.viewRecord.sections.relatedPublications.title,
-        childrenSearchKey: {
-            key: 'fez_record_search_key_has_related_datasets',
-            pid: 'rek_has_related_datasets',
-            title: 'rek_has_related_datasets_lookup',
-            order: 'rek_has_related_datasets_order',
-        },
-        showPublicationTitle: false,
-    };
-
-    renderList = (publication, parentSearchKey, childrenSearchKey, showPublicationTitle) => {
-        const parents = (parentSearchKey && publication[parentSearchKey.key]) || [];
-        const children = publication[childrenSearchKey.key];
-        const testId = parentSearchKey && parentSearchKey.pid.replace(/_/g, '-');
-
-        return (
-            <Box component={'ul'} sx={{ margin: 0 }} className={'publicationList'}>
-                {this.renderSubList(parents, parentSearchKey)}
-                {showPublicationTitle && (
-                    <li key={'current'}>
-                        <Typography variant="body2" data-testid={`${testId}-current`}>
-                            {publication.rek_title}
-                            <b>{' (' + locale.viewRecord.sections.relatedPublications.currentRecord + ')'}</b>
-                        </Typography>
-                    </li>
-                )}
-                {this.renderSubList(children, childrenSearchKey)}
-            </Box>
-        );
-    };
-
-    renderSubList = (subList, searchKey) => {
+    const renderSubList = (subList, searchKey) => {
         if (subList && searchKey) {
             const testId = searchKey.pid.replace(/_/g, '-');
             return subList
@@ -59,7 +24,7 @@ export class RelatedPublications extends PureComponent {
                     return (
                         <li key={`${searchKey.key}-${index}`}>
                             <Typography variant="body2" data-testid={`${testId}-${index}`}>
-                                {this.renderTitle(item, searchKey)}
+                                {renderTitle(item, searchKey)}
                             </Typography>
                         </li>
                     );
@@ -69,29 +34,60 @@ export class RelatedPublications extends PureComponent {
         }
     };
 
-    renderTitle = (item, searchKey) => {
-        const pid = item[searchKey.pid];
-        return <Link to={pathConfig.records.view(pid)}>{item[searchKey.title]}</Link>;
-    };
-
-    render() {
-        const { publication, parentSearchKey, childrenSearchKey, title, showPublicationTitle } = this.props;
-
-        if (
-            (!parentSearchKey || !publication[parentSearchKey.key] || publication[parentSearchKey.key].length === 0) &&
-            (!publication[childrenSearchKey.key] || publication[childrenSearchKey.key].length === 0)
-        ) {
-            return null;
-        }
+    const renderList = (publication, parentSearchKey, childrenSearchKey, showPublicationTitle) => {
+        const parents = (parentSearchKey && publication[parentSearchKey.key]) || [];
+        const children = publication[childrenSearchKey.key];
+        const testId = parentSearchKey && parentSearchKey.pid.replace(/_/g, '-');
 
         return (
-            <Grid xs={12}>
-                <StandardCard title={title} className="relatedPublications">
-                    {this.renderList(publication, parentSearchKey, childrenSearchKey, showPublicationTitle)}
-                </StandardCard>
-            </Grid>
+            <Box component={'ul'} sx={{ margin: 0 }} className={'publicationList'}>
+                {renderSubList(parents, parentSearchKey)}
+                {showPublicationTitle && (
+                    <li key={'current'}>
+                        <Typography variant="body2" data-testid={`${testId}-current`}>
+                            {publication.rek_title}
+                            <b>{' (' + locale.viewRecord.sections.relatedPublications.currentRecord + ')'}</b>
+                        </Typography>
+                    </li>
+                )}
+                {renderSubList(children, childrenSearchKey)}
+            </Box>
         );
+    };
+
+    if (
+        (!parentSearchKey || !publication[parentSearchKey.key] || publication[parentSearchKey.key].length === 0) &&
+        (!publication[childrenSearchKey.key] || publication[childrenSearchKey.key].length === 0)
+    ) {
+        return null;
     }
-}
+
+    return (
+        <Grid xs={12}>
+            <StandardCard title={title} className="relatedPublications">
+                {renderList(publication, parentSearchKey, childrenSearchKey, showPublicationTitle)}
+            </StandardCard>
+        </Grid>
+    );
+};
+
+RelatedPublications.propTypes = {
+    publication: PropTypes.object.isRequired,
+    title: PropTypes.string,
+    parentSearchKey: PropTypes.object,
+    childrenSearchKey: PropTypes.object.isRequired,
+    showPublicationTitle: PropTypes.bool,
+};
+
+RelatedPublications.defaultProps = {
+    title: locale.viewRecord.sections.relatedPublications.title,
+    childrenSearchKey: {
+        key: 'fez_record_search_key_has_related_datasets',
+        pid: 'rek_has_related_datasets',
+        title: 'rek_has_related_datasets_lookup',
+        order: 'rek_has_related_datasets_order',
+    },
+    showPublicationTitle: false,
+};
 
 export default RelatedPublications;
