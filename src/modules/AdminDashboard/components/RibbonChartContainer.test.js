e5cf158f83dc72edaaf0d0037663db3b2d5a977d
Lee S <lsibbald100+git@gmail.com>
2024-06-03 16:03:55 +1000

    [#187673389] Additional tests

--- src/modules/AdminDashboard/components/RibbonChartContainer.test.js
+++ src/modules/AdminDashboard/components/RibbonChartContainer.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render, preview } from 'test-utils';
+import { render } from 'test-utils';
 import { adminDashboardToday } from 'mock/data/testing/adminDashboard';
 
 import RibbonChartContainer from './RibbonChartContainer';
@@ -56,13 +56,14 @@ describe('RibbonChartContainer', () => {
         expect(getByTestId('testTable')).toBeInTheDocument();
 
         // check table header count
-        const headerExpected = Object.keys(locale).length;
-        const headerActual = container.querySelectorAll('thead th').length;
-        expect(headerExpected).toBe(headerActual);
+        const columnsExpected = Object.keys(locale).filter(item => locale[item].hasOwnProperty('label')).length;
+        const headerActual = container.querySelectorAll('thead th').length - 1; // -1 is to exclude the default first empty header cell
+        expect(columnsExpected).toBe(headerActual);
 
         // check table cell count
         const rowActual = container.querySelectorAll('tbody td').length;
-        expect(headerExpected).toBe(rowActual);
+        // +1 is to include the default first row cell, which contains the graph
+        expect(columnsExpected + 1).toBe(rowActual);
 
         // check specific cell values
         expect(container.querySelectorAll('tbody td')[1]).toHaveTextContent('150');
@@ -70,35 +71,6 @@ describe('RibbonChartContainer', () => {
         expect(container.querySelectorAll('tbody td')[3]).toHaveTextContent('15 (10%');
         expect(container.querySelectorAll('tbody td')[4]).toHaveTextContent('135 (90%)');
     });
-
-    // it('should render component', () => {
-    //     const data = adminDashboardToday.systemalerts;
-    //     const { container, getByText, getByTestId } = setup({
-    //         label,
-    //         data,
-    //         children,
-    //         colours: {},
-    //         'data-testid': 'testTable',
-    //     });
-    //     expect(getByText(label)).toBeInTheDocument();
-    //     expect(getByTestId('test-child')).toBeInTheDocument();
-    //     expect(getByTestId('testTable')).toBeInTheDocument();
-
-    //     // check table header count
-    //     const headerExpected = Object.keys(locale).length;
-    //     const headerActual = container.querySelectorAll('thead th').length;
-    //     expect(headerExpected).toBe(headerActual);
-
-    //     // check table cell count
-    //     const rowActual = container.querySelectorAll('tbody td').length;
-    //     expect(headerExpected).toBe(rowActual);
-
-    //     // check specific cell values
-    //     expect(container.querySelectorAll('tbody td')[1]).toHaveTextContent('150');
-    //     expect(container.querySelectorAll('tbody td')[2]).toHaveTextContent('25');
-    //     expect(container.querySelectorAll('tbody td')[3]).toHaveTextContent('15 (10%');
-    //     expect(container.querySelectorAll('tbody td')[4]).toHaveTextContent('135 (90%)');
-    // });
 
     it('should render colours', () => {
         const data = adminDashboardToday.systemalerts;
@@ -118,7 +90,5 @@ describe('RibbonChartContainer', () => {
         expect(container.querySelectorAll('thead th')[2]).toHaveStyle('border-bottom: 3px solid #00FF00');
         expect(container.querySelectorAll('thead th')[3]).toHaveStyle('border-bottom: 3px solid #0000FF');
         expect(container.querySelectorAll('thead th')[4]).toHaveStyle('border-bottom: 3px solid #000000');
-
-        preview.debug();
     });
 });
