c66fc8826819cf28f0ab643912e223bdd022afed
Lee S <lsibbald100+git@gmail.com>
2024-06-07 11:59:50 +1000

    [#187673389] Test case for Quick Link admin form

--- src/modules/AdminDashboard/components/QuickLinkAdmin.js
+++ src/modules/AdminDashboard/components/QuickLinkAdmin.js
@@ -6,6 +6,9 @@ import { useForm, Controller } from 'react-hook-form';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
+const rootId = 'quicklinks-admin';
 
 const QuickLinkAdmin = ({ locale, item, action, onSubmitClick, onCancelClick, busy = false }) => {
     const {
@@ -14,66 +17,97 @@ const QuickLinkAdmin = ({ locale, item, action, onSubmitClick, onCancelClick, bu
         formState: { errors },
     } = useForm({ defaultValues: item });
 
-    const isDelete = action === 'DELETE';
-    const primaryButtonLabel = isDelete ? locale.button.delete : locale.button.save;
-    const primaryButtonLabelBusy = isDelete ? locale.button.deleteBusy : locale.button.saveBusy;
+    const { isDelete, primaryButtonLabel, primaryButtonLabelBusy } = React.useMemo(() => {
+        const isDelete = action === 'DELETE';
+        const primaryButtonLabel = isDelete ? locale.button.delete : locale.button.save;
+        const primaryButtonLabelBusy = isDelete ? locale.button.deleteBusy : locale.button.saveBusy;
+        return { isDelete, primaryButtonLabel, primaryButtonLabelBusy };
+    }, [action, locale.button.delete, locale.button.deleteBusy, locale.button.save, locale.button.saveBusy]);
 
     const buttonArray = [
-        <Button type="submit" fullWidth color={isDelete ? 'error' : 'primary'} variant="contained" disabled={busy}>
+        <Button
+            type="submit"
+            fullWidth
+            color={isDelete ? 'error' : 'primary'}
+            variant="contained"
+            disabled={busy}
+            data-testid={`${rootId}-action-button`}
+        >
             {busy ? primaryButtonLabelBusy : primaryButtonLabel}
         </Button>,
-        <Button fullWidth variant="contained" color="default" onClick={onCancelClick} disabled={busy}>
+        <Button
+            fullWidth
+            variant="contained"
+            color="default"
+            onClick={onCancelClick}
+            disabled={busy}
+            data-testid={`${rootId}-alt-button`}
+        >
             {locale.button.cancel}
         </Button>,
     ];
     const buttons = isDelete ? buttonArray.reverse() : buttonArray;
 
     return (
-        <form onSubmit={handleSubmit(onSubmitClick)} className="formAddQuickLink" data-testid="quicklinks-admin-form">
+        <form onSubmit={handleSubmit(onSubmitClick)} className="formAddQuickLink" data-testid={`${rootId}-form`}>
             <Grid container mt={2} spacing={2}>
                 <Grid item xs={12}>
-                    <Controller
-                        render={({ field }) => (
-                            <TextField
-                                {...field}
-                                fullWidth
-                                variant="standard"
-                                label={locale.fields.title}
-                                required
-                                multiline
-                                maxRows={2}
-                                disabled={isDelete}
-                                inputProps={{
-                                    'data-testid': 'qlk_title',
-                                }}
-                            />
+                    <Box data-testid="qlk_title">
+                        <Controller
+                            render={({ field }) => (
+                                <TextField
+                                    {...field}
+                                    fullWidth
+                                    variant="standard"
+                                    label={`${locale.fields.title}*`}
+                                    multiline
+                                    maxRows={2}
+                                    disabled={isDelete}
+                                    error={!!!field.value}
+                                    inputProps={{
+                                        'data-testid': 'qlk_title-input',
+                                    }}
+                                />
+                            )}
+                            rules={{ required: true }}
+                            name="qlk_title"
+                            control={control}
+                        />
+                        {errors.qlk_title?.type === 'required' && (
+                            <Box sx={{ color: 'error.main' }} fontSize={'0.75rem'} role="alert">
+                                Required
+                            </Box>
                         )}
-                        name="qlk_title"
-                        control={control}
-                    />
-                    {errors.title && errors.title.message}
+                    </Box>
                 </Grid>
                 <Grid item xs={12}>
-                    <Controller
-                        render={({ field }) => (
-                            <TextField
-                                {...field}
-                                fullWidth
-                                variant="standard"
-                                label={locale.fields.link}
-                                required
-                                multiline
-                                maxRows={3}
-                                disabled={isDelete}
-                                inputProps={{
-                                    'data-testid': 'qlk_link',
-                                }}
-                            />
+                    <Box data-testid="qlk_link">
+                        <Controller
+                            render={({ field }) => (
+                                <TextField
+                                    {...field}
+                                    fullWidth
+                                    variant="standard"
+                                    label={`${locale.fields.link}*`}
+                                    multiline
+                                    maxRows={3}
+                                    disabled={isDelete}
+                                    error={!!!field.value}
+                                    inputProps={{
+                                        'data-testid': 'qlk_link-input',
+                                    }}
+                                />
+                            )}
+                            rules={{ required: true }}
+                            name="qlk_link"
+                            control={control}
+                        />
+                        {errors.qlk_link?.type === 'required' && (
+                            <Box sx={{ color: 'error.main' }} fontSize={'0.75rem'} role="alert">
+                                Required
+                            </Box>
                         )}
-                        name="qlk_link"
-                        control={control}
-                    />
-                    {errors.target && errors.target.message}
+                    </Box>
                 </Grid>
                 {buttons.map((button, index) => (
                     <Grid item xs={12} key={index}>
