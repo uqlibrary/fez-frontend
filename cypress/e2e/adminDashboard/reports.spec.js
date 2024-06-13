0e5944689585da7a1ef238a2928fc3443f9deead
Lee S <lsibbald100+git@gmail.com>
2024-06-13 11:28:44 +1000

    [#187673389] Reports cypress test

--- cypress/e2e/adminDashboard/reports.spec.js
+++ cypress/e2e/adminDashboard/reports.spec.js
@@ -1,35 +1,4 @@
 context('Admin Dashboard - Reports tab', () => {
-    const getRow = row => {
-        return cy.get('[role=row]').eq(row);
-    };
-    const getCell = cell => {
-        return cy.get('[role=gridcell]').eq(cell);
-    };
-
-    const assertDetailDrawer = data => {
-        cy.data('system-alert-detail').should('be.visible');
-        if (!!data) {
-            cy.data('system-alert-detail').within(() => {
-                !!data?.title && cy.data('system-alert-detail-title').contains(data.title);
-                if (!!data?.link) {
-                    cy.data('system-alert-detail-link')
-                        .should('have.attr', 'href', data.link)
-                        .contains(data.link);
-                    cy.data('system-alert-detail-link').within(() => {
-                        cy.data('OpenInNewIcon').should('be.visible');
-                    });
-                }
-                !!data?.alertId && cy.data('system-alert-detail-id').contains(data.alertId);
-                !!data?.createdDate && cy.data('system-alert-detail-date-created').contains(data.createdDate);
-                !!data?.description && cy.data('system-alert-detail-description').contains(data.description);
-                !!data?.status && cy.data('system-alert-detail-assignee-input').should('have.value', data.status);
-                if (data?.status === 'Unassigned') {
-                    cy.data('system-alert-detail-action-button').should('not.exist');
-                } else cy.data('system-alert-detail-action-button').should('exist');
-            });
-        }
-    };
-
     beforeEach(() => {
         cy.loadAdminDashboard();
         cy.get('[role=tab]')
@@ -117,13 +86,19 @@ context('Admin Dashboard - Reports tab', () => {
         cy.data('export-report-progress').should('not.exist');
     });
 
-    it('downloads a display report', () => {
+    it('displays and downloads a display report', () => {
+        // works history
         cy.data('report-display-export-input').click();
         cy.get('[role=option]').should('have.length', 2);
         cy.get('[role=option]')
             .contains('Works history')
             .click();
         cy.data('report-display-button').click();
+        cy.data('report-display-data-grid').should('exist');
+        cy.data('report-display-data-grid').within(() => {
+            cy.get('[role=columnheader]').should('have.length', 5);
+            cy.get('[role=row]').should('have.length', 7); // +1 for header
+        });
 
         cy.task('downloads', Cypress.config('downloadsFolder')).then(before => {
             cy.data('report-display-export-button')
@@ -133,5 +108,115 @@ context('Admin Dashboard - Reports tab', () => {
                 expect(after.length).to.be.eq(before.length + 1);
             });
         });
+
+        // system alerts
+        cy.data('report-display-export-input').click();
+        cy.get('[role=option]').should('have.length', 2);
+        cy.get('[role=option]')
+            .contains('System alert log')
+            .click();
+        cy.data('report-display-button').click();
+        cy.data('report-display-data-grid').should('exist');
+        cy.data('report-display-data-grid').within(() => {
+            cy.get('[role=columnheader]').should('have.length', 8);
+            cy.get('[role=row]').should('have.length', 8); // +1 for header
+        });
+
+        cy.task('downloads', Cypress.config('downloadsFolder')).then(before => {
+            cy.data('report-display-export-button')
+                .should('not.be.disabled')
+                .click();
+            cy.task('downloads', Cypress.config('downloadsFolder')).then(after => {
+                expect(after.length).to.be.eq(before.length + 1);
+            });
+        });
+    });
+
+    it('validates the filters', () => {
+        // use system alerts report as it includes
+        // an additional field, but otherwise the
+        // validation is the same
+        cy.data('report-display-export-input').click();
+        cy.get('[role=option]').should('have.length', 2);
+        cy.get('[role=option]')
+            .contains('System alert log')
+            .click();
+
+        cy.data('report-display-button').should('not.be.disabled');
+        cy.data('report-display-date-from-input').should('not.be.disabled');
+        cy.data('report-display-date-to-input').should('not.be.disabled');
+        cy.data('report-display-system-alert-id-input').should('exist');
+
+        // check the mui calendar appears if button clicked.
+        cy.data('report-display-date-from').within(() => {
+            cy.get('button').click();
+        });
+        cy.get('.MuiPickersPopper-root').should('exist');
+        cy.data('report-display-date-to').within(() => {
+            cy.get('button').click();
+        });
+        cy.get('.MuiPickersPopper-root').should('exist');
+        cy.data('report-display-date-to').within(() => {
+            cy.get('button').click();
+        });
+        cy.get('.MuiPickersPopper-root').should('not.exist');
+        cy.data('report-display-date-to-input').type('01/01/2020');
+        // date from should be in error state
+        cy.data('report-display-date-from-input').should('have.attr', 'required');
+        cy.data('report-display-date-from').contains('Required');
+        cy.data('report-display-date-from-input')
+            .parent()
+            .should('have.class', 'Mui-error');
+        cy.data('report-display-button').should('be.disabled');
+        cy.data('report-display-date-to-input').clear();
+        cy.data('report-display-button').should('not.be.disabled');
+
+        cy.data('report-display-date-from-input').type('01/01/2020');
+        // to date should be in error state
+        cy.data('report-display-date-to-input').should('have.attr', 'required');
+        cy.data('report-display-date-to').contains('Required');
+        cy.data('report-display-date-to-input')
+            .parent()
+            .should('have.class', 'Mui-error');
+
+        cy.data('report-display-button').should('be.disabled');
+        cy.data('report-display-date-from-input').clear();
+        cy.data('report-display-button').should('not.be.disabled');
+
+        // to before from
+        cy.data('report-display-date-to-input').type('01/01/2025');
+        cy.data('report-display-date-from-input').type('01/01/2026');
+        cy.data('report-display-date-from').contains('Must not be after "to" date');
+        cy.data('report-display-date-from-input')
+            .parent()
+            .should('have.class', 'Mui-error');
+
+        cy.data('report-display-button').should('be.disabled');
+        cy.data('report-display-date-from-input')
+            .clear()
+            .type('01/01/2024');
+
+        // system alert id field
+        cy.data('report-display-system-alert-id-input').type('abc');
+        cy.data('report-display-system-alert-id-input')
+            .parent()
+            .should('have.class', 'Mui-error');
+        cy.data('report-display-system-alert-id').contains('Must be a positive whole number');
+        cy.data('report-display-button').should('be.disabled');
+        cy.data('report-display-system-alert-id-input').clear();
+
+        cy.data('report-display-system-alert-id-input').type('-1');
+        cy.data('report-display-system-alert-id').contains('Must be a positive whole number');
+        cy.data('report-display-system-alert-id-input').clear();
+
+        cy.data('report-display-system-alert-id-input').type('1.0');
+        cy.data('report-display-system-alert-id').contains('Must be a positive whole number');
+        cy.data('report-display-system-alert-id-input').clear();
+
+        cy.data('report-display-button').should('not.be.disabled');
+
+        cy.data('report-display-date-to-input').type('01/01/2025');
+        cy.data('report-display-date-from-input').type('01/01/2024');
+        cy.data('report-display-system-alert-id-input').type('10');
     });
 });
