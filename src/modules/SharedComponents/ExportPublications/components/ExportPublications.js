4a46df52a74fb1d33fd0db12d9cac3c4a12abba4
Lee S <lsibbald100+git@gmail.com>
2024-06-26 15:52:37 +1000

    [#187217071] Contributors, Export Publications and GranListEditor class migration

--- src/modules/SharedComponents/ExportPublications/components/ExportPublications.js
+++ src/modules/SharedComponents/ExportPublications/components/ExportPublications.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { locale } from 'locale';
 import Select from '@mui/material/Select';
@@ -6,60 +6,57 @@ import MenuItem from '@mui/material/MenuItem';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 
-export default class ExportPublications extends PureComponent {
-    static propTypes = {
-        disabled: PropTypes.bool,
-        onChange: PropTypes.func,
-        exportData: PropTypes.object,
+export const ExportPublications = ({ disabled, onChange, exportData }) => {
+    const formatChanged = event => {
+        onChange(event.target.value);
     };
 
-    formatChanged = event => {
-        this.props.onChange(event.target.value);
-    };
+    const txt = exportData && exportData.format && exportData.format.length > 0 ? exportData : locale.components.export;
 
-    render() {
-        const txt =
-            this.props.exportData && this.props.exportData.format && this.props.exportData.format.length > 0
-                ? this.props.exportData
-                : locale.components.export;
+    return (
+        <FormControl variant="standard" fullWidth>
+            <InputLabel id="export-publications-format-label" shrink>
+                {txt.label}
+            </InputLabel>
+            <Select
+                variant="standard"
+                labelId="export-publications-format-label"
+                data-testid="export-publications-format"
+                onChange={formatChanged}
+                disabled={disabled}
+                value={-1}
+                displayEmpty
+                SelectDisplayProps={{
+                    'data-testid': 'export-publications-format-select',
+                }}
+                inputProps={{
+                    'data-testid': 'export-publications-format-input',
+                }}
+            >
+                <MenuItem key={-1} value={-1} disabled>
+                    Please select
+                </MenuItem>
+                {txt.format.map((item, index) => {
+                    return (
+                        <MenuItem
+                            key={index}
+                            value={item.value}
+                            data-testid={`export-publication-option-${index}`}
+                            id={`export-publication-option-${index}`}
+                        >
+                            {item.label}
+                        </MenuItem>
+                    );
+                })}
+            </Select>
+        </FormControl>
+    );
+};
 
-        return (
-            <FormControl variant="standard" fullWidth>
-                <InputLabel id="export-publications-format-label" shrink>
-                    {txt.label}
-                </InputLabel>
-                <Select
-                    variant="standard"
-                    labelId="export-publications-format-label"
-                    data-testid="export-publications-format"
-                    onChange={this.formatChanged}
-                    disabled={this.props.disabled}
-                    value={-1}
-                    displayEmpty
-                    SelectDisplayProps={{
-                        'data-testid': 'export-publications-format-select',
-                    }}
-                    inputProps={{
-                        'data-testid': 'export-publications-format-input',
-                    }}
-                >
-                    <MenuItem key={-1} value={-1} disabled>
-                        Please select
-                    </MenuItem>
-                    {txt.format.map((item, index) => {
-                        return (
-                            <MenuItem
-                                key={index}
-                                value={item.value}
-                                data-testid={`export-publication-option-${index}`}
-                                id={`export-publication-option-${index}`}
-                            >
-                                {item.label}
-                            </MenuItem>
-                        );
-                    })}
-                </Select>
-            </FormControl>
-        );
-    }
-}
+ExportPublications.propTypes = {
+    disabled: PropTypes.bool,
+    onChange: PropTypes.func,
+    exportData: PropTypes.object,
+};
+
+export default React.memo(ExportPublications);
