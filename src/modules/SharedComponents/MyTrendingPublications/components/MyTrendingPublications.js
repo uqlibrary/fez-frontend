94b02ec6c894f746acea7017a711bce7a66f3965
Lee S <lsibbald100+git@gmail.com>
2024-07-01 12:06:01 +1000

    [#187217071] Migrated MyTrendingPubs component

--- src/modules/SharedComponents/MyTrendingPublications/components/MyTrendingPublications.js
+++ src/modules/SharedComponents/MyTrendingPublications/components/MyTrendingPublications.js
@@ -1,85 +1,80 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { locale } from 'locale';
+import * as actions from 'actions';
+
 import { PublicationsList } from 'modules/SharedComponents/PublicationsList';
 import { InlineLoader } from 'modules/SharedComponents/Toolbox/Loaders';
 import { HelpIcon } from 'modules/SharedComponents/Toolbox/HelpDrawer';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
-export class MyTrendingPublications extends PureComponent {
-    static propTypes = {
-        trendingPublicationsList: PropTypes.array,
-        loadingTrendingPublications: PropTypes.bool,
-        actions: PropTypes.object,
-        accountAuthorDetailsLoading: PropTypes.bool,
-    };
+export const MyTrendingPublications = () => {
+    const dispatch = useDispatch();
+    const { trendingPublicationsList, loadingTrendingPublications } = useSelector(state =>
+        state.get('myTrendingPublicationsReducer'),
+    );
 
-    static defaultProps = {
-        trendingPublicationsList: [],
-        loadingTrendingPublications: false,
-    };
+    const { author } = useSelector(state => state.get('accountReducer')) || false;
 
-    componentDidMount() {
-        if (!this.props.accountAuthorDetailsLoading) {
-            this.props.actions.searchTrendingPublications(locale.components.myTrendingPublications.recordsPerSource);
+    React.useEffect(() => {
+        if (!!author) {
+            dispatch(actions.searchTrendingPublications(locale.components.myTrendingPublications.recordsPerSource));
         }
-    }
+    }, [author, dispatch]);
 
-    render() {
-        const txt = locale.components.myTrendingPublications;
-        if (this.props.loadingTrendingPublications) {
-            return (
-                <Grid container alignItems={'center'}>
-                    <Grid item>
-                        <InlineLoader message={txt.loading} />
-                    </Grid>
-                </Grid>
-            );
-        }
-        if (this.props.trendingPublicationsList.length === 0) {
-            return <div className="empty" />;
-        }
+    const txt = locale.components.myTrendingPublications;
+    if (loadingTrendingPublications) {
         return (
-            <Grid container spacing={3} id={'myTrendingPublications'}>
-                <Grid item xs={12}>
-                    {this.props.trendingPublicationsList.map(({ key, values }, metricIndex) => {
-                        return (
-                            <Grid container key={metricIndex} spacing={3}>
-                                <Grid item xs>
-                                    <Typography variant={'h6'}>
-                                        <div className={`fez-icon ${key} xxlarge`} />
-                                        {txt.metrics[key].title}
-                                    </Typography>
-                                    <Typography variant={'subtitle1'}>{txt.metrics[key].subtitle}</Typography>
-                                </Grid>
-                                <Grid item>
-                                    <HelpIcon {...locale.components.trendingPublicationHelp} />
-                                </Grid>
-                                <Grid
-                                    item
-                                    xs={12}
-                                    sx={{
-                                        '& .citationMetrics': {
-                                            '& .count': {
-                                                fontSize: '1.75rem',
-                                            },
-                                            '& .difference': {
-                                                fontSize: '1rem',
-                                            },
-                                        },
-                                    }}
-                                >
-                                    <PublicationsList publicationsList={values} showMetrics />
-                                </Grid>
-                            </Grid>
-                        );
-                    })}
+            <Grid container alignItems={'center'}>
+                <Grid item>
+                    <InlineLoader message={txt.loading} />
                 </Grid>
             </Grid>
         );
     }
-}
+    if (trendingPublicationsList.length === 0) {
+        return <div className="empty" />;
+    }
+    return (
+        <Grid container spacing={3} id={'myTrendingPublications'}>
+            <Grid item xs={12}>
+                {trendingPublicationsList.map(({ key, values }, metricIndex) => {
+                    return (
+                        <Grid container key={metricIndex} spacing={3}>
+                            <Grid item xs>
+                                <Typography variant={'h6'}>
+                                    <div className={`fez-icon ${key} xxlarge`} />
+                                    {txt.metrics[key].title}
+                                </Typography>
+                                <Typography variant={'subtitle1'}>{txt.metrics[key].subtitle}</Typography>
+                            </Grid>
+                            <Grid item>
+                                <HelpIcon {...locale.components.trendingPublicationHelp} />
+                            </Grid>
+                            <Grid
+                                item
+                                xs={12}
+                                sx={{
+                                    '& .citationMetrics': {
+                                        '& .count': {
+                                            fontSize: '1.75rem',
+                                        },
+                                        '& .difference': {
+                                            fontSize: '1rem',
+                                        },
+                                    },
+                                }}
+                            >
+                                <PublicationsList publicationsList={values} showMetrics />
+                            </Grid>
+                        </Grid>
+                    );
+                })}
+            </Grid>
+        </Grid>
+    );
+};
 
-export default MyTrendingPublications;
+export default React.memo(MyTrendingPublications);
