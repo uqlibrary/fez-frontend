c2e4c7ab49afbc27f20eaebe636edb1361c921de
Lee S <lsibbald100+git@gmail.com>
2024-07-05 15:53:51 +1000

    [#187217071] HelpDrawer and icon

--- src/modules/SharedComponents/Toolbox/HelpDrawer/components/HelpDrawer.js
+++ src/modules/SharedComponents/Toolbox/HelpDrawer/components/HelpDrawer.js
@@ -1,86 +1,88 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
-export class HelpDrawer extends Component {
-    static propTypes = {
-        buttonLabel: PropTypes.string,
-        hide: PropTypes.func.isRequired,
-        open: PropTypes.bool.isRequired,
-        text: PropTypes.any.isRequired,
-        title: PropTypes.string.isRequired,
-    };
-    static defaultProps = {
-        buttonLabel: 'CLOSE',
+export const HelpDrawer = ({ buttonLabel }) => {
+    const dispatch = useDispatch();
+    const { open = false, title = '', text = '' } = useSelector(state => state.get('helpDrawer'));
+
+    let indexedText = null;
+    const hide = () => {
+        dispatch(hide());
     };
 
-    render() {
-        const { buttonLabel, hide, open, text, title } = this.props;
-        let indexedText = null;
-        if (this.props.text && this.props.text.props && this.props.text.props.children) {
-            indexedText = React.Children.map(this.props.text.props.children, (child, index) => {
-                if (child.type) {
-                    return React.cloneElement(child, { key: index });
-                } else {
-                    return child;
-                }
-            });
-        }
-        return (
-            <Drawer
-                sx={{
-                    '& .MuiDrawer-paper': {
-                        width: '320px',
-                        padding: { xs: 3, sm: 5 },
-                        paddingBottom: 0,
-                        maxHeight: '100%',
-                        boxSizing: 'border-box',
-                    },
-                }}
-                open={open}
-                anchor="right"
-                onClose={hide}
-            >
-                <Grid container spacing={5} id="help-drawer">
-                    <Grid item xs={12}>
-                        <Typography
-                            color={'primary.main'}
-                            component={'h3'}
-                            data-testid="help-drawer-title"
-                            id="help-drawer-title"
-                            key={'title'}
-                            variant={'h6'}
-                        >
-                            {title}
-                        </Typography>
-                        <Typography
-                            component={'span'}
-                            data-testid="help-drawer-message"
-                            id={`help-drawer-text-${title.replace(/\s/g, '')}`}
-                            key={'text'}
-                            variant="body2"
-                        >
-                            {indexedText || text}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} id="help-drawer-button">
-                        <Button
-                            children={buttonLabel}
-                            sx={{ float: 'right', marginBottom: '60px' }}
-                            color="primary"
-                            data-analyticsid="help-drawer-close"
-                            data-testid="help-drawer-close"
-                            onClick={hide}
-                            variant="contained"
-                        />
-                    </Grid>
-                </Grid>
-            </Drawer>
-        );
+    if (text && text.props && text.props.children) {
+        indexedText = React.Children.map(text.props.children, (child, index) => {
+            if (child.type) {
+                return React.cloneElement(child, { key: index });
+            } else {
+                return child;
+            }
+        });
     }
-}
+    return (
+        <Drawer
+            sx={{
+                '& .MuiDrawer-paper': {
+                    width: '320px',
+                    padding: { xs: 3, sm: 5 },
+                    paddingBottom: 0,
+                    maxHeight: '100%',
+                    boxSizing: 'border-box',
+                },
+            }}
+            open={open}
+            anchor="right"
+            onClose={hide}
+        >
+            <Grid container spacing={5} id="help-drawer">
+                <Grid item xs={12}>
+                    <Typography
+                        color={'primary.main'}
+                        component={'h3'}
+                        data-testid="help-drawer-title"
+                        id="help-drawer-title"
+                        key={'title'}
+                        variant={'h6'}
+                    >
+                        {title}
+                    </Typography>
+                    <Typography
+                        component={'span'}
+                        data-testid="help-drawer-message"
+                        id={`help-drawer-text-${title.replace(/\s/g, '')}`}
+                        key={'text'}
+                        variant="body2"
+                    >
+                        {indexedText || text}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} id="help-drawer-button">
+                    <Button
+                        children={buttonLabel}
+                        sx={{ float: 'right', marginBottom: '60px' }}
+                        color="primary"
+                        data-analyticsid="help-drawer-close"
+                        data-testid="help-drawer-close"
+                        onClick={hide}
+                        variant="contained"
+                    />
+                </Grid>
+            </Grid>
+        </Drawer>
+    );
+};
 
-export default HelpDrawer;
+HelpDrawer.propTypes = {
+    buttonLabel: PropTypes.string,
+};
+HelpDrawer.defaultProps = {
+    buttonLabel: 'CLOSE',
+};
+
+export default React.memo(HelpDrawer);
