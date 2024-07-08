5c946c9076c50df3ab63943dab8ab124ed3c82d6
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2024-07-08 12:38:54 +1000

    [#187890417] fixed significance editor to handle ZERO significance on records that should have it.

--- cypress/e2e/admin-add/creativeWork.spec.js
+++ cypress/e2e/admin-add/creativeWork.spec.js
@@ -83,52 +83,6 @@ context('As an admin, I can', () => {
         const numberOfRecords = expectedRowId + 1;
         const ordinalLabelList = ['First', 'Second', 'Third', 'Fourth'];
         const authorTextOrdinal = ordinalLabelList[expectedRowId];
-        // cy.get('[data-testid=ntro-section-content]')
-        //     .as('NTRO')
-        //     .within(() => {
-        //         cy.get('.AdminCard')
-        //             .eq(0)
-        //             .within(() => {
-        //                 // cy.get('[data-testid="rek-significance-showhidebutton"]')
-        //                 //     .should('exist')
-        //                 //     .click();
-        //                 // cy.get('button[data-testid="rek-significance-add"]')
-        //                 //     .should('exist')
-        //                 //     .should('contain', 'ADD');
-        //                 // cy.get('[data-testid="rek-significance-select"]').should('contain', '');
-        //                 // cy.assertCKEditorEmpty('rek-creator-contribution-statement');
-        //             });
-        //     });
-
-        // // popup appears at foot of markup, outside Admin section
-        // cy.assertChangeSelectFromTo('rek-significance', '', scaleName); // was unselected
-
-        // cy.get('[data-testid=ntro-section-content]')
-        //     .as('NTRO')
-        //     .within(() => {
-        //         cy.get('.AdminCard')
-        //             .eq(0)
-        //             .within(() => {
-        //                 cy.typeCKEditor('rek-creator-contribution-statement', statement);
-
-        //                 saveFormChanges('ADD');
-
-        //                 // the newly added item appears correctly
-        //                 cy.get('[data-testid="rek-significance-list"]')
-        //                     .children()
-        //                     .should('have.length', numberOfRecords);
-        //                 cy.get(`[data-testid="scalesignif-author-${expectedRowId}"]`).should(
-        //                     'contain',
-        //                     authorTextOrdinal,
-        //                 );
-        //                 cy.get(`[data-testid="scale-item-${expectedRowId}"]`).should('contain', scaleName);
-        //                 cy.get(`[data-testid="statement-item-${expectedRowId}"]`).should('contain', statement);
-
-        //                 // the show hide status is correct
-        //                 cy.waitUntil(() => cy.get('[data-testid="rek-significance-showhidebutton"]').should('exist'));
-        //                 assertFormIsHidden();
-        //             });
-        //     });
     }
 
     function clickRowEditButton(rowId) {
@@ -141,10 +95,6 @@ context('As an admin, I can', () => {
         cy.visit('/admin/add?user=uqstaff');
 
         loadNtroTabAdminAdd(0, 'Creative Work - Textual');
-
-        // const newContributionStatementText = 'another entry';
-        // const secondRowId = 1;
-        // const originalScale = 'Major';
 
         assertCreateAuthor('Test Author');
         assertCreateAuthor('Second Author');
@@ -183,41 +133,6 @@ context('As an admin, I can', () => {
         cy.get('[data-testid="empty-significance-statement-input"]').click();
         cy.get('[data-testid="rek-significance-add"]').click();
         cy.get('[data-testid="statement-item-0"]').should('contain', 'Missing');
-
-        // assertCanEnterScaleStatementListItem('Minor', 'new entry', 0);
-        // assertCanEnterScaleStatementListItem(originalScale, newContributionStatementText, secondRowId);
-        // assertCanEnterScaleStatementListItem('Major', 'entry with duplicate scale', 2);
-
-        // // when trying to edit an added entry, the form loads properly
-        // assertFormIsHidden();
-        // clickRowEditButton(secondRowId);
-        // cy.get('[data-testid="rek-significance-form"]')
-        //     .should('be.visible')
-        //     .scrollIntoView();
-        // cy.get('[data-testid="rek-significance-select"]')
-        //     .should('exist')
-        //     .should('contain', originalScale);
-        // cy.readCKEditor('rek-creator-contribution-statement').then(text => {
-        //     expect(text).to.contain(newContributionStatementText);
-        // });
-        // // change values of fields and save
-        // const addTextToContributionStatement = ' 123';
-        // cy.typeCKEditor('rek-creator-contribution-statement', addTextToContributionStatement);
-        // const newScale = 'Minor';
-        // cy.assertChangeSelectFromTo('rek-significance', originalScale, newScale);
-        // saveFormChanges('UPDATE');
-        // // data has updated in list editor
-        // cy.get(`[data-testid="scalesignif-author-${secondRowId}"]`).should('contain', 'Second');
-        // cy.get(`[data-testid="scale-item-${secondRowId}"]`).should('contain', newScale);
-        // cy.get(`[data-testid="statement-item-${secondRowId}"]`).should(
-        //     'contain',
-        //     `${newContributionStatementText}${addTextToContributionStatement}`,
-        // );
-        // // editing an entry does not create a new entry
-        // cy.get('[data-testid="rek-significance-list"]')
-        //     .should('exist')
-        //     .children()
-        //     .should('have.length', 3);
     });
 
     it('delete all added creative work scale of significance and creative statements', () => {
