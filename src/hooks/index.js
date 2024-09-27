24217ba13d7578c115623c9f331d94ab4013e57b
Lee S <lsibbald100+git@gmail.com>
2024-09-27 15:48:00 +1000

    cherry pick merge
    origin/feature-rhf-batch-import-form-#188335355

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -48,3 +48,4 @@ export { useIsUserSuperAdmin } from './useIsUserSuperAdmin';
 export { useRecordsSelector } from './useRecordsSelector';
 export { useWidth } from './useWidth';
 export { withIsMobileView, useIsMobileView } from './useIsMobileView';
+export { useValidatedForm } from './useValidatedForm';
