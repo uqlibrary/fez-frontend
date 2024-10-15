ef7983a6f11afc47aeef74172ac83f2179366214
John Zeng <john.zeng@uq.edu.au>
2024-10-16 09:13:12 +1000

    [#188392437] convert to react form

--- src/modules/Admin/ControlledVocabularies/components/AdminPanel.js
+++ src/modules/Admin/ControlledVocabularies/components/AdminPanel.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { propTypes } from 'redux-form/immutable';
+import { useForm, Controller } from 'react-hook-form';
 
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
@@ -18,32 +18,6 @@ import { ACTION } from '../ControlledVocabularyContext';
 
 const rootId = 'update_dialog';
 
-const RenderCheckbox = ({ label, inputProps, disabled, ...props }) => {
-    const [checked, setChecked] = useState(false);
-
-    const handleChange = event => {
-        setChecked(event.target.checked);
-    };
-
-    return (
-        <Checkbox
-            label={label}
-            checked={checked}
-            onChange={handleChange}
-            disabled={disabled}
-            inputProps={inputProps}
-            {...props}
-        />
-    );
-};
-
-// Define prop types for validation
-RenderCheckbox.propTypes = {
-    label: PropTypes.string,
-    inputProps: PropTypes.object,
-    disabled: PropTypes.bool,
-};
-
 const AdminPanel = ({
     action,
     locale,
@@ -56,13 +30,19 @@ const AdminPanel = ({
     onClose,
     noMinContentWidth,
     error,
-    handleSubmit,
-    submitting,
-    pristine,
-    valid,
     parentId,
     ...props
 }) => {
+    const {
+        control,
+        handleSubmit,
+        formState: { isSubmitting, isValid },
+        reset,
+    } = useForm({
+        defaultValues: props.initialValues,
+        mode: 'onChange', // Enables validation to update on input change
+    });
+
     const componentId = `${rootId}-${id}`;
 
     const theme = useTheme();
@@ -88,11 +68,8 @@ const AdminPanel = ({
     const _onCancelAction = () => {
         onClose?.();
         onCancelAction?.();
+        reset(); // Resets the form when cancel is clicked
     };
-    React.useEffect(() => {
-        if (!props.initialized) props.initialize(props.initialValues);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [props.initialized]);
 
     return (
         <>
@@ -105,7 +82,7 @@ const AdminPanel = ({
                     data-testid={`${componentId}-container`}
                 >
                     <StandardCard title={title} standardCardId={`${componentId}`} subCard>
-                        <form onSubmit={handleSubmit}>
+                        <form onSubmit={handleSubmit(props.onSubmit)}>
                             <Box
                                 id={`${componentId}-vc-content`}
                                 data-testid={`${componentId}-vc-content`}
@@ -114,74 +91,107 @@ const AdminPanel = ({
                                 <Grid container padding={0} spacing={2}>
                                     <Grid item xs={12}>
                                         <label htmlFor="cvo_title">{locale.form.title}</label>
-                                        <TextField
-                                            variant="standard"
-                                            required
-                                            inputProps={{ maxLength: 255 }}
-                                            fullWidth
-                                            textFieldId="cvo-title"
+                                        <Controller
                                             name="cvo_title"
-                                            disabled={submitting}
+                                            control={control}
+                                            rules={{ required: 'Required' }}
+                                            render={({ field }) => (
+                                                <TextField
+                                                    {...field}
+                                                    variant="standard"
+                                                    required
+                                                    fullWidth
+                                                    inputProps={{ maxLength: 255 }}
+                                                    disabled={isSubmitting}
+                                                />
+                                            )}
                                         />
                                     </Grid>
                                     <Grid item xs={12}>
                                         <label htmlFor="cvo_desc">{locale.form.description}</label>
-                                        <TextField
-                                            variant="standard"
-                                            inputProps={{ maxLength: 255 }}
-                                            fullWidth
-                                            textFieldId="cvo-desc"
+                                        <Controller
                                             name="cvo_desc"
-                                            multiline
-                                            minRows={2}
-                                            disabled={submitting}
+                                            control={control}
+                                            render={({ field }) => (
+                                                <TextField
+                                                    {...field}
+                                                    variant="standard"
+                                                    fullWidth
+                                                    multiline
+                                                    minRows={2}
+                                                    inputProps={{ maxLength: 255 }}
+                                                    disabled={isSubmitting}
+                                                />
+                                            )}
                                         />
                                     </Grid>
                                     <Grid item xs={12}>
-                                        <label htmlFor="cvo_external_id" style={{ display: 'block' }}>
-                                            {locale.form.externalId}
-                                        </label>
-                                        <TextField
-                                            variant="standard"
-                                            textFieldId="cvo-external-id"
-                                            inputProps={{ maxLength: 10 }}
+                                        <label htmlFor="cvo_external_id">{locale.form.externalId}</label>
+                                        <Controller
                                             name="cvo_external_id"
-                                            disabled={submitting}
+                                            control={control}
+                                            render={({ field }) => (
+                                                <TextField
+                                                    {...field}
+                                                    variant="standard"
+                                                    inputProps={{ maxLength: 10 }}
+                                                    disabled={isSubmitting}
+                                                />
+                                            )}
                                         />
                                     </Grid>
                                     <Grid item xs={12}>
-                                        <label htmlFor="cvo_hide" style={{ display: 'block' }}>
-                                            {locale.form.inactive}
-                                        </label>
-                                        <RenderCheckbox
-                                            id="cvo-hide"
-                                            inputProps={{
-                                                id: 'cvo-hide-input',
-                                                'data-analyticsid': 'cvo-hide-input',
-                                                'data-testid': 'cvo-hide-input',
-                                            }}
+                                        <label htmlFor="cvo_image_filename">{locale.form.filename}</label>
+                                        <Controller
+                                            name="cvo_image_filename"
+                                            control={control}
+                                            render={({ field }) => (
+                                                <TextField
+                                                    {...field}
+                                                    variant="standard"
+                                                    fullWidth
+                                                    inputProps={{ maxLength: 64 }}
+                                                    disabled={isSubmitting}
+                                                />
+                                            )}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <label htmlFor="cvo_order">{locale.form.order}</label>
+                                        <Controller
+                                            name="cvo_order"
+                                            control={control}
+                                            render={({ field }) => (
+                                                <TextField {...field} variant="standard" disabled={isSubmitting} />
+                                            )}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <label htmlFor="cvo_hide">{locale.form.inactive}</label>
+                                        <Controller
                                             name="cvo_hide"
-                                            type="checkbox"
+                                            control={control}
+                                            render={({ field }) => (
+                                                <Checkbox
+                                                    {...field}
+                                                    checked={field.value}
+                                                    onChange={field.onChange}
+                                                    disabled={isSubmitting}
+                                                />
+                                            )}
                                         />
                                     </Grid>
                                 </Grid>
                             </Box>
                             {(!hideCancelButton || !hideActionButton) && (
-                                <Grid
-                                    container
-                                    id={`${rootId}-actions`}
-                                    data-testid={`${rootId}-actions`}
-                                    sx={{ marginTop: 2 }}
-                                >
+                                <Grid container sx={{ marginTop: 2 }}>
                                     <Grid item xs={12} justifyContent="flex-end" display={'flex'}>
                                         {!hideCancelButton && (
                                             <Button
-                                                variant={'outlined'}
+                                                variant="outlined"
                                                 onClick={_onCancelAction}
-                                                id={`${rootId}-cancel-button`}
-                                                data-testid={`${rootId}-cancel-button`}
+                                                disabled={isSubmitting}
                                                 fullWidth={isMobileView}
-                                                disabled={submitting}
                                                 sx={{ marginInlineEnd: 2 }}
                                             >
                                                 {locale.cancelButtonLabel}
@@ -190,22 +200,13 @@ const AdminPanel = ({
                                         {!hideActionButton && (
                                             <Button
                                                 variant="contained"
-                                                autoFocus
-                                                color={'primary'}
-                                                type={'submit'}
-                                                // onClick={_onAction}
-                                                id={`${rootId}-action-button`}
-                                                data-testid={`${rootId}-action-button`}
+                                                type="submit"
+                                                color="primary"
+                                                disabled={!isValid || isSubmitting}
                                                 fullWidth={isMobileView}
-                                                disabled={pristine || submitting || !valid}
                                             >
-                                                {submitting ? (
-                                                    <CircularProgress
-                                                        color="inherit"
-                                                        size={25}
-                                                        id={`${rootId}-progress`}
-                                                        data-testid={`${rootId}-progress`}
-                                                    />
+                                                {isSubmitting ? (
+                                                    <CircularProgress size={25} />
                                                 ) : (
                                                     locale.confirmButtonLabel
                                                 )}
@@ -214,7 +215,7 @@ const AdminPanel = ({
                                     </Grid>
                                 </Grid>
                             )}
-                            {!!error && (
+                            {error && (
                                 <Grid container>
                                     <Grid item xs={12}>
                                         <Alert
@@ -235,12 +236,10 @@ const AdminPanel = ({
 };
 
 AdminPanel.propTypes = {
-    ...propTypes, // all redux-form props
     action: PropTypes.oneOf(['', 'add', 'edit']).isRequired,
     locale: PropTypes.object.isRequired,
     id: PropTypes.string.isRequired,
     title: PropTypes.string,
-    row: PropTypes.object,
     isOpen: PropTypes.bool,
     noMinContentWidth: PropTypes.bool,
     hideActionButton: PropTypes.bool,
@@ -248,9 +247,10 @@ AdminPanel.propTypes = {
     onAction: PropTypes.func,
     onCancelAction: PropTypes.func,
     onClose: PropTypes.func,
-    props: PropTypes.object,
-    alertProps: PropTypes.object,
-    isBusy: PropTypes.bool,
+    error: PropTypes.object,
+    initialValues: PropTypes.object,
+    parentId: PropTypes.string,
+    onSubmit: PropTypes.func,
 };
 
 export default React.memo(AdminPanel);
--- src/modules/Admin/ControlledVocabularies/components/AdminPanelContainer.js
+++ src/modules/Admin/ControlledVocabularies/components/AdminPanelContainer.js
@@ -1,51 +1,19 @@
-import { connect } from 'react-redux';
-import { reduxForm, getFormValues, getFormSyncErrors } from 'redux-form/immutable';
-import Immutable from 'immutable';
-import { bindActionCreators } from 'redux';
-
-import * as actions from 'actions/viewControlledVocab';
 import AdminPanel from './AdminPanel';
+import PropTypes from 'prop-types';
+import React from 'react';
 
-const FORM_NAME = 'ControlledVocabAdmin';
-
-const rxWholeNumberOnly = new RegExp(/^\d+$/);
-
-export const validate = data => {
-    const values = data.toJS();
-    const errors = {};
-    if (!values.cvo_title) {
-        errors.cvo_title = 'Required';
-    }
-
-    if (typeof values.cvo_order !== 'undefined' && values.cvo_order !== '' && values.cvo_order !== null) {
-        const num = Number(values.cvo_order);
-        if (isNaN(num)) errors.cvo_order = 'Must be a number';
-        else if (!rxWholeNumberOnly.test(values.cvo_order)) {
-            errors.cvo_order = 'Must be a whole number above zero';
-        }
-    }
-
-    return errors;
-};
-
-const mapStateToProps = (state, props) => {
-    const formErrors = getFormSyncErrors(FORM_NAME)(state);
-    return {
-        onSubmit: props.onAction(props.parentId ?? null, props.rootVocabId),
-        formValues: getFormValues(FORM_NAME)(state) || Immutable.Map({}),
-        formErrors: formErrors,
-        disableSubmit: formErrors && !(formErrors instanceof Immutable.Map),
-        initialValues: state?.get('vocabAdminReducer')?.vocab || {},
+const AdminPanelContainer = props => {
+    const onSubmit = data => {
+        props.onAction(props.parentId ?? null)(data);
     };
+
+    return <AdminPanel onSubmit={onSubmit} initialValues={props.initialValues} {...props} />;
 };
 
-const mapDispatchToProps = dispatch => ({
-    actions: bindActionCreators(actions, dispatch),
-});
+AdminPanelContainer.propTypes = {
+    onAction: PropTypes.func,
+    parentId: PropTypes.string,
+    initialValues: PropTypes.object,
+};
 
-const AdminPanelContainer = reduxForm({
-    form: FORM_NAME,
-    validate,
-})(AdminPanel);
-
-export default connect(mapStateToProps, mapDispatchToProps)(AdminPanelContainer);
+export default AdminPanelContainer;
