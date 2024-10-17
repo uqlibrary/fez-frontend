ca5324d310d06ad0504435da0a5f9bd13dd308a7
John Zeng <john.zeng@uq.edu.au>
2024-10-17 10:30:01 +1000

    [#188392437] fix show error test

--- src/modules/Admin/ControlledVocabularies/ControlledVocabularies.js
+++ src/modules/Admin/ControlledVocabularies/ControlledVocabularies.js
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import { createPortal } from 'react-dom';
 import { styled } from '@mui/material/styles';
 import { useDispatch, useSelector } from 'react-redux';
-import { SubmissionError } from 'redux-form/immutable';
 
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
@@ -84,7 +83,7 @@ const ControlledVocabularies = () => {
             })
             .catch(error => {
                 console.error(error);
-                throw new SubmissionError({ _error: error });
+                throw error;
             });
     };
 
