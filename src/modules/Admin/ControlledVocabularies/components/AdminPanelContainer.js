6524073fdeb3974285c92e375d8e41f3d9110bea
John Zeng <john.zeng@uq.edu.au>
2024-10-23 13:33:42 +1000

    [#188392437] fix console warning: A component is changing an uncontrolled input to be controlled. This is likely caused by the value changing from undefined to a defined value, which should not happen.

--- src/modules/Admin/ControlledVocabularies/components/AdminPanelContainer.js
+++ src/modules/Admin/ControlledVocabularies/components/AdminPanelContainer.js
@@ -6,6 +6,8 @@ import AdminPanel from './AdminPanel';
 
 const mapStateToProps = (state, props) => {
     const formValues = state?.get('vocabAdminReducer')?.vocab || {};
+    formValues.cvo_title = formValues.cvo_title ?? '';
+    formValues.cvo_external_id = formValues.cvo_external_id ?? '';
     return {
         onAction: props.onAction(props.parentId ?? null, props.rootVocabId),
         initialValues: formValues,
