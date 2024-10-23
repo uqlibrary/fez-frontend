ff94348dcb1cb81bfb58cc7ca09f80547a743c8e
John Zeng <john.zeng@uq.edu.au>
2024-10-23 13:32:58 +1000

    [#188392437] remove unused code

--- src/modules/Admin/ControlledVocabularies/components/AdminPanelContainer.js
+++ src/modules/Admin/ControlledVocabularies/components/AdminPanelContainer.js
@@ -1,17 +1,13 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import Immutable from 'immutable';
 
 import * as actions from 'actions/viewControlledVocab';
 import AdminPanel from './AdminPanel';
 
 const mapStateToProps = (state, props) => {
-    const formValues = state?.get('vocabAdminReducer')?.vocab || Immutable.Map({});
-    const formErrors = {}; // Add validation logic here, if needed
+    const formValues = state?.get('vocabAdminReducer')?.vocab || {};
     return {
         onAction: props.onAction(props.parentId ?? null, props.rootVocabId),
-        formValues: formValues,
-        formErrors: formErrors,
         initialValues: formValues,
     };
 };
