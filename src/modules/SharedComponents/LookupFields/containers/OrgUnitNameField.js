9326356c83051c93936c7820489d2d3fed2beb0b
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2024-11-04 08:38:56 +1000

    Revert "- removing unnecessary condition path"
    This reverts commit cd4c0757a9c93c5f107556e1a8b06f4eb530e2de.

--- src/modules/SharedComponents/LookupFields/containers/OrgUnitNameField.js
+++ src/modules/SharedComponents/LookupFields/containers/OrgUnitNameField.js
@@ -27,11 +27,22 @@ export const OrgUnitNameField = props => {
             allowFreeText
             getOptionLabel={item => (!!item && String(item.value)) || ''}
             filterOptions={options => options}
-            defaultValue={(!!props.input.value && { value: props.input.value }) || null}
-            error={!!props.meta.error}
-            errorText={props.meta.error || ''}
-            onChange={item => props.input?.onChange(item.value)}
-            onClear={() => props.input?.onChange(null)}
+            // If form key is set in props.meta object then it's a redux-form Field
+            {...(!!((props || {}).meta || {}).form
+                ? {
+                      defaultValue: (!!props.input.value && { value: props.input.value }) || null,
+                      error: !!props.meta.error,
+                      errorText: props.meta.error || '',
+                      onChange: item => props.input.onChange(item.value),
+                      onClear: () => props.input.onChange(null),
+                  }
+                : {
+                      defaultValue: (!!props.value && { value: props.value }) || '',
+                      error: props.error,
+                      errorText: props.errorText || '',
+                      onChange: item => props.onChange(item),
+                      onClear: () => props.onChange({ value: null }),
+                  })}
             loadSuggestions={loadSuggestions}
         />
     );
