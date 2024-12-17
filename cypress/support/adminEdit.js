6f94bad035fb4aaa232cb0fc834b93e03cb524c9
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2024-12-17 12:12:02 +1000

    Fix unit test for it.only on creativeWork.spec

--- cypress/support/adminEdit.js
+++ cypress/support/adminEdit.js
@@ -177,7 +177,7 @@ Cypress.Commands.add('editAffiliationAndAssert', (currentOrgId, nextOrgId, nextO
 
 Cypress.Commands.add(
     'assertAffiliationsAllowed',
-    ({ authorName, orgName, rowId, allowed = false, tabbed = true } = {}) => {
+    ({ authorName, orgName, rowId, allowed = false, tabbed = true, ntro = false } = {}) => {
         if (tabbed) {
             cy.adminEditTabbedView();
         }
@@ -195,6 +195,8 @@ Cypress.Commands.add(
         cy.get('[data-testid=rek-author-add-save]').click();
         cy.get('[data-testid^=contributor-errorIcon]').should(allowed ? 'exist' : 'not.exist');
         // Material Table hardcored data-testid to mtablebodyrow for every row
+        // these tests do not apply for NTRO admin form
+        !ntro &&
         cy.get(`[id=rek-author-list-row-${rowId}]`).within(() => {
             cy.get(allowed ? '[data-testid^=expandPanelIcon-]' : '[data-testid=ChevronRightIcon]').should('exist');
             cy.wait(200);
