bab5972cf149922d3794f1a0b3e7d6034afd3706
John Zeng <john.zeng@uq.edu.au>
2025-02-03 14:26:48 +1000

    [#188756086] fix test: Unable to find an element by: [data-testid="mtablebodyrow"]

--- src/modules/Admin/ManageUsers/ManageUsers.test.js
+++ src/modules/Admin/ManageUsers/ManageUsers.test.js
@@ -455,6 +455,8 @@ describe('ManageUsers', () => {
 
     it('should render updated info after editing', async () => {
         mockApi
+            .onGet(repository.routes.USERS_SEARCH_API({}).apiUrl, { params: { query: 'uqtname', rule: 'lookup' } })
+            .replyOnce(200, {})
             .onGet(new RegExp(repository.routes.MANAGE_USERS_LIST_API({}).apiUrl))
             .replyOnce(200, {
                 data: [
