a791410f37e67c31d4070434b87bb7ecc004a896
John Zeng <john.zeng@uq.edu.au>
2025-02-28 11:26:23 +1000

    try delete a form

--- src/modules/SharedComponents/PublicationsList/components/BulkUpdatesForms/index.js
+++ src/modules/SharedComponents/PublicationsList/components/BulkUpdatesForms/index.js
@@ -1,4 +1,3 @@
-export { default as ChangeAuthorIdForm } from './ChangeAuthorIdForm';
 export { default as ChangeDisplayTypeForm } from './ChangeDisplayTypeForm';
 export { default as ChangeSearchKeyValueForm } from './ChangeSearchKeyValueForm';
 export { default as CopyToOrRemoveFromCollectionForm } from './CopyToOrRemoveFromCollectionForm';
