844f299bad6d94bbb74d812b8d7cc12dcf0d2acf
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2025-03-25 17:02:18 +1000

    - redux-form compatibility clean up [#188759830]

--- src/modules/SharedComponents/Toolbox/ReactHookForm/components/Controller.js
+++ src/modules/SharedComponents/Toolbox/ReactHookForm/components/Controller.js
@@ -13,16 +13,16 @@ const getDecoratedField = (field, fieldState, formState) => {
     const decoratedField = field;
     const input = {
         ...field,
-        meta: {
-            error: fieldState.error?.message,
-            value: formState?.defaultValues?.[field.name],
-        },
+        ref: null,
         // required to avoid "A component is changing an uncontrolled input to be controlled" warnings
         value: decoratedField.value || '',
     };
     // required to make it compatible with SelectFieldWrapper,
     decoratedField.input = input;
-    decoratedField.meta = input.meta;
+    decoratedField.meta = {
+        error: fieldState.error?.message,
+        value: formState?.defaultValues?.[field.name],
+    };
     // to avoid `ref` & forwardRef() errors
     decoratedField.ref = null;
     decoratedField.value = input.value;
