35d76aa4dff5863a7bd1cd42efdba7c72f5d2191
Lee S <lsibbald100+git@gmail.com>
2025-03-31 09:59:21 +1000

    [#189017358] Further increased timeout in test that works locally but fails during build.

--- src/modules/AddMissingRecord/components/steps/NewRecord.test.js
+++ src/modules/AddMissingRecord/components/steps/NewRecord.test.js
@@ -13,7 +13,6 @@ import {
     waitToBeEnabled,
     assertInstanceOfFile,
     userEvent,
-    preview,
 } from 'test-utils';
 import { NEW_RECORD_API } from 'repositories/routes';
 
@@ -114,9 +113,7 @@ describe('Add new record', () => {
         fireEvent.click(getByRole('listitem', { name: 'Select this author (author) to assign it as you' }));
         const mockFile = ['myTestImage.png'];
         addFilesToFileUploader(mockFile);
-
-        await setFileUploaderFilesToClosedAccess(mockFile, 2000);
-        preview.debug();
+        await setFileUploaderFilesToClosedAccess(mockFile, 4000);
         await waitToBeEnabled(getByRole('button', { name: 'Submit for approval' }));
         // submit to trigger confirmation box
         await userEvent.click(getByRole('button', { name: 'Submit for approval' }));
