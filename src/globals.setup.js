99f436af938cc8eb7de6768f9e40bd27308b4863
Lee S <lsibbald100+git@gmail.com>
2025-05-21 09:32:09 +1000

    tweak to the new global setup file to avoid the crypto errors

--- src/globals.setup.js
+++ src/globals.setup.js
@@ -3,5 +3,5 @@ import { ArrayBuffer, TextDecoder, TextEncoder, Uint8Array } from 'util';
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
-global.ArrayBuffer = ArrayBuffer;
-global.Uint8Array = Uint8Array;
+// global.ArrayBuffer = ArrayBuffer;
+// global.Uint8Array = Uint8Array;
