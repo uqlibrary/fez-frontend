53c008ac1924dc26285da23f0e11fccd03d150b6
Lee S <lsibbald100+git@gmail.com>
2025-06-03 17:14:32 +1000

    [AD-233] WIP migrating from MTC to DataTable. Table is rendering with some minor UI issues, fields can be edited again with some UI issues, but saving causes a row dupe across the whole table. Need to fix that and also hook back in to espace API to save changes etc

--- src/modules/Admin/FavouriteSearch/components/FavouriteSearchListIcons.js
+++ src/modules/Admin/FavouriteSearch/components/FavouriteSearchListIcons.js
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import React from 'react';
 
 import ArrowDownward from '@mui/icons-material/ArrowDownward';
 import Check from '@mui/icons-material/Check';
@@ -7,9 +7,9 @@ import Delete from '@mui/icons-material/Delete';
 import Edit from '@mui/icons-material/Edit';
 
 export const tableIcons = {
-    Check: forwardRef((props, ref) => <Check {...props} ref={ref} id="favourite-search-list-item-save" />),
-    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} id="favourite-search-list-item-cancel-save" />),
-    Delete: forwardRef((props, ref) => <Delete {...props} ref={ref} id="favourite-search-list-item-delete" />),
-    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} id="favourite-search-list-item-edit" />),
-    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    Check: (props, ref) => <Check {...props} ref={ref} id="favourite-search-list-item-save" />,
+    Clear: (props, ref) => <Clear {...props} ref={ref} id="favourite-search-list-item-cancel-save" />,
+    Delete: (props, ref) => <Delete {...props} ref={ref} id="favourite-search-list-item-delete" />,
+    Edit: (props, ref) => <Edit {...props} ref={ref} id="favourite-search-list-item-edit" />,
+    SortArrow: (props, ref) => <ArrowDownward {...props} ref={ref} />,
 };
