513dba18599816f7e7d68e8990a69e678f2f66db
Lee S <lsibbald100+git@gmail.com>
2025-06-23 17:48:44 +1000

    [AD-246] Added Add functionality. It's almost working, but there are some edgecase issues still to resolve e.g. the new row remains in the table after the server response is inserted (since the server data will have a different ID)

--- src/hooks/useDataGrid.js
+++ src/hooks/useDataGrid.js
@@ -1,6 +1,7 @@
 import { useCallback, useState } from 'react';
 import { GridRowModes } from '@mui/x-data-grid';
 
+export const NEWROWID = 0;
 /**
  *
  * @param {Array} list Array of items to show in the DataGrid
@@ -24,20 +25,41 @@ import { GridRowModes } from '@mui/x-data-grid';
         handleCancelClick,
     };
  */
-export const useDataGrid = ({ list, handleRowUpdate, handleRowDelete }) => {
+export const useDataGrid = ({ list, handleRowAdd, handleRowUpdate, handleRowDelete }) => {
     const [rows, setRows] = useState(list);
     const [busy, setBusy] = useState(false);
     const [rowModesModel, setRowModesModel] = useState({});
     const [deleteRowId, setDeleteRowId] = useState(null);
 
+    const addNewRow = useCallback(newRow => {
+        setRows(prevRows => [
+            {
+                ...newRow,
+                isNew: true,
+            },
+            ...prevRows,
+        ]);
+    }, []);
+
     const handleUpdateRow = useCallback(
         async (newData, oldData) => {
             setBusy(true);
-            const res = await handleRowUpdate(newData, oldData);
+
+            const res = newData?.isNew ? await handleRowAdd(newData, rows) : await handleRowUpdate(newData, oldData);
+            // if (!res)
+            //     setRows(res);
+            console.log(res);
             setBusy(false);
-            return res;
+            if (!res) {
+                if (newData?.isNew) {
+                    return null;
+                }
+                return oldData;
+            }
+
+            return newData?.isNew ? { ...res, _action: 'delete' } : res;
         },
-        [handleRowUpdate],
+        [handleRowAdd, handleRowUpdate, rows],
     );
 
     const handleDeleteRow = useCallback(
@@ -80,12 +102,15 @@ export const useDataGrid = ({ list, handleRowUpdate, handleRowDelete }) => {
     );
 
     const handleCancelClick = useCallback(
-        id => () => {
-            setRowModesModel({
-                ...rowModesModel,
-                [id]: { mode: GridRowModes.View, ignoreModifications: true },
-            });
-
+        ({ apiRef, params }) => async () => {
+            if (params.row?.isNew) {
+                apiRef.current.stopRowEditMode({ id: params.id, ignoreModifications: true });
+            } else {
+                setRowModesModel({
+                    ...rowModesModel,
+                    [params.id]: { mode: GridRowModes.View, ignoreModifications: true },
+                });
+            }
             setDeleteRowId(null);
         },
         [rowModesModel],
@@ -97,6 +122,7 @@ export const useDataGrid = ({ list, handleRowUpdate, handleRowDelete }) => {
         rows,
         setRows,
         rowModesModel,
+        addNewRow,
         handleUpdateRow,
         handleDeleteRow,
         handleRowModesModelChange,
