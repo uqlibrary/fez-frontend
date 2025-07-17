f036ca11ab5020b3169d7d1eebf9a313bc053595
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2025-07-17 11:42:03 +1000

    - converting admin-add/community.spec.js [AD-178]

---
+++ playwright/tests/admin-add/community.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '../../lib/fixture';
+
+test.describe('As an admin,', () => {
+    test('I can create a community', async ({ page }) => {
+        await page.goto('/admin/community?user=uqstaff');
+        await expect(page.locator('h2')).toHaveText(/Add a missing community/);
+        await expect(page.locator('h3')).toHaveText(/Community details/);
+        await page.getByTestId('rek-title-input').fill('Fez E2E Test Community');
+        await page.getByTestId('rek-description-input').fill('Fez E2E Test Community description');
+        await page.getByTestId('rek-keywords-input').fill('testing');
+        await page.getByTestId('rek-keywords-add').click();
+        await page.getByTestId('submit-community').click();
+        await expect(page.locator('h3')).toHaveText(/Community added successfully/);
+        await page
+            .locator('button')
+            .getByText(/Add another community/)
+            .first()
+            .click();
+        await expect(
+            page
+                .locator('h3')
+                .getByText(/Community details/)
+                .first(),
+        ).toBeVisible();
+    });
+});
