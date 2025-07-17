edd38eed49118094c9f47313f44864a0e95139a8
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2025-07-17 11:40:59 +1000

    - converting collection.spec.js [AD-178]

---
+++ playwright/tests/admin-add/collection.spec.ts
@@ -0,0 +1,82 @@
+import { test, expect } from '../../lib/fixture';
+
+test.describe('As an admin,', () => {
+    test('I can create a collection', async ({ page }) => {
+        await page.goto('/admin/collection?user=uqstaff');
+        await expect(
+            page
+                .locator('h3')
+                .getByText(/Add a missing collection/)
+                .first(),
+        ).toBeVisible();
+        await expect(
+            page.locator('.StandardCard').filter({
+                has: page
+                    .locator('h3')
+                    .getByText(/Add a missing collection/)
+                    .first(),
+            }),
+        ).toHaveText(/Select a community/);
+        const communitySelector = page
+            .locator('.StandardCard')
+            .filter({
+                has: page
+                    .locator('h3')
+                    .getByText(/Add a missing collection/)
+                    .first(),
+            })
+            .getByTestId('rek-ismemberof-select');
+        await expect(
+            page
+                .locator('.StandardCard')
+                .filter({
+                    has: page
+                        .locator('h3')
+                        .getByText(/Add a missing collection/)
+                        .first(),
+                })
+                .getByTestId('rek-ismemberof-select'),
+        ).toBeVisible();
+        await page.waitForTimeout(1000); // Wait for event handlers to attach
+        await communitySelector.click();
+        await page
+            .getByTestId('rek-ismemberof-options')
+            .locator('li', { hasText: /Advanced Computational/ })
+            .first()
+            .click();
+        await expect(
+            page
+                .locator('h3')
+                .getByText(/Collection details/)
+                .first(),
+        ).toBeVisible();
+        await page.getByTestId('rek-title-input').fill('E2E Testing Collection');
+        await page.getByTestId('rek-description-input').fill('This collection is created through automated testing.');
+        await page.getByTestId('rek-keywords-input').fill('testing');
+        await page.getByTestId('rek-keywords-add').click();
+        await expect(page.getByTestId('rek-keywords-list')).toBeVisible();
+        await expect(
+            page
+                .getByTestId('rek-keywords-list')
+                .locator('p', { hasText: /testing/ })
+                .first(),
+        ).toBeVisible();
+        await page
+            .locator('button')
+            .getByText(/Add collection/)
+            .first()
+            .click();
+        await expect(page.locator('h3').getByText(/Collection added successfully/)).toBeVisible();
+        await page
+            .locator('button')
+            .getByText(/Add another collection/)
+            .first()
+            .click();
+        await expect(
+            page
+                .locator('h3')
+                .getByText(/Add a missing collection/)
+                .first(),
+        ).toBeVisible();
+    });
+});
