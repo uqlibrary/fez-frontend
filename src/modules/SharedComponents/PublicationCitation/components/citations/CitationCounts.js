8120511721c5ee7af55d09b0b4eba1b2a18922ba
John Zeng <john.zeng@uq.edu.au>
2025-08-05 09:20:50 +1000

    [AD-337] get openalex count from rek_openalex_citation_count, need to fix mock data and API

--- src/modules/SharedComponents/PublicationCitation/components/citations/CitationCounts.js
+++ src/modules/SharedComponents/PublicationCitation/components/citations/CitationCounts.js
@@ -9,6 +9,8 @@ import * as Partials from './partials';
 import Grid from '@mui/material/Grid';
 
 export const CitationCounts = ({ publication, hideViewFullStatisticsLink }) => {
+    console.log('publication=', publication);
+    console.trace('CitationCounts');
     const theme = useTheme();
     const getTitle = title =>
         locale.components.publicationCitation.linkWillOpenInNewWindow.replace('[destination]', title);
