6ebf2ca7b2e7e42cbfb79e984531a28d8cc88ded
John Zeng <john.zeng@uq.edu.au>
2025-08-05 09:09:04 +1000

    [AD-337] get openalex count from rek_openalex_citation_count

--- src/modules/SharedComponents/PublicationCitation/components/citations/CitationCounts.js
+++ src/modules/SharedComponents/PublicationCitation/components/citations/CitationCounts.js
@@ -17,6 +17,9 @@ export const CitationCounts = ({ publication, hideViewFullStatisticsLink }) => {
     const { sources } = locale.global;
 
     const counts = {
+        openalex: publication.hasOwnProperty('rek_openalex_citation_count')
+            ? publication.rek_openalex_citation_count
+            : null,
         wos: publication.hasOwnProperty('rek_thomson_citation_count') ? publication.rek_thomson_citation_count : null,
         scopus: publication.hasOwnProperty('rek_scopus_citation_count') ? publication.rek_scopus_citation_count : null,
         google: publication.hasOwnProperty('rek_gs_citation_count') ? publication.rek_gs_citation_count : null,
