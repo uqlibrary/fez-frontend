0bd2c5b61a3374a11ff5ecbd4b52830faf1c6d84
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2025-09-05 11:45:08 +1000

    - clean up [AD-354]

--- src/modules/AuthorIdentifiers/components/Orcid.js
+++ src/modules/AuthorIdentifiers/components/Orcid.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createHash } from 'crypto';
 import { parse } from 'querystring';
 
