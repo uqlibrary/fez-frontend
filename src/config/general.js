fdc280d58627c7f2317e8ef88d27789b6ded75b8
John Zeng <john.zeng@uq.edu.au>
2025-09-11 13:24:13 +1000

    [AD-466] Change "Not yet assessed (Unpaywall)" to "Not yet assessed (OpenAlex)"

--- src/config/general.js
+++ src/config/general.js
@@ -1684,7 +1684,7 @@ export const OA_STATUS = [
     { value: '453700', text: 'Mediated Access' },
     { value: '453954', text: 'PMC' },
     { value: '454116', text: 'RDM open' },
-    { value: '454118', text: 'Not yet assessed (Unpaywall)' },
+    { value: '454118', text: 'Not yet assessed (OpenAlex)' },
 ];
 
 export const OA_STATUS_TYPE = [
