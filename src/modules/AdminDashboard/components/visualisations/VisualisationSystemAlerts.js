ec95f341c1a5938c4eaeb06150d673a7a199d61b
Lee S <lsibbald100+git@gmail.com>
2025-10-16 13:28:10 +1000

    [AD-494] Updated charts in Today tab, plus related spec, to display the charts in the correct position. Position had shifted once charts were bumped to v8. Have also tweaked the layout breakpoints because at certain md breakpoints the charts collide

--- src/modules/AdminDashboard/components/visualisations/VisualisationSystemAlerts.js
+++ src/modules/AdminDashboard/components/visualisations/VisualisationSystemAlerts.js
@@ -5,14 +5,41 @@ import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import { ChartDataProvider, ChartsSurface, BarPlot } from '@mui/x-charts';
 
+import debounce from 'debounce-promise';
+
 const VisualisationSystemAlerts = ({
     assigned,
     remaining,
     id = 'system-alerts',
     colours = { assigned: '#338CFA', remaining: '#B60DCE' },
 }) => {
+    const [parentSize, setParentSize] = React.useState({ width: 0, height: 0 });
+    const _parentRef = React.useRef();
+
+    const handleResize = debounce(() => {
+        setParentSize(_parentRef?.current?.getBoundingClientRect() ?? { width: 0, height: 0 });
+    }, 200);
+
+    React.useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [handleResize]);
+
+    React.useEffect(() => {
+        handleResize();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
-        <Box data-testid={`chart-container-${id}`} sx={{ width: 200, height: 40 }}>
+        <Box
+            data-testid={`chart-container-${id}`}
+            ref={_parentRef}
+            sx={{
+                '& rect': {
+                    height: '20px !important',
+                },
+            }}
+        >
             <ChartDataProvider
                 series={[
                     { data: [assigned], type: 'bar', stack: 'SystemAlerts', layout: 'horizontal' },
@@ -24,22 +51,17 @@ const VisualisationSystemAlerts = ({
                     },
                 ]}
                 height={40}
+                width={200}
                 colors={[colours.assigned, colours.remaining]}
                 yAxis={[
                     {
-                        data: ['A', 'B'],
+                        data: [''],
                         scaleType: 'band',
                         id: 'y-axis-id',
                     },
                 ]}
-                margin={{
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    bottom: 0,
-                }}
                 disableAxisListener
-                sx={{ '> rect': { height: '20px !important' } }}
+                margin={{ top: parentSize.height / 4, right: 0, bottom: 0, left: -(parentSize.width / 4) }}
             >
                 <ChartsSurface>
                     <BarPlot />
