dc1a17ad1b0a1ab81fce34f57cf7dc2e62259d59
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2026-05-21 08:27:34 +1000

    - updating label text [AD-909]

--- src/modules/SharedComponents/Toolbox/CommissionedResearchReportField/locale.js
+++ src/modules/SharedComponents/Toolbox/CommissionedResearchReportField/locale.js
@@ -1,3 +1,3 @@
 export default {
-    label: 'This report was commissioned by an external client/organisation.',
+    label: 'This report was commissioned by an external client/organisation. (If the report was not commissioned externally, please submit this work as a Research Report - Other or Citation only).',
 };
