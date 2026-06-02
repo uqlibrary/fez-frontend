e0678d06cd3da8c4bd62e143743863f4cd7c327e
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2026-06-02 11:16:08 +1000

    - fixing import [AD-955]

--- src/modules/SharedComponents/Toolbox/ReactHookForm/components/ValidatedField.tsx
+++ src/modules/SharedComponents/Toolbox/ReactHookForm/components/ValidatedField.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Field } from 'modules/SharedComponents/Toolbox/ReactHookForm';
 import { useValidatedFormField } from 'hooks/useValidatedFormField';
-import { FieldProps } from 'modules/SharedComponents/Toolbox/ReactHookForm/components/Field';
+import Field, { FieldProps } from 'modules/SharedComponents/Toolbox/ReactHookForm/components/Field';
 
 const ValidatedField = (props: FieldProps) => {
     useValidatedFormField(props?.name);
