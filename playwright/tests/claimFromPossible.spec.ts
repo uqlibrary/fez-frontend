7f6d8edfb35df04c80f12f6afd524d4173b86ab7
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2026-07-28 19:42:11 +1000

    Further attempts to rectify flake tests

--- playwright/tests/claimFromPossible.spec.ts
+++ playwright/tests/claimFromPossible.spec.ts
@@ -240,10 +240,13 @@ test.describe('Claim possible work', () => {
             await page.locator('.menu-item-container').getByText('My works').click();
 
             // confirm
-            const confirmationDialog = page.locator('div[role="dialog"]');
-            await page.getByTestId('confirm-dialog-box').click();
-            await expect(confirmationDialog).not.toBeVisible();
-            // navigate away
+            const confirmationDialog = page
+                .getByRole('dialog')
+                .filter({ hasText: claimFormLocale.cancelWorkflowConfirmation.confirmationTitle });
+            await Promise.all([
+                page.waitForURL('**/records/mine'),
+                confirmationDialog.getByTestId('confirm-dialog-box').click(),
+            ]);
             await expect(page).toHaveURL('/records/mine');
         });
 
