f558ea5e4ce398bbf3ed20c3c64866960ac3699f
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2026-08-14 10:02:16 +1000

    - fixing createListSharingUrl - tests [AD-1068]

--- src/modules/JournalUserLists/useColumns.tsx
+++ src/modules/JournalUserLists/useColumns.tsx
@@ -43,7 +43,7 @@ export const createListUrl = (id: string | number) =>
     `${pathConfig.journals.search}?${encodeURI(`activeFacets[filters][ShowFavouritedOnly]=${id}&keywords[Keyword-all-journals][type]=Keyword&keywords[Keyword-all-journals][text]=all+journals&keywords[Keyword-all-journals][id]=Keyword-all-journals&keywords[Keyword-all-journals][operand]=AND#/journals/search/?activeFacets[filters][ShowFavouritedOnly]=true&page=1&keywords[Keyword-all-journals][type]=Keyword&keywords[Keyword-all-journals][text]=all+journals&keywords[Keyword-all-journals][id]=Keyword-all-journals&keywords[Keyword-all-journals][operand]=AND`)}`;
 
 export const createListSharingUrl = (id: string | number) =>
-    `${APP_URL.replace(/\/$/, window.location.hash ? /* istanbul ignore next */ '/#' : '')}${createListUrl(id)}`;
+    `${APP_URL.replace(/\/#?\/?$/, window.location.hash ? /* istanbul ignore next */ '/#' : '')}${createListUrl(id)}`;
 
 export const useColumns = ({
     txt,
